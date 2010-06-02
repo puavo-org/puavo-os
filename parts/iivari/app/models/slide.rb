9c05e1351e52c92cd029222c0c495673951d2146
Jouni Korhonen <jouni.korhonen@opinsys.fi>
2010-06-02 10:50:22 +0300

    Moved client template to app/views/screen directory.

--- parts/iivari/app/models/slide.rb
+++ parts/iivari/app/models/slide.rb
@@ -4,7 +4,7 @@ class Slide < ActiveRecord::Base
   def slide_html
     view = ActionView::Base.new(ActionController::Base.view_paths, {})
     # FIXME, set path prefix
-    view.render( :partial => "slides/client_" + self.template,
+    view.render( :partial => "screen/client_" + self.template,
                  :format => :html,
                  :locals => { :slide => self } )
   end
