df14206c19b0c05080143f3ce0c6bdd8773f9871
Jouni Korhonen <jouni.korhonen@opinsys.fi>
2010-06-04 09:01:52 +0300

    Removed javascript debug log code

--- parts/iivari/public/javascripts/slideshow.js
+++ parts/iivari/public/javascripts/slideshow.js
@@ -3,7 +3,6 @@ function updateSlideData(url, cache) {
     if(cache == true) {
 	$.retrieveJSON(url, function(json, status) {
 	    if (status != "notmodified") {
-		console.log("Update slide data");
 		slideData.json = json;
 	    }
 	});
@@ -18,16 +17,12 @@ function updateSlideData(url, cache) {
 function showNextSlide() {
     // wait one second if slideData.json is not defined yet
     if ( typeof(slideData.json) == "undefined" ) {
-	//console.log("slideData.json is undfined");
 	setTimeout("showNextSlide()", 1000);
     }
     else {
 	if ( slideNumber > slideData.json.length - 1 ) {
 	    slideNumber = 0;
 	}
-	console.log("Changed to the next slide");
-	console.log("slide_data: " + slideData.json[slideNumber] )
-	console.log("slide_data: " + slideData.json[slideNumber]["slide_html"] )
 	$("#content").empty().append(slideData.json[slideNumber]["slide_html"]);
 	slideNumber = slideNumber + 1
 	setTimeout("showNextSlide()", 5000);
