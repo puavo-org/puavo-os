e062cda08d7ccac8dfc1bf8ffa998a918ce3b1fd
Jouni Korhonen <jouni.korhonen@opinsys.fi>
2010-07-30 10:46:24 +0300

    Added step to channel_steps.rb

--- parts/iivari/features/step_definitions/channel_steps.rb
+++ parts/iivari/features/step_definitions/channel_steps.rb
@@ -12,3 +12,10 @@ end
 Then /^I should see the following channels:$/ do |expected_channels_table|
   expected_channels_table.diff!(tableish('table tr', 'td,th'))
 end
+
+When /^I choose "([^\"]*)" link for the (\d+)(?:st|nd|rd|th) channel$/ do |link_name, pos|
+  visit channels_path
+  within("table tr:nth-child(#{pos.to_i+1})") do
+    click_link link_name
+  end
+end
