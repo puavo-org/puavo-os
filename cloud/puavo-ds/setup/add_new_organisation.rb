83a6ad5edd51a65fd1a84ff300c84fb6dbcf041d
Veli-Matti Lintu <veli-matti.lintu@opinsys.fi>
2010-09-22 09:01:34 +0000

    Initial support for kerberos settings

--- cloud/puavo-ds/setup/add_new_organisation.rb
+++ cloud/puavo-ds/setup/add_new_organisation.rb
@@ -33,6 +33,15 @@ require 'users/user_error'
 require 'users/user'
 require 'users/samba_domain'
 require 'users/ldap_organisation'
+require 'kerberos'
+
+def newpass( len )
+  chars = ("a".."z").to_a + ("A".."Z").to_a + ("0".."9").to_a
+  newpass = ""
+  1.upto(len) { |i| newpass << chars[rand(chars.size-1)] }
+  return newpass
+end
+
 
 # FIXME: puavo configuration?
 organisation_base_template = "dc=edu,dc=%s,dc=fi"
@@ -121,6 +130,40 @@ group = Group.create!( :displayName => group_name,
 
 # Added association
 role.groups << group
+
+# Create kerberos realm
+
+#`mkdir -p /etc/krb5kdc/masterkeys`
+#`chmod 0700 /etc/krb5kdc/masterkeys`
+
+kerberos_masterpw = newpass(20)
+puts "Initializing kerberos realm with master key: #{kerberos_masterpw}"
+
+realm = KerberosRealm.new( :ldap_server => configurations["settings"]["ldap_server"],
+                           :realm => kerberos_realm,
+                           :masterpw => kerberos_masterpw,
+                           :suffix => suffix,
+                           :domain => domain )
+
+conf = KerberosRealm.create_kerberos_configuration(configurations["settings"]["ldap_server"])
+
+File.open("/etc/krb5kdc/kdc.conf", "w") {|file|
+        file.write(conf.kdc_conf)
+}
+
+File.open("/etc/krb5.conf", "w") {|file|
+        file.write(conf.krb5_conf)
+}
+
+File.open("/etc/krb5kdc/kadm5.acl", "w") {|file|
+        file.write(conf.kadm5_acl)
+}
+
+File.open("/etc/default/krb5-kdc", "w") {|file|
+        file.write(conf.daemon_args)
+}
+
+realm.create_ldap_tree
 
 # User
 puts "Create organisation owner:"
