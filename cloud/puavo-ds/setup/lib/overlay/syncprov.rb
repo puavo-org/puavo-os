b83db36d97751a9dc636c7094069601f09a3609c
Veli-Matti Lintu <veli-matti.lintu@opinsys.fi>
2010-10-07 21:41:16 +0300

    Removed syncprov settings that broke slave syncrepl with CSN too old errors

--- cloud/puavo-ds/setup/lib/overlay/syncprov.rb
+++ cloud/puavo-ds/setup/lib/overlay/syncprov.rb
@@ -3,9 +3,8 @@ class Syncprov < Overlay
   def self.add_overlay_config(args)
     # Save without validation
     self.new( "olcOverlay" => "{#{self.index}}syncprov",
-              "objectClass" => ['olcSyncProvConfig', 'olcOverlayConfig'],
-              "olcSpNoPresent" => "TRUE",
-              "olcSpReloadHint" => "TRUE" ).save(false)
+              "objectClass" => ['olcSyncProvConfig', 'olcOverlayConfig']
+              ).save(false)
   end
 
   def self.index
