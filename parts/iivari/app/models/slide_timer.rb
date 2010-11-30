a18c11d9579f38a1ba52b8d1a2c5e426e37332af
Jouni Korhonen <jouni.korhonen@opinsys.fi>
2010-11-30 11:20:59 +0200

    Update channel's updated_at attribute when change slide's timers

--- parts/iivari/app/models/slide_timer.rb
+++ parts/iivari/app/models/slide_timer.rb
@@ -1,3 +1,15 @@
 class SlideTimer < ActiveRecord::Base
   belongs_to :slide
+
+  after_update :set_channel_updated_at
+  after_create :set_channel_updated_at
+  after_update :set_channel_updated_at
+  before_destroy :set_channel_updated_at
+
+  private
+
+  def set_channel_updated_at
+    self.slide.channel.updated_at = Time.now
+    self.slide.channel.save
+  end
 end
