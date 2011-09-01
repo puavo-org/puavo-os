200b83d4a947b07df9b5c7deab6707f38de62db9
Jouni Korhonen <jouni.korhonen@opinsys.fi>
2011-09-01 15:37:01 +0300

    Add links to other schools

--- parts/iivari/server/app/helpers/application_helper.rb
+++ parts/iivari/server/app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def schools_menu
+    content_tag :ul do
+      @schools.map do |school|
+        content_tag :li do
+          link_to( school.name,  channels_path(school.puavo_id) )
+        end
+      end.join.html_safe
+    end.html_safe
+  end
 end
