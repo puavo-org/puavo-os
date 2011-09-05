6005556044ce10e506172489d9d874e5e3af6879
Jouni Korhonen <jouni.korhonen@opinsys.fi>
2011-09-05 14:29:44 +0300

    Add note

--- parts/iivari/server/app/models/display.rb
+++ parts/iivari/server/app/models/display.rb
@@ -4,6 +4,7 @@ class Display < OrganisationData
   validates_presence_of :hostname
 
   def self.find_all_by_school_id(school_id, puavo_api)
+    # FIXME: get devices by type
     puavo_devices = puavo_api.devices.find_by_school_id(school_id)
     all.select do |display|
       puavo_devices.map{ |d| d.puavoHostname.to_s }.include?(display.hostname)
