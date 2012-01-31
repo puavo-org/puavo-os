00adbdb7092146376baab6359b227cc9fc099b2e
lamikae <lamikae@opinsys.fi>
2012-01-31 17:49:30 +0200

    Read global iivarirc configuration first, override from user config.

--- parts/iivari/client/iivari/settings.py
+++ parts/iivari/client/iivari/settings.py
@@ -20,28 +20,27 @@ COOKIE_PATH = os.path.join(CACHE_PATH, 'cookiejar.txt')
 LOG_FILE = os.path.join(IIVARIDIR, 'log', 'iivari.log')
 
 # Rest of the settings may be configured from iivarirc file.
-# The iivarirc is json configuration file placed to 
-# either ~/.iivarirc or /etc/iivarirc.
-primary_rc = os.path.join(os.environ['HOME'], ".iivarirc")
-secondary_rc = "/etc/iivarirc"
-#secondary_rc = "iivarirc" # DEBUG
+# /etc/iivarirc is global and individual settings may be overridden
+# from ~/.iivarirc
+global_rc = "/etc/iivarirc"
+user_rc = os.path.join(os.environ['HOME'], ".iivarirc")
 rc_config = {}
+def read_config(rc_file):
+    # use a little bit of regexp to allow comments in json data
+    s = open(rc_file, "r").read()
+    s2 = re.sub(r"(?:^#|[\n\s]#).*", "", s) # strip comments
+    m = re.search(r'({.*})', s2, re.DOTALL) # extract json
+    return json.loads(m.group(1))
 try:
-    rc_file = None
-    if os.path.exists(primary_rc):
-        rc_file = primary_rc
-    elif os.path.exists(secondary_rc):
-        rc_file = secondary_rc
-    else:
-        print "WARNING: ~/.iivarirc nor /etc/iivarirc was not found, using defaults"
-
-    if rc_file:
-        # use a little bit of regexp to allow comments in json data
-        s = open(rc_file, "r").read()
-        s2 = re.sub(r"(?:^#|[\n\s]#).*", "", s) # strip comments
-        m = re.search(r'({.*})', s2, re.DOTALL) # extract json
-        rc_config = json.loads(m.group(1))
+    if os.path.exists(global_rc):
+        rc_config = read_config(global_rc)
+        print "INFO: using /etc/iivarirc"
+    # override from user config
+    if os.path.exists(user_rc):
+        rc_config.update(read_config(user_rc))
+        print "INFO: using ~/.iivarirc"
 except Exception, e:
+    # print and ignore errors, use defaults
     print e
 
 # iivari server base URL
@@ -60,9 +59,6 @@ else:
 # use fancy shell coloring in log?
 if 'LOG_COLORS' in rc_config:
     colors = rc_config['LOG_COLORS']
-    if colors == "False":
-        LOG_COLORS = False
-    else:
-        LOG_COLORS = True
+    LOG_COLORS = (colors == "True")
 else:
     LOG_COLORS = True
