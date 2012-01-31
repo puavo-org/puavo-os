9984db7af7cfa6f65ef1afc8b6e1001dfc9a36d4
lamikae <lamikae@opinsys.fi>
2012-01-31 16:17:51 +0200

    fix tab-indentation to 4 spaces

--- parts/iivari/client/iivari/settings.py
+++ parts/iivari/client/iivari/settings.py
@@ -27,42 +27,42 @@ secondary_rc = "/etc/iivarirc"
 #secondary_rc = "iivarirc" # DEBUG
 rc_config = {}
 try:
-	rc_file = None
-	if os.path.exists(primary_rc):
-		rc_file = primary_rc
-	elif os.path.exists(secondary_rc):
-		rc_file = secondary_rc
-	else:
-		print "WARNING: ~/.iivarirc nor /etc/iivarirc was not found, using defaults"
+    rc_file = None
+    if os.path.exists(primary_rc):
+        rc_file = primary_rc
+    elif os.path.exists(secondary_rc):
+        rc_file = secondary_rc
+    else:
+        print "WARNING: ~/.iivarirc nor /etc/iivarirc was not found, using defaults"
 
-	if rc_file:
-		# use a little bit of regexp to allow comments in json data
-		s = open(rc_file, "r").read()
-		s2 = re.sub(r"(?:^#|[\n\s]#).*", "", s) # strip comments
-		m = re.search(r'({.*})', s2, re.DOTALL) # extract json
-		rc_config = json.loads(m.group(1))
+    if rc_file:
+        # use a little bit of regexp to allow comments in json data
+        s = open(rc_file, "r").read()
+        s2 = re.sub(r"(?:^#|[\n\s]#).*", "", s) # strip comments
+        m = re.search(r'({.*})', s2, re.DOTALL) # extract json
+        rc_config = json.loads(m.group(1))
 except Exception, e:
-	print e
+    print e
 
 # iivari server base URL
 if 'SERVER_BASE' in rc_config:
-	SERVER_BASE = rc_config['SERVER_BASE']
+    SERVER_BASE = rc_config['SERVER_BASE']
 else:
-	SERVER_BASE = "http://localhost:3000"
+    SERVER_BASE = "http://localhost:3000"
 
 # logger level -- FATAL, WARN, INFO, DEBUG
 if 'LOG_LEVEL' in rc_config:
-	level = rc_config['LOG_LEVEL']
-	LOG_LEVEL = logging.__dict__[level]
+    level = rc_config['LOG_LEVEL']
+    LOG_LEVEL = logging.__dict__[level]
 else:
-	LOG_LEVEL = logging.DEBUG
+    LOG_LEVEL = logging.DEBUG
 
 # use fancy shell coloring in log?
 if 'LOG_COLORS' in rc_config:
-	colors = rc_config['LOG_COLORS']
-	if colors == "False":
-		LOG_COLORS = False
-	else:
-		LOG_COLORS = True
+    colors = rc_config['LOG_COLORS']
+    if colors == "False":
+        LOG_COLORS = False
+    else:
+        LOG_COLORS = True
 else:
-	LOG_COLORS = True
+    LOG_COLORS = True
