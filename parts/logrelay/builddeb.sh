ccc2958c284bef69ea1d556dd8e288962d35b1c6
Esa-Matti Suuronen <esa-matti@suuronen.org>
2012-09-27 09:45:59 +0300

    Add deb build script

---
+++ parts/logrelay/builddeb.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Run from parent directory!
+
+FILES="logrelay/Makefile logrelay/README.md logrelay/server.rb logrelay/debian/"
+
+set -eu
+
+# Remove previous build
+rm -rf logrelay_build/ || true
+
+mkdir logrelay_build
+
+cp -r $FILES logrelay_build/
+
+cd logrelay_build/
+
+debuild -us -uc
+
