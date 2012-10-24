e66ef92935b87edc17037de578dfbefb732753cc
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2012-10-24 12:04:25 +0300

    Add some needed packages to bootserver build, and log vmbuilder output.

--- parts/rules/build.sh
+++ parts/rules/build.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-set -e
+set -eu
 
 cleanup() {
-  test -n "$srcdir" && rm -rf $srcdir
+  test -n "$srccopydir" && rm -rf $srccopydir
 }
 
 usage() {
@@ -18,46 +18,64 @@ if [ "$(id -u)" = "0" ]; then
   exit 1
 fi
 
-hosttype=$1
-hostname=$2
+{
+  set +u
+  hosttype=$1
+  targethostname=$2
+}
 
-test -z "$hosttype" -o -z "$hostname" && usage
-
-srcdir=$(mktemp -d /tmp/ltsp-build-$USER-$$.XXXXXXXXXXX)
-cp -a "$(dirname $0)" $srcdir
+test -z "$hosttype" -o -z "$targethostname" && usage
 
 case "$hosttype" in
   boot)
-    extraopts="--addpkg ltsp-server-standalone \
-	       --arch amd64 \
-    	       --dest /images/$hostname \
-    	       --flavour=server \
-    	       --hostname $hostname \
-    	       --suite=precise"
+    extraopts="--addpkg   bridge-utils \
+	       --addpkg   isc-dhcp-server \
+	       --addpkg   nfs-kernel-server \
+	       --addpkg   tshark \
+	       --addpkg   vlan \
+	       --arch     amd64 \
+    	       --dest     /images/$targethostname \
+    	       --flavour  server \
+    	       --hostname $targethostname \
+    	       --suite    precise \
+	      "
     ;;
   ltsp)
-    extraopts="--addpkg ltsp-client-core \
-	       --arch i386 \
-    	       --dest /images/$hostname \
-    	       --flavour=generic \
-    	       --hostname $hostname \
-    	       --suite=quantal"
+    extraopts="--addpkg   ltsp-client-core \
+	       --arch     i386 \
+    	       --dest     /images/$targethostname \
+    	       --flavour  generic \
+    	       --hostname $targethostname \
+    	       --suite    quantal \
+	      "
     ;;
   *)
     usage
     ;;
 esac
 
+srcdir=$(dirname $0)
+srccopydir=$(mktemp -d /tmp/ltsp-build-$USER-$$.XXXXXXXXXXX)
+cp -a $srcdir $srccopydir
+
+build_version=$targethostname-$hosttype-$(date +%Y-%m-%d-%H%M%S)
+buildlogfile=$srcdir/log/$build_version.log
+
+# workaround the bug:
+# https://bugs.launchpad.net/ubuntu/+source/vm-builder/+bug/1037607
+proc_bugfix="--addpkg linux-image-generic"
+
 sudo \
   vmbuilder kvm ubuntu \
-    --addpkg linux-image-generic \
-    --addpkg openssh-server \
-    --user opinsys \
-    --pass opinsys \
-    --mirror http://10.246.131.53:9999/fi.archive.ubuntu.com/ubuntu \
-    --debug \
-    -v \
-    --exec $srcdir/setup.sh \
-    --rootsize 20480 \
-    --tmp /virtualtmp \
-    $extraopts
+    $proc_bugfix \
+    --addpkg     openssh-server \
+    --user       opinsys \
+    --pass       opinsys \
+    --mirror     http://10.246.131.53:9999/fi.archive.ubuntu.com/ubuntu \
+    --debug      \
+    -v           \
+    --exec       $srccopydir/setup.sh \
+    --rootsize   20480 \
+    --tmp        /virtualtmp \
+    $extraopts   \
+  2>&1 | tee $buildlogfile
