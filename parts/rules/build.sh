5f13048c3c7a63f992c00e2779fdc85f77be4b0c
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2012-11-01 12:06:57 +0200

    Build an ltsp image and log output from build scripts.

--- parts/rules/build.sh
+++ parts/rules/build.sh
@@ -8,19 +8,33 @@ cleanup() {
 
 trap cleanup EXIT
 
+arch=i386
 basedir=/virtualtmp/$USER
 mirror=http://localhost:3142/fi.archive.ubuntu.com/ubuntu
 
 srcdir=$(dirname $0)
 srccopydir=$(mktemp -d /tmp/ltsp-builder-$USER.XXXXXXXXXX)
 
+build_date=$(date +%Y-%m-%d-%H%M%S)
+build_name=$(git branch | awk '$1 == "*" { print $2 }')
+build_version=ltsp-$build_name-$build_date
+build_logfile=$srcdir/log/$build_version.log
+
 cp -a $srcdir $srccopydir
-sudo env LTSP_BUILD_CLIENT_DIR=$srccopydir/ltsp-build-client \
-       $srccopydir/ltsp-build-client/ltsp-build-client \
-       --arch           i386 \
-       --base           $basedir \
-       --config         $srccopydir/ltsp-build-client/config \
-       --mirror         $mirror \
-       --purge-chroot   \
-       --serial-console \
-       --skipimage
+
+{
+  sudo env LTSP_BUILD_CLIENT_DIR=$srccopydir/ltsp-build-client \
+	 $srccopydir/ltsp-build-client/ltsp-build-client \
+	 --arch           $arch \
+	 --base           $basedir \
+	 --config         $srccopydir/ltsp-build-client/config \
+	 --mirror         $mirror \
+	 --purge-chroot   \
+	 --serial-console \
+	 --skipimage
+
+  sudo mksquashfs \
+	 $basedir/$arch /images/$build_version-$arch.img \
+	 -noappend -no-progress -no-recovery \
+	 -wildcards -ef /etc/ltsp/ltsp-update-image.excludes
+} 2>&1 | tee $build_logfile
