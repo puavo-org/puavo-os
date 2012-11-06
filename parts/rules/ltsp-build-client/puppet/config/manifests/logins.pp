726fa579078a90012e33d660a65db4b475765463
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2012-11-06 15:31:02 +0200

    Add some puppet classes to do useful things (add admin users).

---
+++ parts/rules/ltsp-build-client/puppet/config/manifests/logins.pp
@@ -0,0 +1,9 @@
+class config::logins {
+  $admin_homedir        = '/adm-home'
+  $admin_min_uid        = '1001'
+  $admin_max_uid        = '1099'
+  $admin_user_username  = 'ltspadmin'
+  $admin_user_groupname = 'ltspadmin'
+  $admin_user_uid       = '1000'
+  $admin_user_gid       = '1000'
+}
