9f600c5852a331aa9834e3ff8dfbd3d71ccab580
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2012-11-12 15:49:59 +0200

    Ensure /etc/ssh/ssh_known_hosts does not exist, causes login issues on ltsp clients.

--- parts/rules/ltsp-build-client/puppet/ltsp/ssh_client/manifests/init.pp
+++ parts/rules/ltsp-build-client/puppet/ltsp/ssh_client/manifests/init.pp
@@ -5,6 +5,11 @@ class ssh_client {
     '/etc/ssh/ssh_config':
       content => template('ssh_client/ssh_config'),
       require => Package['openssh-client'];
+
+    # this contains the builder key, remove it, this is not used and causes
+    # login issues
+    '/etc/ssh/ssh_known_hosts':
+      ensure => absent;
   }
 
   Package <| title == "openssh-client" |>
