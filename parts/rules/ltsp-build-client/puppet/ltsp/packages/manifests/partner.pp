0e15ef2f950732d34e578e859e9655e37dc39423
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2012-11-12 17:03:58 +0200

    Start using Ubuntu/Canonical partner-repository as well.

---
+++ parts/rules/ltsp-build-client/puppet/ltsp/packages/manifests/partner.pp
@@ -0,0 +1,25 @@
+class packages::partner {
+  include packages
+
+  # install all ubuntu packages listed in packages
+  Package <| tag == "partner" |> {
+    ensure  => present,
+    require => [ Exec['apt update'],
+                 File['/etc/apt/sources.list.d/partner.list'], ],
+  }
+
+  # XXX this belongs a different place ... perhaps "apt"-module?
+  # XXX sources list might belong there as well
+  # XXX also, puppet stages would be nice here
+  exec {
+    'apt update':
+      command     => '/usr/bin/apt-get update',
+      refreshonly => true;
+  }
+
+  file {
+    '/etc/apt/sources.list.d/partner.list':
+      content => template('packages/partner.list'),
+      notify  => Exec['apt update'];
+  }
+}
