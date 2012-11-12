56fb2c4deae88a96db87c1ff8ddf46fc9ed446bd
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2012-11-12 17:03:58 +0200

    Start using Ubuntu/Canonical partner-repository as well.

--- parts/rules/ltsp-build-client/puppet/ltsp/packages/manifests/all.pp
+++ parts/rules/ltsp-build-client/puppet/ltsp/packages/manifests/all.pp
@@ -1,5 +1,7 @@
 class packages::all {
-  include packages
+  include packages,
+          packages::partner,
+          packages::ubuntu
 
   # install all packages listed in packages
   Package <| |> { ensure => present, }
