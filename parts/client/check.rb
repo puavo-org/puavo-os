c9e07fb45ceb7f93a693d507efdb735a561a2c49
Esa-Matti Suuronen <esa-matti@suuronen.org>
2012-11-22 16:35:07 +0200

    Add small checking tool

---
+++ parts/client/check.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+
+def has_content(path)
+  File.open(path) { |f| f.read }.size > 0
+end
+
+[
+  "/etc/puavo/hostname",
+  "/etc/puavo/host_configuration",
+  "/etc/puavo/certs/host.key",
+  "/etc/puavo/certs/rootca.pem",
+  "/etc/puavo/certs/orgcabundle.pem",
+  "/etc/puavo/certs/host.crt",
+  "/etc/puavo/ldap/password",
+  "/etc/puavo/ldap/base",
+  "/etc/puavo/ldap/dn",
+  "/etc/puavo/kerberos/realm",
+].each do |path|
+
+  if not has_content(path)
+    raise "File missing: #{ path }"
+  end
+
+end
