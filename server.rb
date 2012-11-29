efcb358e68e0ed0149ca6233ad3db95a4f9ebe0e
Esa-Matti Suuronen <esa-matti@suuronen.org>
2012-11-29 16:44:56 +0200

    Clean up requires

--- server.rb
+++ server.rb
@@ -1,16 +1,10 @@
 
-require "eventmachine"
-require "socket"
 
 require "./lib/tftpserver"
-require "./lib/cachedfilereader"
-require "./lib/log"
 
 PORT = 69
 ROOT = "./test/tftpboot/"
 
 EventMachine::run do
-  l = EventMachine::open_datagram_socket("0.0.0.0", PORT, TFTP::Server, ROOT)
-  log "TFTP server now listening on port #{ PORT }, #{ l.inspect }"
+  EventMachine::open_datagram_socket("0.0.0.0", PORT, TFTP::Server, ROOT)
 end
-
