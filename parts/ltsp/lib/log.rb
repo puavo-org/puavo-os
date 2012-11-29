95b4e054efab5875e3ae20a67ff05902bbbd9787
Esa-Matti Suuronen <esa-matti@suuronen.org>
2012-11-29 18:11:32 +0200

    Implement final timeout and improve logging

--- parts/ltsp/lib/log.rb
+++ parts/ltsp/lib/log.rb
@@ -3,12 +3,15 @@ def log(*args)
   puts(*args)
 end
 
-def error(*args)
-  args[0] = "ERROR: " + args[0]
+def warn(*args)
+  args[0] = "WARN: " + args[0]
   puts(*args)
 end
 
 def debug(*args)
-  # args[0] = "DEBUG: " + args[0]
-  # puts(*args)
+  if $tftp_debug
+    args[0] = "DEBUG: " + args[0]
+    puts(*args)
+  end
 end
+
