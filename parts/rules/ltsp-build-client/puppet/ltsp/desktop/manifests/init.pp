7cb7bb788167ca97fdac93e83656bc792dacb1f3
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2012-11-30 14:34:12 +0200

    Include some dconf-settings for desktop.

--- parts/rules/ltsp-build-client/puppet/ltsp/desktop/manifests/init.pp
+++ parts/rules/ltsp-build-client/puppet/ltsp/desktop/manifests/init.pp
@@ -1,3 +1,26 @@
 class desktop {
-  # ...
+  include packages
+
+  exec {
+    'update dconf':
+      command     => '/usr/bin/dconf update',
+      refreshonly => true;
+  }
+
+  file {
+    [ '/etc/dconf/db/user.d'
+    , '/etc/dconf/db/user.d/locks' ]:
+      ensure => directory;
+
+    '/etc/dconf/db/user.d/locks/locklist':
+      content => template('desktop/dconf_user_profile_locks'),
+      notify  => Exec['update dconf'];
+
+    '/etc/dconf/db/user.d/profile':
+      content => template('desktop/dconf_user_profile'),
+      notify  => Exec['update dconf'],
+      require => Package['ubuntu-mono'];
+  }
+
+  Package <| title == ubuntu-mono |>
 }
