5acfc2797acbebda4732251cfec599cf4d5f01a9
Esa-Matti Suuronen <esa-matti@suuronen.org>
2012-11-30 15:29:51 +0200

    Add constants.rb

--- parts/ltsp/lib/tftpserver.rb
+++ parts/ltsp/lib/tftpserver.rb
@@ -2,52 +2,20 @@
 require "eventmachine"
 require "socket"
 
+require "./lib/constants"
 require "./lib/cachedfilereader"
 require "./lib/log"
 
 # http://tools.ietf.org/html/rfc1350
-
 module TFTP
 
-  # http://tools.ietf.org/html/rfc1350#section-5
-  module Opcode
-    RRQ = 1
-    WRQ = 2
-    DATA = 3
-    ACK = 4
-    ERROR = 5
-    OACK = 6
-  end
-
-  # http://tools.ietf.org/html/rfc1350#page-10
-  module ErrorCode
-    NOT_DEFINED = 0
-    NOT_FOUND = 1
-    ACCESS_VIOLATION = 2
-    DISK_FULL = 3
-    ILLEGAL_TFTP_OPERATION = 4
-    UNKNOWN_TRANSFER_ID = 5
-    FILE_ALREADY_EXISTS = 6
-    NO_SUCH_USER = 7
-  end
-
-  ERROR_DESCRIPTIONS = {
-    ErrorCode::NOT_FOUND => "File not found.",
-    ErrorCode::ACCESS_VIOLATION => "Access violation.",
-    ErrorCode::DISK_FULL => "Disk full or allocation exceeded.",
-    ErrorCode::ILLEGAL_TFTP_OPERATION => "Illegal TFTP operation.",
-    ErrorCode::UNKNOWN_TRANSFER_ID => "Unknown transfer ID.",
-    ErrorCode::FILE_ALREADY_EXISTS => "File already exists.",
-    ErrorCode::NO_SUCH_USER => "No such user."
-  }
-
-  OPCODE_HANDLERS = {
-    Opcode::RRQ => :handle_get,
-    Opcode::ACK => :handle_ack,
-    Opcode::ERROR => :handle_error
-  }
-
   class TFTPConnection < EventMachine::Connection
+
+    OPCODE_HANDLERS = {
+      Opcode::RRQ => :handle_get,
+      Opcode::ACK => :handle_ack,
+      Opcode::ERROR => :handle_error
+    }
 
     def receive_data(data)
       # debug "Server got data #{ data.inspect }"
@@ -79,6 +47,7 @@ module TFTP
     end
 
   end
+
 
   # TFTP server listening on a fixed port (default 69)
   class Server < TFTPConnection
