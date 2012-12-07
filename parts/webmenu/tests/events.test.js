13c545caf9f14b748b2343bae4fdde8484409ea6
Esa-Matti Suuronen <esa-matti@suuronen.org>
2012-12-07 11:10:14 +0200

    Squashed 'content/scripts/vendor/backbone.viewmaster/' content from commit 43f7493
    git-subtree-dir: content/scripts/vendor/backbone.viewmaster
    git-subtree-split: 43f749389a2d8992fb1510cff9dee6487389529c

---
+++ parts/webmenu/tests/events.test.js
@@ -0,0 +1,353 @@
+/*global describe:true, it:true, expect: true, Backbone: true, beforeEach: true, chai:true, _:true*/
+/*jshint expr:true, browser:true */
+
+
+describe("Events", function() {
+
+  describe("binding", function() {
+
+    var model = new Backbone.Model();
+
+    function createView(bindTestEvent, done){
+      var View = Backbone.ViewMaster.extend({
+        constructor: function() {
+          Backbone.ViewMaster.prototype.constructor.apply(this, arguments);
+          bindTestEvent.call(this);
+        },
+        callback: function() {
+          done(null, this);
+        }
+      });
+      return new View({ model: model });
+    }
+
+
+    it(".bindTo(emitter, event, Function)", function(done){
+      createView(function(){
+        this.bindTo(this.model, "test", this.callback);
+      }, done);
+      model.trigger("test");
+    });
+
+    it(".bindTo(emitter, event, method string)", function(done){
+      createView(function(){
+        this.bindTo(this.model, "test", "callback");
+      }, done);
+      model.trigger("test");
+    });
+
+    it(".bindTo(emitter, event, Function, context)", function(done){
+      var myContext = {};
+      createView(function(){
+        this.bindTo(this.model, "test", this.callback, myContext);
+      }, function(err, context) {
+        expect(context === myContext).to.be.ok;
+        done();
+      });
+      model.trigger("test");
+    });
+
+    it("unbindFrom(binding) removes binding", function(done) {
+      var view = new Backbone.ViewMaster();
+      var emitter = new Backbone.Model();
+      var binding = view.bindTo(emitter, "throw", function() {
+        throw new Error("Event not unbound!");
+      });
+
+      view.unbindFrom(binding);
+      emitter.trigger("throw");
+      setTimeout(done, 1);
+    });
+
+    it("should be removed on view.remove()", function(){
+      var model = new Backbone.Model();
+      var view = new Backbone.ViewMaster();
+      var spy = chai.spy();
+
+      view.bindTo(model, "test", spy);
+      model.trigger("test");
+      view.remove();
+      model.trigger("test");
+
+      expect(spy).to.have.been.called.once;
+    });
+
+    it("is unbound on parent.remove()", function(){
+      var child = new Backbone.ViewMaster();
+      child.template = function() {
+        return "<p>child</p>";
+      };
+
+      var parent = new Backbone.ViewMaster();
+      parent.template = function() {
+        return "<div class=container ></div>";
+      };
+
+      parent.setView(".container", child);
+      parent.render();
+
+      var spy = chai.spy();
+      var emitter = new Backbone.Model();
+      child.bindTo(emitter, "test", spy);
+
+      parent.remove();
+
+      emitter.trigger("test");
+      expect(spy).to.not.have.been.called.once;
+    });
+
+  });
+
+
+
+  describe("bubble", function() {
+
+    it("from children to parent", function(){
+      var child = new Backbone.ViewMaster();
+      child.template = function() {
+        return "<p>child</p>";
+      };
+
+      var parent = new Backbone.ViewMaster();
+      parent.template = function() {
+        return "<div class=container ></div>";
+      };
+
+      // to make sure there are no double event bindings
+      parent.setView(".container", child);
+      parent.setView(".container", child);
+
+      parent.render();
+
+      var spy = chai.spy();
+      parent.on("test", spy);
+      child.trigger("test");
+      expect(spy).to.have.been.called.once;
+    });
+
+    it("passes options to parent", function(done){
+      var child = new Backbone.ViewMaster();
+      child.template = function() {
+        return "<p>child</p>";
+      };
+
+      var parent = new Backbone.ViewMaster();
+      parent.template = function() {
+        return "<div class=container ></div>";
+      };
+
+      parent.setView(".container", child);
+
+      parent.render();
+
+      parent.on("test", function(arg1, arg2) {
+        expect(arg1).to.eq(1);
+        expect(arg2).to.eq(2);
+        done();
+      });
+
+      child.trigger("test", 1, 2);
+    });
+
+    it("multiple levels", function(done){
+      var child = new Backbone.ViewMaster();
+      child.template = function() {
+        return "<p>child</p>";
+      };
+
+      var parent = new Backbone.ViewMaster();
+      parent.template = function() {
+        return "<div class=container ></div>";
+      };
+
+      var grandparent = new Backbone.ViewMaster();
+      grandparent.template = function() {
+        return "<div class=container ></div>";
+      };
+
+      parent.setView(".container", child);
+      grandparent.setView(".container", parent);
+
+      grandparent.render();
+
+      grandparent.on("test", function(arg1, arg2) {
+        expect(arg1).to.eq(1);
+        expect(arg2).to.eq(2);
+        done();
+      });
+
+      child.trigger("test", 1, 2);
+    });
+
+    describe("options", function() {
+      function bubbleEnv(){
+        var child = new Backbone.ViewMaster();
+        child.template = function() {
+          return "<p>child</p>";
+        };
+
+        var parent = new Backbone.ViewMaster();
+        parent.template = function() {
+          return "<div class=container ></div>";
+        };
+
+        parent.setView(".container", child);
+
+        parent.render();
+
+        var spy = chai.spy();
+        parent.on("test", spy);
+
+        return {
+          parent: parent,
+          child: child,
+          spy: spy
+        };
+      }
+
+      it("`{parent: false}` prevents bubbling", function(){
+        var env = bubbleEnv();
+        env.child.trigger("test", { parent: false });
+        expect(env.spy).to.not.have.been.called.once;
+      });
+
+      it("`{parent: null}` prevents bubbling", function(){
+        var env = bubbleEnv();
+        env.child.trigger("test", { parent: null });
+        expect(env.spy).to.not.have.been.called.once;
+      });
+
+      it("`{parent: true}` does not prevent bubbling", function(){
+        var env = bubbleEnv();
+        env.child.trigger("test", { parent: true });
+        expect(env.spy).to.have.been.called.once;
+      });
+
+      it("`{parent: undefined}` does not prevent bubbling", function(){
+        var env = bubbleEnv();
+        env.child.trigger("test", { parent: undefined });
+        expect(env.spy).to.have.been.called.once;
+      });
+
+      it("`{}` does not prevent bubbling", function(){
+        var env = bubbleEnv();
+        env.child.trigger("test", {});
+        expect(env.spy).to.have.been.called.once;
+      });
+
+    });
+
+
+    it("dont break dom bubbling", function(){
+      var child = new Backbone.ViewMaster();
+      child.template = function() {
+        return "<p>child</p>";
+      };
+
+      var parent = new Backbone.ViewMaster();
+      parent.template = function() {
+        return "<div class=container ></div>";
+      };
+
+      // to make sure there are no double event bindings
+      parent.setView(".container", child);
+      parent.setView(".container", child);
+
+      parent.render();
+
+      var spy = chai.spy();
+
+      parent.$el.on("click", spy);
+      child.$("p").click();
+      expect(spy).to.have.been.called.once;
+
+    });
+
+
+    _.each(["setView", "appendView", "prependView"], function(viewMethod) {
+      it("to new parent after a parent change with " + viewMethod, function(){
+        var Parent = Backbone.ViewMaster.extend({
+          template: function() {
+            return "<div class=container></div>";
+          }
+        });
+
+        var parent1 = new Parent();
+        var parent2 = new Parent();
+        var child = new Backbone.ViewMaster();
+        child.template = function() {
+          return "<span class=child>child</span>";
+        };
+
+        parent1[viewMethod](".container", child);
+        parent1.render();
+
+        parent2[viewMethod](".container", child);
+
+        parent2.render();
+        parent1.render();
+
+        parent1.on("test", function() {
+          throw new Error("Event on old parent");
+        });
+
+        var spy = chai.spy();
+        parent2.on("test", spy);
+        child.trigger("test");
+
+        expect(spy).to.have.been.called.once;
+      });
+    });
+
+    it("is unbound on remove()", function(){
+      var child = new Backbone.ViewMaster();
+      child.template = function() {
+        return "<p>child</p>";
+      };
+
+      var parent = new Backbone.ViewMaster();
+      parent.template = function() {
+        return "<div class=container ></div>";
+      };
+
+      parent.setView(".container", child);
+      parent.render();
+      child.remove();
+
+      var spy = chai.spy();
+      parent.on("test", spy);
+      child.trigger("test");
+
+      expect(spy).to.not.have.been.called.once;
+    });
+
+    it("is unbound when parent is removed", function(){
+      var child = new Backbone.ViewMaster();
+      child.template = function() {
+        return "<p>child</p>";
+      };
+
+      var parent = new Backbone.ViewMaster();
+      parent.template = function() {
+        return "<div class=container ></div>";
+      };
+
+      parent.setView(".container", child);
+      parent.render();
+
+      var spy = chai.spy();
+      parent.on("test", spy);
+
+      parent.remove();
+
+      child.trigger("test");
+
+      expect(spy).to.not.have.been.called.once;
+    });
+
+
+  });
+
+
+
+});
---
+++ parts/webmenu/tests/viewmaster.test.js
@@ -0,0 +1,377 @@
+/*global window:true,describe:true, it:true, expect: true, Backbone: true, beforeEach: true, chai:true, _:true*/
+/*jshint expr:true */
+
+describe("ViewMaster", function(){
+
+  var Puppet = Backbone.ViewMaster.extend({
+    name: "puppet",
+
+    constructor: function(opts) {
+      Backbone.ViewMaster.prototype.constructor.apply(this, arguments);
+      if (opts && opts.name) {
+        this.name = opts.name;
+      }
+    },
+
+    template: function(){
+      return "<h1 class=child>" + this.name + "</h1>";
+    }
+  });
+
+
+  var Master = Backbone.ViewMaster.extend({
+    template: function(){
+      return '<div class="container"></div>';
+    }
+  });
+
+  it("is installed to Backbone namespace", function(){
+    expect(Backbone.ViewMaster).to.be.ok;
+  });
+
+  it("returns null for unknown view container", function() {
+    var view = new Backbone.ViewMaster();
+    expect(view.getViews("something")).to.eq(undefined);
+  });
+
+  it("Adds model to instance", function(){
+    var model = new Backbone.Model();
+    var view = new Backbone.ViewMaster({
+      model: model
+    });
+    expect(view.model === model).to.be.ok;
+  });
+
+
+  it("renders child view from setView", function(){
+    var m = new Master();
+    m.setView(".container", new Puppet());
+    m.render();
+    expect(m.$el).to.have.text("puppet");
+  });
+
+  it("renders child view from setView using an array", function(){
+    var m = new Master();
+    m.setView(".container", [new Puppet()]);
+    m.render();
+    expect(m.$el).to.have.text("puppet");
+  });
+
+
+  it("does not render child views twice", function(){
+    var m = new Master();
+    var p = new Puppet();
+    var spy = p.render = chai.spy(p.render);
+
+    m.setView(".container", p);
+    m.render();
+    m.render();
+    expect(spy).to.have.been.called.once;
+  });
+
+
+  it("only new child views are rendered on ViewMaster#render()", function(){
+    var m = new Master();
+    var first = new Puppet({ name: "first" });
+    first.render = chai.spy(first.render);
+
+    m.setView(".container", first);
+    m.render();
+
+    var second = new Puppet({ name: "second" });
+    second.render = chai.spy(second.render);
+    m.appendView(".container", second);
+    m.renderViews();
+
+    expect(first.render).to.have.been.called.once;
+    expect(second.render).to.have.been.called.once;
+
+    expect(m.$el).to.have(first.$el);
+    expect(m.$el).to.have(second.$el);
+  });
+
+
+  it("force option renders child views", function(){
+    var m = new Master();
+    var p = new Puppet();
+    var spy = p.render = chai.spy(p.render);
+
+    m.setView(".container", p);
+    m.render();
+    m.render({ force: true });
+    expect(spy).to.have.been.called.twice;
+  });
+
+  it("can add new views with appendView() after initial render", function(){
+    var m = new Master();
+    m.setView(".container", new Puppet());
+    m.render();
+
+    m.appendView(".container", new Puppet({ name: "newview" }));
+    m.render();
+    var childs = m.$(".child");
+
+    expect(childs[0].innerHTML).to.eq("puppet");
+    expect(childs[1].innerHTML).to.eq("newview");
+  });
+
+  it("can insert view to specific index with insertView(...)", function(){
+    var parent = new Master();
+    parent.setView(".container", new Puppet());
+    parent.render();
+
+    var first = new Puppet({ name: "first" });
+    var last = new Puppet({ name: "last" });
+    parent.setView(".container", [first, last]);
+    parent.render();
+
+    expect(parent.$el).to.have(first.$el);
+    expect(parent.$el).to.have(last.$el);
+
+    var middle = new Puppet({ name: "middle" });
+    parent.insertView(".container", 1, middle);
+    parent.renderViews();
+
+    expect(parent.$el).to.have(middle.$el);
+    expect(parent.getViews(".container")).to.deep.eq([first, middle, last]);
+
+  });
+
+  it("can add new views with prependView() after initial render", function(){
+    var m = new Master();
+    m.setView(".container", new Puppet());
+    m.render();
+
+    m.prependView(".container", new Puppet({ name: "newview" }));
+    m.renderViews();
+    var childs = m.$(".child");
+    expect(childs[1].innerHTML).to.eq("puppet");
+    expect(childs[0].innerHTML).to.eq("newview");
+  });
+
+  it("updates view order", function(){
+    var m = new Master();
+    var first = new Puppet({ name: "first" });
+    var second = new Puppet({ name: "second" });
+
+    m.setView(".container", [first, second]);
+    m.render();
+
+    m.setView(".container", [second, first]);
+    m.renderViews();
+
+    var childs = m.$(".child");
+    expect(childs[0].innerHTML).to.eq("second");
+    expect(childs[1].innerHTML).to.eq("first");
+  });
+
+  it("setView&renderViews() removes old views", function(){
+    var m = new Master();
+    var views = [
+      new Puppet({ name: "first" }),
+      new Puppet({ name: "second" })
+    ];
+
+    m.setView(".container", views);
+    m.render();
+
+    m.setView(".container", views[1]);
+    m.renderViews();
+
+    expect(m.$el).to.not.have(views[0].$el);
+    expect(m.$el).to.have(views[1].$el);
+
+  });
+
+
+  it("does not render already rendered views", function(){
+    var master = new Master();
+    var child = new Puppet();
+    child.render = chai.spy(child.render);
+    child.render();
+
+    master.setView(".container", child);
+    master.render();
+
+    expect(child.render).to.have.been.called.once;
+    expect(master.$el).to.have(child.$el);
+  });
+
+  it("child.remove() removes from parent", function(){
+    var parent = new Master();
+    var child = new Puppet({ name: "first" });
+
+    parent.setView(".container", child);
+    parent.render();
+    expect(_.contains(parent.getViews(".container"), child)).to.be.ok;
+
+    child.remove();
+    parent.render();
+    expect(parent.$el).to.not.have(child.$el);
+    expect(_.contains(parent.getViews(".container"), child)).to.not.be.ok;
+  });
+
+  it("child.detach() removes from parent and DOM", function(){
+    var parent = new Master();
+    var child = new Puppet({ name: "first" });
+
+    parent.setView(".container", child);
+    parent.render();
+    expect(_.contains(parent.getViews(".container"), child)).to.be.ok;
+
+    child.detach();
+    expect(parent.$el).to.not.have(child.$el);
+    expect(_.contains(parent.getViews(".container"), child)).to.not.be.ok;
+  });
+
+  it("child.detach() does not clear dom events", function(){
+    var child = new (Backbone.ViewMaster.extend({
+
+      template: function() {
+        return "<button>child</button>";
+      },
+
+      events: {
+        "click button": "spy"
+      },
+
+      spy: chai.spy()
+
+    }))();
+
+    var parent = new Backbone.ViewMaster();
+    parent.template = function() {
+      return "<div class=container ></div>";
+    };
+
+    parent.setView(".container", child);
+    parent.render();
+
+    child.detach();
+    child.$("button").click();
+    expect(child.spy).to.have.been.called.once;
+  });
+
+
+  describe("with deeply nested views", function(){
+
+    var ViewList = Backbone.ViewMaster.extend({
+      template: function() {
+        return '<h1>List<h1><div class="items"></div>';
+      }
+    });
+
+    var Layout = Backbone.ViewMaster.extend({
+
+      template: function() {
+        return '<h1>Title<h1><div class="container"></div>';
+      }
+
+    });
+
+    it("renders them all", function(){
+      var children = [
+        new Puppet({ name: "first" }),
+        new Puppet({ name: "second" })
+      ];
+      var list = new ViewList();
+      list.setView(".items", children);
+      var layout = new Layout();
+      layout.setView(".container", list);
+
+      layout.render();
+
+      expect(layout.$el).to.contain("List");
+      expect(layout.$el).to.contain("first");
+      expect(layout.$el).to.contain("second");
+
+    });
+
+    it("does not render nested views twice", function(){
+
+      var children = [
+        new Puppet({ name: "first" }),
+        new Puppet({ name: "second" })
+      ];
+
+      var spy = children[1].render = chai.spy(children[1].render);
+
+      var list = new ViewList();
+      list.setView(".items", children);
+
+      var layout = new Layout();
+      layout.setView(".container", list);
+
+      layout.render();
+      layout.render();
+
+      expect(spy).to.have.been.called.once;
+
+    });
+
+    it("option force calls nested views twice", function(){
+
+      var children = [
+        new Puppet({ name: "first" }),
+        new Puppet({ name: "second" })
+      ];
+
+      var spy = children[1].render = chai.spy(children[1].render);
+
+      var list = new ViewList();
+      list.setView(".items", children);
+
+      var layout = new Layout();
+      layout.setView(".container", list);
+
+      layout.render();
+      layout.render({ force: true });
+
+      expect(spy).to.have.been.called.twice;
+
+    });
+
+    it("can move child to other parent", function(){
+      var Parent = Backbone.ViewMaster.extend({
+        template: function() {
+          return "<div class=container></div>";
+        }
+      });
+
+      var parent1 = new Parent();
+      var parent2 = new Parent();
+      var child = new Backbone.ViewMaster();
+      child.template = function() {
+        return "<span class=child>child</span>";
+      };
+
+      parent1.setView(".container", child);
+      parent1.render();
+
+      parent2.setView(".container", child);
+
+      parent2.render();
+      parent1.render();
+
+      expect(parent2.$el).to.have(child.$el);
+      expect(parent1.$el).to.not.have(child.$el);
+
+    });
+
+
+  });
+
+  describe("CoffeeScript", function() {
+    it("works with CoffeeScript classes", function(done){
+      var model = new Backbone.Model();
+      model.on("done", done);
+
+      var view = new window.CoffeeView({
+        model: model
+      });
+
+      model.trigger("test");
+
+    });
+  });
+});
