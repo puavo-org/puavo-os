904cdcc241f248bd56665ca18d61a15188be198e
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2012-12-12 12:06:26 +0200

    Add support for apt-keys.

---
+++ parts/rules/ltsp-build-client/puppet/ltsp/organisation_apt_repositories/manifests/init.pp
@@ -0,0 +1,3 @@
+class organisation_apt_repositories {
+  # ...
+}
--- parts/rules/ltsp-build-client/puppet/ltsp/packages/manifests/opinsys.pp
+++ parts/rules/ltsp-build-client/puppet/ltsp/packages/manifests/opinsys.pp
@@ -1,11 +1,13 @@
 class packages::opinsys {
-  include apt::repositories,
-          packages
+  # XXX this class perhaps belongs elsewhere?
+
+  include organisation_apt_repositories
 
   # apply all package definitions with "opinsys"-tag listed in packages
   Package <| tag == opinsys |> {
     require => [ Apt::Repository['liitu'], Exec['apt update'], ],
   }
 
+  Apt::Key        <| title == "opinsys-repo.gpgkey" |>
   Apt::Repository <| title == liitu |>
 }
