199fdeeb99b34a2e8f389c75b42f6f0ec004668d
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2012-12-27 16:21:19 +0200

    Install google-talkplugin, but do not setup apt-key and apt sources for it.

---
+++ parts/rules/ltsp-build-client/puppet/ltsp/google_talkplugin/manifests/init.pp
@@ -0,0 +1,11 @@
+class google_talkplugin {
+  include packages
+
+  file {
+    '/etc/default/google-talkplugin':
+      ensure => present,
+      before => Package['google-talkplugin'];
+  }
+
+  Package <| title == google-talkplugin |>
+}
