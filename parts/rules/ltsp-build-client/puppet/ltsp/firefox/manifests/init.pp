5683de36df94812a0fd7c95ce28b963c31ab582f
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2012-12-28 10:19:28 +0200

    Setup Firefox-settings in such a way that they are actually effective.

--- parts/rules/ltsp-build-client/puppet/ltsp/firefox/manifests/init.pp
+++ parts/rules/ltsp-build-client/puppet/ltsp/firefox/manifests/init.pp
@@ -1,9 +1,19 @@
 class firefox {
   include firefox
 
+  # Firefox configuration system is still a mess... if there really is a more
+  # straightforward way, I would like to hear about it.
   file {
     '/etc/firefox/syspref.js':
       content => template('firefox/syspref.js'),
+      require => File['/usr/lib/firefox/firefox-puavodesktop.js'];
+
+    '/usr/lib/firefox/firefox-puavodesktop.js':
+      content => template('firefox/firefox-puavodesktop.js'),
+      require => File['/etc/firefox/puavodesktop.js'];
+
+    '/etc/firefox/puavodesktop.js':
+      content => template('firefox/puavodesktop.js'),
       require => Package['firefox'];
   }
 
---
+++ parts/rules/ltsp-build-client/puppet/ltsp/firefox/templates/puavodesktop.js
@@ -0,0 +1,10 @@
+defaultPref("browser.search.defaultenginename", "Google");
+defaultPref("browser.search.selectedEngine", "Google");
+lockPref("browser.cache.disk.capacity", 0);
+lockPref("browser.cache.disk.enable", false);
+lockPref("browser.safebrowsing.enabled", false);
+lockPref("browser.safebrowsing.malware.enabled", false);
+lockPref("network.dns.disableIPv6", true);
+lockPref("network.negotiate-auth.trusted-uris", "weblogin.opinsys.fi");
+lockPref("print.postscript.paper_size", "a4");
+lockPref("toolkit.storage.synchronous", 0);
