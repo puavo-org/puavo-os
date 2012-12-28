ae51970c84963d7385543b7a89a33b07363b8075
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2012-12-28 10:19:28 +0200

    Setup Firefox-settings in such a way that they are actually effective.

--- parts/rules/ltsp-build-client/puppet/ltsp/firefox/templates/syspref.js
+++ parts/rules/ltsp-build-client/puppet/ltsp/firefox/templates/syspref.js
@@ -1,10 +1,7 @@
-defaultPref("browser.search.defaultenginename", "Google");
-defaultPref("browser.search.selectedEngine", "Google");
-lockPref("browser.cache.disk.capacity", 0);
-lockPref("browser.cache.disk.enable", false);
-lockPref("browser.safebrowsing.enabled", false);
-lockPref("browser.safebrowsing.malware.enabled", false);
-lockPref("network.dns.disableIPv6", true);
-lockPref("network.negotiate-auth.trusted-uris", "weblogin.opinsys.fi");
-lockPref("print.postscript.paper_size", "a4");
-lockPref("toolkit.storage.synchronous", 0);
+// This file can be used to configure global preferences for Firefox
+// Example: Homepage
+//pref("browser.startup.homepage", "http://www.weebls-stuff.com/wab/");
+
+// setup puavodesktop settings
+pref("general.config.obscure_value", 0);
+pref("general.config.filename", "firefox-puavodesktop.js");
