83571e2e0d0c26aac098a3b3d39ecb9e570d426d
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2013-01-16 13:22:52 +0200

    Add a comment clarifying that we do not intend to turn this off for all machines (in the long run).

--- parts/rules/ltsp-build-client/puppet/ltsp/cpu_frequency_scaling/manifests/init.pp
+++ parts/rules/ltsp-build-client/puppet/ltsp/cpu_frequency_scaling/manifests/init.pp
@@ -1,4 +1,8 @@
 class cpu_frequency_scaling {
+  # XXX
+  # Disable CPU frequency scaling for now.  Perhaps we will disable this only
+  # on some machines, because this is rather drastic.
+
   file {
     '/etc/init.d/ondemand':
       ensure => absent;
