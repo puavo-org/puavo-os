27beb11c74e9c304bb8e6d5b2b51be42e260b62b
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2013-01-29 11:44:54 +0200

    Disable whoopsie (but keep it still installed).

---
+++ parts/rules/ltsp-build-client/puppet/ltsp/crash_reporting/manifests/init.pp
@@ -0,0 +1,11 @@
+class crash_reporting {
+  include packages
+
+  file {
+    '/etc/default/whoopsie':
+      content => template('crash_reporting/etc_default_whoopsie'),
+      require => Package['whoopsie'];
+  }
+
+  Package <| title == whoopsie |>
+}
