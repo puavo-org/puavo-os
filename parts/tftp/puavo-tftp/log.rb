45be6479314202fdec698094569f3f120f9a6428
Tuomas Räsänen <tuomasjjrasanen@opinsys.fi>
2013-02-11 11:38:25 +0200

    Prefix each log message with a timestamp

--- parts/tftp/puavo-tftp/log.rb
+++ parts/tftp/puavo-tftp/log.rb
@@ -1,17 +1,18 @@
 
 def log(*args)
+  args[0] = Time.now.to_s + " " + args[0]
   puts(*args)
 end
 
 def warn(*args)
   args[0] = "WARN: " + args[0]
-  puts(*args)
+  log(*args)
 end
 
 def debug(*args)
   if $tftp_debug
     args[0] = "DEBUG: " + args[0]
-    puts(*args)
+    log(*args)
   end
 end
 
