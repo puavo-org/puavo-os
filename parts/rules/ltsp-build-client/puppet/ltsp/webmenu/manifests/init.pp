5888388d7384cc4f8f159df7c8e62d99756781e4
Antti Sokero <antti.sokero@opinsys.fi>
2013-02-12 15:33:47 +0200

    Add Opinsys specifig stuff.

--- parts/rules/ltsp-build-client/puppet/ltsp/webmenu/manifests/init.pp
+++ parts/rules/ltsp-build-client/puppet/ltsp/webmenu/manifests/init.pp
@@ -1,16 +1,20 @@
 class webmenu {
   include packages
 
-  # XXX these might be unnecessary once we get these into webmenu-package
-  # XXX itself
+  File { require => [ Package['liitu-themes']
+		    , Package['webmenu'] ], }
   file {
+    '/etc/webmenu':
+      ensure => directory;
+
+    '/etc/webmenu/config.json':
+      content => template('webmenu/config.json');
+
     '/etc/xdg/autostart/webmenu.desktop':
-      content => template('webmenu/webmenu.desktop'),
-      require => Package['webmenu'];
+      content => template('webmenu/webmenu.desktop');
 
     '/usr/share/applications/webmenu-spawn.desktop':
-      content => template('webmenu/webmenu-spawn.desktop'),
-      require => Package['webmenu'];
+      content => template('webmenu/webmenu-spawn.desktop');
   }
 
   Package <| (title == liitu-themes)
