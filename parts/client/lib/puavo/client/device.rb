d1e39bd3118ef0d938f59f7a5922a3b076f9b372
Jouni Korhonen <jouni.korhonen@opinsys.fi>
2013-02-13 11:12:20 +0200

    Device class use api version 2

--- parts/client/lib/puavo/client/device.rb
+++ parts/client/lib/puavo/client/device.rb
@@ -1,7 +1,7 @@
 module Puavo
   module Client
     class Device < Model
-      model_path :prefix => '/devices', :path => "/devices"
+      model_path :prefix => '/devices/api/v2', :path => "/devices"
     end
   end
 end
