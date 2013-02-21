dee954b69ac5fe53b4f664609e014f8ada34e0f4
Jouni Korhonen <jouni.korhonen@opinsys.fi>
2013-02-21 15:35:29 +0200

    Lts: add methods for setting keep system services

--- parts/client/lib/puavo/lts/base.rb
+++ parts/client/lib/puavo/lts/base.rb
@@ -124,6 +124,16 @@ module Puavo
       def boot_server_fqdn
         "#{PUAVO_ETC.hostname}.#{PUAVO_ETC.domain}"
       end
+
+      def keep_services_tty_services
+        "tty1 tty2 tty3 tty4 tty5 tty6"
+      end
+
+      def keep_services_tty_and_nfs_services
+        "#{keep_services_tty_services} gssd idmapd rpcbind-boot portmap"
+      end
+
+
     end
   end
 end
