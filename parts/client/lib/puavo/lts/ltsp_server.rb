d42e20dfabadda387b55383ce0d44507754fd0a7
Jouni Korhonen <jouni.korhonen@opinsys.fi>
2013-02-21 15:39:35 +0200

    Lts: update ltsp_server configuration

--- parts/client/lib/puavo/lts/ltsp_server.rb
+++ parts/client/lib/puavo/lts/ltsp_server.rb
@@ -7,7 +7,23 @@ module Puavo
         @school = school
         @device = device
 
-        @lts_data = {}
+        @lts_data = {
+          'KEEP_SYSTEM_SERVICES' => keep_services_tty_and_nfs_services,
+          'LOCAL_APPS'           => 'False',
+          'LOCALDEV'             => 'False',
+          'LTSP_FATCLIENT'       => 'False',
+          'LTSP_SERVER'          => 'True',
+          'SERVER'               => boot_server_fqdn,
+          'USE_NFS_HOMES'        => 'True',
+        }
+        
+        @lts_data.merge!( define_tags )
+        @lts_data.merge!( define_default_printer )
+        @lts_data.merge!( define_xserver_driver )
+        @lts_data.merge!( define_xrandrs )
+        @lts_data.merge!( define_resolution )
+        @lts_data.merge!( define_auto_power_off )
+        @lts_data.merge!( define_system_services )
       end
 
     end
