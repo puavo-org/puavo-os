0af1a613f5d516aedfaa81d2216312ce05a46b6d
Jouni Korhonen <jouni.korhonen@opinsys.fi>
2013-02-22 09:45:04 +0200

    Refactor organisation creation. See log history from puavo-tools repository:
    https://github.com/opinsys/puavo-tools/compare/afbaf308f623955afa39dfb4f6664000adc57f50...b0debdf5b02a1081ed994a1887e0cc25cd61b520

--- cloud/puavo-ds/master/add_new_organisation.rb
+++ cloud/puavo-ds/master/add_new_organisation.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/ruby
+# -*- coding: utf-8 -*-
 #
 # Usage: ruby add_new_organisation.rb <organisation name>
 #
@@ -7,6 +8,62 @@ $LOAD_PATH.unshift( File.join( File.dirname(__FILE__), 'lib' ) )
 
 require 'rubygems'
 require 'active_ldap'
+require 'optparse'
+require 'readline'
+#require 'ruby-debug'
+
+options = {}
+a = OptionParser.new do |opts|
+  opts.banner = "Usage: add_new_organisation [options]"
+
+  opts.on("-y", "--yes", "Automatic yes to prompts") do |y|
+    options[:yes] = y
+  end
+
+  opts.on("-o=ORGANISATION", "Organisation") do |organisation|
+    options[:organisation] = organisation
+  end
+
+  opts.on("--domain [DOMAIN]", "Domain") do |domain|
+    options[:domain] = domain
+  end
+  opts.on("--organisation_name [ORGANISATION_NAME]", "Organisation name") do |organisation|
+    options[:organisation_name] = organisation
+  end
+  opts.on("--legal_name [LEGAL_NAME]", "Legal name") do |legal_name|
+    options[:legal_name] = legal_name
+  end
+  opts.on("--samba_domain [SAMBA_DOMAIN]", "Samba domain") do |samba_domain|
+    options[:samba_domain] = samba_domain
+  end
+  opts.on("--suffix [SUFFIX]", "Suffix") do |suffix|
+    options[:suffix] = suffix
+  end
+
+  opts.on("--given_name [GIVEN_NAME]", "Given name") do |given_name|
+    options[:given_name] = given_name
+  end
+
+  opts.on("--surname [SURNAME]", "Surname") do |surname|
+    options[:surname] = surname
+  end
+  opts.on("--username [USERNAME]", "Username") do |username|
+    options[:username] = username
+  end
+  opts.on("--password [PASSWORD]", "Password") do |password|
+    options[:password] = password
+  end
+  opts.on_tail("-h", "--help", "Show this message") do
+    puts opts
+    exit
+  end
+end.parse!
+
+unless options.has_key?(:organisation)
+  puts "Required option --organisation missing"
+  exit
+end
+
 # LDAP configuration
 if configurations = YAML.load_file("config/ldap.yml") rescue nil
   ActiveLdap::Base.configurations = configurations
@@ -36,7 +93,9 @@ require 'users/user'
 require 'users/samba_domain'
 require 'users/ldap_organisation'
 require 'kerberos'
-require 'readline'
+require 'oauth'
+require 'oauth_client'
+require 'oauth_token'
 
 def newpass( len )
   chars = ("a".."z").to_a + ("A".."Z").to_a + ("0".."9").to_a
@@ -45,7 +104,6 @@ def newpass( len )
   return newpass
 end
 
-puppet_host_template = configurations["settings"]["templates"]["puppet_host"]
 samba_domain_template = configurations["settings"]["templates"]["samba_domain"]
 suffix_template = configurations["settings"]["templates"]["suffix"]
 domain_template = configurations["settings"]["templates"]["domain"]
@@ -53,83 +111,15 @@ domain_template = configurations["settings"]["templates"]["domain"]
 # This needs to be cleaned up once the actual settings and needs
 # have been figured out
 
-case ARGV.length
-        when 1
-		orgname = ARGV.first
-		domain = domain_template % orgname.downcase
+orgname = options[:organisation]
+domain = options.has_key?(:domain) ? options[:domain] : domain_template % orgname.downcase 
 
-		suffix = suffix_template % orgname.downcase
-		suffix_start = suffix.split(',')[0]
-		organisation_name = orgname
-		legal_name = organisation_name
+suffix = options.has_key?(:suffix) ? options[:suffix] : suffix_template % orgname.downcase
+suffix_start = suffix.split(',')[0]
+organisation_name = options.has_key?(:organisation_name) ? options[:organisation_name] : orgname 
+legal_name = options.has_key?(:legal_name) ? options[:legal_name] : organisation_name
 
-		puppet_host = puppet_host_template % orgname.downcase
-		samba_domain = samba_domain_template % orgname.upcase
-        when 2
-		orgname = ARGV.first
-		domain = ARGV[1]
-		
-		suffix = suffix_template % orgname.downcase
-		suffix_start = suffix.split(',')[0]
-		organisation_name = orgname
-		legal_name = organisation_name
-
-		puppet_host = puppet_host_template % orgname.downcase
-		samba_domain = samba_domain_template % orgname.upcase
-        when 3
-		orgname = ARGV.first
-		domain = ARGV[1]
-
-		suffix = suffix_template % orgname.downcase
-		suffix_start = suffix.split(',')[0]
-		organisation_name = ARGV[2]
-		legal_name = organisation_name
-		puppet_host = puppet_host_template % orgname.downcase
-		samba_domain = samba_domain_template % orgname.upcase
-        when 4
-		orgname = ARGV.first
-		domain = ARGV[1]
-
-		suffix = suffix_template % orgname.downcase
-		suffix_start = suffix.split(',')[0]
-		organisation_name = ARGV[2]
-		legal_name = ARGV[3]
-		puppet_host = puppet_host_template % orgname.downcase
-		samba_domain = samba_domain_template % orgname.upcase
-        when 5
-		orgname = ARGV.first
-		domain = ARGV[1]
-
-		suffix = suffix_template % orgname.downcase
-		suffix_start = suffix.split(',')[0]
-		organisation_name = ARGV[2]
-		legal_name = ARGV[3]
-		samba_domain = ARGV[4]
-		puppet_host = puppet_host_template % orgname.downcase
-        when 6
-		orgname = ARGV.first
-		domain = ARGV[1]
-
-		suffix = suffix_template % orgname.downcase
-		suffix_start = suffix.split(',')[0]
-		organisation_name = ARGV[2]
-		legal_name = ARGV[3]
-		samba_domain = ARGV[4]
-		puppet_host = ARGV[5]
-        when 7
-		orgname = ARGV.first
-		domain = ARGV[1]
-
-		suffix = ARGV[6]
-		suffix_start = suffix.split(',')[0]
-		organisation_name = ARGV[2]
-		legal_name = ARGV[3]
-		samba_domain = ARGV[4]
-		puppet_host = ARGV[5]
-	else
-                puts "Usage: $0 orgname [domain_name] [Organisation name] [Legal name] [samba domain] [puppet host] [suffix]"
-		exit
-end
+samba_domain = options.has_key?(:samba_domain) ? options[:samba_domain] : samba_domain_template % orgname.upcase
 
 puts "******************************************************"
 puts "  Initialising organisation: #{organisation_name}"
@@ -143,10 +133,52 @@ puts "* Kerberos realm: #{kerberos_realm}"
 puts "* Legal name: #{legal_name}"
 puts "* Samba: #{samba_domain}"
 puts "* Domain: #{domain}"
-puts "* Puppet host: #{puppet_host}"
 puts "* Suffix start: #{suffix_start}"
 
-Readline.readline('OK?', true)
+Readline.readline('OK?', true) unless options[:yes]
+
+# User
+puts "\nCreate organisation owner:"
+
+if options.has_key?(:given_name)
+  given_name = options[:given_name]
+  puts "Given name: #{given_name}"
+else
+  print "Given name: "
+  given_name = STDIN.gets.chomp
+end
+
+if options.has_key?(:surname)
+  surname = options[:surname]
+  puts "Surname: #{surname}"
+else
+  print "Surname: "
+  surname = STDIN.gets.chomp
+end
+
+if options.has_key?(:username)
+  username =  options[:username] 
+  puts "Username: #{username}"
+else
+  print "Username: "
+  username =  STDIN.gets.chomp
+end
+if options.has_key?(:password)
+  password = options[:password]
+else
+  system('stty','-echo');
+  print "Password: "
+  password = STDIN.gets.chomp
+  print "\nPassword confirmation: "
+  password_confirmation = STDIN.gets.chomp
+  system('stty','echo');
+end
+
+# FIXME: asking whether the user wants to configure kerberos?
+puts "\nStop krb5-kdc and kadmind services\n\n"
+`/etc/init.d/krb5-kdc stop`
+`/etc/init.d/puavo_kadmind stop`
+
 begin
   new_db = Database.new( "olcSuffix" => suffix,
                          "olcRootDN" => rootDN,
@@ -155,8 +187,7 @@ begin
   # Save without validation
   new_db.save(false)
 rescue => e
-  puts e
-  exit
+  raise e
 end
 
 new_db = Database.find(:first, :attribute => 'olcSuffix', :value => suffix)
@@ -164,11 +195,6 @@ new_db = Database.find(:first, :attribute => 'olcSuffix', :value => suffix)
 puts "* Setting up overlay configuration to database"
 Overlay.create_overlays(:database => new_db,
                         :kerberos_realm => kerberos_realm)
-
-if ActiveLdap::Base.configurations["settings"]["syncrepl"]["nodes"]
-  puts "* Setting up replication configuration"
-  new_db.set_replication_settings
-end
 
 # Create organisation and set LdapOrganisationBase LDAP connection
 puts "* Create organisation root"
@@ -180,7 +206,6 @@ organisation = Organisation.create( :owner => configurations["settings"]["ldap_s
                                     :cn => organisation_name,
                                     :description => organisation_name,
                                     :eduOrgLegalName => legal_name,
-                                    :puavoPuppetHost => puppet_host,
                                     :sambaDomainName => samba_domain )
 
 puts "* Add organizational units: People, Groups, Hosts, Automount, etc..."
@@ -191,6 +216,9 @@ Automount.create_automount_configuration
 
 puts "* Setting up Samba configuration"
 Samba.create_samba_configuration(organisation_name, samba_domain, suffix_start)
+
+puts "* Create OAuth subtree"
+OAuth.create_oauth_branch
 
 puts "* Create System Groups"
 SystemGroup.create_system_groups
@@ -224,75 +252,36 @@ group = Group.create!( :displayName => group_name,
 # Added association
 role.groups << group
 
-# Create kerberos realm
-
-#`mkdir -p /etc/krb5kdc/masterkeys`
-#`chmod 0700 /etc/krb5kdc/masterkeys`
-
-`echo "#{configurations["settings"]["kdc"]["password"]}\\n#{configurations["settings"]["kdc"]["password"]}\\n" | /usr/sbin/kdb5_ldap_util stashsrvpw -f /etc/krb5.secrets "#{configurations["settings"]["kdc"]["bind_dn"]}" 2>/dev/null`
-`echo "#{configurations["settings"]["kadmin"]["password"]}\\n#{configurations["settings"]["kadmin"]["password"]}\\n" | /usr/sbin/kdb5_ldap_util stashsrvpw -f /etc/krb5.secrets "#{configurations["settings"]["kadmin"]["bind_dn"]}" 2>/dev/null`
-
 kerberos_masterpw = newpass(20)
-puts "Initializing kerberos realm with master key: #{kerberos_masterpw}"
+puts "\nInitializing kerberos realm with master key: #{kerberos_masterpw}\n"
+
+kerberos_configuration = KerberosSettings.new(:ldap_server => configurations["settings"]["ldap_server"])
+
+kerberos_configuration.write_configurations_to_file
+
+kerberos_configuration.replace_server_configurations
 
 realm = KerberosRealm.new( :ldap_server => configurations["settings"]["ldap_server"],
                            :realm => kerberos_realm,
                            :masterpw => kerberos_masterpw,
                            :suffix => suffix,
                            :domain => domain )
+realm.save
 
-conf = KerberosRealm.create_kerberos_configuration(configurations["settings"]["ldap_server"])
+puts "Update keytab file"
+kerberos_configuration.generate_new_keytab_file
+kerberos_configuration.replace_keytab_file
 
-File.open("/etc/krb5kdc/kdc.conf", "w") {|file|
-        file.write(conf.kdc_conf)
-}
+# FIXME: asking whether the user wants to configure kerberos?
+puts "Start krb5-kdc and kadmind services"
+`/etc/init.d/krb5-kdc start`
+`/etc/init.d/puavo_kadmind start`
 
-File.open("/etc/krb5.conf", "w") {|file|
-        file.write(conf.krb5_conf)
-}
+puts "Restart slapd"
+`/etc/init.d/slapd restart`
 
-File.open("/etc/krb5kdc/kadm5.acl", "w") {|file|
-        file.write(conf.kadm5_acl)
-}
-
-File.open("/etc/default/krb5-kdc", "w") {|file|
-        file.write(conf.daemon_args)
-}
-
-realm.create_ldap_tree
-
-puts configurations["settings"]["puppetmaster"]["enable"]
-
-if configurations["settings"]["puppetmaster"]["enable"]
-  `mkdir -p #{configurations["settings"]["puppetmaster"]["file_dir"]}/etc/krb5kdc/`
-  `mkdir -p #{configurations["settings"]["puppetmaster"]["file_dir"]}/etc/default/`
-  `cp /etc/krb5kdc/* #{configurations["settings"]["puppetmaster"]["file_dir"]}/etc/krb5kdc/`
-  `cp /etc/krb5.conf #{configurations["settings"]["puppetmaster"]["file_dir"]}/etc/`
-  `cp /etc/krb5.secrets #{configurations["settings"]["puppetmaster"]["file_dir"]}/etc/`
-  `cp /etc/default/krb5-kdc #{configurations["settings"]["puppetmaster"]["file_dir"]}/etc/default/krb5-kdc`
-  `chown -R puppet #{configurations["settings"]["puppetmaster"]["file_dir"]}/*`
-
-  puts "Puppet kerberos files updated"
-else
-  puts "Puppet configuration disabled"
-end
-
-# User
-puts "Create organisation owner:"
-
-print "Given name: "
-given_name = STDIN.gets.chomp
-
-print "Surname: "
-surname = STDIN.gets.chomp
-print "Username: "
-username = STDIN.gets.chomp
-system('stty','-echo');
-print "Password: "
-password = STDIN.gets.chomp
-print "\nPassword confirmation: "
-password_confirmation = STDIN.gets.chomp
-system('stty','echo');
+puts "Waiting 3 seconds"
+sleep(3)
 
 user = User.new
 
@@ -310,10 +299,14 @@ while user_save != true
     user.save!
     user_save = true
   rescue Exception => e
-    puts
-    puts e
-    puts "Cannot save user, press enter to try again"
-    STDIN.gets
+    if options[:yes]
+      raise e
+    else
+      puts
+      puts e
+      puts "Cannot save user, press enter to try again"
+      STDIN.gets
+    end
   end
 end
 
@@ -327,3 +320,4 @@ puts "\nSets the user (#{user.uid}) as the owner of the organisation"
 ldap_organisation = LdapOrganisation.first
 ldap_organisation.owner = Array(ldap_organisation.owner).push user.dn
 ldap_organisation.save
+
--- cloud/puavo-ds/master/lib/kerberos.rb
+++ cloud/puavo-ds/master/lib/kerberos.rb
@@ -1,6 +1,51 @@
 class KerberosSettings
-  def initialize(organisations)
-    @organisations = organisations
+
+  attr_accessor :organisations
+
+  TMP = File.expand_path('kerberos_tmp')
+
+  def self.find_all_organisations(ldap_server)
+    databases = `ldapsearch -Z -x -D #{ldap_server['bind_dn']} -w #{ldap_server['password']} -H ldap://#{ldap_server['host']} -s base -b "" "(objectClass=*)" namingContexts 2>/dev/null | grep namingContexts:`
+
+    kerberos_organisations = []
+
+    databases.split("\n").each do |line|
+      if /namingContexts: (.*)/.match(line)
+        suffix = $1
+        organisation = {}
+        
+        if not /o=puavo/i.match(suffix) 
+          organisation['suffix'] = suffix
+
+          tmp_dbinfo = `ldapsearch -LLL -D #{ldap_server['bind_dn']} -w #{ldap_server['password']} -H ldap://#{ldap_server['host']} -x -s base -b #{suffix} -Z 2> /dev/null`
+
+          if /puavoDomain: (.*)/.match(tmp_dbinfo)
+            organisation['domain'] = $1
+          end
+
+          if /puavoKerberosRealm: (.*)/.match(tmp_dbinfo)
+            organisation['realm'] = $1
+          end
+
+          if /puavoKadminPort: (.*)/.match(tmp_dbinfo)
+            organisation['kadmin_port'] = $1
+          end
+
+          if organisation['domain'] and organisation['realm'] and organisation['kadmin_port']
+            kerberos_organisations.push organisation
+          end
+        end
+      end
+    end
+
+    return kerberos_organisations
+
+  end
+
+  def initialize(args)
+    @ldap_server = args[:ldap_server]
+    
+    @organisations = self.class.find_all_organisations(@ldap_server)
   end
 
   def kdc_conf
@@ -43,11 +88,94 @@ class KerberosSettings
   end
 
   def daemon_args
-    return "DAEMON_ARGS=\"" + @organisations.collect! {|org| "-r " + org['realm']}.join(" ") + "\""
+    return "DAEMON_ARGS=\"" + @organisations.collect {|org| "-r " + org['realm']}.join(" ") + "\""
   end
 
   def getBinding
     return binding()
+  end
+
+  def write_configurations_to_file
+    # Generate configuration by ldap data
+    begin
+      File.new(TMP)
+    rescue Errno::ENOENT
+      Dir.mkdir(TMP)
+    end
+
+    # Create new konfiguration files to tmp directory
+    File.open("#{TMP}/kdc.conf", "w") do |file|
+      file.write(self.kdc_conf)
+    end
+
+    File.open("#{TMP}/krb5.conf", "w") do |file|
+      file.write(self.krb5_conf)
+    end
+
+    File.open("#{TMP}/kadm5.acl", "w") do |file|
+      file.write(self.kadm5_acl)
+    end
+
+    File.open("#{TMP}/krb5-kdc", "w") do |file|
+      file.write(self.daemon_args)
+    end
+  end
+
+  def diff
+    puts "Show differences: #{TMP}/kdc.conf /etc/krb5kdc/kdc.conf"
+    print `diff #{TMP}/kdc.conf /etc/krb5kdc/kdc.conf`
+    puts
+
+    puts "Show differences: #{TMP}/krb5.conf /etc/krb5.conf"
+    print `diff #{TMP}/krb5.conf /etc/krb5.conf`
+    puts
+
+    puts "Show differences: #{TMP}/kadm5.acl /etc/krb5kdc/kadm5.acl"
+    print `diff #{TMP}/kadm5.acl /etc/krb5kdc/kadm5.acl`
+    puts
+
+    puts "Show differences: #{TMP}/krb5-kdc /etc/default/krb5-kdc"
+    print `diff #{TMP}/krb5-kdc /etc/default/krb5-kdc`
+    puts
+  end
+
+  def replace_server_configurations
+    `mv #{TMP}/kdc.conf /etc/krb5kdc/kdc.conf`
+    `mv #{TMP}/krb5.conf /etc/krb5.conf`
+    `mv #{TMP}/kadm5.acl /etc/krb5kdc/kadm5.acl`
+    `mv #{TMP}/krb5-kdc /etc/default/krb5-kdc`
+  end
+
+  def generate_new_keytab_file
+    hostname = `hostname -f`.chomp
+
+    @organisations.each do |organisation|
+      smbkrb5pwd_princ = `kadmin.local -r #{organisation['realm']} -q "listprincs" | grep smbkrb5pwd/#{hostname}@#{organisation['realm']}`.chomp
+
+      if smbkrb5pwd_princ.empty?
+        puts "Creating smbkrb5pwd/#{hostname}@#{organisation['realm']} principal"
+        `kadmin.local -r #{organisation['realm']} -q "addprinc -randkey smbkrb5pwd/#{hostname}@#{organisation['realm']}"`
+      end
+
+      puts "Exporting smbkrb5pwd/#{hostname}@#{organisation['realm']} to keytab"
+      puts `kadmin.local -r #{organisation['realm']} -q "ktadd -norandkey -k #{TMP}/openldap-krb5.keytab smbkrb5pwd/#{hostname}@#{organisation['realm']}"`
+
+      ldap_princ = `kadmin.local -r #{organisation['realm']} -q "listprincs" | grep ldap/#{hostname}@#{organisation['realm']}`.chomp
+
+      if ldap_princ.empty?
+        puts "Creating ldap/#{hostname}@#{organisation['realm']} principal"
+        `kadmin.local -r #{organisation['realm']} -q "addprinc -randkey ldap/#{hostname}@#{organisation['realm']}"`
+      end
+      
+      puts "Exporting ldap/#{hostname}@#{organisation['realm']} to keytab"
+      puts `kadmin.local -r #{organisation['realm']} -q "ktadd -norandkey -k #{TMP}/openldap-krb5.keytab ldap/#{hostname}@#{organisation['realm']}"`
+    end
+  end
+
+  def replace_keytab_file
+    `mv #{TMP}/openldap-krb5.keytab /etc/ldap/slapd.d/openldap-krb5.keytab`
+    `chown root.openldap /etc/ldap/slapd.d/openldap-krb5.keytab`
+    `chmod 0640 /etc/ldap/slapd.d/openldap-krb5.keytab`
   end
 end
   
@@ -66,50 +194,8 @@ class KerberosRealm
     self.domain = args[:domain]
   end
 
-  def create_ldap_tree
-    puts self.masterpw
-
-    KerberosRealm.create_kerberos_configuration(ldap_server)
-
-    puts "echo \"#{self.masterpw}\\n#{self.masterpw}\\n\" | /usr/sbin/kdb5_ldap_util -D #{ldap_server['bind_dn']} create -k aes256-cts-hmac-sha1-96 -subtrees \"#{self.suffix}\" -s -sf /etc/krb5kdc/stash.#{self.domain} -H ldaps://#{ldap_server['host']} -r \"#{self.realm}\" -w #{ldap_server['password']} 2>/dev/null"
-
+  # Create kerberos ldap tree and stash file
+  def save
     puts `echo "#{self.masterpw}\\n#{self.masterpw}\\n" | /usr/sbin/kdb5_ldap_util -D #{ldap_server["bind_dn"]} create -k aes256-cts-hmac-sha1-96 -subtrees "#{self.suffix}" -s -sf /etc/krb5kdc/stash.#{self.domain} -H ldaps://#{ldap_server["host"]} -r "#{self.realm}" -w #{ldap_server["password"]} 2>/dev/null`
-  end
-
-  def self.create_kerberos_configuration(ldap_server)
-    databases = `ldapsearch -Z -x -D #{ldap_server['bind_dn']} -w #{ldap_server['password']} -H ldap://#{ldap_server['host']} -s base -b "" "(objectClass=*)" namingContexts 2>/dev/null | grep namingContexts:`
-
-    @organisations = []
-
-    databases.split("\n").each do |line|
-      if /namingContexts: (.*)/.match(line)
-        suffix = $1
-        organisation = {}
-        
-        if not /o=puavo/i.match(suffix) 
-          organisation['suffix'] = suffix
-
-          tmp_dbinfo = `ldapsearch -LLL -D #{ldap_server['bind_dn']} -w #{ldap_server['password']} -H ldap://#{ldap_server['host']} -x -s base -b #{suffix} -Z 2> /dev/null`
-
-          if /puavoDomain: (.*)/.match(tmp_dbinfo)
-            organisation['domain'] = $1
-          end
-
-          if /puavoKerberosRealm: (.*)/.match(tmp_dbinfo)
-            organisation['realm'] = $1
-          end
-
-          if /puavoKadminPort: (.*)/.match(tmp_dbinfo)
-            organisation['kadmin_port'] = $1
-          end
-
-          if organisation['domain'] and organisation['realm'] and organisation['kadmin_port']
-            @organisations.push organisation
-          end
-        end
-      end
-    end
-
-    settings = KerberosSettings.new(@organisations)
   end
 end
