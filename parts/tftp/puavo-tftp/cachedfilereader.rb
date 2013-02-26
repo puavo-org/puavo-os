51f2065961b41f423c32ab7ba554f6eca9d66fa5
Esa-Matti Suuronen <esa-matti@suuronen.org>
2013-02-26 17:43:25 +0200

    Bust caches with File.mtime

--- parts/tftp/puavo-tftp/cachedfilereader.rb
+++ parts/tftp/puavo-tftp/cachedfilereader.rb
@@ -1,6 +1,8 @@
 
 require "puavo-tftp/log"
 
+# Ruby EvenMachine does not have async file io operations. So workaround just
+# by caching everything to memory.
 module PuavoTFTP
   class CachedFileReader
 
@@ -9,15 +11,30 @@ module PuavoTFTP
       @cache = {}
     end
 
+
     def read(name)
       name = File.join(@root, name)
+      file = cache_file(name)
 
-      if data = @cache[name]
-        return data
+      current_mtime = File.mtime(name)
+
+      if file[:mtime] != current_mtime
+        log "Reading #{ name } to cache"
+        file[:data] = File.open(name, "rb").read
+        file[:mtime] = current_mtime
       end
 
-      log "Synchronously reading #{ name }"
-      return @cache[name] = File.open(name, "rb").read
+      return file[:data]
+    end
+
+    private
+
+    def cache_file(name)
+      if ob = @cache[name]
+        return ob
+      else
+        return @cache[name] = {}
+      end
     end
 
   end
