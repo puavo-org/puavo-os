ef82430765dc85fc418d8ed47e48ef4e164a0094
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2013-03-13 13:01:28 +0200

    Fix resource order in firefox-class.

--- parts/rules/ltsp-build-client/puppet/ltsp/firefox/manifests/init.pp
+++ parts/rules/ltsp-build-client/puppet/ltsp/firefox/manifests/init.pp
@@ -4,6 +4,10 @@ class firefox {
   # Firefox configuration system is still a mess... if there really is a more
   # straightforward way, I would like to hear about it.
   file {
+    '/etc/firefox/puavodesktop.js':
+      content => template('firefox/puavodesktop.js'),
+      require => Package['firefox'];
+
     '/etc/firefox/syspref.js':
       content => template('firefox/syspref.js'),
       require => File['/usr/lib/firefox/firefox-puavodesktop.js'];
@@ -11,10 +15,6 @@ class firefox {
     '/usr/lib/firefox/firefox-puavodesktop.js':
       content => template('firefox/firefox-puavodesktop.js'),
       require => File['/etc/firefox/puavodesktop.js'];
-
-    '/etc/firefox/puavodesktop.js':
-      content => template('firefox/puavodesktop.js'),
-      require => Package['firefox'];
   }
 
   Package <| title == firefox |>
