a24ead29bb2522201e1cf03a76525b626d8db266
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2013-03-15 16:11:16 +0200

    Install puavo-vpn-client, and document the puavo_openvpn module.

--- parts/rules/ltsp-build-client/puppet/ltsp/puavo_openvpn/manifests/init.pp
+++ parts/rules/ltsp-build-client/puppet/ltsp/puavo_openvpn/manifests/init.pp
@@ -5,8 +5,10 @@ class puavo_openvpn {
   file {
     '/usr/share/puavo-ltsp/init-puavo.d/90-puavo-openvpn':
       content => template('puavo_openvpn/90-puavo-openvpn'),
-      require => Package['puavo-ltsp-client'];
+      require => [ Package['puavo-ltsp-client']
+                 , Package['puavo-vpn-client'] ];
   }
 
-  Package <| title == puavo-ltsp-client |>
+  Package <| title == puavo-ltsp-client
+          or title == puavo-vpn-client  |>
 }
