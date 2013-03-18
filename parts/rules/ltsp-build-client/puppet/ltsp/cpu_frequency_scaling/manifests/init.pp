7b0c14af0801e79c4a6ac9f66053133042663a16
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2013-03-18 12:56:18 +0200

    "/usr/sbin/update-rc.d -f ondemand disable S 2 3 4 5" is probably the right way to disable a service (so that package updates will not overwrite the change).

--- parts/rules/ltsp-build-client/puppet/ltsp/cpu_frequency_scaling/manifests/init.pp
+++ parts/rules/ltsp-build-client/puppet/ltsp/cpu_frequency_scaling/manifests/init.pp
@@ -4,7 +4,7 @@ class cpu_frequency_scaling {
   # on some machines, because this is rather drastic.
 
   exec {
-    '/usr/sbin/update-rc.d -f ondemand remove':
+    '/usr/sbin/update-rc.d -f ondemand disable S 2 3 4 5':
       onlyif => '/usr/bin/test -e /etc/rc2.d/S99ondemand';
   }
 }
