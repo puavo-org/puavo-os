9752de5ac7e7d87bb8db35827388877275a395e8
Jouni Korhonen <jouni.korhonen@opinsys.fi>
2013-04-03 13:03:11 +0300

    Add Template class

---
+++ parts/client/lib/puavo/template.rb
@@ -0,0 +1,28 @@
+module Puavo
+  class Template
+
+    def initialize(template_dir)
+      @template_dir = template_dir
+    end
+
+    def write(filename, version=nil, secure=false)
+      template_file = filename
+
+      if version
+        template_file = "#{filename}-#{version}"
+      end
+
+      conf_template = File.read( File.join(@template_dir, "templates", template_file)
+      conf = ERB.new(conf_template, 0, "%<>")
+
+      perm = secure ? 0600 : 0644
+
+      File.open(filename, "w", perm) do |f|
+        f.write conf.result
+      end
+
+      File.chmod(perm, filename)
+    end
+
+  end
+end
