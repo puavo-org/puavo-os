5f393b0933ba799cf50774915bce106d18e573a6
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2013-04-08 14:47:33 +0300

    Add locale-specific dconf-files.  It is actually wrong we have to do this, but it seems that the only other alternative is patching Nautilus.

--- parts/rules/ltsp-build-client/puppet/ltsp/desktop/manifests/dconf/puavodesktop.pp
+++ parts/rules/ltsp-build-client/puppet/ltsp/desktop/manifests/dconf/puavodesktop.pp
@@ -1,13 +1,24 @@
 class desktop::dconf::puavodesktop {
   include desktop::dconf
 
+  define locale {
+    $lang = $title
+
+    file {
+      "/etc/dconf/profile/user-${lang}":
+        content => template("desktop/dconf_profile_user-${lang}");
+    }
+  }
+
+  locale { [ 'en', 'fi', 'sv', ]: ; }
+
   file {
     [ '/etc/dconf/db/puavodesktop.d'
     , '/etc/dconf/db/puavodesktop.d/locks' ]:
       ensure => directory;
 
     '/etc/dconf/profile/user':
-      content => template('desktop/dconf_profile_user');
+      content => template('desktop/dconf_profile_user-en');
 
     '/etc/environment':
       content => template('desktop/environment');
