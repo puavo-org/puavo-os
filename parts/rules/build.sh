441e2c69a9f305b27d196ec36f4acd99675adcba
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2013-04-08 11:59:47 +0300

    Run ltsp-related commands with "setarch i386", so that we can build more equivalent ltsp images with 64-bit hosts.

--- parts/rules/build.sh
+++ parts/rules/build.sh
@@ -2,13 +2,16 @@
 
 set -eu
 
+arch=i386
+
 cleanup() {
   test -n "$srccopydir" && rm -rf $srccopydir
 }
 
-run_sudo() {
+sudo_for_ltsp() {
   sudo env LTSP_CUSTOM_PLUGINS="$srccopydir/ltsp-build-client/plugins" \
            PATH="$srccopydir/ltsp-build-client:$PATH" \
+           setarch $arch \
            "$@"
 }
 
@@ -21,13 +24,11 @@ trap cleanup EXIT
 
 # we use the Ubuntu quantal-distribution as a default
 distribution=${DISTRIBUTION:-quantal}
+mirror=http://localhost:3142/fi.archive.ubuntu.com/ubuntu
 
 # fasttmp should be mounted on a tmpfs partition
 buildtmp=/virtualtmp
 basedir=$buildtmp/$USER
-
-arch=i386
-mirror=http://localhost:3142/fi.archive.ubuntu.com/ubuntu
 
 srcdir=$(dirname $0)
 srccopydir=$(mktemp -d /tmp/ltsp-builder-$USER.XXXXXXXXXX)
@@ -44,14 +45,14 @@ cp -pLR $srcdir $srccopydir
 puppet_module_dirs=$srccopydir/ltsp-build-client/puppet/opinsys:$srccopydir/ltsp-build-client/puppet/ltsp
 
 for mntpoint in dev/pts dev proc sys; do
-  run_sudo umount -f $basedir/$arch/$mntpoint 2>/dev/null \
+  sudo umount -f $basedir/$arch/$mntpoint 2>/dev/null \
     || true
 done
 
 {
   case "$mode" in
     build)
-      run_sudo $srccopydir/ltsp-build-client/ltsp-build-client \
+      sudo_for_ltsp $srccopydir/ltsp-build-client/ltsp-build-client \
           --arch               $arch \
           --base               $basedir \
           --config             $configfile \
@@ -65,30 +66,30 @@ done
           --skipimage
       ;;
     chroot)
-      run_sudo ltsp-chroot --base $basedir --mount-all
+      sudo_for_ltsp ltsp-chroot --base $basedir --mount-all
       ;;
     image)
       ltspimage_name=$build_version-$arch.img
-      run_sudo sh -c "
+      sudo sh -c "
         mkdir -p $basedir/$arch/etc/ltsp; \
         echo $ltspimage_name > $basedir/$arch/etc/ltsp/this_ltspimage_name
       "
 
       # XXX ltsp-build-client --onlyimage ?
-      run_sudo mksquashfs \
-                 $basedir/$arch /opt/ltsp/images/$ltspimage_name \
-                 -noappend -no-recovery -wildcards \
-                 -ef $srccopydir/ltsp-build-client/ltsp-update-image.excludes
+      sudo mksquashfs \
+          $basedir/$arch /opt/ltsp/images/$ltspimage_name \
+          -noappend -no-recovery -wildcards \
+          -ef $srccopydir/ltsp-build-client/ltsp-update-image.excludes
       ;;
     update-chroot)
-      run_sudo ltsp-apply-puppet \
+      sudo_for_ltsp ltsp-apply-puppet \
           --config             $configfile \
           --ltsp-chroot-opts   "--mount-all" \
           --puppet-module-dirs $puppet_module_dirs \
           --targetroot         "$basedir/$arch"
       ;;
     update-local)
-      run_sudo ltsp-apply-puppet \
+      sudo_for_ltsp ltsp-apply-puppet \
           --config             $configfile \
           --puppet-module-dirs $puppet_module_dirs
       ;;
