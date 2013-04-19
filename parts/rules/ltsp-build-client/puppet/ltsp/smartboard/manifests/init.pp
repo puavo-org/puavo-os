d049a56ce4a523de20e241911b7c5e791af395fe
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2013-04-19 12:14:16 +0300

    Add smartboard configurations so we can get things like gallery working.  But this is only the configuration as those have been installed by the smartboard packages, so this commit does not change anything.

--- parts/rules/ltsp-build-client/puppet/ltsp/smartboard/manifests/init.pp
+++ parts/rules/ltsp-build-client/puppet/ltsp/smartboard/manifests/init.pp
@@ -1,23 +1,7 @@
 class smartboard {
   require packages
-
-  file {
-    '/etc/xdg/autostart/smart_1-_Service.desktop':
-      content => template('smartboard/smart_1-_Service.desktop'),
-      mode    => 755;
-
-    '/etc/xdg/autostart/smart_1-_Tools.desktop':
-      content => template('smartboard/smart_1-_Tools.desktop'),
-      mode    => 755;
-
-    '/usr/local/bin/start_smartboard_service':
-      content => template('smartboard/start_smartboard_service'),
-      mode    => 755;
-
-    '/usr/local/bin/start_smartboard_tools':
-      content => template('smartboard/start_smartboard_tools'),
-      mode    => 755;
-  }
+  include smartboard::config,
+          smartboard::startup
 
   Package <| tag == whiteboard-smartboard |>
 }
