733ed98d729c75342cd06d52d66cfa80e34fbc11
Tuomas Räsänen <tuomasjjrasanen@tjjr.fi>
2013-04-22 10:46:21 +0300

    Enable zram again
    We are now using much newer kernel, zram might be more stable
    now. Fingers crossed.

--- parts/rules/ltsp-build-client/puppet/ltsp/packages/manifests/purged.pp
+++ parts/rules/ltsp-build-client/puppet/ltsp/packages/manifests/purged.pp
@@ -17,9 +17,8 @@ class packages::purged {
                                 # we do not actually use tftpd on ltsp-server
                                 # (we use a separate boot server)
 
-    , 'tracker'                 # this uses too much resources when using nfs
-    , 'zram-config' ]:          # zram triggers bugs in 3.6.6 and probably some
-                                # other kernels as well
+    , 'tracker' ]:              # this uses too much resources when using nfs
+
       tag => [ 'ubuntu', ];
   }
 }
