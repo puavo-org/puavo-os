21c6814ca6fe630dc6d9dbfbc0e775b79f41c43c
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2013-04-26 13:55:35 +0300

    Fix smartboard gallery cache wrapper.  Appears to work okay.

--- parts/rules/ltsp-build-client/puppet/ltsp/smartboard/manifests/notebook_cache_wrapper.pp
+++ parts/rules/ltsp-build-client/puppet/ltsp/smartboard/manifests/notebook_cache_wrapper.pp
@@ -3,15 +3,15 @@ class smartboard::notebook_cache_wrapper {
   require packages
 
   dpkg::divert {
-    '/opt/SMART Technologies/Notebook Software/bin/Notebook/notebook':
-      dest => '/opt/SMART Technologies/Notebook Software/bin/Notebook/notebook.real';
+    '/opt/SMART Technologies/Notebook Software/bin/Notebook/.notebook_elf':
+      dest => '/opt/SMART Technologies/Notebook Software/bin/Notebook/.notebook_elf.real';
   }
 
   file {
-    '/opt/SMART Technologies/Notebook Software/bin/Notebook/notebook':
+    '/opt/SMART Technologies/Notebook Software/bin/Notebook/.notebook_elf':
       content => template('smartboard/notebook-gallerycache-wrapper'),
       mode    => 755,
-      require => Dpkg::Divert['/opt/SMART Technologies/Notebook Software/bin/Notebook/notebook'];
+      require => Dpkg::Divert['/opt/SMART Technologies/Notebook Software/bin/Notebook/.notebook_elf'];
   }
 
   Package <| title == smart-notebook |>
