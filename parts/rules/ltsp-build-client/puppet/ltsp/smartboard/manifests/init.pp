3b38ce526e9f5a88418af40dad8ee8245b0f9104
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2013-04-26 13:02:47 +0300

    Add a cache wrapper for smartboard notebook so that galleries load up more quickly.

--- parts/rules/ltsp-build-client/puppet/ltsp/smartboard/manifests/init.pp
+++ parts/rules/ltsp-build-client/puppet/ltsp/smartboard/manifests/init.pp
@@ -1,6 +1,7 @@
 class smartboard {
   require packages
   include smartboard::config,
+          smartboard::notebook_cache_wrapper,
           smartboard::startup
 
   Package <| tag == whiteboard-smartboard |>
