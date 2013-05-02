052961cd25ed2cedf237a560ba08ec45a174c052
Tuomas Räsänen <tuomasjjrasanen@tjjr.fi>
2013-05-02 17:28:57 +0300

    Add -edge -prefixed kernel links

--- parts/rules/ltsp-build-client/puppet/ltsp/kernels/manifests/init.pp
+++ parts/rules/ltsp-build-client/puppet/ltsp/kernels/manifests/init.pp
@@ -12,16 +12,33 @@ class kernels {
     }
   }
 
+  define edge_kernel_link {
+    $filename = $title
+
+    file {
+      "/boot/$filename":
+       ensure => link,
+       target => "${filename}-${kernels::edge_kernel}";
+    }
+  }
+
   case $lsbdistcodename {
     'quantal': {
       $default_kernel = '3.8.8.opinsys1'
+      $edge_kernel = '3.8.11.opinsys1'
 
       default_kernel_link {
         [ 'initrd.img', 'nbi.img', 'vmlinuz', ]:
           require => Packages::Kernel_package_for_version[$default_kernel];
       }
 
+      edge_kernel_link {
+        [ 'initrd.img-edge', 'nbi.img-edge', 'vmlinuz-edge', ]:
+          require => Packages::Kernel_package_for_version[$edge_kernel];
+      }
+
       Packages::Kernel_package_for_version <| title == $default_kernel |>
+      Packages::Kernel_package_for_version <| title == $edge_kernel |>
     }
   }
 }
