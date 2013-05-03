5fcce0a640a4bbd83db4b54e88e4c8e89f45765e
Esa-Matti Suuronen <esa-matti@suuronen.org>
2013-05-03 11:07:06 +0000

    copy_external_files: Move to Puavo::Files namespace

--- parts/client/lib/puavo/copy_external_files.rb
+++ parts/client/lib/puavo/copy_external_files.rb
@@ -1,50 +1,53 @@
 require 'digest/sha1'
+require 'fileutils'
 require 'puavo'
 
-def files_with_hashes(dirpath)
-  file_hashes = {}
+module Puavo
+  module Files
 
-  Dir.entries(dirpath).each do |name|
-    begin
-      File.open(File.join(dirpath, name), "rb") do |f|
+    def self.read_current_files(dirpath)
+      file_hashes = {}
 
-        sha1 = Digest::SHA1.new
+      Dir.entries(dirpath).each do |name|
+        begin
+          File.open(File.join(dirpath, name), "rb") do |f|
 
-        while data = f.read(512)
-          sha1.update(data)
+            sha1 = Digest::SHA1.new
+
+            while data = f.read(512)
+              sha1.update(data)
+            end
+
+            file_hashes[name] = {
+              :data_hash => sha1.hexdigest
+            }
+          end
+        rescue Errno::EISDIR
+        end
+      end
+
+      return file_hashes
+    end
+
+    def self.copy_external_files(external_files, target)
+      FileUtils.mkdir_p(target)
+
+      current_files = read_current_files(target)
+
+      external_files.all.each do |ef|
+        if current_files[ef.name] &&
+          current_files[ef.name][:data_hash] == ef.data_hash
+          next
         end
 
-        file_hashes[name] = {
-          :data_hash => sha1.hexdigest
-        }
+        file_path = File.join(target, ef.name)
+
+        File.open(file_path, "w") do |f|
+          f.write(ef.get_data)
+        end
+
       end
-    rescue Errno::EISDIR
+
     end
   end
-
-  return file_hashes
-end
-
-def copy_external_files(external_files, target)
-  current_files = files_with_hashes(target)
-
-  external_files.all.each do |ef|
-    if current_files[ef.name] &&
-      current_files[ef.name][:data_hash] == ef.data_hash
-      next
-    end
-
-    file_path = File.join(target, ef.name)
-
-    File.open(file_path, "w") do |f|
-      f.write(ef.get_data)
-    end
-
-  end
-end
-
-
-if __FILE__ == $0
-  c = Puavo::Client::Base.new("localhost:3000", "albus", "albus", false)
-  copy_external_files(c.external_files, "/home/vagrant/external_files")
 end
