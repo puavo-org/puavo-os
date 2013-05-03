50a1f2cad55aaf36711e5a9a3f31505cc4aaee6c
Esa-Matti Suuronen <esa-matti@suuronen.org>
2013-05-03 08:05:39 +0000

    Move external files data fetch to model level

--- parts/client/lib/puavo/client/api/external_files.rb
+++ parts/client/lib/puavo/client/api/external_files.rb
@@ -3,12 +3,6 @@ module Puavo
     module API
       class ExternalFiles < Base
         model_name "ExternalFile"
-
-        def file_data(name)
-          file_path = "#{ url_prefix }/#{ self.model_name.model_path }/#{ name }"
-          rest file_path
-        end
-
       end
     end
   end
--- parts/client/lib/puavo/client/external_file.rb
+++ parts/client/lib/puavo/client/external_file.rb
@@ -2,8 +2,13 @@ module Puavo
   module Client
     class ExternalFile < Model
       extend Puavo::Client::HashMixin::ExternalFile
-
       model_path :prefix => '/api/v2', :path => "/external_files"
+
+      def get_data
+        file_path = "#{ @api.url_prefix }/#{ @api.model_name.model_path }/#{ name }"
+        @api.rest(file_path)
+      end
+
     end
   end
 end
