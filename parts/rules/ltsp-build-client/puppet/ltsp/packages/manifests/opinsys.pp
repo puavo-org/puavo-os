25c905839ac4ef8f9bbc395a0d8896f5f4a565ca
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2013-05-15 13:33:41 +0300

    Use a bit more repositories, for now.

--- parts/rules/ltsp-build-client/puppet/ltsp/packages/manifests/opinsys.pp
+++ parts/rules/ltsp-build-client/puppet/ltsp/packages/manifests/opinsys.pp
@@ -1,9 +1,11 @@
 class packages::opinsys {
-  # XXX this class perhaps belongs elsewhere?
+  # XXX this class perhaps belongs elsewhere?  really!
 
   include packages
 
   Apt::Key        <| title == "opinsys-repo.gpgkey" |>
-  Apt::Repository <| title == liitu
-                  or title == taulu |>
+  Apt::Repository <| title == repo
+                  or title == archive
+                  or title == private-archive
+                  or title == termiikki |>
 }
