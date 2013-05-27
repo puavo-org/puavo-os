96ef002eca8c8a79d649bb1b1dd62467807e72c1
Jouni Korhonen <jouni.korhonen@opinsys.fi>
2013-05-27 09:15:02 +0300

    Puavo::Client: fix typo

--- parts/client/lib/puavo/client/server.rb
+++ parts/client/lib/puavo/client/server.rb
@@ -1,7 +1,7 @@
 module Puavo
   module Client
     class Server < Model
-      inlucde Puavo::Client::HashMixin::Server
+      include Puavo::Client::HashMixin::Server
 
       model_path :prefix => '/devices/api/v2', :path => "/servers"
     end
