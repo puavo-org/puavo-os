82fe57ffd5dd06a6baaacf47593c64b2c200fc76
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2013-06-05 15:14:23 +0300

    Move smartboard configurations to use the "external files" mechanism from Puavo.

--- parts/rules/ltsp-build-client/puppet/ltsp/smartboard/manifests/config.pp
+++ parts/rules/ltsp-build-client/puppet/ltsp/smartboard/manifests/config.pp
@@ -1,4 +1,5 @@
 class smartboard::config {
+  include puavo_external_files
   require packages
 
   file {
@@ -9,6 +10,11 @@ class smartboard::config {
     '/etc/xdg/SMART Technologies/SMART Notebook.conf':
       mode   => 644,
       source => 'puppet:///modules/smartboard/SMART_Notebook.conf';
+  }
+
+  puavo_external_files::external_file {
+    '/opt/SMART Technologies/common/data/.mp.1.1.dat':
+      external_file_name => 'smartlicense';
   }
 
   Package <| tag == whiteboard-smartboard |>
