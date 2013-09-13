b3af3b73f6212c8365f11a9a8fa89d2956102579
Esa-Matti Suuronen <esa-matti@suuronen.org>
2013-09-13 15:48:15 +0300

    Firefox: Negotiate auth on api.opinsys.fi
    Kerberos!

--- parts/rules/ltsp-build-client/puppet/ltsp/firefox/templates/puavodesktop.js
+++ parts/rules/ltsp-build-client/puppet/ltsp/firefox/templates/puavodesktop.js
@@ -5,6 +5,6 @@ lockPref("browser.cache.disk.enable", false);
 lockPref("browser.safebrowsing.enabled", false);
 lockPref("browser.safebrowsing.malware.enabled", false);
 lockPref("network.dns.disableIPv6", true);
-lockPref("network.negotiate-auth.trusted-uris", "weblogin.opinsys.fi");
+lockPref("network.negotiate-auth.trusted-uris", "api.opinsys.fi");
 lockPref("print.postscript.paper_size", "a4");
 lockPref("toolkit.storage.synchronous", 1);
