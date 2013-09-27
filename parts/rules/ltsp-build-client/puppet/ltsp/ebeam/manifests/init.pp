fdc5456aef40ff4e0232ead5ef6bb2ffc57d8c29
Tuomas Räsänen <tuomasjjrasanen@tjjr.fi>
2013-09-27 16:07:17 +0300

    ebeam: add desktop file for the menu

--- parts/rules/ltsp-build-client/puppet/ltsp/ebeam/manifests/init.pp
+++ parts/rules/ltsp-build-client/puppet/ltsp/ebeam/manifests/init.pp
@@ -2,5 +2,10 @@ class ebeam {
   require packages
   include ebeam::startup
 
+  file {
+    '/usr/share/applications/ebeam.desktop':
+      content => template('ebeam/ebeam.desktop');
+  }
+
   Package <| tag == whiteboard-ebeam |>
 }
