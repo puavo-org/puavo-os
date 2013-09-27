011f1aa5d7cc4f4d9cc994e6d301e159e8c38e3d
Tuomas Räsänen <tuomasjjrasanen@tjjr.fi>
2013-09-27 13:11:18 +0300

    Copy defaults.list also to /etc/gnome

--- parts/rules/ltsp-build-client/puppet/ltsp/desktop/manifests/mimedefaults.pp
+++ parts/rules/ltsp-build-client/puppet/ltsp/desktop/manifests/mimedefaults.pp
@@ -6,5 +6,10 @@ class desktop::mimedefaults {
        content => template('desktop/defaults.list');
   }
 
+  file {
+    '/etc/gnome/defaults.list':
+       content => template('desktop/defaults.list');
+  }
+
   Package <| title == desktop-file-utils |>
 }
