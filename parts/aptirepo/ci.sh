c3c8640a92e863fb11b2391bf56ecd40d80dc08d
Esa-Matti Suuronen <esa-matti@suuronen.org>
2013-11-01 14:28:13 +0200

    Dogfood aptirepo-upload

--- parts/aptirepo/ci.sh
+++ parts/aptirepo/ci.sh
@@ -4,12 +4,11 @@ set -eu
 set -x
 
 sudo apt-get update
-sudo apt-get install -y puavo-devscripts
-
-aptirepo_url="$1"
+sudo apt-get install -y --force-yes puavo-devscripts aptirepo-upload
 
 puavo-build-debian-dir
 sudo puavo-install-deps debian/control
 puavo-dch $(cat VERSION)
 puavo-debuild
-puavo-upload-packages "$aptirepo_url" ../aptirepo*.changes "git-$(echo "$GIT_BRANCH" | cut -d / -f 2)"
+
+aptirepo-upload -b "git-$(echo "$GIT_BRANCH" | cut -d / -f 2)" ../aptirepo*.changes
