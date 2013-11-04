cccb73ba5a98b54f540342606ef26ece24711ed0
Tuomas Räsänen <tuomasjjrasanen@tjjr.fi>
2013-11-04 12:11:45 +0200

    Make apt-ftparchive binary cache db relative to the rootdir

--- parts/aptirepo/aptirepo/lib/_core.py
+++ parts/aptirepo/aptirepo/lib/_core.py
@@ -84,23 +84,26 @@ class Aptirepo:
 
     def __write_contents(self, pool, codename, comp):
         path = self.__join("dists", codename, comp, "Contents.gz")
+        dbpath = self.__join("db")
         with gzip.open(path, 'wb') as f:
-            subprocess.check_call(["apt-ftparchive", "--db", "db",
+            subprocess.check_call(["apt-ftparchive", "--db", dbpath,
                                    "contents", self.__join(pool, comp)],
                                   stdout=f)
 
     def __write_packages(self, pool, codename, comp, arch):
         path = self.__join("dists", codename, comp, "binary-%s" % arch,
                            "Packages")
+        dbpath = self.__join("db")
         with open(path, "w") as f:
-            subprocess.check_call(["apt-ftparchive", "--db", "db",
+            subprocess.check_call(["apt-ftparchive", "--db", dbpath,
                                    "packages", self.__join(pool, comp)],
                                   stdout=f)
         _gz(path)
 
     def __write_release(self, codename, comps, archs):
+        dbpath = self.__join("db")
         with open(self.__join("dists", codename, "Release"), "w") as f:
-            subprocess.check_call(["apt-ftparchive", "--db", "db",
+            subprocess.check_call(["apt-ftparchive", "--db", dbpath,
                                    "-o", "APT::FTPArchive::Release::Codename=%s" % codename,
                                    "-o", "APT::FTPArchive::Release::Components=%s" % " ".join(comps),
                                    "-o", "APT::FTPArchive::Release::Architectures=%s" % " ".join(archs),
