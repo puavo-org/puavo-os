088b1e2537c62243fadf059e1d25b1fdde016555
Tuomas Räsänen <tuomasjjrasanen@tjjr.fi>
2014-01-20 16:35:59 +0200

    Prohibit google-earth-stable from adding Google's APT repository and key

---
+++ parts/rules/ltsp-build-client/puppet/ltsp/google-earth-stable/manifests/init.pp
@@ -0,0 +1,11 @@
+class google-earth-stable {
+  include packages
+
+  file {
+    '/etc/default/google-earth':
+      ensure => present,
+      before => Package['google-earth-stable'];
+  }
+
+  Package <| title == google-earth-stable |>
+}
