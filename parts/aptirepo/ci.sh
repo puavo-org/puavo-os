5e814a12ccf73f87828d0de0382c6be1f82f2809
Esa-Matti Suuronen <esa-matti@suuronen.org>
2014-02-28 08:07:04 +0000

    Dogfood aptirepo upload package

--- parts/aptirepo/ci.sh
+++ parts/aptirepo/ci.sh
@@ -3,12 +3,18 @@
 set -eu
 set -x
 
+# Load environment. Is missing on Trusty...
+. /etc/environment
+
 sudo apt-get update
-sudo apt-get install -y --force-yes puavo-devscripts aptirepo-upload
+sudo apt-get install -y --force-yes puavo-devscripts
 
 puavo-build-debian-dir
 sudo puavo-install-deps debian/control
 puavo-dch $(cat VERSION)
 puavo-debuild
+
+dpkg -i ../aptirepo-upload*.deb
+apt-get install -f -y --force-yew
 
 aptirepo-upload -r $APTIREPO_REMOTE -b "git-$(echo "$GIT_BRANCH" | cut -d / -f 2)" ../aptirepo*.changes
