8cf4176edc5082e110e19474a7dba4505a0912e2
Tuomas Räsänen <tuomasjjrasanen@tjjr.fi>
2014-04-11 11:18:18 +0300

    Cleaner fix for udev problems related devnode permissions

--- parts/rules/ltsp-build-client/puppet/ltsp/udev/manifests/udevtrigger_workaround.pp
+++ parts/rules/ltsp-build-client/puppet/ltsp/udev/manifests/udevtrigger_workaround.pp
@@ -1,12 +1,35 @@
 class udev::udevtrigger_workaround {
-  # XXX /etc/init/udevtrigger.conf does not always do its job properly,
-  # XXX but some device files (such as /dev/fuse, but there are probably
-  # XXX others) will not always have correct permissions after boot.
-  # XXX Put a bandaid to /etc/rc.local to try to fix the issues.
+  include dpkg,
+    packages
+
+  # XXX The default /etc/init/udevtrigger.conf does not always do its
+  # job properly, probably because some of the rule files have been
+  # changed or created after udevd has started and becuase inotify does
+  # not seem to work properly with overlayfs [1].
+  #
+  # Symptoms include:
+  #   - /dev/fuse has invalid permissions
+  #   - Mimio usb device node has invalid permissions
+  #
+  # Telling udevd to reload rules (/sbin/udevadm control --reload-rules)
+  # before triggering buffered kernel events seems to fix the issue.
+  #
+  # [1]: https://bugs.launchpad.net/ubuntu/+source/linux/+bug/882147
+
+  dpkg::divert {
+    '/etc/init/udevtrigger.conf':
+      dest => '/etc/init/udevtrigger.conf.dist';
+  }
+
+  File {
+    require => Package['udev']
+  }
 
   file {
-    '/etc/rc.local':
-      content => template('udev/rc.local'),
-      mode    => 755;
+    '/etc/init/udevtrigger.conf':
+      content => template('udev/udevtrigger.conf'),
+      require => Dpkg::Divert['/etc/init/udevtrigger.conf'];
   }
+
+  Package <| (title == udev) |>
 }
