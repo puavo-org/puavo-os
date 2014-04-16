afdcc57a74860af9f7267918631ea4256c15e767
Tuomas Räsänen <tuomasjjrasanen@tjjr.fi>
2014-04-16 15:18:35 +0300

    ktouch: fix dirpaths once more

--- parts/rules/ltsp-build-client/puppet/ltsp/ktouch/manifests/init.pp
+++ parts/rules/ltsp-build-client/puppet/ltsp/ktouch/manifests/init.pp
@@ -8,13 +8,13 @@ class ktouch {
   $ktouch_prefix = '/usr/share/kde4/apps/ktouch/Lecture'
 
   file {
-    "${ktouch_prefix}/finnish/fi.junior-remake.ktouch.xml":
+    "${ktouch_prefix}/Finnish/fi.junior-remake.ktouch.xml":
       content => template('ktouch/fi.junior-remake.ktouch.xml');
 
-    "${ktouch_prefix}/english/en.junior.hard.ktouch.xml":
+    "${ktouch_prefix}/English/en.junior.hard.ktouch.xml":
       content => template('ktouch/en.junior.hard.ktouch.xml');
 
-    "${ktouch_prefix}/english/en.junior.easy.ktouch.xml":
+    "${ktouch_prefix}/English/en.junior.easy.ktouch.xml":
       content => template('ktouch/en.junior.easy.ktouch.xml');
   }
 
