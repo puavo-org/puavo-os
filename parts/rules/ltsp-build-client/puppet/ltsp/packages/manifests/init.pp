5cfdf0ee8d38cc3a597360cf340f7ea528910d29
Tuomas Räsänen <tuomasjjrasanen@tjjr.fi>
2014-04-16 14:07:44 +0300

    Add texlive stuff

--- parts/rules/ltsp-build-client/puppet/ltsp/packages/manifests/init.pp
+++ parts/rules/ltsp-build-client/puppet/ltsp/packages/manifests/init.pp
@@ -458,6 +458,10 @@ class packages {
     [ 'google-earth-stable'
     , 'googleearth'
     , 'av4kav'
+    , 'texlive-fonts-extra'
+    , 'texlive-fonts-recommended'
+    , 'texlive-latex-extra'
+    , 'texlive-latex-recommended'
     , 'vstloggerpro' ]:
       tag => [ 'science', 'opinsys', ];
 
