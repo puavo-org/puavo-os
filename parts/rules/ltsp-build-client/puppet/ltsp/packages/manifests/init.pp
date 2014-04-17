114b98604029655cecd468394a235f9a9efbb0e3
Tuomas Räsänen <tuomasjjrasanen@tjjr.fi>
2014-04-17 09:01:25 +0300

    Update edge kernel to 3.14.1.opinsys1

--- parts/rules/ltsp-build-client/puppet/ltsp/packages/manifests/init.pp
+++ parts/rules/ltsp-build-client/puppet/ltsp/packages/manifests/init.pp
@@ -812,7 +812,7 @@ class packages {
       kernel_package_for_version {
         [ '3.10.28.opinsys1'
         , '3.12.10.opinsys4'
-        , '3.13.1.opinsys2' ]:
+        , '3.14.1.opinsys1' ]:
           package_tag => 'opinsys',
           with_extra  => false;
       }
