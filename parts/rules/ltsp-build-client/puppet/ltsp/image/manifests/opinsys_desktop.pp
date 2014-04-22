ff4fe0e868ec7a269dcadaa11fc3bfcaa9c1ce3e
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2014-04-22 13:03:17 +0300

    Remove a trick that was intended only for debugging.

--- parts/rules/ltsp-build-client/puppet/ltsp/image/manifests/opinsys_desktop.pp
+++ parts/rules/ltsp-build-client/puppet/ltsp/image/manifests/opinsys_desktop.pp
@@ -1,8 +1,4 @@
 class image::opinsys_desktop {
-  file { "/tmp/facts.yaml":
-    content => inline_template("<%= scope.to_hash.reject { |k,v| !( k.is_a?(String) && v.is_a?(String) ) }.to_yaml %>"),
-  }
-
   case $operatingsystem {
     'Ubuntu': {
       case $lsbdistcodename {
