2c5b666f1153ac251fd3d23f13f7530cc1c778ac
Veli-Matti Lintu <veli-matti.lintu@opinsys.fi>
2014-04-24 17:21:31 +0300

    Dynamically create buttons and set logo image from /etc/webkiosk.menu configuration file.

--- parts/webkiosk-language-selector/app/index.js
+++ parts/webkiosk-language-selector/app/index.js
@@ -1,4 +1,45 @@
 require('nw.gui').Window.get().resizeTo(screen.width, screen.height);
+fs = require('fs');
+
+var json = JSON.parse(fs.readFileSync('/etc/webkiosk.menu', 'utf8'));
+console.log(json);
+
+if (json.hasOwnProperty("background")) {
+  document.querySelector("body").setAttribute("background", json["background"]);
+}
+
+/* Add logo if it is defined in webkiosk.menu */
+
+if (json.hasOwnProperty("logo")) {
+  var logo_div = document.querySelector("#logo");
+
+  logo = document.createElement("img");
+  logo.appendChild(document.createTextNode(""));
+  logo.setAttribute("class", "logo");
+  logo.setAttribute("src", json["logo"]);
+
+  logo_div.appendChild(logo);
+}
+
+/*
+
+Add buttons for different languages
+
+*/
+
+var button_div = document.querySelector("#buttons");
+var buttons=json["buttons"];
+
+for (var key in buttons) {
+  if (buttons.hasOwnProperty(key)) {
+    button = document.createElement("button");
+    button.appendChild(document.createTextNode(buttons[key]));
+    button.setAttribute("value", key);
+    button.setAttribute("class", "button");
+
+    button_div.appendChild(button);
+  }
+}
 
 var buttons = document.querySelectorAll("button");
 
