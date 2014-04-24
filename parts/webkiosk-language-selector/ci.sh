4d73d8fc4b91e6116d1328875e3c7282bc130a1e
Veli-Matti Lintu <veli-matti.lintu@opinsys.fi>
2014-04-24 17:31:04 +0300

    Add debian packaging.

---
+++ parts/webkiosk-language-selector/ci.sh
@@ -0,0 +1,15 @@
+
+#!/bin/sh
+
+set -eu
+set -x
+
+sudo apt-get update
+sudo apt-get install -y --force-yes puavo-devscripts aptirepo-upload
+
+puavo-build-debian-dir
+sudo puavo-install-deps debian/control
+puavo-dch $(cat VERSION)
+puavo-debuild
+
+aptirepo-upload -r $APTIREPO_REMOTE -b "git-$(echo "$GIT_BRANCH" | cut -d / -f 2)" ../ltsp-lightdm*.changes
