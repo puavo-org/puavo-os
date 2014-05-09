87ac09318df778a3b8bd4d927e309c38b5c5cf50
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2014-05-09 10:06:57 +0300

    Make the pycharm puppet module conform to JPNF (Juha's Puppet Normalized Format).

--- parts/rules/ltsp-build-client/puppet/ltsp/pycharm/manifests/init.pp
+++ parts/rules/ltsp-build-client/puppet/ltsp/pycharm/manifests/init.pp
@@ -1,21 +1,24 @@
 class pycharm {
   include packages
 
-  file { "/opt/pycharm/opinsys-default-options":
-    source  => "puppet:///modules/pycharm/opinsys-default-options",
-    recurse => true,
-    require => Package["pycharm"];
+  file {
+    '/opt/pycharm/opinsys-default-options':
+      recurse => true,
+      require => Package['pycharm'],
+      source  => 'puppet:///modules/pycharm/opinsys-default-options';
   }
 
-  file { "/opt/pycharm/bin/pycharm-wrapper":
-    source  => "puppet:///modules/pycharm/pycharm-wrapper",
-    mode    => 0755,
-    require => File["/opt/pycharm/opinsys-default-options"];
+  file {
+    '/opt/pycharm/bin/pycharm-wrapper':
+      mode    => 0755,
+      require => File['/opt/pycharm/opinsys-default-options'],
+      source  => 'puppet:///modules/pycharm/pycharm-wrapper';
   }
 
-  file { "/usr/share/applications/pycharm.desktop":
-    source  => "puppet:///modules/pycharm/pycharm.desktop",
-    require => File["/opt/pycharm/bin/pycharm-wrapper"];
+  file {
+    '/usr/share/applications/pycharm.desktop':
+      require => File['/opt/pycharm/bin/pycharm-wrapper'],
+      source  => 'puppet:///modules/pycharm/pycharm.desktop';
   }
 
   Package <| title == pycharm |>
