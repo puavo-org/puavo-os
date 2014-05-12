c29d25ab783f639ecdff67127be8fc7dc4beb483
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2014-05-12 13:40:01 +0300

    Remove all mimio-related .desktop files in case the mimio license does not exist on the system.

--- parts/rules/ltsp-build-client/puppet/ltsp/mimio/manifests/license.pp
+++ parts/rules/ltsp-build-client/puppet/ltsp/mimio/manifests/license.pp
@@ -2,6 +2,12 @@ class mimio::license {
   require packages,
           puavo_external_files
 
+  file {
+    '/etc/puavo-external-files-actions.d/mimio':
+      content => template('mimio/puavo-external-files-actions.d/mimio'),
+      mode    => 755;
+  }
+
   puavo_external_files::external_file {
     '/var/opt/mimio/global/global.reg':
       external_file_name => 'mimio_license_global';
