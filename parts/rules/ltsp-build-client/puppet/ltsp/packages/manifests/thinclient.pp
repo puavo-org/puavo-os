7a85a66fe5695188e2b28b4b6c13ec957bbe821f
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2014-05-22 13:00:56 +0300

    Include "private-repo" now that "repo" means a public blobs repo.

--- parts/rules/ltsp-build-client/puppet/ltsp/packages/manifests/thinclient.pp
+++ parts/rules/ltsp-build-client/puppet/ltsp/packages/manifests/thinclient.pp
@@ -5,6 +5,7 @@ class packages::thinclient {
   Apt::Repository <| title == archive
                   or title == kernels
                   or title == private-archive
+                  or title == private-repo
                   or title == repo |>
 
   # apply only thinclient package definitions listed in packages
