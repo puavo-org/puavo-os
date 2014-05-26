b099817882769aa34999e7c1c9f0172f3a70a111
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2014-05-26 12:10:59 +0300

    Move setting libreoffice default formats to a separate puppet class. And do not do it!  It does not work right now, so this should be looked into.  And perhaps we do not even want to mess with this anymore?

--- parts/rules/ltsp-build-client/puppet/ltsp/libreoffice/manifests/init.pp
+++ parts/rules/ltsp-build-client/puppet/ltsp/libreoffice/manifests/init.pp
@@ -1,43 +1,4 @@
 class libreoffice {
-  require packages
   include libreoffice::config
-
-  # Set libreoffice default formats to MS Office formats (yack!).
-
-  dpkg::divert {
-    '/usr/lib/libreoffice/share/registry/calc.xcd':
-      before => File['/usr/lib/libreoffice/share/registry/calc.xcd'],
-      dest   => '/usr/lib/libreoffice/share/registry/calc.xcd.distrib';
-
-    '/usr/lib/libreoffice/share/registry/impress.xcd':
-      before => File['/usr/lib/libreoffice/share/registry/impress.xcd'],
-      dest   => '/usr/lib/libreoffice/share/registry/impress.xcd.distrib';
-
-    '/usr/lib/libreoffice/share/registry/print.xcd':
-      before => File['/usr/lib/libreoffice/share/registry/print.xcd'],
-      dest   => '/usr/lib/libreoffice/share/registry/print.xcd.distrib';
-
-    '/usr/lib/libreoffice/share/registry/writer.xcd':
-      before => File['/usr/lib/libreoffice/share/registry/writer.xcd'],
-      dest   => '/usr/lib/libreoffice/share/registry/writer.xcd.distrib';
-  }
-
-  file {
-    '/usr/lib/libreoffice/share/registry/calc.xcd':
-      source => 'puppet:///modules/libreoffice/calc.xcd';
-
-    '/usr/lib/libreoffice/share/registry/impress.xcd':
-      source => 'puppet:///modules/libreoffice/impress.xcd';
-
-    # Use Gnome print dialog
-    '/usr/lib/libreoffice/share/registry/print.xcd':
-      source => 'puppet:///modules/libreoffice/print.xcd';
-
-    '/usr/lib/libreoffice/share/registry/writer.xcd':
-      source => 'puppet:///modules/libreoffice/writer.xcd';
-  }
-
-  Package <| title == libreoffice-calc
-          or title == libreoffice-impress
-          or title == libreoffice-writer |>
+	  # libreoffice::default_formats
 }
