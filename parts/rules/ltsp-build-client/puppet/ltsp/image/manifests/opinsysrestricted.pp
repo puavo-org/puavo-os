fe588dde375c8fed16368fdbc18c10f28878fd8e
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2014-06-30 13:09:16 +0300

    Put modules that are common in "image::opinsys" and "image::opinsysrestricted" to "image::bundle::opinsys".

--- parts/rules/ltsp-build-client/puppet/ltsp/image/manifests/opinsysrestricted.pp
+++ parts/rules/ltsp-build-client/puppet/ltsp/image/manifests/opinsysrestricted.pp
@@ -4,37 +4,16 @@ class image::opinsysrestricted {
       case $lsbdistcodename {
         'trusty': {
           include citrix,
-		  crash_reporting,
-		  desktop,
-		  disable_accounts_service,
-		  disable_geoclue,
-		  disable_gnome_keyring_autostart,
 		  ebeam,
-		  firefox,
 		  google-earth-stable,
 		  google_talkplugin,
-		  graphics_drivers,
-		  image::bundle::basic,
-		  kaffeine,
-		  keyutils,
-		  libreoffice,
+		  image::bundle::opinsys,
 		  ltspimage_java,
-		  ltspimage_kdump,
-		  ltspimage_opinsys_desktop,
-		  ltspimage_plymouth_theme,
 		  mimio,
-		  network_manager,
-		  open-sankore,
 		  packages::opinsysrestricted,
 		  primus,
 		  promethean,
-		  puavo_openvpn,
-		  puavo_wlan,
-		  pycharm,
-		  smartboard,
-		  tuxpaint,
-		  wacom,
-		  xexit
+		  smartboard
         }
       }
     }
