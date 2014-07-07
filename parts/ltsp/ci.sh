4c5a1c4ea921e029bdc0e6120a9bd5e446641776
Tuomas Räsänen <tuomasjjrasanen@tjjr.fi>
2014-07-07 14:55:20 +0300

    ci.sh: cleaner aptirepo upload block

--- parts/ltsp/ci.sh
+++ parts/ltsp/ci.sh
@@ -16,5 +16,16 @@ puavo-debuild
 
 if [ -n "${APTIREPO_REMOTE:-}" ]; then
     sudo apt-get install -y --force-yes aptirepo-upload
-    aptirepo-upload -r $APTIREPO_REMOTE -b "git-$(echo "$GIT_BRANCH" | cut -d / -f 2)" ../puavo-ltsp*.changes
+
+    if [ -z "${GIT_BRANCH:-}" ]; then
+        GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+    fi
+
+    local_git_branch=$(echo "${GIT_BRANCH}" | cut -d / -f 2)
+    aptirepo_branch=git-"${local_git_branch}"
+
+    version=$(dpkg-parsechangelog --show-field Version)
+    build_arch=$(dpkg-architecture -qDEB_BUILD_ARCH)
+    changes_file="../puavo-ltsp_${version}_${build_arch}.changes"
+    aptirepo-upload -r "${APTIREPO_REMOTE}" -b "${aptirepo_branch}" "${changes_file}"
 fi
