8a8c6b38ac98143c88f97c25356f3e41c39bf2fd
Tuomas Räsänen <tuomasjjrasanen@tjjr.fi>
2014-07-07 15:23:10 +0300

    ci.sh: now that deb packages can be built via Makefile, ci.sh is dedicated to CI alone

--- parts/ltsp/ci.sh
+++ parts/ltsp/ci.sh
@@ -7,23 +7,16 @@ set -x
 . /etc/environment
 
 sudo apt-get update
-sudo apt-get install -y --force-yes puavo-devscripts
+sudo apt-get install -y --force-yes puavo-devscripts aptirepo-upload
 
 sudo puavo-install-deps debian.default/control
 make deb
 
-if [ -n "${APTIREPO_REMOTE:-}" ]; then
-    sudo apt-get install -y --force-yes aptirepo-upload
+git_branch=$(echo "${GIT_BRANCH}" | cut -d / -f 2)
+aptirepo_branch=git-"${git_branch}"
 
-    if [ -z "${GIT_BRANCH:-}" ]; then
-        GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-    fi
+version=$(dpkg-parsechangelog --show-field Version)
+build_arch=$(dpkg-architecture -qDEB_BUILD_ARCH)
+changes_file="../puavo-ltsp_${version}_${build_arch}.changes"
 
-    local_git_branch=$(echo "${GIT_BRANCH}" | cut -d / -f 2)
-    aptirepo_branch=git-"${local_git_branch}"
-
-    version=$(dpkg-parsechangelog --show-field Version)
-    build_arch=$(dpkg-architecture -qDEB_BUILD_ARCH)
-    changes_file="../puavo-ltsp_${version}_${build_arch}.changes"
-    aptirepo-upload -r "${APTIREPO_REMOTE}" -b "${aptirepo_branch}" "${changes_file}"
-fi
+aptirepo-upload -r "${APTIREPO_REMOTE}" -b "${aptirepo_branch}" "${changes_file}"
