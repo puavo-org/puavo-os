71d57c2d92a27d818588f0de78294682a91ac802
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2014-07-14 14:40:36 +0300

    Use the default Ubuntu /etc/sudoers, we do not need any specific configurations there.

--- parts/rules/ltsp-build-client/puppet/ltsp/sudo/manifests/init.pp
+++ parts/rules/ltsp-build-client/puppet/ltsp/sudo/manifests/init.pp
@@ -3,15 +3,9 @@ class sudo {
           packages
 
   file {
-    '/etc/sudoers':
-      content => template('sudo/sudoers'),
-      mode    => 440,
-      require => Package['sudo'];
-
     '/etc/sudoers.d/ltspadmins':
       content => template('sudo/sudoers.d/ltspadmins'),
-      mode    => 440,
-      require => File['/etc/sudoers'];
+      mode    => 440;
   }
 
   Package <| title == sudo |>
