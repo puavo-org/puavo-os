4d8000f32fe165fdaf44f13fe17a269f6b52070f
Tuomas Räsänen <tuomasjjrasanen@tjjr.fi>
2014-07-15 12:58:33 +0300

    Enable keyring daemon, but disbale keyring prompts
    Keyring daemon is required for nm-applet to work correctly. nm-applet
    uses keyring daemon to ask and save network passwords. If keyring daemon
    services are disabled, clicking a connection from the menu does not do
    anything. When keyring daemon is enabled, a prompt dialog is displayed
    for user to enter the password. By default, the keyring daemon also
    saves passwords, but we have disabled this feature in our network
    manager: see opinsys-debs for commit
    525a947c0d7cdabf05b8aebcf30bde635973a2af.
    
    However, when keyring daemons are enabled, a default keyring is opened
    if it exists or created if it does not. This causes several prompt
    dialogs to be shown to the user. We do not want this because it confuses
    our users. This commit prevents prompt dialogs from popping up by
    removing their dbus services files.

--- parts/rules/ltsp-build-client/puppet/ltsp/disable_gnome_keyring_autostart/manifests/init.pp
+++ parts/rules/ltsp-build-client/puppet/ltsp/disable_gnome_keyring_autostart/manifests/init.pp
@@ -8,8 +8,8 @@ class disable_gnome_keyring_autostart {
     , '/etc/xdg/autostart/gnome-keyring-pkcs11.desktop'
     , '/etc/xdg/autostart/gnome-keyring-secrets.desktop'
     , '/etc/xdg/autostart/gnome-keyring-ssh.desktop'
-    , '/usr/share/dbus-1/services/org.freedesktop.secrets.service'
-    , '/usr/share/dbus-1/services/org.gnome.keyring.service' ]:
+    , '/usr/share/dbus-1/services/org.gnome.keyring.PrivatePrompter.service'
+    , '/usr/share/dbus-1/services/org.gnome.keyring.SystemPrompter.service' ]:
       ;
   }
 
