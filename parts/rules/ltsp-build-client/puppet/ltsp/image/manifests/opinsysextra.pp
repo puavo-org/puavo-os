984b0f001ae2b3489c4719aedc67f35aa4d3f3e3
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2014-08-08 15:46:27 +0300

    Add chromium_with_chrome_flash module that sets up chromium to use chrome flash.  Only on opinsysextra image for now.

--- parts/rules/ltsp-build-client/puppet/ltsp/image/manifests/opinsysextra.pp
+++ parts/rules/ltsp-build-client/puppet/ltsp/image/manifests/opinsysextra.pp
@@ -3,7 +3,8 @@ class image::opinsysextra {
     'Ubuntu': {
       case $lsbdistcodename {
         'trusty': {
-          include google-earth-stable,
+          include chromium_with_chrome_flash,
+		  google-earth-stable,
 		  image::bundle::opinsys,
 		  ltspimage_java,
 		  packages::opinsysextra
