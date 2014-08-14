80ebd79892436d864d55b60b310e71b594d8eb9a
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2014-08-14 08:25:10 +0300

    The new smartboard package "smart-product-drivers" does not install on our image build process, unless it thinks we are Linkat or Suse. We choose to mask ourselves as Linkat (Ubuntu based distribution used in Spain, see http://linkat.xtec.cat/portal/index.php).

--- parts/rules/ltsp-build-client/puppet/ltsp/smartboard/manifests/init.pp
+++ parts/rules/ltsp-build-client/puppet/ltsp/smartboard/manifests/init.pp
@@ -1,8 +1,18 @@
 class smartboard {
-  require packages
-  include smartboard::config,
+  include packages,
+          smartboard::config,
           smartboard::notebook_cache_wrapper,
           smartboard::startup
+
+  # smart-product-drivers does not install on our image build process,
+  # unless it thinks we are Linkat or Suse.  We choose to mask ourselves as
+  # Linkat (Ubuntu based distribution used in Spain, see
+  # http://linkat.xtec.cat/portal/index.php).
+  file {
+    '/etc/Linkat':
+      before => Package['smart-product-drivers'],
+      ensure => present;
+  }
 
   Package <| tag == whiteboard-smartboard |>
 }
