a90f6978e53278d018c631a2e8f14caf191812b3
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2014-09-02 09:55:50 +0300

    Revert "Update our kernel."
    This reverts commit ee0c9cdd1f57eec6633398a83d4bb8c2fbdbcad9.
    
    Do not update the kernel yet, we have some custom patches
    that we still need (I think).

--- parts/rules/ltsp-build-client/puppet/ltsp/kernels/manifests/init.pp
+++ parts/rules/ltsp-build-client/puppet/ltsp/kernels/manifests/init.pp
@@ -34,7 +34,7 @@ class kernels {
 
   case $lsbdistcodename {
     'trusty': {
-      $default_kernel = '3.13.0-35-generic'
+      $default_kernel = '3.13.0-29-generic'
 
       default_kernel_link {
         [ 'initrd.img', 'nbi.img', 'vmlinuz', ]:
