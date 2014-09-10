d4500b246a66ad45a4a67d83931c7fef536c2dab
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2014-09-10 13:01:04 +0300

    Rename "/etc/polkit-1/localauthority/50-local.d/10.org.freedesktop.networkmanager.pkla" to "/etc/polkit-1/localauthority/50-local.d/10.org.freedesktop.networkmanager.allow_modify.pkla" to better reflect what it does.

--- parts/rules/ltsp-build-client/puppet/ltsp/network_manager/manifests/allow_modify.pp
+++ parts/rules/ltsp-build-client/puppet/ltsp/network_manager/manifests/allow_modify.pp
@@ -2,8 +2,8 @@ class network_manager::allow_modify {
   require packages
 
   file {
-    '/etc/polkit-1/localauthority/50-local.d/10.org.freedesktop.networkmanager.pkla':
-      content => template('network_manager/10.org.freedesktop.networkmanager.pkla');
+    '/etc/polkit-1/localauthority/50-local.d/10.org.freedesktop.networkmanager.allow_modify.pkla':
+      content => template('network_manager/10.org.freedesktop.networkmanager.allow_modify.pkla');
   }
 
   Package <| title == policykit-1 |>
