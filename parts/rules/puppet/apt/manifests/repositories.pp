4957b76014a92a34c4159fdd26299aca90e36682
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2014-10-09 13:23:10 +0300

    apt-module, moved from the (private) opinsys-repository.

---
+++ parts/rules/puppet/apt/manifests/repositories.pp
@@ -0,0 +1,25 @@
+class apt::repositories {
+  include apt
+
+  $mirror = $lsbdistcodename ? {
+    'quantal'          => 'old-releases.ubuntu.com',
+    default            => 'fi.archive.ubuntu.com',
+  }
+
+  $securitymirror = $lsbdistcodename ? {
+    'quantal'          => 'old-releases.ubuntu.com',
+    default            => 'security.ubuntu.com',
+  }
+
+  file {
+    '/etc/apt/sources.list':
+      content => template('apt/sources.list'),
+      notify  => Exec['apt update'];
+  }
+
+  # define some apt keys and repositories for use
+  @apt::repository {
+    'partner':
+      aptline => "http://archive.canonical.com/ubuntu $lsbdistcodename partner";
+  }
+}
