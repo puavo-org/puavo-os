ef0ec4e9748344afd5b5beb8a1d526f43ff74ab9
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2014-10-14 10:44:07 +0300

    Use an absolute path to desktop-module in images::desktop as well.

--- parts/rules/rules/puppet/image/manifests/desktop.pp
+++ parts/rules/rules/puppet/image/manifests/desktop.pp
@@ -2,7 +2,7 @@ class image::desktop {
   case $operatingsystem {
     'Ubuntu': {
       include crash_reporting,
-	      desktop,
+	      ::desktop,
 	      disable_accounts_service,
 	      disable_geoclue,
 	      disable_gnome_keyring_autostart,
