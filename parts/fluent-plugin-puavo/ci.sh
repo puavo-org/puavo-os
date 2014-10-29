391747cf092f6ccdbc1c75441b0615bf5b464ade
Tuomas Räsänen <tuomasjjrasanen@tjjr.fi>
2014-10-29 15:27:23 +0200

    ci.sh: fix dependecy-deb building

--- parts/fluent-plugin-puavo/ci.sh
+++ parts/fluent-plugin-puavo/ci.sh
@@ -6,7 +6,7 @@ set -x
 sudo apt-get update
 sudo apt-get install -y --force-yes puavo-devscripts aptirepo-upload
 
-sudo puavo-install-deps debian/control
+sudo puavo-install-deps debian.default/control
 make deb
 
 aptirepo-upload -r $APTIREPO_REMOTE -b "git-$(echo "$GIT_BRANCH" | cut -d / -f 2)" ../fluent-plugin-puavo*.changes
