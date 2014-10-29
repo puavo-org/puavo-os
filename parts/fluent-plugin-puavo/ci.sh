e06393a79f1a853827fd33f12aef2007897d7a8d
Tuomas Räsänen <tuomasjjrasanen@tjjr.fi>
2014-10-29 14:06:01 +0200

    ci.sh: use make deb to build deb

--- parts/fluent-plugin-puavo/ci.sh
+++ parts/fluent-plugin-puavo/ci.sh
@@ -6,9 +6,7 @@ set -x
 sudo apt-get update
 sudo apt-get install -y --force-yes puavo-devscripts aptirepo-upload
 
-puavo-build-debian-dir
 sudo puavo-install-deps debian/control
-puavo-dch $(cat VERSION)
-puavo-debuild
+make deb
 
 aptirepo-upload -r $APTIREPO_REMOTE -b "git-$(echo "$GIT_BRANCH" | cut -d / -f 2)" ../fluent-plugin-puavo*.changes
