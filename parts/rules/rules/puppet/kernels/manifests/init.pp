6d63239f3a1519218040935108fad1d586c86507
Tuomas Räsänen <tuomasjjrasanen@tjjr.fi>
2014-10-29 11:32:42 +0200

    Add symbolic versions kernels: edge and stable

--- parts/rules/rules/puppet/kernels/manifests/init.pp
+++ parts/rules/rules/puppet/kernels/manifests/init.pp
@@ -33,9 +33,19 @@ class kernels {
   }
 
   $default_kernel = $lsbdistcodename ? {
-                      'precise' => '3.2.0-69-generic',
-                      'trusty'  => '3.13.0-36-generic',
-                    }
+    'precise' => '3.2.0-69-generic',
+    'trusty'  => '3.13.0-36-generic',
+  }
+
+  $edge_kernel = $lsbdistcodename ? {
+    'precise' => $default_kernel,
+    'trusty'  => $default_kernel,
+  }
+
+  $stable_kernel = $lsbdistcodename ? {
+    'precise' => $default_kernel,
+    'trusty'  => $default_kernel,
+  }
 
   case $lsbdistcodename {
     'precise', 'trusty': {
@@ -45,7 +55,19 @@ class kernels {
           require => Packages::Kernels::Kernel_package[$default_kernel];
       }
 
+      edge_kernel_link {
+        [ 'initrd.img', 'nbi.img', 'vmlinuz', ]:
+          require => Packages::Kernels::Kernel_package[$edge_kernel];
+      }
+
+      stable_kernel_link {
+        [ 'initrd.img', 'nbi.img', 'vmlinuz', ]:
+          require => Packages::Kernels::Kernel_package[$stable_kernel];
+      }
+
       Packages::Kernels::Kernel_package <| title == $default_kernel |>
+      Packages::Kernels::Kernel_package <| title == $edge_kernel    |>
+      Packages::Kernels::Kernel_package <| title == $stable_kernel  |>
     }
   }
 }
