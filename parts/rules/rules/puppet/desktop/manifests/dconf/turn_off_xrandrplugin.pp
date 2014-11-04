f7b2c0ef3f72683c3d222f109aa520f3dc142933
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2014-11-04 13:39:13 +0200

    Add a new (partial) dconf profile: "turn_off_xrandrplugin".

---
+++ parts/rules/rules/puppet/desktop/manifests/dconf/turn_off_xrandrplugin.pp
@@ -0,0 +1,12 @@
+class desktop::dconf::turn_off_xrandrplugin {
+  include desktop::dconf
+
+  file {
+    '/etc/dconf/db/turn_off_xrandrplugin.d':
+      ensure => directory;
+
+    '/etc/dconf/db/turn_off_xrandrplugin.d/turn_off_xrandrplugin':
+      content => template('desktop/dconf_turn_off_xrandrplugin_profile'),
+      notify  => Exec['update dconf'];
+  }
+}
