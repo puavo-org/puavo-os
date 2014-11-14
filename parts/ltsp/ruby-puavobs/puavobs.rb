498add91e2bb2fade54d9a7db5532ec36c1ce240
Tuomas Räsänen <tuomasjjrasanen@tjjr.fi>
2014-11-14 15:28:50 +0200

    ruby-puavobs: switch the standard net/https to The HTTP Gem
    The SSL peer certificate verification seems to fail randomly with the
    standard library tools.

--- parts/ltsp/ruby-puavobs/puavobs.rb
+++ parts/ltsp/ruby-puavobs/puavobs.rb
@@ -1,69 +1,64 @@
+require_relative 'puavobs-vendor/bundler/setup.rb'
+
 ## Standard libraries.
+require 'base64'
 require 'json'
-require 'net/https'
 require 'rexml/document'
 require 'securerandom'
 require 'tempfile'
+require 'uri'
 
 ## 3rd-party libraries.
 require 'highline/import'
+require 'http'
 
 module PuavoBS
 
-  def self.with_https(host, port, &block)
-    https              = Net::HTTP.new(host, port)
-    https.use_ssl      = true
-    https.verify_mode  = OpenSSL::SSL::VERIFY_PEER
-    https.verify_depth = 5
-    https.start(&block)
+  def self.basic_auth(username, password)
+    basic_auth_creds = Base64.strict_encode64("#{username}:#{password}")
+    "Basic #{basic_auth_creds}"
   end
 
-  def self.with_puavo_https(&block)
-    server = File.read('/etc/puavo/domain').strip()
-
-    self.with_https(server, 443, &block)
-  end
-
-  def self.with_api_https(&block)
+  def self.get_api_url(path)
     uri = IO.popen('puavo-resolve-api-server') do |io|
       output = io.read().strip()
       io.close()
       $?.success? ? URI(output) : nil
     end
+    raise "failed to resolve the address of the API server" if uri.nil?
+    uri.path = path
+    uri.to_s
+  end
 
-    self.with_https(uri.host, uri.port, &block)
+  def self.get_puavo_url(path)
+    server = File.read('/etc/puavo/domain').strip()
+    uri = URI("https://#{server}")
+    uri.path = path
+    uri.to_s
+  end
+
+  def self.check_response_code(code)
+    if !code.between?(200, 206) && code != 302 then
+      raise "request failed with status code #{response.code}"
+    end
   end
 
   def self.get_school(username, password, school_id)
-    self.with_puavo_https() do |https|
-      request = Net::HTTP::Get.new("/users/schools/#{school_id}.json")
-      request.basic_auth(username, password)
-      request['Accept'] = 'application/json'
-
-      response = https.request(request)
-      response_code = Integer(response.code)
-      if response_code == 302 then
-        ## Puavo seems to respond with 302 in some, yet successful
-        ## cases.
-        return JSON.parse(response.body())
-      end
-      response.value()
-
-      JSON.parse(response.body())
-    end
+    url = self.get_puavo_url("/users/schools/#{school_id}.json")
+    response = HTTP
+      .auth(self.basic_auth(username, password))
+      .accept(:json)
+      .get(url)
+    self.check_response_code(response.code)
+    JSON.parse(response.body)
   end
 
   def self.get_school_and_device_ids(hostname)
-    device_json = self.with_api_https() do |https|
-      request = Net::HTTP::Get.new("/v3/devices/#{hostname}")
-      request['Accept'] = 'application/json'
-
-      response = https.request(request)
-      response.value()
-
-      JSON.parse(response.body())
-    end
-
+    response = HTTP
+      .accept(:json)
+      .get(self.get_api_url("/v3/devices/#{hostname}"))
+    self.check_response_code(response.code)
+    device_json = JSON.parse(response.body)
     school_id = Integer(/^puavoId=([0-9]+),/.match(device_json['school_dn'])[1])
     device_id = Integer(device_json['puavo_id'])
     [school_id, device_id]
@@ -71,19 +66,15 @@ module PuavoBS
 
   def self.get_school_ids(username, password)
     puavo_id = Integer(File.read('/etc/puavo/id').strip())
-
-    schools = self.with_puavo_https() do |https|
-      request = Net::HTTP::Get.new("/devices/servers/#{puavo_id}.xml")
-      request.basic_auth(username, password)
-      request['Accept'] = 'application/xml'
-
-      response = https.request(request)
-      response.value()
-
-      doc = REXML::Document.new(response.body())
-      doc.elements.collect('/server/puavoSchools/puavoSchool') do |element|
-        Integer(/^puavoId=([0-9]+),/.match(element.text())[1])
-      end
+    url = self.get_puavo_url("/devices/servers/#{puavo_id}.xml")
+    response = HTTP
+      .auth(self.basic_auth(username, password))
+      .accept(:xml)
+      .get(url)
+    self.check_response_code(response.code)
+    doc = REXML::Document.new(response.body)
+    doc.elements.collect('/server/puavoSchools/puavoSchool') do |element|
+      Integer(/^puavoId=([0-9]+),/.match(element.text())[1])
     end
   end
 
@@ -113,52 +104,41 @@ module PuavoBS
 
   def self.register_device(username, password, school_id,
                               hostname, mac, hosttype, tags)
-    register_json = JSON.generate("puavoHostname"   => hostname,
-                                  "macAddress"      => mac,
-                                  "puavoTag"        => tags,
-                                  "puavoDeviceType" => hosttype,
-                                  "classes"         => ["puavoNetbootDevice"])
-
-    self.with_puavo_https() do |https|
-      request = Net::HTTP::Post.new("/devices/#{school_id}/devices.json")
-      request.basic_auth(username, password)
-      request['Content-Type'] = 'application/json'
-      request['Accept'] = 'application/json'
-      request.body = register_json
-
-      response = https.request(request)
-      response.value()
-      Integer(response.code)
-    end
+    url = self.get_puavo_url("/devices/#{school_id}/devices.json")
+    response = HTTP
+      .auth(self.basic_auth(username, password))
+      .accept(:json)
+      .post(url, :json => {
+              "puavoHostname"   => hostname,
+              "macAddress"      => mac,
+              "puavoTag"        => tags,
+              "puavoDeviceType" => hosttype,
+              "classes"         => ["puavoNetbootDevice"]
+            })
+    self.check_response_code(response.code)
+    response.code
   end
 
   def self.unregister_device(username, password, hostname)
     school_id, device_id = self.get_school_and_device_ids(hostname)
-
-    self.with_puavo_https() do |https|
-      path = "/devices/#{school_id}/devices/#{device_id}.xml"
-      request = Net::HTTP::Delete.new(path)
-      request.basic_auth(username, password)
-
-      response = https.request(request)
-      response.value()
-      Integer(response.code)
-    end
+    url = self.get_puavo_url("/devices/#{school_id}/devices/#{device_id}.xml")
+    response = HTTP
+      .auth(self.basic_auth(username, password))
+      .delete(url)
+    self.check_response_code(response.code)
+    response.code
   end
 
   def self.get_role_ids(username, password, school_id)
-    self.with_puavo_https() do |https|
-      request = Net::HTTP::Get.new("/users/#{school_id}/roles.xml")
-      request.basic_auth(username, password)
-      request['Accept'] = 'application/xml'
-
-      response = https.request(request)
-      response.value()
-
-      doc = REXML::Document.new(response.body())
-      doc.elements.collect('/roles/role/dn') do |element|
-        Integer(/^puavoId=([0-9]+),/.match(element.text())[1])
-      end
+    url = self.get_puavo_url("/users/#{school_id}/roles.xml")
+    response = HTTP
+      .auth(self.basic_auth(username, password))
+      .accept(:xml)
+      .get(url)
+    self.check_response_code(response.code)
+    doc = REXML::Document.new(response.body)
+    doc.elements.collect('/roles/role/dn') do |element|
+      Integer(/^puavoId=([0-9]+),/.match(element.text())[1])
     end
   end
 
@@ -172,57 +152,41 @@ module PuavoBS
     testuser_username = "test.user.#{SecureRandom.hex(10)}"
     testuser_password = SecureRandom.hex(32)
 
-    self.with_puavo_https() do |https|
-      request = Net::HTTP::Post.new("/users/#{school_id}/users")
-      request.basic_auth(username, password)
-      form_data = {
-        "user[givenName]"                 => "test",
-        "user[sn]"                        => "user",
-        "user[uid]"                       => testuser_username,
-        "user[puavoEduPersonAffiliation]" => "testuser",
-        "user[role_ids][]"                => testuser_role_id,
-        "user[new_password]"              => testuser_password,
-        "user[new_password_confirmation]" => testuser_password,
-      }
-      request.set_form_data(form_data)
-
-      response = https.request(request)
-      response_code = Integer(response.code)
-      if response_code == 302 then
-        ## Puavo seems to redirect on success.
-        break
-      end
-      response.value()
-    end
+    url = self.get_puavo_url("/users/#{school_id}/users")
+    response = HTTP
+      .auth(self.basic_auth(username, password))
+      .post(url, :form => {
+              "user[givenName]"                 => "test",
+              "user[sn]"                        => "user",
+              "user[uid]"                       => testuser_username,
+              "user[puavoEduPersonAffiliation]" => "testuser",
+              "user[role_ids][]"                => testuser_role_id,
+              "user[new_password]"              => testuser_password,
+              "user[new_password_confirmation]" => testuser_password,
+            })
+    self.check_response_code(response.code)
     [testuser_username, testuser_password]
   end
 
   def self.get_user_id(username, password, user_username)
-    user_json = self.with_api_https() do |https|
-      request = Net::HTTP::Get.new("/v3/users/#{user_username}")
-      request.basic_auth(username, password)
-      request['Accept'] = 'application/json'
-
-      response = https.request(request)
-      response.value()
-
-      JSON.parse(response.body())
-    end
+    response = HTTP
+      .auth(self.basic_auth(username, password))
+      .accept(:json)
+      .get(self.get_api_url("/v3/users/#{user_username}"))
+    self.check_response_code(response.code)
+    user_json = JSON.parse(response.body)
     Integer(/^puavoId=([0-9]+),/.match(user_json['dn'])[1])
   end
 
   def self.remove_user(username, password, school_id, user_username)
     user_id = self.get_user_id(username, password, user_username)
 
-    self.with_puavo_https() do |https|
-      path = "/users/#{school_id}/users/#{user_id}.xml"
-      request = Net::HTTP::Delete.new(path)
-      request.basic_auth(username, password)
-
-      response = https.request(request)
-      response.value()
-      Integer(response.code)
-    end
+    url = self.get_puavo_url("/users/#{school_id}/users/#{user_id}.xml")
+    response = HTTP
+      .auth(self.basic_auth(username, password))
+      .delete(url)
+    self.check_response_code(response.code)
+    response.code
   end
 
   def self.virsh_define_testclient(orig_hostname=nil)
