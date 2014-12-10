c8682f79fd47948a6c173f9434ae3de3d2db8592
Tuomas Räsänen <tuomasjjrasanen@tjjr.fi>
2014-12-10 10:24:41 +0200

    bootserver_krb5kdc: add monthly logrotate

--- parts/rules/rules/puppet/bootserver_krb5kdc/manifests/init.pp
+++ parts/rules/rules/puppet/bootserver_krb5kdc/manifests/init.pp
@@ -3,6 +3,9 @@ class bootserver_krb5kdc {
   file {
     '/etc/init/krb5-kdc.override':
       source => 'puppet:///modules/bootserver_krb5kdc/krb5-kdc.override';
+
+    '/etc/logrotate.d/kdc':
+      source => 'puppet:///modules/bootserver_krb5kdc/logrotate.conf';
   }
 
 }
