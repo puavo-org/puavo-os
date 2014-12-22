7089cb85674b7461e7c16cc4b22a62ed04f30466
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2014-12-22 16:10:08 +0200

    Move stuff from image::desktop to image::bundle::desktop.

--- parts/rules/rules/puppet/image/manifests/desktop.pp
+++ parts/rules/rules/puppet/image/manifests/desktop.pp
@@ -1,28 +1,8 @@
 class image::desktop {
   case $operatingsystem {
     'Ubuntu': {
-      include acroread,
-	      crash_reporting,
-	      ::desktop,
-	      disable_accounts_service,
-	      disable_geoclue,
-	      disable_gnome_keyring_autostart,
-	      firefox,
-	      gnome_terminal,
-	      graphics_drivers,
-	      image::bundle::basic,
-	      kaffeine,
-	      keyutils,
-	      laptop_mode_tools,
-	      libreoffice,
-	      network_manager,
-	      packages::puavo,
-	      puavo_wlan,
-	      pycharm,
-	      tuxpaint,
-	      wacom,
-	      workaround_firefox_local_swf_bug,
-	      xexit
+      include image::bundle::desktop,
+	      packages::puavo
     }
   }
 }
