1db2a681135bd345c2da0ee8406fc658ee417d1d
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2014-12-23 12:53:10 +0200

    I do not think x2go repository is needed on the thinclient images.

--- parts/rules/rules/puppet/image/manifests/thinclient.pp
+++ parts/rules/rules/puppet/image/manifests/thinclient.pp
@@ -5,8 +5,7 @@ class image::thinclient {
   Apt::Key        <| title == "opinsys-repo.gpgkey" |>
   Apt::Repository <| title == archive
                   or title == kernels
-                  or title == repo
-                  or title == x2go |>
+                  or title == repo |>
 
   # apply only thinclient package definitions listed in packages
   Package <| tag == thinclient |>
