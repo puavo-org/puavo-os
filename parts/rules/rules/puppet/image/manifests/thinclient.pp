dd76160a1b53dae349183d65a1dfa4f63015ff89
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2014-12-23 09:50:45 +0200

    Merge packages::puavo and packages::thinclient to their image::* classes, no need for separate classes.  Add package definitions for image::builder.

--- parts/rules/rules/puppet/image/manifests/thinclient.pp
+++ parts/rules/rules/puppet/image/manifests/thinclient.pp
@@ -1,4 +1,13 @@
 class image::thinclient {
   include image::bundle::basic,
-          packages::thinclient
+          packages
+
+  Apt::Key        <| title == "opinsys-repo.gpgkey" |>
+  Apt::Repository <| title == archive
+                  or title == kernels
+                  or title == repo
+                  or title == x2go |>
+
+  # apply only thinclient package definitions listed in packages
+  Package <| tag == thinclient |>
 }
