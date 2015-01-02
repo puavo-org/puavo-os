0bd045467f89b4ad65d4eac859c7f44a48ac7a79
Tuomas Räsänen <tuomasjjrasanen@tjjr.fi>
2015-01-02 15:39:56 +0200

    Use orgjson to retrieve ssh data for get-ssh-public-keys

--- parts/admins/get-ssh-public-keys.c
+++ parts/admins/get-ssh-public-keys.c
@@ -15,15 +15,58 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/* Standard libarary includes. */
 #include <stdio.h>
+#include <string.h>
+
+/* Local includes. */
+#include "orgjson.h"
 
 int main(const int argc, const char *const *const argv)
 {
+        orgjson_t *orgjson;
+        struct orgjson_error error;
+        int retval;
+
         if (argc != 2) {
                 fprintf(stderr, "ERROR: invalid number of arguments, "
                         "expetected 1, got %d\n", argc - 1);
                 return 1;
         }
 
-        return 0;
+        orgjson = orgjson_load(&error);
+        if (!orgjson) {
+                fprintf(stderr, "ERROR: failed to load puavoadmin database: %s\n",
+                        error.text);
+                return 1;
+        }
+
+        for (size_t i = 0; i < orgjson_get_owner_count(orgjson); ++i) {
+                struct orgjson_owner owner;
+
+                if (!orgjson_get_owner(orgjson, i, &owner, &error)) {
+                        fprintf(stderr, "ERROR: failed to get entry from "
+                                "puavoadmin database by id %ld: %s\n",
+                                i, error.text);
+                        retval = 1;
+                        goto out;
+                }
+
+                if (!strcmp(owner.username, argv[1])) {
+                        if (owner.ssh_public_key)
+                                printf("%s\n", owner.ssh_public_key);
+                        retval = 0;
+                        goto out;
+                }
+        }
+
+        fprintf(stderr, "ERROR: user '%s' not found\n", argv[1]);
+        retval = 1;
+
+out:
+
+        orgjson_free(orgjson);
+        orgjson = NULL;
+
+        return retval;
 }
