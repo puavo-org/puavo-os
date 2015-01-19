a4534f9d8b78b3bbb6681cfd40942659a8d28439
Tuomas Räsänen <tuomasjjrasanen@tjjr.fi>
2015-01-19 16:27:32 +0200

    ci.sh: simplify building

--- parts/devscripts/ci.sh
+++ parts/devscripts/ci.sh
@@ -6,16 +6,13 @@ set -x
 env
 
 sudo apt-get update
-sudo apt-get install -y --force-yes aptirepo-upload make devscripts equivs git-core
+sudo apt-get install -y aptirepo-upload make equivs
 
-# Dog food itself
-echo "Running in $(pwd)"
-make
-sudo make install
-
-puavo-build-debian-dir
-sudo puavo-install-deps debian/control
-puavo-dch $(cat VERSION)
-puavo-debuild
+sudo make install-deb-deps
+if [ "${CI_TARGET_ARCH}" = i386 ]; then
+    make deb
+else
+    make deb-binary-arch
+fi
 
 aptirepo-upload -r $APTIREPO_REMOTE -b "git-$(echo "$GIT_BRANCH" | cut -d / -f 2)" ../puavo-devscripts*.changes
