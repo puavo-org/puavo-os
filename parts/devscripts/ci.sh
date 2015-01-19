fdf6eabd12fd7933da61eac3a05c99515ee808f3
Tuomas Räsänen <tuomasjjrasanen@tjjr.fi>
2015-01-19 17:14:39 +0200

    ci.sh: build all on i386, on others, build just binary-arch

--- parts/devscripts/ci.sh
+++ parts/devscripts/ci.sh
@@ -9,6 +9,10 @@ sudo apt-get update
 sudo apt-get install -y --force-yes aptirepo-upload make devscripts equivs git-core
 
 sudo make install-deb-debs
-make deb
+if [ "${CI_TARGET_ARCH}" = i386 ]; then
+    make deb
+else
+    make deb-binary-arch
+fi
 
 aptirepo-upload -r $APTIREPO_REMOTE -b "git-$(echo "$GIT_BRANCH" | cut -d / -f 2)" ../puavo-devscripts*.changes
