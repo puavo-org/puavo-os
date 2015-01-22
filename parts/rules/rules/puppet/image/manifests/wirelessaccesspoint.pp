7e21010f076dba6c2db8ff57fad48a3efc9abe71
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2015-01-22 17:13:27 +0200

    Added image::wirelessaccesspoint class for an image type/series especially suited for wireless accesspoints.  Meaning that the image should be quite small and be able to provide the necessary tools for running a wireless accesspoint (obviously), and also a digital signage system (not so obviously, but anyway).

---
+++ parts/rules/rules/puppet/image/manifests/wirelessaccesspoint.pp
@@ -0,0 +1,13 @@
+class image::wirelessaccesspoint {
+  include image::bundle::basic,
+	  packages
+
+  Apt::Key        <| title == "opinsys-repo.gpgkey" |>
+  Apt::Repository <| title == archive
+                  or title == kernels
+                  or title == repo |>
+
+  # apply only builder package definitions listed in packages
+  Package <| tag == admin
+          or tag == basic |>
+}
