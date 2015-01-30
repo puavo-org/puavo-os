1fe1079b9dc6f221f4f0965999d754b7bd904a37
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2015-01-30 17:16:47 +0200

    Change Firefox search bar behaviour back to what it was before Firefox 34.  The main problem with the new UI is that with new users and guest users it takes some time (about ten seconds) before the search bar actually becomes useable at all. Perhaps we will change the setting back once this issue is solved.

--- parts/rules/rules/puppet/firefox/templates/puavodesktop.js
+++ parts/rules/rules/puppet/firefox/templates/puavodesktop.js
@@ -1,15 +1,16 @@
 defaultPref("browser.search.defaultenginename", "Google");
 defaultPref("browser.search.selectedEngine", "Google");
-lockPref("extensions.blocklist.enabled", false);
 lockPref("browser.cache.disk.capacity", 0);
 lockPref("browser.cache.disk.enable", false);
 lockPref("browser.safebrowsing.enabled", false);
 lockPref("browser.safebrowsing.malware.enabled", false);
+lockPref("browser.search.showOneOffButtons", false);
+lockPref("extensions.blocklist.enabled", false);
 lockPref("network.dns.disableIPv6", true);
 lockPref("network.seer.enabled", false);
 <% if not @api_server.nil? -%>
-lockPref("network.negotiate-auth.trusted-uris", "<%= @api_server %>");
 lockPref("network.negotiate-auth.delegation-uris", "<%= @api_server %>");
+lockPref("network.negotiate-auth.trusted-uris", "<%= @api_server %>");
 <% end -%>
 lockPref("print.postscript.paper_size", "iso_a4");
 lockPref("toolkit.storage.synchronous", 1);
