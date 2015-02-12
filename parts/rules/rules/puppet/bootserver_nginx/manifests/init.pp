feaa4d5dd93be8ce80879896bbe6f4313152c048
Tuomas Räsänen <tuomasjjrasanen@tjjr.fi>
2015-02-12 16:33:48 +0200

    bootserver_nginx: refresh by reloading instead of restarting

--- parts/rules/rules/puppet/bootserver_nginx/manifests/init.pp
+++ parts/rules/rules/puppet/bootserver_nginx/manifests/init.pp
@@ -5,9 +5,16 @@ class bootserver_nginx {
     file {
       "/etc/nginx/sites-enabled/${service_name}":
 	ensure => link,
-	notify => Service['nginx'],
+	notify => Exec['reload nginx'],
 	target => "/etc/nginx/sites-available/${service_name}";
     }
+  }
+
+  exec {
+    'reload nginx':
+      command     => '/usr/sbin/service nginx reload',
+      refreshonly => true,
+      require     => Service['nginx'];
   }
 
   service {
