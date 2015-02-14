a5d0d01590c83fde1b460ac6a037ed46a2b163c7
Tuomas Räsänen <tuomasjjrasanen@tjjr.fi>
2015-02-14 22:10:09 +0200

    bootserver_munin: overwrite munin-node.conf

--- parts/rules/rules/puppet/bootserver_munin/manifests/init.pp
+++ parts/rules/rules/puppet/bootserver_munin/manifests/init.pp
@@ -2,6 +2,11 @@ class bootserver_munin {
   include bootserver_nginx
 
   file {
+    '/etc/munin/munin-node.conf':
+      content => template('bootserver_munin/munin-node.conf'),
+      mode    => 0644,
+      require => Package['munin-node'];
+
     '/etc/nginx/sites-available/munin':
       content => template('bootserver_munin/nginx_conf'),
       mode    => 0644,
