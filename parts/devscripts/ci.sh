479324d86a604297ec1e92fbfcfe5e2ac30f96e2
Tuomas Räsänen <tuomasjjrasanen@tjjr.fi>
2015-03-04 15:47:16 +0200

    ci.sh: upload to build distribution -specific codename

--- parts/devscripts/ci.sh
+++ parts/devscripts/ci.sh
@@ -8,4 +8,8 @@ sudo apt-get install -y --force-yes aptirepo-upload make devscripts equivs
 sudo make install-deb-debs
 make deb
 
-aptirepo-upload -r $APTIREPO_REMOTE -b "git-$(echo "$GIT_BRANCH" | cut -d / -f 2)" ../puavo-devscripts*.changes
+aptirepo-upload \
+    -c "${CI_TARGET_DIST}" \
+    -r "${APTIREPO_REMOTE}" \
+    -b "git-$(echo "$GIT_BRANCH" | cut -d / -f 2)" \
+    ../puavo-devscripts*.changes
