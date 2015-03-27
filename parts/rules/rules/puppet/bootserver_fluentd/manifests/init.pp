035af181e0f9bf3bdf86a91e73581067d4d5ffea
Tuomas Räsänen <tuomasjjrasanen@tjjr.fi>
2015-03-27 20:52:38 +0200

    Ensure fluentd's puavo.conf is good

--- parts/rules/rules/puppet/bootserver_fluentd/manifests/init.pp
+++ parts/rules/rules/puppet/bootserver_fluentd/manifests/init.pp
@@ -5,6 +5,10 @@ class bootserver_fluentd {
       notify  => Service['fluentd'],
       source => 'puppet:///modules/bootserver_fluentd/01-puavo.conf';
 
+    '/etc/fluent/conf.d/puavo.conf':
+      notify  => Service['fluentd'],
+      source => 'puppet:///modules/bootserver_fluentd/puavo.conf';
+
     '/usr/local/bin/puavo-bootserver-autopilot-cat':
       mode   => 755,
       source => 'puppet:///modules/bootserver_fluentd/puavo-bootserver-autopilot-cat';
