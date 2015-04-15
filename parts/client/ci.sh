5ca5e8ab782229f01f5f0b02c9bb305c75f642de
Esa-Matti Suuronen <esa-matti@suuronen.org>
2015-04-15 12:24:44 +0300

    Build source packages only on i686

--- parts/client/ci.sh
+++ parts/client/ci.sh
@@ -9,7 +9,7 @@ sudo apt-get install -y wget make devscripts git equivs
 
 sudo make install-build-dep
 
-if [ "${CI_TARGET_ARCH}" = i386 ]; then
+if [ "$(uname -p)" = "i686" ]; then
         make deb
     else
         make deb-binary-arch
