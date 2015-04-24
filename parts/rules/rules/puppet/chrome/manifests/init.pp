033a1f713ed7351a823c38db7c20cf1b3690c23b
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2015-04-24 13:51:45 +0300

    Do not do the default browser check for chromium/chrome. These need a bit different handling because of technical reasons. Rename "chromium_with_chrome_flash"-module to "chromium", because that is now more descriptive.

--- parts/rules/rules/puppet/chrome/manifests/init.pp
+++ parts/rules/rules/puppet/chrome/manifests/init.pp
@@ -1,9 +1,16 @@
 class chrome {
-  include packages
+  include dpkg,
+          packages
+
+  dpkg::simpledivert { '/usr/bin/google-chrome-stable': ; }
 
   file {
     '/etc/apt/sources.list.d/google-chrome.list':
       require => Package['google-chrome-stable'],
       source  => 'puppet:///modules/chrome/google-chrome.list';
+
+    '/usr/bin/google-chrome-stable':
+      mode   => 755,
+      source => 'puppet:///modules/chrome/google-chrome-stable';
   }
 }
