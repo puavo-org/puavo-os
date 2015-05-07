ce44968eea34e329da242a7995a12706f8c0bfe9
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2015-05-07 16:12:39 +0300

    Add some French language packages.

--- parts/rules/rules/puppet/packages/manifests/init.pp
+++ parts/rules/rules/puppet/packages/manifests/init.pp
@@ -302,6 +302,26 @@ class packages {
     , 'thunderbird-locale-fi' ]:
       tag => [ 'language-fi', 'ubuntu', ];
 
+    # fr - French packages
+
+    [ 'language-pack-gnome-fr' ]:
+      tag => [ 'language-fr', 'thinclient', 'ubuntu', ];
+
+    [ 'firefox-locale-fr'
+    , 'gimp-help-fr'
+    , 'hyphen-fr'
+    , 'kde-l10n-fr'
+    , 'language-pack-fr'
+    , 'language-pack-kde-fr'
+    , 'libreoffice-help-fr'
+    , 'libreoffice-l10n-fr'
+    , 'myspell-fr'
+    , 'mythes-fr'
+    , 'thunderbird-locale-fr' ]
+      tag => [ 'language-sv', 'ubuntu', ];
+
+    # sv - Swedish packages
+
     [ 'language-pack-gnome-sv' ]:
       tag => [ 'language-sv', 'thinclient', 'ubuntu', ];
 
