cb743a35cd0241456e2b9a0364765bcfad1ccd06
Tuomas Räsänen <tuomasjjrasanen@tjjr.fi>
2015-05-25 12:49:36 +0300

    restricted-packages: new package format

--- parts/ltsp/client/restricted-packages/Makefile
+++ parts/ltsp/client/restricted-packages/Makefile
@@ -2,7 +2,6 @@ prefix = /usr/local
 exec_prefix = $(prefix)
 sbindir = $(exec_prefix)/sbin
 libdir = $(prefix)/lib
-datarootdir = $(prefix)/share
 
 INSTALL = install
 INSTALL_PROGRAM = $(INSTALL)
@@ -16,21 +15,17 @@ installdirs :
 	mkdir -p $(DESTDIR)$(sbindir)
 	mkdir -p $(DESTDIR)$(libdir)/puavo-ltsp-client/restricted-packages
 	mkdir -p $(DESTDIR)$(libdir)/puavo-ltsp-client/restricted-packages/commands
-	mkdir -p $(DESTDIR)$(datarootdir)/puavo-ltsp-client/restricted-packages
 
 .PHONY : install
 install : installdirs
 	$(INSTALL_PROGRAM) -t $(DESTDIR)$(libdir)/puavo-ltsp-client/restricted-packages \
 		lib/common.bash
-	
+
 	$(INSTALL_PROGRAM) -t $(DESTDIR)$(libdir)/puavo-ltsp-client/restricted-packages/commands \
 		lib/commands/*
-	
+
 	$(INSTALL_PROGRAM) -t $(DESTDIR)$(sbindir) \
 		sbin/puavo-restricted-package-tool
-	
-	cp -r -t $(DESTDIR)$(datarootdir)/puavo-ltsp-client/restricted-packages \
-		packages/*
 
 .PHONY : clean
 clean :
--- parts/ltsp/client/restricted-packages/lib/common.bash
+++ parts/ltsp/client/restricted-packages/lib/common.bash
@@ -53,19 +53,14 @@ is_md5sum_check_required() {
 }
 
 configure_package() {
-    local package=$1
-    local packagedir=$(packagedir "${package}") || return 1
-    local installdir="${packagedir}/i"
-    local prpdir="${packagedir}/PRP"
+    local package_path=$1
+    local pkgdir="${RESTRICTED_PKG_STATEDIR}/${package_path}"
+    local upstream_pack="${pkgdir}/upstream.pack"
+    local upstreamdir="${pkgdir}/upstream"
 
-    has_install_link "$package" || {
-        echo "E: failed to configure package '${package}' because it is not installed" >&2
-        return 1
-    }
-
-    if [ -x "${prpdir}/configure" ]; then
-        "${prpdir}/configure" "${installdir}" "${prpdir}" || {
-            echo "E: failed to configure package '${package}' because '${prpdir}/configure' failed" >&2
+    if [ -x "${pkgdir}/PRP/configure" ]; then
+        "${pkgdir}/PRP/configure" "${upstreamdir}" "${pkgdir}/PRP" || {
+            echo "E: failed to configure package '${package_path}'" >&2
             return 1
         }
     fi
@@ -75,137 +70,81 @@ configure_package() {
 
 unconfigure_package()
 {
-    local package=$1
-    local packagedir=$(packagedir "$package") || return 1
-    local installdir="${packagedir}/i"
-    local prpdir="${packagedir}/PRP"
+    local package_path=$1
+    local pkgdir="${RESTRICTED_PKG_STATEDIR}/${package_path}"
+    local upstream_pack="${pkgdir}/upstream.pack"
+    local upstreamdir="${pkgdir}/upstream"
 
-    has_install_link "$package" || {
-        echo "E: failed to unconfigure package '${package}' because it is not installed" >&2
-        return 1
-    }
-
-    if [ -x "${prpdir}/unconfigure" ]; then
-        "${prpdir}/unconfigure" "${installdir}" "${prpdir}" || {
-            echo "E: failed to unconfigure package '${package}' because '${prpdir}/unconfigure' failed" >&2
+    if [ -x "${pkgdir}/PRP/unconfigure" ]; then
+        "${pkgdir}/PRP/unconfigure" "${upstreamdir}" "${pkgdir}/PRP" || {
+            echo "E: failed to unconfigure package '${package_path}'" >&2
             return 1
         }
     fi
-
-    return 0
-}
-
-uninstall_package()
-{
-    local package=$1
-    local packagedir=$(packagedir "$package") || return 1
-    local installdir="${packagedir}/i"
-    local prpdir="${packagedir}/PRP"
-
-    remove_install_link "$package" || return 1
-
-    for imglink in $(packagebase "$package")/*.img; do
-        if [ ! -h "$imglink" ]; then
-            # last install link removed, garbage collect this
-            rm -rf "${installdir}" "${prpdir}"
-            break
-        fi
-    done
 
     return 0
 }
 
 unpack_package()
 {
-    local package=$1
-    local packagedir=$(packagedir "$package") || return 1
-    local installdir="${packagedir}/i"
-    local prpdir="${packagedir}/PRP"
-    local upstream_pack_md5sum=$(upstream_pack_md5sum "$package") || return 1
-    local upstream_pack="${packagedir}/upstream.pack"
+    local package_path=$1
+    local pkgdir="${RESTRICTED_PKG_STATEDIR}/${package_path}"
+    local upstream_pack="${pkgdir}/upstream.pack"
+    local upstreamdir="${pkgdir}/upstream"
 
-    has_install_link "$package" && return 0
-
-    if [ -d "${installdir}" ]; then
-        make_install_link "$packagedir" "$package" || return 1
+    if [ -d "${upstreamdir}" ]; then
+        echo "I: upstream pack is already unpacked" >&2
         return 0
     fi
 
-    [ ! -f "${packagedir}/upstream.pack" ] && {
-        echo "E: cannot unpack package '${package}' because the upstream pack" \
-            "is missing, download it first" >&2
+    check_md5sum "${pkgdir}/PRP/upstream.pack.md5sum" "${upstream_pack}" || {
+        echo "E: upstream pack has incorrect checksum, " \
+            "perhaps you should purge the package and download it again?" >&2
         return 1
     }
 
-    mkdir "${prpdir}"
-    cp -a -T "${RESTRICTED_PKG_SHAREDIR}/${package}" "${prpdir}" || {
-        rm -rf "${prpdir}"
-        return 1
-    }
+    rm -rf "${upstreamdir}.tmp"
+    mkdir "${upstreamdir}.tmp" || return 1
 
-    if is_md5sum_check_required "${package}"; then
-        check_upstream_pack_md5sum "${upstream_pack_md5sum}" "${upstream_pack}" || {
-            echo "E: md5 checksum of the upstream pack '${upstream_pack}' does not" \
-                "match the known checksum ${upstream_pack_md5sum}," \
-                "perhaps you should purge the package and download it again?" >&2
-            rm -rf "${prpdir}"
+    if [ -x "${pkgdir}/PRP/unpack" ]; then
+        "${pkgdir}/PRP/unpack" "${upstream_pack}" "${upstreamdir}.tmp" || {
+            rm -rf "${upstreamdir}.tmp"
             return 1
         }
     fi
 
-    # We own this directory!
-    rm -rf "${installdir}.tmp"
-    mkdir "${installdir}.tmp" || {
-        rm -rf "${prpdir}"
-        return 1
-    }
-
-    if [ -x "${prpdir}/unpack" ]; then
-        "${prpdir}/unpack" "${upstream_pack}" "${installdir}.tmp" || {
-            rm -rf "${prpdir}"
-            rm -rf "${installdir}.tmp"
-            return 1
-        }
-    fi
-
-    mv -T "${installdir}.tmp" "${installdir}" || {
-        rm -rf "${prpdir}"
-        rm -rf "${installdir}.tmp"
-        return 1
-    }
-
-    make_install_link "$packagedir" "$package" || {
-        rm -rf "${prpdir}"
+    mv -T "${upstreamdir}.tmp" "${upstreamdir}" || {
+        rm -rf "${upstreamdir}.tmp"
         return 1
     }
 
     return 0
 }
 
+check_md5sum()
+{
+    local md5sum_file=$1
+    local file=$2
+
+    [ -r "${md5sum_file}" ] || return 0
+    local md5sum_str=$(cat "${md5sum_file}") || return 1
+
+    md5sum --check --status 2>/dev/null <<<"${md5sum_str}  ${file}"
+}
+
 download_package()
 {
-    local package=$1
-    local custom_script="${RESTRICTED_PKG_SHAREDIR}/${package}/download"
-    local packagedir=$(packagedir "$package") || return 1
-    local upstream_pack_md5sum=$(upstream_pack_md5sum "$package") || return 1
-    local upstream_pack="${packagedir}/upstream.pack"
-    local cache_path=$(upstream_pack_cache_path "$package") || return 1
+    local package_path=$1
+    local pkgdir="${RESTRICTED_PKG_STATEDIR}/${package_path}"
+    local upstream_pack="${pkgdir}/upstream.pack"
 
-    mkdir -p "$packagedir"
-
-    if [ -e "$cache_path" -a ! "$cache_path" -ef "$upstream_pack" ]; then
-        cp -u "$cache_path" "$upstream_pack" || return 1
+    if check_md5sum "${pkgdir}/PRP/upstream.pack.md5sum" "${upstream_pack}"; then
+        echo "I: upstream pack is already downloaded" >&2
+        return 0
     fi
 
-    check_upstream_pack_md5sum "${upstream_pack_md5sum}" "${upstream_pack}" && {
-        echo "I: upstream pack is already in place and has a correct checksum" >&2
-        return 0
-    }
-
-    local url_file="${RESTRICTED_PKG_SHAREDIR}/${package}/upstream.pack.url"
-
-    if [ -x "${custom_script}" ]; then
-        "${custom_script}" "${url_file}" "${upstream_pack}.tmp" || {
+    if [ -x "${pkgdir}/PRP/download" ]; then
+        "${pkgdir}/PRP/download" "${pkgdir}/PRP/upstream.pack.url" "${upstream_pack}.tmp" || {
             echo "E: package downloader returned an error!" >&2
             rm -rf "${upstream_pack}.tmp"
             return 1
@@ -216,20 +155,18 @@ download_package()
             --no-verbose \
             --no-check-certificate \
             --no-cookies \
-            --input-file "${url_file}" \
+            --input-file "${pkgdir}/PRP/upstream.pack.url" \
             --output-document "${upstream_pack}.tmp" || {
             rm -rf "${upstream_pack}.tmp"
             return 1
         }
     fi
 
-    if is_md5sum_check_required "${package}"; then
-        check_upstream_pack_md5sum "${upstream_pack_md5sum}" "${upstream_pack}.tmp" || {
-            echo "E: downloaded upstream pack has incorrect checksum" >&2
-            rm -rf "${upstream_pack}.tmp"
-            return 1
-        }
-    fi
+    check_md5sum "${pkgdir}/PRP/upstream.pack.md5sum" "${upstream_pack}.tmp" || {
+        echo "E: downloaded upstream pack has incorrect checksum" >&2
+        rm -rf "${upstream_pack}.tmp"
+        return 1
+    }
 
     mv "${upstream_pack}.tmp" "${upstream_pack}" || {
         rm -rf "${upstream_pack}.tmp"
@@ -291,4 +228,43 @@ download_packagelicense()
     }
 
     return 0
+}
+
+get_md5sum()
+{
+    local package_file=$1
+    local output=$(md5sum "${package_file}") || return 1
+
+    awk '{print $1}' <<<"${output}"
+}
+
+extract_package_file()
+{
+    local package_file=$1
+    local package_md5sum=$(get_md5sum "${package_file}") || return 1
+    local package_name=$(basename "${package_file}" .tar.gz) || return 1
+    local package_path="${package_name}/${package_md5sum}"
+
+    local package_base="${RESTRICTED_PKG_STATEDIR}/${package_name}"
+    local package_dir="${package_base}/${package_md5sum}"
+
+    if [ -d "${package_dir}" ]; then
+        echo "I: package is already extracted" >&2
+        echo "${package_path}"
+        return
+    fi
+
+    mkdir -p "${package_base}" || return 1
+    mkdir "${package_dir}" || return 1
+    mkdir "${package_dir}/PRP" || {
+        rm -rf "${package_dir}"
+        return 1
+    }
+
+    tar -z -x -f "${package_file}" -C "${package_dir}/PRP" --strip-components=1 || {
+        rm -rf "${package_dir}"
+        return 1
+    }
+
+    echo "${package_path}"
 }
