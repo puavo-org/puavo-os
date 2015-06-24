244c70648bdae22181801c81e5cdd5d3b7a9808d
Tuomas Räsänen <tuomasjjrasanen@tjjr.fi>
2015-06-24 09:47:20 +0300

    ci.sh: make the uploaded changes file more explicit

--- parts/pkg/ci.sh
+++ parts/pkg/ci.sh
@@ -8,8 +8,11 @@ sudo apt-get install -y --force-yes aptirepo-upload make devscripts equivs
 sudo make install-deb-deps
 make deb
 
+version=$(dpkg-parsechangelog --show-field Version)
+arch=$(dpkg-architecture -qDEB_BUILD_ARCH)
+
 aptirepo-upload \
     -c "${CI_TARGET_DIST}" \
     -r "${APTIREPO_REMOTE}" \
     -b "git-$(echo "$GIT_BRANCH" | cut -d / -f 2)" \
-    ../puavo-pkg*.changes
+    "../puavo-pkg_${version}_${arch}.changes"
