27e9f4f61b15de1481f4a7d3c990bdd943d370d9
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2015-06-24 10:09:29 +0300

    puavo-local-config has been split into new version and old (relatively unmaintained) version, and the configuration mechanism has been dropped as a result (and perhaps it is better to implement it as command-line options anyway).  Thus, drop configurations for it.

--- parts/rules/rules/puppet/puavolocalconfigui/manifests/nolocalusers.pp
+++
@@ -1,5 +0,0 @@
-class puavolocalconfigui::nolocalusers inherits puavolocalconfigui {
-  File['/etc/puavo-local-config/puavo-local-config-ui.conf'] {
-    content => template('puavolocalconfigui/puavo-local-config-ui-nolocalusers.conf'),
-  }
-}
--- parts/rules/rules/puppet/puavolocalconfigui/manifests/nopuavopkg.pp
+++
@@ -1,5 +0,0 @@
-class puavolocalconfigui::nopuavopkg inherits puavolocalconfigui {
-  File['/etc/puavo-local-config/puavo-local-config-ui.conf'] {
-    content => template('puavolocalconfigui/puavo-local-config-ui-nopuavopkg.conf'),
-  }
-}
