8418c7f8483cc7e89c414d0c1b470e5af17399e4
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2015-08-11 11:47:48 +0300

    Install some dkms module packages ('bcmwl-kernel-sources', 'nvidia') before any kernels, that way we should get those modules for all kernels that they can be built against.

---
+++ parts/rules/rules/puppet/kernels/manifests/dkms.pp
@@ -0,0 +1,10 @@
+class kernels::dkms {
+  # Install dkms module packages before any kernels, that way we should get
+  # them for all packages.
+
+  $dkms_module_packages = [ 'bcmwl-kernel-source', 'nvidia-304', ]
+
+  Package <| tag == kernel |> {
+    require +> Package[ $dkms_module_packages ],
+  }
+}
