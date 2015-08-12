65fda9a99d07f89216bbcf08c5726221a64bc258
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2015-08-12 09:35:31 +0300

    We do not need bcmwl-kernel-source and nvidia-304 packages on Precise (and actually I do not even know if they exist for that).

--- parts/rules/rules/puppet/kernels/manifests/dkms.pp
+++ parts/rules/rules/puppet/kernels/manifests/dkms.pp
@@ -2,7 +2,10 @@ class kernels::dkms {
   # Install dkms module packages before any kernels, that way we should get
   # them for all packages.
 
-  $dkms_module_packages = [ 'bcmwl-kernel-source', 'nvidia-304', ]
+  $dkms_module_packages = $lsbdistcodename ? {
+                            'precise' => [],
+                            default   => [ 'bcmwl-kernel-source', 'nvidia-304', ],
+                          }
 
   Package <| tag == kernel |> {
     require +> Package[ $dkms_module_packages ],
