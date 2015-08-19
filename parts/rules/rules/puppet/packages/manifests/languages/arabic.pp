89bfd5681600b2c1e562345e80f5187f9a25cb39
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2015-08-19 08:47:51 +0300

    Add packages::languages::arabic, contains something that might help the arabic world.

---
+++ parts/rules/rules/puppet/packages/manifests/languages/arabic.pp
@@ -0,0 +1,25 @@
+class packages::languages::arabic {
+  @package {
+    [ 'language-pack-ar' ]:
+      tag => [ 'language-ar', 'thinclient', 'ubuntu', ];
+
+    [ 'aspell-ar-large'
+    , 'firefox-locale-ar'
+    , 'hunspell-ar'
+    , 'kde-l10n-ar'
+    , 'language-pack-gnome-ar'
+    , 'language-pack-kde-ar'
+    , 'libreoffice-l10n-ar'
+    , 'thunderbird-locale-ar' ]:
+      tag => [ 'language-ar', 'ubuntu', ];
+
+    # XXX ?
+    [ 'othman'
+    , 'tesseract-ocr-ara'
+    , 'thawab'
+    , 'ubuntu-keyboard-arabic'
+    , 'texlive-lang-arabic'
+    , 'xfonts-intl-arabic'
+    ]: tag => [ 'language-ar', 'ubuntu', ];
+  }
+}
