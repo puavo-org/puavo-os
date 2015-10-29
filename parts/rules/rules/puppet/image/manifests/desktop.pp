eda00c9deddb6fc22c6ae1732c8343bea98aa721
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2015-10-29 12:26:10 +0200

    Update xorg-drivers to intel, but add a tag trusty_xorg_drivers to switch the old trusty drivers back on.

--- parts/rules/rules/puppet/image/manifests/desktop.pp
+++ parts/rules/rules/puppet/image/manifests/desktop.pp
@@ -1,13 +1,15 @@
 class image::desktop {
   include image::bundle::desktop,
-          packages
+          packages,
+          xorg_driver_switches
 
   Apt::Key        <| title == "opinsys-repo.gpgkey" |>
   Apt::Repository <| title == archive
                   or title == kernels
                   or title == proposed
                   or title == repo
-                  or title == x2go |>
+                  or title == x2go
+                  or title == xorg-updates |>
 
   # apply package definitions listed in packages with "puavo" or "ubuntu" tags
   Package <| tag == puavo
