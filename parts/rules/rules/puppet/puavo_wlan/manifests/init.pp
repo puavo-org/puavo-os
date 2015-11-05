2a8c3838367ee66d95e5157f3f8ba429b730f898
Tuomas Räsänen <tuomasjjrasanen@tjjr.fi>
2015-11-05 14:37:30 +0200

    Revert "puavo_wlan: remove config templates, puavo-wlanap requires no config"
    We still want to use the old, battle-tested version of the puavo-wlanap
    package.
    
    This reverts commit 3926a49f147f1501d41b01509b6cc7fdd1566716.
    
    Conflicts:
    	rules/puppet/puavo_wlan/manifests/init.pp

--- parts/rules/rules/puppet/puavo_wlan/manifests/init.pp
+++ parts/rules/rules/puppet/puavo_wlan/manifests/init.pp
@@ -2,5 +2,14 @@ class puavo_wlan {
   include puavo_wlan::rt2800usb_shutdown_workaround
   require packages
 
+  file {
+    '/etc/default/puavo-wlanap':
+      content => template('puavo_wlan/ap.default');
+
+    '/etc/puavo-wlanap/config':
+      content => template('puavo_wlan/ap.config');
+  }
+
   Package <| title == puavo-wlanap |>
+
 }
