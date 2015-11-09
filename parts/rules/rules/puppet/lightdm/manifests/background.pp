3208e03f0e3afa115b34b70777adf36b1d0655c9
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2015-11-09 10:43:53 +0200

    Move default backgrounds to /usr/share/backgrounds/puavo-greeter/default.

--- parts/rules/rules/puppet/lightdm/manifests/background.pp
+++ parts/rules/rules/puppet/lightdm/manifests/background.pp
@@ -13,9 +13,12 @@ class lightdm::background {
     default   => 'ubuntu-wallpapers-saucy',
   }
 
+  $backgrounds_dir = '/usr/share/backgrounds/puavo-greeter/default'
+
   file {
     [ '/usr/share/backgrounds'
-    , '/usr/share/backgrounds/puavo-greeter' ]:
+    , '/usr/share/backgrounds/puavo-greeter'
+    , $backgrounds_dir ]:
       ensure => directory;
 
     '/etc/dconf/db/lightdm.d/lightdm_background_profile':
@@ -25,83 +28,83 @@ class lightdm::background {
   }
 
   @file {
-    '/usr/share/backgrounds/puavo-greeter/Beach_by_Renato_Giordanelli.jpg':
+    "${backgrounds_dir}/Beach_by_Renato_Giordanelli.jpg":
       ensure  => link,
       require => Package['ubuntu-wallpapers-trusty'],
       tag     => [ 'backgroundimages', 'trusty', ],
-      target  => '../Beach_by_Renato_Giordanelli.jpg';
+      target  => '../../Beach_by_Renato_Giordanelli.jpg';
 
-    '/usr/share/backgrounds/puavo-greeter/Cairn_by_Sylvain_Naudin.jpg':
+    "${backgrounds_dir}/Cairn_by_Sylvain_Naudin.jpg":
       ensure  => link,
       require => Package['ubuntu-wallpapers-quantal'],
       tag     => [ 'backgroundimages', 'trusty', ],
-      target  => '../Cairn_by_Sylvain_Naudin.jpg';
+      target  => '../../Cairn_by_Sylvain_Naudin.jpg';
 
-    '/usr/share/backgrounds/puavo-greeter/Early_Morning_by_Robert_Katzki.jpg':
+    "${backgrounds_dir}/Early_Morning_by_Robert_Katzki.jpg":
       ensure  => link,
       require => Package['ubuntu-wallpapers-quantal'],
       tag     => [ 'backgroundimages', 'trusty', ],
-      target  => '../Early_Morning_by_Robert_Katzki.jpg';
+      target  => '../../Early_Morning_by_Robert_Katzki.jpg';
 
-    '/usr/share/backgrounds/puavo-greeter/Fleurs_de_Prunus_24_by_Jérôme_Boivin.jpg':
+    "${backgrounds_dir}/Fleurs_de_Prunus_24_by_Jérôme_Boivin.jpg":
       ensure  => link,
       require => Package['ubuntu-wallpapers-raring'],
       tag     => [ 'backgroundimages', 'trusty', ],
-      target  => '../Fleurs_de_Prunus_24_by_Jérôme_Boivin.jpg';
+      target  => '../../Fleurs_de_Prunus_24_by_Jérôme_Boivin.jpg';
 
-    '/usr/share/backgrounds/puavo-greeter/Ibanez_Infinity_by_Jaco_Kok.jpg':
+    "${backgrounds_dir}/Ibanez_Infinity_by_Jaco_Kok.jpg":
       ensure  => link,
       require => Package['ubuntu-wallpapers-trusty'],
       tag     => [ 'backgroundimages', 'trusty', ],
-      target  => '../Ibanez_Infinity_by_Jaco_Kok.jpg';
+      target  => '../../Ibanez_Infinity_by_Jaco_Kok.jpg';
 
-    '/usr/share/backgrounds/puavo-greeter/Morning_Sun_on_Frost-Covered_Leaves_by_Gary_A_Stafford.jpg':
+    "${backgrounds_dir}/Morning_Sun_on_Frost-Covered_Leaves_by_Gary_A_Stafford.jpg":
       ensure  => link,
       require => Package['ubuntu-wallpapers-raring'],
       tag     => [ 'backgroundimages', 'trusty', ],
-      target  => '../Morning_Sun_on_Frost-Covered_Leaves_by_Gary_A_Stafford.jpg';
+      target  => '../../Morning_Sun_on_Frost-Covered_Leaves_by_Gary_A_Stafford.jpg';
 
-    '/usr/share/backgrounds/puavo-greeter/Nylon_Rainbow_by_Sam_Hewitt.jpg':
+    "${backgrounds_dir}/Nylon_Rainbow_by_Sam_Hewitt.jpg":
       ensure  => link,
       require => Package['ubuntu-wallpapers-saucy'],
       tag     => [ 'backgroundimages', 'trusty', ],
-      target  => '../Nylon_Rainbow_by_Sam_Hewitt.jpg';
+      target  => '../../Nylon_Rainbow_by_Sam_Hewitt.jpg';
 
-    '/usr/share/backgrounds/puavo-greeter/Pantano_de_Orellana_by_mgarciaiz.jpg':
+    "${backgrounds_dir}/Pantano_de_Orellana_by_mgarciaiz.jpg":
       ensure  => link,
       require => Package['ubuntu-wallpapers-quantal'],
       tag     => [ 'backgroundimages', 'trusty', ],
-      target  => '../Pantano_de_Orellana_by_mgarciaiz.jpg';
+      target  => '../../Pantano_de_Orellana_by_mgarciaiz.jpg';
 
-    '/usr/share/backgrounds/puavo-greeter/Partitura_by_Vincijun.jpg':
+    "${backgrounds_dir}/Partitura_by_Vincijun.jpg":
       ensure  => link,
       require => Package['ubuntu-wallpapers-trusty'],
       tag     => [ 'backgroundimages', 'trusty', ],
-      target  => '../Partitura_by_Vincijun.jpg';
+      target  => '../../Partitura_by_Vincijun.jpg';
 
-    '/usr/share/backgrounds/puavo-greeter/Speaker_Weave_by_Phil_Jackson.jpg':
+    "${backgrounds_dir}/Speaker_Weave_by_Phil_Jackson.jpg":
       ensure  => link,
       require => Package['ubuntu-wallpapers-precise'],
       tag     => [ 'backgroundimages', 'precise', 'trusty', ],
-      target  => '../Speaker_Weave_by_Phil_Jackson.jpg';
+      target  => '../../Speaker_Weave_by_Phil_Jackson.jpg';
 
-    '/usr/share/backgrounds/puavo-greeter/Tie_My_Boat_by_Ray_García.jpg':
+    "${backgrounds_dir}/Tie_My_Boat_by_Ray_García.jpg":
       ensure  => link,
       require => Package['ubuntu-wallpapers-precise'],
       tag     => [ 'backgroundimages', 'precise', 'trusty', ],
-      target  => '../Tie_My_Boat_by_Ray_García.jpg';
+      target  => '../../Tie_My_Boat_by_Ray_García.jpg';
 
-    '/usr/share/backgrounds/puavo-greeter/Vanishing_by_James_Wilson.jpg':
+    "${backgrounds_dir}/Vanishing_by_James_Wilson.jpg":
       ensure  => link,
       require => Package['ubuntu-wallpapers-quantal'],
       tag     => [ 'backgroundimages', 'trusty', ],
-      target  => '../Vanishing_by_James_Wilson.jpg';
+      target  => '../../Vanishing_by_James_Wilson.jpg';
 
-    '/usr/share/backgrounds/puavo-greeter/Winter_Morning_by_Shannon_Lucas.jpg':
+    "${backgrounds_dir}/Winter_Morning_by_Shannon_Lucas.jpg":
       ensure  => link,
       require => Package['ubuntu-wallpapers-precise'],
       tag     => [ 'backgroundimages', 'precise', 'trusty', ],
-      target  => '../Winter_Morning_by_Shannon_Lucas.jpg';
+      target  => '../../Winter_Morning_by_Shannon_Lucas.jpg';
   }
 
   File    <| tag == backgroundimages and tag == $lsbdistcodename |>
