c6b667fcd639ea9734e12304e0f8e93b37f07576
Tuomas Räsänen <tuomasjjrasanen@tjjr.fi>
2016-01-27 15:02:08 +0200

    parts/conf: add API doc

--- parts/conf/conf.h
+++ parts/conf/conf.h
@@ -22,13 +22,89 @@ static const char *const PUAVO_CONF_DEFAULT_DB_FILEPATH = DEFAULT_DB_FILEPATH;
 
 typedef struct puavo_conf puavo_conf_t;
 
+/**
+ * puavo_conf_init() - allocate and initialize a config object
+ *
+ * @confp - a pointer to an uninitialized config object pointer
+ *
+ * After a successful call, an initialized config object is allocated on
+ * the heap and its address is returned via @confp. The caller is
+ * responsible for calling puavo_conf_free() on the config object
+ * afterwards.
+ *
+ * Return 0 on success, non-zero otherwise.
+ */
 int puavo_conf_init(puavo_conf_t **confp);
+
+/**
+ * puavo_conf_free() - release resources reserved by a config object
+ *
+ * @conf - an initialized config object pointer
+ *
+ * Calling puavo_conf_free() multiple times on the same object is a
+ * programmer error and leads to undefined behavior.
+ */
 void puavo_conf_free(puavo_conf_t *conf);
 
+/**
+ * puavo_conf_open_db() - create and/or open a database
+ *
+ * @conf        - an initialized config object pointer
+ *
+ * @db_filepath - the filepath for the database, if NULL,
+ *                PUAVO_CONF_DEFAULT_DB_FILEPATH is used instead
+ *
+ * If the database does not exist, it will be created. After a
+ * successful call, the caller is responsible for closing the database
+ * by calling puavo_conf_close_db().
+ *
+ * Return 0 on success, non-zero otherwise.
+ */
 int puavo_conf_open_db(puavo_conf_t *conf, const char *db_filepath);
+
+/**
+ * puavo_conf_close_db() - close an open database
+ *
+ * @conf - an initialized config object pointer
+ *
+ * Close the database. This must be called to ensure all database
+ * operations get finished and all resources get released properly.
+ *
+ * Return 0 on success, non-zero otherwise.
+ */
 int puavo_conf_close_db(puavo_conf_t *conf);
 
+/**
+ * puavo_conf_set() - store a parameter as a key-value pair
+ *
+ * @conf  - an initialized config object pointer
+ *
+ * @key   - a nul-terminated string
+ *
+ * @value - a nul-terminated string
+ *
+ * If the key already exists in the database, the value is overwritten.
+ *
+ * Return 0 on success, non-zero otherwise.
+ */
 int puavo_conf_set(puavo_conf_t *conf, char *key, char *value);
+
+/**
+ * puavo_conf_get() - retrieve the value of an parameter
+ *
+ * @conf   - an initialized config object pointer
+ *
+ * @key    - a nul-terminated string
+ *
+ * @valuep - a pointer to an uninitialized string
+ *
+ * After a successful call, a nul-terminated string, containing the
+ * value for @key, is allocated on the heap and its address is returned
+ * via @valuep. The caller is responsible for calling free() on the
+ * string afterwards.
+ *
+ * Return 0 on success, non-zero otherwise.
+ */
 int puavo_conf_get(puavo_conf_t *conf, char *key, char **valuep);
 
 #endif /* CONF_H */
