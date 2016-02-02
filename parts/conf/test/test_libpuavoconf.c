a7c30228c7fe0ecc4d3e166fa2a52df2b04481cf
Tuomas Räsänen <tuomasjjrasanen@tjjr.fi>
2016-02-02 21:53:29 +0200

    parts/conf: test: use common test fixture for empty db case

--- parts/conf/test/test_libpuavoconf.c
+++ parts/conf/test/test_libpuavoconf.c
@@ -5,24 +5,27 @@
 
 #include "../conf.h"
 
-START_TEST(test_puavo_conf_get_empty_db)
+puavo_conf_t *conf;
+
+void setup_empty_db()
 {
-	puavo_conf_t *conf;
-	char *value;
+	puavo_conf_init(&conf);
+	puavo_conf_open_db(conf, "test.db");
+	puavo_conf_clear_db(conf);
+}
+
+void teardown_empty_db()
+{
+	puavo_conf_free(conf);
+}
+
+START_TEST(test_list_empty_db)
+{
 	struct puavo_conf_param *params;
 	size_t len;
 
-	ck_assert_int_eq(0, puavo_conf_init(&conf));
-	ck_assert_int_eq(0, puavo_conf_open_db(conf, "test.db"));
-
-	ck_assert_int_eq(0, puavo_conf_clear_db(conf));
 	ck_assert_int_eq(0, puavo_conf_list(conf, &params, &len));
 	ck_assert(len == 0);
-	ck_assert_int_eq(-PUAVO_CONF_ERR_DB,
-			 puavo_conf_get(conf, "somekey", &value));
-
-	ck_assert_int_eq(0, puavo_conf_close_db(conf));
-	puavo_conf_free(conf);
 }
 END_TEST
 
@@ -35,7 +38,9 @@ static Suite *puavo_conf_suite(void)
 
 	tc_empty_db = tcase_create("Empty database");
 
-	tcase_add_test(tc_empty_db, test_puavo_conf_get_empty_db);
+	tcase_add_checked_fixture(tc_empty_db, setup_empty_db,
+				  teardown_empty_db);
+	tcase_add_test(tc_empty_db, test_list_empty_db);
 
 	suite_add_tcase(s, tc_empty_db);
 
