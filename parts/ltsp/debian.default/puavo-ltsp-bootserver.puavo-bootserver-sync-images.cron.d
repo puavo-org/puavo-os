fcf433dcef0039f882c9bb09134817f4b124f8a4
Tuomas Räsänen <tuomasjjrasanen@tjjr.fi>
2016-02-24 12:54:30 +0200

    Remove separate debian packaging from parts

--- parts/ltsp/debian.default/puavo-ltsp-bootserver.puavo-bootserver-sync-images.cron.d
+++
@@ -1,6 +0,0 @@
-SHELL=/bin/bash
-
-PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-
-00 20 * * * root sleep $(( RANDOM \% 7200 )) && /sbin/start --quiet puavo-bootserver-sync-images >/dev/null 2>&1
-00 07 * * * root /sbin/stop --quiet puavo-bootserver-sync-images >/dev/null 2>&1
