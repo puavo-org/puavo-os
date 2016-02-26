b90e3861ea844d541b1a4c0dea6735168355b84d
Tuomas Räsänen <tuomasjjrasanen@tjjr.fi>
2016-02-26 10:20:46 +0200

    parts/rules: disable_suspend_by_tag: use puavo-conf

--- parts/rules/rules/puppet/disable_suspend_by_tag/manifests/init.pp
+++ parts/rules/rules/puppet/disable_suspend_by_tag/manifests/init.pp
@@ -6,7 +6,12 @@ class disable_suspend_by_tag {
       mode    => 755,
       require => Package['pm-utils'],
       source  => 'puppet:///modules/disable_suspend_by_tag/02_nosuspendtag_test';
+
+    '/usr/share/puavo-conf/parameters/puavo-rules-disable_suspend_by_tag.json':
+      require => Package['puavo-conf'],
+      source  => 'puppet:///modules/disable_suspend_by_tag/puavo-conf-parameters.json';
   }
 
   Package <| title == pm-utils |>
+  Package <| title == puavo-conf |>
 }
