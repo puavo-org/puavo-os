bf3d8b9455cd1d5cb1a85f7621a0b6a07e8d0e6f
Tuomas Räsänen <tuomasjjrasanen@tjjr.fi>
2016-03-10 14:04:22 +0200

    aptirepo: update copyright years

--- parts/aptirepo/aptirepo/lib/_core.py
+++ parts/aptirepo/aptirepo/lib/_core.py
@@ -1,5 +1,5 @@
 # aptirepo - Simple APT Repository Tool
-# Copyright (C) 2013,2014,2015 Opinsys
+# Copyright (C) 2013,2014,2015,2016 Opinsys
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
