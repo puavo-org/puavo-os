d532076917355df1a24e55438dbf4f1e9576479c
Jouni Korhonen <jouni.korhonen@opinsys.fi>
2016-05-04 12:07:55 +0000

    client: refactor

--- parts/client/lib/puavo/image_client.rb
+++ parts/client/lib/puavo/image_client.rb
@@ -32,6 +32,64 @@ class PuavoImageClient
     end
   end
 
+  class Upload
+
+    attr_accessor :username, :password, :meta_server, :domain
+
+    def initialize(args)
+      @client = HTTPClient.new()
+      @client.force_basic_auth = true
+      @client.send_timeout = 60 * 60 * 2
+      @client.receive_timeout = 60 * 60 * 2
+
+      @meta_server = args[:meta_server]
+      @domain = args[:domain]
+    end
+
+    def post(*args)
+      self.request("post", *args)
+    end
+
+    def put(*args)
+      self.request("put", *args)
+    end
+
+    def request(method, *args)
+      counter = 0
+      headers = {}
+      while response = @client.send(method, *args, headers) do
+        if response.status == 401
+          puts "Invalid username or password!" if counter > 0
+          self.username = ask("Enter your username: ") { |q| q.echo = true } if self.username.nil?
+          self.password = ask("Enter your password: ") { |q| q.echo = "*" } if self.password.nil?
+          @client.set_auth(self.meta_server, username, password)
+          headers = { :host => self.domain }
+          counter += 1
+          next
+        end
+
+        break
+      end
+
+      if response.status =! 200
+        puts "Something went wrong! Response status: #{ response.status }, body: #{ response.body }"
+        Process.exit(1)
+      end
+
+      begin
+        response_data = JSON.parse(response.body)
+      rescue JSON::ParserError
+        puts "Request: #{ method }: #{ args.first }"
+        puts "An invalid response data was received: " + response.body
+        Process.exit(1)
+      end
+
+      return response_data
+    end
+
+
+  end
+
   class SHA256MismatchError < RuntimeError;  end
   class OutputFileError < RuntimeError;  end
 end
