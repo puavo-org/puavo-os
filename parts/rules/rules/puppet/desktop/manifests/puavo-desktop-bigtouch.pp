569670983010ccce2c835111be74c44c29772be0
Tuomas Räsänen <tuomasjjrasanen@tjjr.fi>
2016-05-11 16:38:19 +0300

    parts/rules: autostart onboard if puavo.onscreenkeyboard.enabled == true

--- parts/rules/rules/puppet/desktop/manifests/puavo-desktop-bigtouch.pp
+++ parts/rules/rules/puppet/desktop/manifests/puavo-desktop-bigtouch.pp
@@ -1,7 +1,13 @@
 class desktop::puavo-desktop-bigtouch {
   include desktop::dconf,
+          dpkg,
           gnome_shell_extensions,
           packages
+
+  dpkg::simpledivert {
+    '/etc/xdg/autostart/onboard-autostart.desktop':
+      before => File['/etc/xdg/autostart/onboard-autostart.desktop'];
+  }
 
   file {
     '/etc/dconf/db/puavo-desktop-bigtouch.d':
@@ -10,6 +16,19 @@ class desktop::puavo-desktop-bigtouch {
     '/etc/dconf/db/puavo-desktop-bigtouch.d/profile':
       content => template('desktop/puavo-desktop-bigtouch/profile'),
       notify  => Exec['update dconf'];
+
+    '/etc/xdg/autostart/onboard-autostart.desktop':
+      content => template('desktop/puavo-desktop-bigtouch/onboard-autostart.desktop'),
+      require => Package['onboard'];
+
+    '/usr/local/lib/puavo-onboard':
+      content => template('desktop/puavo-desktop-bigtouch/puavo-onboard'),
+      mode    => 0755,
+      require => Package['onboard'];
+
+    '/usr/share/puavo-conf/parameters/puavo-onboard.json':
+      content => template('desktop/puavo-desktop-bigtouch/puavo-conf-parameters.json');
   }
 
+  Package <| title == onboard |>
 }
