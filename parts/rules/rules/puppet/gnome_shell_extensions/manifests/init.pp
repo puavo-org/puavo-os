708a746c793db17f93f53105098c4d9ac68cdfe3
Tuomas Räsänen <tuomasjjrasanen@tjjr.fi>
2016-05-23 13:56:33 +0300

    desktop-bigtouch: rename extension: Bigtouch panel => Bigtouch User Experience

--- parts/rules/rules/puppet/gnome_shell_extensions/manifests/init.pp
+++ parts/rules/rules/puppet/gnome_shell_extensions/manifests/init.pp
@@ -2,8 +2,8 @@ class gnome_shell_extensions {
   include packages
 
   file {
-    '/usr/share/gnome-shell/extensions/bigtouch-panel@puavo.org':
-      source  => 'puppet:///modules/gnome_shell_extensions/bigtouch-panel',
+    '/usr/share/gnome-shell/extensions/bigtouch-ux@puavo.org':
+      source  => 'puppet:///modules/gnome_shell_extensions/bigtouch-ux',
       recurse => true,
       require => Package['gnome-shell-extensions'];
 
