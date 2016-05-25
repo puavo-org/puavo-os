468062131e1ec36f696b41bae6831a2b901f01ef
Tuomas Räsänen <tuomasjjrasanen@tjjr.fi>
2016-05-25 10:22:03 +0300

    parts/rules: remove dash-to-dock GNOME Shell extension

--- parts/rules/rules/puppet/gnome_shell_extensions/manifests/init.pp
+++ parts/rules/rules/puppet/gnome_shell_extensions/manifests/init.pp
@@ -7,10 +7,6 @@ class gnome_shell_extensions {
       recurse => true,
       require => Package['gnome-shell-extensions'];
 
-    '/usr/share/gnome-shell/extensions/dash-to-dock@micxgx.gmail.com':
-      source  => 'puppet:///modules/gnome_shell_extensions/dash-to-dock',
-      recurse => true,
-      require => Package['gnome-shell-extensions'];
   }
 
   Package <| title == gnome-shell-extensions |>
