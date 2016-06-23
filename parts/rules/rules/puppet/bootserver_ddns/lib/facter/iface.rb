fcd1f3dce5069da56750469bc25134772895128e
Tuomas Räsänen <tuomasjjrasanen@tjjr.fi>
2016-06-23 14:47:20 +0300

    bootserver_ddns: do not add any iface facts if ipaddress module is missing

--- parts/rules/rules/puppet/bootserver_ddns/lib/facter/iface.rb
+++ parts/rules/rules/puppet/bootserver_ddns/lib/facter/iface.rb
@@ -1,100 +1,105 @@
-require 'ipaddress'
+begin
+  require 'ipaddress'
+rescue LoadError
+  # NOP
+else
+  ENV['LANG'] = 'C'
 
-ENV['LANG'] = 'C'
+  class Interface
+    def initialize(name)
+      @name = name
+      @ifconfig = `ifconfig #{name}`
+    end
 
-class Interface
-  def initialize(name)
-    @name = name
-    @ifconfig = `ifconfig #{name}`
-  end
+    def ip
+      if /inet addr:(\d+)\.(\d+)\.(\d+)\.(\d+)/.match(@ifconfig)
+        ip = "#{$1}.#{$2}.#{$3}.#{$4}"
 
-  def ip
-    if /inet addr:(\d+)\.(\d+)\.(\d+)\.(\d+)/.match(@ifconfig)
-      ip = "#{$1}.#{$2}.#{$3}.#{$4}"
+        if /Mask:(\d+)\.(\d+)\.(\d+)\.(\d+)/.match(@ifconfig)
+          mask = "#{$1}.#{$2}.#{$3}.#{$4}"
 
-      if /Mask:(\d+)\.(\d+)\.(\d+)\.(\d+)/.match(@ifconfig)
-        mask = "#{$1}.#{$2}.#{$3}.#{$4}"
-
-        return IPAddress "#{ip}/#{mask}"
+          return IPAddress "#{ip}/#{mask}"
+        end
       end
+
+      return nil
     end
 
-    return nil
-  end
+    def ranges(mask)
+      subnets = self.ip.subnet(mask)
+      ranges = Array.new
+      iface_ip = self.ip
 
-  def ranges(mask)
-    subnets = self.ip.subnet(mask)
-    ranges = Array.new
-    iface_ip = self.ip
-
-    subnets.each do |subnet|
-      if !subnet.include?(iface_ip)
-        ranges << [subnet.first.to_s, subnet.last.to_s]
+      subnets.each do |subnet|
+        if !subnet.include?(iface_ip)
+          ranges << [subnet.first.to_s, subnet.last.to_s]
+        end
       end
+
+      ranges
     end
-
-    ranges
   end
-end
 
-Facter.add('ltsp_iface_ip') do
-  setcode do
-    Interface.new('ltsp0').ip.to_s
-  end
-end
-
-Facter.add('wlan_iface_ip') do
-  setcode do
-    Interface.new('wlan0').ip.to_s
-  end
-end
-
-Facter.add('ltsp_iface_subnet') do
-  setcode do
-    iface = Interface.new('ltsp0')
-    iface.ip.network.address
-  end
-end
-
-Facter.add('wlan_iface_subnet') do
-  setcode do
-    iface = Interface.new('wlan0')
-    iface.ip.network.address
-  end
-end
-
-Facter.add('ltsp_iface_netmask') do
-  setcode do
-    iface = Interface.new('ltsp0')
-    iface.ip.netmask
-  end
-end
-
-Facter.add('wlan_iface_netmask') do
-  setcode do
-    iface = Interface.new('wlan0')
-    iface.ip.netmask
-  end
-end
-
-Facter.add('ltsp_iface_ranges') do
-  setcode do
-    ranges = []
-    iface = Interface.new('ltsp0')
-    iface.ranges(24)[0..-2].each do |range|
-      ranges << range.join(':')
+  Facter.add('ltsp_iface_ip') do
+    setcode do
+      Interface.new('ltsp0').ip.to_s
     end
-    ranges.join(',')
   end
-end
 
-Facter.add('wlan_iface_ranges') do
-  setcode do
-    ranges = []
-    iface = Interface.new('wlan0')
-    iface.ranges(24)[0..-2].each do |range|
-      ranges << range.join(':')
+  Facter.add('wlan_iface_ip') do
+    setcode do
+      Interface.new('wlan0').ip.to_s
     end
-    ranges.join(',')
   end
+
+  Facter.add('ltsp_iface_subnet') do
+    setcode do
+      iface = Interface.new('ltsp0')
+      iface.ip.network.address
+    end
+  end
+
+  Facter.add('wlan_iface_subnet') do
+    setcode do
+      iface = Interface.new('wlan0')
+      iface.ip.network.address
+    end
+  end
+
+  Facter.add('ltsp_iface_netmask') do
+    setcode do
+      iface = Interface.new('ltsp0')
+      iface.ip.netmask
+    end
+  end
+
+  Facter.add('wlan_iface_netmask') do
+    setcode do
+      iface = Interface.new('wlan0')
+      iface.ip.netmask
+    end
+  end
+
+  Facter.add('ltsp_iface_ranges') do
+    setcode do
+      ranges = []
+      iface = Interface.new('ltsp0')
+      iface.ranges(24)[0..-2].each do |range|
+        ranges << range.join(':')
+      end
+      ranges.join(',')
+    end
+  end
+
+  Facter.add('wlan_iface_ranges') do
+    setcode do
+      ranges = []
+      iface = Interface.new('wlan0')
+      iface.ranges(24)[0..-2].each do |range|
+        ranges << range.join(':')
+      end
+      ranges.join(',')
+    end
+  end
+
 end
