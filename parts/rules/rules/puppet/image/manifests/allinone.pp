ce5ef5901db2e4679d20487a165c92a91fa3bac6
Tuomas Räsänen <tuomasjjrasanen@tjjr.fi>
2016-08-11 14:53:46 +0300

    parts/rules: add image::allinone class

---
+++ parts/rules/rules/puppet/image/manifests/allinone.pp
@@ -0,0 +1,38 @@
+class image::allinone {
+  include ::autopoweroff,
+          ::chromium,
+          ::console,
+          ::desktop,
+          ::disable_accounts_service,
+          ::disable_geoclue,
+          ::disable_suspend_by_tag,
+          ::disable_suspend_on_halt,
+          ::disable_suspend_on_nbd_devices,
+          ::disable_unclutter,
+          ::fontconfig,
+          ::gnome_terminal,
+          ::image::bundle::basic,
+          ::kaffeine,
+          ::keyutils,
+          ::ktouch,
+          ::laptop_mode_tools,
+          ::lightdm,
+          ::network_manager,
+          ::packages,
+          ::packages,
+          ::packages::languages::de,
+          ::packages::languages::en,
+          ::packages::languages::fi,
+          ::packages::languages::fr,
+          ::packages::languages::sv,
+          ::picaxe_udev_rules,
+          ::plymouth,
+          ::ssh_client,
+          ::sysctl,
+          ::udev,
+          ::use_urandom,
+          ::wacom
+
+  Package <| tag == 'tag_debian'
+          or tag == 'tag_puavo' |>
+}
