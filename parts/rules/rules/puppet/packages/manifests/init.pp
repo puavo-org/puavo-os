6f67ac8d33f75bea617a1026a12736025af65f03
Tuomas Räsänen <tuomasjjrasanen@tjjr.fi>
2016-08-15 11:19:35 +0300

    parts: remove puavo-monitor
    Monitoring service chain is already broken, and the current
    implementation is suboptimal.
    
    Some kind of status monitoring is required, but its requirements need
    to be carefully considered before implementing yet another half-baked
    solution to the wrong problem.

--- parts/rules/rules/puppet/packages/manifests/init.pp
+++ parts/rules/rules/puppet/packages/manifests/init.pp
@@ -683,7 +683,6 @@ class packages {
     , 'puavo-hw-log'
     , 'puavo-ltsp-client'
     , 'puavo-ltsp-install'
-    , 'puavo-monitor'
     , 'puavo-vpn-client' ]:
       tag => [ 'tag_misc', 'tag_puavo', 'tag_thinclient', ];
 
