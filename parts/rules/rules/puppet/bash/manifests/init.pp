27e15cbac8bfa05cab80be27a0cafeb7c4fef166
Tuomas Räsänen <tuomasjjrasanen@tjjr.fi>
2016-08-29 13:10:38 +0300

    parts/rules: bash: require bash package

--- parts/rules/rules/puppet/bash/manifests/init.pp
+++ parts/rules/rules/puppet/bash/manifests/init.pp
@@ -1,8 +1,15 @@
 class bash {
+  include packages
+
+  File { require => Package['bash'], }
+
   file {
     '/etc/bash.bashrc':
       content => template('bash/bash.bashrc');
+
     '/etc/skel/.bashrc':
       content => template('bash/etc_skel_.bashrc');
   }
+
+  Package <| title == bash |>
 }
