c32b7e1a36dcd9ed2ba51b4f55e6a1df1d9b5183
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2016-09-01 11:59:17 +0300

    Do not use opinsyspackages in oracle_java::tools, it is no more. And format things properly.

--- parts/rules/rules/puppet/oracle_java/manifests/tools.pp
+++ parts/rules/rules/puppet/oracle_java/manifests/tools.pp
@@ -1,29 +1,30 @@
 class oracle_java::tools {
-  include opinsyspackages
+  include packages
 
   file {
     '/etc/puavo-external-files-actions.d/java':
-      mode        => 755,
-      require     => Package['puavo-ltsp-client'],
-      source      => 'puppet:///modules/oracle_java/puavo-external-files-actions.d/java';
+      mode    => 755,
+      require => Package['puavo-ltsp-client'],
+      source  => 'puppet:///modules/oracle_java/puavo-external-files-actions.d/java';
 
     '/opt/java':
-      ensure      => directory;
+      ensure => directory;
 
     '/opt/java/opinsys-add-cert':
-      mode        => 0700,
-      source      => 'puppet:///modules/oracle_java/opinsys-add-cert';
+      mode   => 0700,
+      source => 'puppet:///modules/oracle_java/opinsys-add-cert';
 
     '/opt/java/opinsys-create-signed-ruleset':
-      mode        => 0700,
-      source      => 'puppet:///modules/oracle_java/opinsys-create-signed-ruleset';
+      mode   => 0700,
+      source => 'puppet:///modules/oracle_java/opinsys-create-signed-ruleset';
 
     '/opt/java/opinsys.keystore':
-      mode        => 0600,
-      source      => 'puppet:///modules/oracle_java/opinsys.keystore';
+      mode   => 0600,
+      source => 'puppet:///modules/oracle_java/opinsys.keystore';
 
     '/opt/java/ruleset.xml':
-      source      => 'puppet:///modules/oracle_java/ruleset.xml';
+      source => 'puppet:///modules/oracle_java/ruleset.xml';
   }
 
+  Package <| title == puavo-ltsp-client |>
 }
