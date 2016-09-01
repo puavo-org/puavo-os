70820135960accf0688d7cd6680241ddc8a09c67
Tuomas Räsänen <tuomasjjrasanen@tjjr.fi>
2016-09-01 10:04:11 +0000

    bootserver_samba: manage /etc/samba/smb.conf

--- rules/puppet/bootserver_samba/manifests/init.pp
+++ rules/puppet/bootserver_samba/manifests/init.pp
@@ -1,8 +1,20 @@
 class bootserver_samba {
+
+  $puavo_samba_domain = generate('/usr/bin/ruby1.9.3', '-e', '
+require "puavo"
+require "puavo/ldap"
+print Puavo::Client::Base.new_by_ldap_entry(
+  Puavo::Ldap.new.organisation).samba_domain_name
+')
+
   file {
     '/etc/pam.d/samba':
       content => template('bootserver_samba/pam.samba'),
       mode    => 644;
+
+    '/etc/samba/smb.conf':
+      content => template('bootserver_samba/smb.conf'),
+      mode    => 0644;
   }
 
   exec {
