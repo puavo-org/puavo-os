b3f889b4ce825e84693516c5c29c05f49e2d0861
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2016-09-01 13:27:52 +0300

    Add module for helping with puavo-pkg installations.

---
+++ parts/rules/rules/puppet/puavo_pkg/manifests/init.pp
@@ -0,0 +1,21 @@
+class puavo_pkg {
+  include packages
+
+  $pkgbasedir = '/usr/share/puavo-pkg/packages'
+
+  # This is the default PUAVO_PKG_ROOTDIR for puavo-pkg that can be changed
+  # in /etc/puavo-pkg/puavo-pkg.conf.
+  $pkgrootdir = '/var/lib/puavo-pkg/packages/skype'
+
+  define install {
+    $pkgname = $title
+
+    exec {
+      "/usr/sbin/puavo-pkg install ${pkgbasedir}/${pkgname}.tar.gz":
+        creates => "${pkgrootdir}/packages/${pkgname}/installed";
+        require => Package['puavo-pkg'];
+    }
+  }
+
+  Package <| title == puavo-pkg |>
+}
