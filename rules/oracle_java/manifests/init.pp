06a29862cd0c5a688f33c79fd19778a727205967
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2016-09-06 14:55:17 +0300

    Realize oracle-java puavo-pkg, do not define it directly.

--- rules/oracle_java/manifests/init.pp
+++ rules/oracle_java/manifests/init.pp
@@ -1,11 +1,10 @@
 class oracle_java {
   include oracle_java::no_revocation_check,
           oracle_java::tools,
-	  puavo_pkg
+	  puavo_pkg::packages
 
-  puavo_pkg::install {
-    'oracle-java':
-       require => [ File['/opt/java/opinsys-add-cert']
-		  , File['/opt/java/opinsys-create-signed-ruleset'] ];
+  Puavo_pkg::Install <| title == 'oracle-java' |> {
+     require +> [ File['/opt/java/opinsys-add-cert']
+		, File['/opt/java/opinsys-create-signed-ruleset'] ]
   }
 }
