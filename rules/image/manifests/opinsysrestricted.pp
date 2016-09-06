47e249dc034052093ed55cf913cbdabb176194ff
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2016-09-06 15:20:55 +0300

    Try to install all available puavo-pkg packages for image::opinsysrestricted.

--- rules/image/manifests/opinsysrestricted.pp
+++ rules/image/manifests/opinsysrestricted.pp
@@ -1,5 +1,9 @@
 class image::opinsysrestricted {
   include ::image::allinone,
           ::oracle_java,
+          ::puavo_pkg::packages,
           ::ssh_server
+
+  # Realize all available puavo-pkg packages
+  Puavo_pkg::Install <| |>
 }
