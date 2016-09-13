12ee339bc81f898741edcf29cc945157b3407324
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2016-09-13 14:38:09 +0300

    Disable mirror.opinsys.fi until it has i386-architecture packages.

--- .opinsys/defaults.mk
+++ .opinsys/defaults.mk
@@ -1,3 +1,4 @@
-debootstrap_mirror	:= http://mirror.opinsys.fi/debian/
+# XXX re-enable our mirror once it has i386-arch for Debian Jessie/Stretch
+# debootstrap_mirror	:= http://mirror.opinsys.fi/debian/
 image_class		:= opinsysrestricted
 rootfs_dir		:= /virtualtmp/opinsys-os/rootfs
