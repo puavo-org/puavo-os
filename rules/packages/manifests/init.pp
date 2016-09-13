445a6a27834c5571adb8b868d63d0521047a798b
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2016-09-13 16:04:39 +0300

    Install some i386-packages into amd64-environment.

--- rules/packages/manifests/init.pp
+++ rules/packages/manifests/init.pp
@@ -708,4 +708,54 @@ class packages {
     , 'lsb-core' ]: # google-earth
       tag => [ 'tag_debian', 'tag_required-by-restricted' ];
   }
+
+  # i386-support packages for the amd64-architecture.
+  # There are explicit or found-by-trial -dependencies of the following
+  # software packages: adobereader-enu, skype, smartboard.
+  if $architecture == 'amd64' {
+    @package {
+      [ 'debianutils:i386'
+      , 'init-system-helpers:i386'
+      , 'libasound2:i386'
+      , 'libasound2-plugins:i386'
+      , 'libbluetooth3:i386'
+      , 'libc6:i386'
+      , 'libcap-ng0:i386'
+      , 'libcurl3:i386'
+      , 'libfontconfig1:i386'
+      , 'libfreetype6:i386'
+      , 'libgcc1:i386'
+      , 'libgl1-mesa-glx:i386'
+      , 'libglib2.0-0:i386'
+      , 'libgtk2.0-0:i386'
+      , 'libice6:i386'
+      , 'libnspr4-0d:i386'
+      , 'libpulse0:i386'
+      , 'libqt4-dbus:i386'
+      , 'libqt4-network:i386'
+      , 'libqt4-xml:i386'
+      , 'libqtcore4:i386'
+      , 'libqtgui4:i386'
+      , 'libqtwebkit4:i386'
+      , 'libselinux1:i386'
+      , 'libsm6:i386'
+      , 'libssl1.0.0:i386'
+      , 'libstdc++6:i386'
+      , 'libudev0:i386'
+      , 'libudev1:i386'
+      , 'libuuid1:i386'
+      , 'libx11-6:i386'
+      , 'libxext6:i386'
+      , 'libxinerama1:i386'
+      , 'libxkbfile1:i386'
+      , 'libxml2:i386'
+      , 'libxrender1:i386'
+      , 'libxss1:i386'
+      , 'libxtst6:i386'
+      , 'libxv1:i386'
+      , 'lsb-base:i386'
+      , 'zlib1g:i386' ]:
+        ensure => present;
+    }
+  }
 }
