ec1355732d5037824c3b58de5b66fb4da8be5b4d
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2016-09-14 11:25:40 +0300

    Add a note on ekapeli-alku distribution rights.

--- rules/image/manifests/opinsysrestricted.pp
+++ rules/image/manifests/opinsysrestricted.pp
@@ -6,4 +6,14 @@ class image::opinsysrestricted {
 
   # Realize all available puavo-pkg packages
   Puavo_pkg::Install <| |>
+
+  # Notes on puavo-pkg distribution in images:
+  #
+  #   ----------------------------------------------------------------------
+  #   ekapeli-alku:
+  #     On 21.5.2015, Ville Mönkkönen, on a private email said:
+  #
+  #     "Voitte toki jaella peliä edelleen käyttäjille, kunhan ette erikseen
+  #      pyydä käyttäjiltä rahaa pelin jakelusta."
+  #   ----------------------------------------------------------------------
 }
