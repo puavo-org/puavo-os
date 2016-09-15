3f429652cb2d3a21304925e4179b4a18e1ad9afd
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2016-09-15 12:49:55 +0300

    Move notes on opinsys-os distribution rights to README.md.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Puavo OS
+# Opinsys OS
 
 To install build tools on Debian Jessie, run:
 
@@ -11,3 +11,13 @@ To build Puavo OS image, run:
     make rootfs-image
 
 Run `make help` to get help.
+
+
+## Notes on distribution rights (to Opinsys)
+
+### Ekapeli-alku
+
+On 21.5.2015, Ville Mönkkönen, on a private email said:
+
+> Voitte toki jaella peliä edelleen käyttäjille, kunhan ette erikseen
+> pyydä käyttäjiltä rahaa pelin jakelusta.
--- rules/image/manifests/opinsysrestricted.pp
+++ rules/image/manifests/opinsysrestricted.pp
@@ -6,14 +6,4 @@ class image::opinsysrestricted {
 
   # Realize all available puavo-pkg packages
   Puavo_pkg::Install <| |>
-
-  # Notes on puavo-pkg distribution in images:
-  #
-  #   ----------------------------------------------------------------------
-  #   ekapeli-alku:
-  #     On 21.5.2015, Ville Mönkkönen, on a private email said:
-  #
-  #     "Voitte toki jaella peliä edelleen käyttäjille, kunhan ette erikseen
-  #      pyydä käyttäjiltä rahaa pelin jakelusta."
-  #   ----------------------------------------------------------------------
 }
