a8a203c415e969562e9636e4c6735121d2e08966
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2016-09-22 16:35:00 +0300

    Fix oracle_java puppet module for Stretch.

--- rules/oracle_java/manifests/tools.pp
+++ rules/oracle_java/manifests/tools.pp
@@ -3,24 +3,24 @@ class oracle_java::tools {
 
   file {
     '/etc/puavo-external-files-actions.d/java':
-      mode   => 755,
+      mode   => '0755',
       source => 'puppet:///modules/oracle_java/puavo-external-files-actions.d/java';
 
     '/opt/java':
       ensure => directory;
 
     '/opt/java/opinsys-add-cert':
-      mode   => 0700,
+      mode   => '0700',
       source => 'puppet:///modules/oracle_java/opinsys-add-cert';
 
     '/opt/java/opinsys-create-signed-ruleset':
-      mode    => 0700,
+      mode    => '0700',
       require => [ File['/opt/java/opinsys.keystore']
 		 , File['/opt/java/ruleset.xml'] ],
       source  => 'puppet:///modules/oracle_java/opinsys-create-signed-ruleset';
 
     '/opt/java/opinsys.keystore':
-      mode   => 0600,
+      mode   => '0600',
       source => 'puppet:///modules/oracle_java/opinsys.keystore';
 
     '/opt/java/ruleset.xml':
