3a01110fec388031fc365254e40c3a6c490acb13
Tuomas Räsänen <tuomasjjrasanen@tjjr.fi>
2016-09-27 10:52:16 +0000

    bootserver_ddns: add unifi configuration

--- rules/bootserver_ddns/manifests/init.pp
+++ rules/bootserver_ddns/manifests/init.pp
@@ -4,11 +4,18 @@ class bootserver_ddns {
     fail("ltsp_iface_ip fact is missing")
   }
 
+  exec {
+    'ensure ubnt.conf exists':
+      command => '/bin/echo \#option ubnt.unifi-address xxx.xxx.xxx.xxx; >/etc/dhcp/ubnt.conf',
+      onlyif  => '/usr/bin/test ! -e /etc/dhcp/ubnt.conf';
+  }
+
   file {
     '/etc/dhcp/dhcpd.conf':
       notify  => Service['isc-dhcp-server'],
-      content => template('bootserver_ddns/dhcpd.conf');
-
+      content => template('bootserver_ddns/dhcpd.conf'),
+      require => Exec['ensure ubnt.conf exists'];
+    
     '/etc/dnsmasq.conf':
       notify  => Service['dnsmasq'],
       content => template('bootserver_ddns/dnsmasq.conf');
