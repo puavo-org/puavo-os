8e7b61073603b30c57acf7e66f69df553a333cdc
Tuomas Räsänen <tuomasjjrasanen@tjjr.fi>
2016-10-03 12:36:59 +0000

    bootserver_ddns: configure the whole DDNS stack

--- rules/bootserver_ddns/manifests/init.pp
+++ rules/bootserver_ddns/manifests/init.pp
@@ -1,7 +1,58 @@
 class bootserver_ddns {
 
+  $arpazone = '10.in-addr.arpa'
+  $ddns_key = '/etc/dhcp/ddns-keys/nsupdate.key'
+
   if $ltsp_iface_ip == undef {
     fail('ltsp_iface_ip fact is missing')
+  }
+
+  define zonefile() {
+    $zone               = $title
+    $zonefile           = "/var/lib/bind/${zone}"
+    $zonefile_by_puppet = "${zonefile}.by_puppet"
+
+    exec {
+      "reset zone ${zone}":
+        command     => "/usr/local/lib/reset-zone '${zonefile_by_puppet}' '${zonefile}'",
+        creates     => $zonefile,
+        notify      => Service['bind9'],
+        require     => [ File[$zonefile_by_puppet]
+                       , Package['bind9utils'] ];
+    }
+
+    file {
+      $zonefile:
+        group   => 'bind',
+        mode    => 0644,
+        owner   => 'bind',
+        require => Exec["reset zone ${zone}"];
+
+      $zonefile_by_puppet:
+        content => template("bootserver_ddns/${zone}"),
+        mode    => 0644;
+    }
+  }
+
+  define scriptfile() {
+    file {
+        "/usr/local/lib/${title}":
+          content => template("bootserver_ddns/${title}"),
+          mode    => 0755;
+    }
+  }
+
+  ::bootserver_ddns::scriptfile {
+    [ 'create-dummy-ubnt-conf'
+    , 'create-ddns-key'
+    , 'reset-zone']:
+      ;
+  }
+
+  ::bootserver_ddns::zonefile {
+    [ 'puavo_domain'
+    , 'puavo_domain_reverse' ]:
+      ;
   }
 
   exec {
@@ -9,19 +60,40 @@ class bootserver_ddns {
       command => '/usr/local/lib/create-dummy-ubnt-conf',
       creates => '/etc/dhcp/ubnt.conf',
       require => Package['isc-dhcp-server'];
+
+    'create ddns key':
+      command => "/usr/local/lib/create-ddns-key '${ddns_key}'",
+      creates => $ddns_key;
   }
 
   file {
-    '/usr/local/lib/create-dummy-ubnt-conf':
-      content => template('bootserver_ddns/create-dummy-ubnt-conf'),
-      mode    => 0755;
+    $ddns_key:
+      group   => 'dhcpd',
+      mode    => 0640,
+      require => Exec['create ddns key'];
 
     '/etc/apparmor.d/local/usr.sbin.dhcpd':
       content => template('bootserver_ddns/dhcpd.apparmor'),
       mode    => 0644,
       notify  => Service['apparmor'],
       require => Package['apparmor'];
-    
+
+    '/etc/bind/named.conf.local':
+      content => template('bootserver_ddns/named.conf.local'),
+      notify  => Service['bind9'];
+
+    '/etc/bind/named.conf.options':
+      content => template('bootserver_ddns/named.conf.options'),
+      notify  => Service['bind9'];
+
+    '/etc/bind/nsupdate.key':
+      group   => 'bind',
+      mode    => 0640,
+      notify  => Service['bind9'],
+      require => [ File[$ddns_key]
+                 , Package['bind9'] ],
+      source  => "file://${ddns_key}";
+
     '/etc/dhcp/dhcpd.conf':
       content => template('bootserver_ddns/dhcpd.conf'),
       notify  => Service['isc-dhcp-server'],
@@ -33,9 +105,10 @@ class bootserver_ddns {
       notify  => Service['dnsmasq'],
       require => Package['dnsmasq'];
   }
-  
+
   package {
     [ 'bind9'
+    , 'bind9utils'
     , 'dnsmasq'
     , 'isc-dhcp-server' ]:
       ensure => present;
@@ -48,11 +121,12 @@ class bootserver_ddns {
 
     'dnsmasq':
       enable => true,
-      ensure => 'running';
+      ensure => 'running',
+      require => Service['bind9'];
 
     'isc-dhcp-server':
       enable => true,
-      ensure => 'running';
+      ensure => 'running',
+      require => [ Service['bind9'], Service['dnsmasq'] ];
   }
-
 }
