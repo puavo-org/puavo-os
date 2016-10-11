15e94097ecc46dcfebb0ed9b127b27d23c459d58
Tuomas Räsänen <tuomasjjrasanen@tjjr.fi>
2016-10-11 20:42:00 +0000

    bootserver_helpers: add puavo-bootserver-last-user-sessions

--- rules/bootserver_helpers/manifests/init.pp
+++ rules/bootserver_helpers/manifests/init.pp
@@ -3,5 +3,9 @@ class bootserver_helpers {
     '/usr/local/bin/puavo-bootserver-list-clients':
       content => template('bootserver_helpers/puavo-bootserver-list-clients'),
       mode    => 0755;
+
+    '/usr/local/sbin/puavo-bootserver-last-user-sessions':
+      content => template('bootserver_helpers/puavo-bootserver-last-user-sessions'),
+      mode    => 0755;
   }
 }
