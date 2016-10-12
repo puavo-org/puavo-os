ef113cce2beb9dcfd8a003eeb4506b744e5b79bc
Tuomas Räsänen <tuomasjjrasanen@tjjr.fi>
2016-10-12 09:18:29 +0000

    bootserver_ltspimages: manage netboot image service

---
+++ rules/bootserver_ltspimages/manifests/init.pp
@@ -0,0 +1,22 @@
+class bootserver_ltspimages {
+  include bootserver_nginx
+
+  bootserver_nginx::enable { 'ltsp-images': ; }
+
+  exec {
+    '/usr/sbin/puavo-bootserver-generate-nbd-exports':
+      loglevel => debug,
+      require  => File['/opt/ltsp/images'];
+  }
+
+  exec {
+    '/usr/sbin/puavo-bootserver-update-tftpboot --prune':
+      loglevel => debug,
+      require  => File['/opt/ltsp/images'];
+  }
+
+  file {
+    [ '/opt/ltsp', '/opt/ltsp/images', ]:
+      ensure => directory;
+  }
+}
