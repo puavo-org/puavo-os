b67c8ea5bc79f6b158dfbc565a8bb9df181864ff
Tuomas Räsänen <tuomasjjrasanen@tjjr.fi>
2016-10-20 08:55:51 +0000

    bootserver_vpn: modify init script to support reloading one VPN connection at a time

--- rules/bootserver_vpn/manifests/init.pp
+++ rules/bootserver_vpn/manifests/init.pp
@@ -17,7 +17,7 @@ class bootserver_vpn {
     'altvpn1':
       ensure  => running,
       require => File['/etc/openvpn/altvpn1.conf'],
-      restart => '/usr/sbin/service openvpn restart altvpn1',
+      restart => '/usr/sbin/service openvpn reload altvpn1',
       start   => '/usr/sbin/service openvpn start altvpn1',
       status  => '/usr/sbin/service openvpn status altvpn1',
       stop    => '/usr/sbin/service openvpn stop altvpn1';
@@ -26,7 +26,7 @@ class bootserver_vpn {
       ensure  => running,
       require => [ File['/etc/openvpn/puavo.conf']
                  , File['/usr/local/lib/puavo-openvpn-up'] ],
-      restart => '/usr/sbin/service openvpn restart puavo',
+      restart => '/usr/sbin/service openvpn reload puavo',
       start   => '/usr/sbin/service openvpn start puavo',
       status  => '/usr/sbin/service openvpn status puavo',
       stop    => '/usr/sbin/service openvpn stop puavo';
