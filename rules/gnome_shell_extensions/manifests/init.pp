d6a49cda7dc337bbdd4ed89ab975f2a14d275d50
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2016-10-28 10:26:28 +0300

    Match puppet directory with extension name (bigtouch-ux@puavo.org).

--- rules/gnome_shell_extensions/manifests/init.pp
+++ rules/gnome_shell_extensions/manifests/init.pp
@@ -3,9 +3,9 @@ class gnome_shell_extensions {
 
   file {
     '/usr/share/gnome-shell/extensions/bigtouch-ux@puavo.org':
-      source  => 'puppet:///modules/gnome_shell_extensions/bigtouch-ux',
       recurse => true,
-      require => Package['gnome-shell-extensions'];
+      require => Package['gnome-shell-extensions'],
+      source  => 'puppet:///modules/gnome_shell_extensions/bigtouch-ux@puavo.org';
 
   }
 
