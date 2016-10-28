b6413e991ed6a2054074efaabf8b3b0350c2f87a
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2016-10-28 10:49:58 +0300

    Add a puppet type for adding a gnome-extension.

--- rules/gnome_shell_extensions/manifests/init.pp
+++ rules/gnome_shell_extensions/manifests/init.pp
@@ -1,12 +1,20 @@
 class gnome_shell_extensions {
   include packages
 
-  file {
-    '/usr/share/gnome-shell/extensions/bigtouch-ux@puavo.org':
-      recurse => true,
-      require => Package['gnome-shell-extensions'],
-      source  => 'puppet:///modules/gnome_shell_extensions/bigtouch-ux@puavo.org';
+  define add_extension () {
+    $extension = $title
 
+    file {
+      "/usr/share/gnome-shell/extensions/${extension}":
+	recurse => true,
+	require => Package['gnome-shell-extensions'],
+	source  => "puppet:///modules/gnome_shell_extensions/${extension}";
+    }
+  }
+
+
+  ::gnome_shell_extensions::add_extension {
+    'bigtouch-ux@puavo.org': ;
   }
 
   Package <| title == gnome-shell-extensions |>
