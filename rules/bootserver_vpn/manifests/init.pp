63abdd44eb08738f1e5d8a5881cf691c74b8fc14
Tuomas Räsänen <tuomasjjrasanen@tjjr.fi>
2016-11-02 10:48:35 +0000

    bootserver_vpn: simplify service configuration
    Ubuntu Trusty's Pupppet (3.4.3) cannot handle service declarations
    whose title does not map to existing upstart configuration file. I.e.
    
    Service named altvpn1 would require /etc/init/altvpn1.conf to
    exist. Which is bullshit, but meh, let's just notify the whole openvpn
    service whenever any vpn configuration file changes.

--- rules/bootserver_vpn/manifests/init.pp
+++ rules/bootserver_vpn/manifests/init.pp
@@ -4,35 +4,21 @@ class bootserver_vpn {
   file {
     '/etc/openvpn/altvpn1.conf':
       content => template('bootserver_vpn/altvpn1.conf'),
-      notify  => Service['altvpn1'];
+      notify  => Service['openvpn'];
 
     '/etc/openvpn/puavo.conf':
       content => template('bootserver_vpn/puavo.conf'),
-      notify  => Service['vpn1'];
+      notify  => Service['openvpn'];
 
     '/usr/local/lib/puavo-openvpn-up':
       content => template('bootserver_vpn/puavo-openvpn-up'),
       mode    => 0755,
-      notify  => Service['vpn1'];
+      notify  => Service['openvpn'];
   }
 
   service {
-    'altvpn1':
-      ensure  => running,
-      require => File['/etc/openvpn/altvpn1.conf'],
-      restart => '/usr/sbin/service openvpn reload altvpn1',
-      start   => '/usr/sbin/service openvpn start altvpn1',
-      status  => '/usr/sbin/service openvpn status altvpn1',
-      stop    => '/usr/sbin/service openvpn stop altvpn1';
-
-    'vpn1':
-      ensure  => running,
-      require => [ File['/etc/openvpn/puavo.conf']
-                 , File['/usr/local/lib/puavo-openvpn-up'] ],
-      restart => '/usr/sbin/service openvpn reload puavo',
-      start   => '/usr/sbin/service openvpn start puavo',
-      status  => '/usr/sbin/service openvpn status puavo',
-      stop    => '/usr/sbin/service openvpn stop puavo';
+    'openvpn':
+      enable => true,
+      ensure => running;
   }
-
 }
