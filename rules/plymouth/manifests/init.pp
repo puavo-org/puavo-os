7a4ec9453e55eb085f6a78b513a850d7be785fbc
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2016-11-04 10:38:54 +0200

    Add a type ::plymouth::install_theme, and use it to install "kites".

--- rules/plymouth/manifests/init.pp
+++ rules/plymouth/manifests/init.pp
@@ -1,19 +1,30 @@
 class plymouth {
   include ::packages
 
+  $default_theme = 'kites'
+
+  define install_theme () {
+    $theme_name = $title
+
+    file {
+      "/usr/share/plymouth/themes/${theme_name}":
+        notify  => Exec['plymouth::set-default-theme'],
+        recurse => true,
+        require => Package['plymouth'],
+        source  => "puppet:///modules/plymouth/theme/${theme_name}";
+    }
+
+  }
+
   exec {
     'plymouth::set-default-theme':
-      command     => '/usr/sbin/plymouth-set-default-theme -R kites',
+      command     => "/usr/sbin/plymouth-set-default-theme -R ${default_theme}",
       refreshonly => true,
       require     => Package['plymouth'];
   }
 
-  file {
-    '/usr/share/plymouth/themes/kites':
-      notify  => Exec['plymouth::set-default-theme'],
-      recurse => true,
-      require => Package['plymouth'],
-      source  => 'puppet:///modules/plymouth/theme/kites';
+  ::plymouth::install_theme {
+    'kites': ;
   }
 
   Package <| title == plymouth |>
