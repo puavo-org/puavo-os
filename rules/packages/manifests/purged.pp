aa77d608667f5cf07341ee16923711a36b27bf46
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2016-11-07 15:23:37 +0200

    To preserve space, keep various lilypond-doc* and texlive-*-doc packages out.  (Of course these are very nice, but very few need these, documentation can be looked up from the internet, and they take too much space).

--- rules/packages/manifests/purged.pp
+++ rules/packages/manifests/purged.pp
@@ -11,10 +11,25 @@ class packages::purged {
     # (runs dpkg-query without speed considerations)
     [ 'im-config'
 
+    , 'lilypond-doc'
+    , 'lilypond-doc-html'
+    , 'lilypond-doc-pdf'
+
     , 'linux-image-generic'             # we want to choose kernels explicitly
 
     , 'samba'				# not needed, gets into system as
 					# some recommendation through winbind
+
+    , 'software-properties-gtk'
+    , 'synaptic'
+
+    , 'texlive-fonts-extra-doc'
+    , 'texlive-fonts-recommended-doc'
+    , 'texlive-latex-base-doc'
+    , 'texlive-latex-extra-doc'
+    , 'texlive-latex-recommended-doc'
+    , 'texlive-pictures-doc'
+    , 'texlive-pstricks-doc'
 
     , 'tftpd-hpa'               # this is suggested by ltsp-server, but
                                 # we do not actually use tftpd on ltsp-server
