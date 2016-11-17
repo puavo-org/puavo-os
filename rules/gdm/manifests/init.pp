4cc869ef2b7694551f5c7ddda0b5bca0a552292d
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2016-11-17 12:35:56 +0200

    Setup xsessions_lock puavo-conf script that, depending on puavo.xsessions.locked value, locks the available xsessions to "puavo-desktop" or not.

--- rules/gdm/manifests/init.pp
+++ rules/gdm/manifests/init.pp
@@ -1,3 +1,12 @@
 class gdm {
-  # ...
+  include ::packages
+  include ::puavo_conf
+
+  ::puavo_conf::script {
+    'xsessions_lock':
+      require => Package['puavo-ltsp-client'],
+      source  => 'puppet:///modules/gdm/xsessions_lock';
+  }
+
+  Package <| title == puavo-ltsp-client |>
 }
