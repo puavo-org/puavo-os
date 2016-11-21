10da832c11169cfbc62e62e560732d0da23b917d
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2016-11-21 13:17:04 +0200

    Set default release in /etc/apt/apt.conf.d/00default-release.

--- rules/apt/manifests/init.pp
+++ rules/apt/manifests/init.pp
@@ -31,4 +31,9 @@ class apt {
       command     => '/usr/bin/apt-get update',
       refreshonly => true;
   }
+
+  file {
+    '/etc/apt/apt.conf.d/00default-release':
+      content => "APT::Default-Release \"${debianversioncodename}\";\n";
+  }
 }
