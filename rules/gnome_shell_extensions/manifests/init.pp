d179b74b2d4b74a097b86fa828591b6f8dabb31f
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2016-11-28 12:55:02 +0200

    Disable "TaskBar@zpydr" gnome-shell extension.  It triggers some bug when used in conjunction with user-theme@gnome-shell-extensions.gcampax.github.com on some hosts. Typically on slower hosts, where login times might grow up to 300%.

--- rules/gnome_shell_extensions/manifests/init.pp
+++ rules/gnome_shell_extensions/manifests/init.pp
@@ -12,13 +12,14 @@ class gnome_shell_extensions {
     }
   }
 
+  # XXX disabled 'TaskBar@zpydr', triggers a bug in some hosts when used with
+  # XXX user-theme@gnome-shell-extensions.gcampax.github.com
 
   ::gnome_shell_extensions::add_extension {
     [ 'appindicatorsupport@rgcjonas.gmail.com'
     , 'bigtouch-ux@puavo.org'
     , 'bottompanel@tmoer93'
     , 'Move_Clock@rmy.pobox.com'
-    , 'TaskBar@zpydr'
     , 'webmenu@puavo.org' ]:
       ;
   }
