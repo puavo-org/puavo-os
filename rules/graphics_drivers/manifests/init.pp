e9524b8f1ed72b07fdc18ce55141e4f8bed6de86
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2017-01-02 09:22:03 +0200

    Re-enable nvidia 304 and 340 now that Debian Stretch has updated packages.

--- rules/graphics_drivers/manifests/init.pp
+++ rules/graphics_drivers/manifests/init.pp
@@ -7,8 +7,8 @@ class graphics_drivers {
   $nvidia_packages = $debianversioncodename ? {
                        'jessie' => [ 'nvidia-304xx-kernel-dkms'
                                    , 'nvidia-340xx-kernel-dkms' ],
-                       default  => [ # XXX broken 'nvidia-304xx-kernel-dkms'
-                                     'nvidia-340xx-kernel-dkms'
+                       default  => [ 'nvidia-304xx-kernel-dkms'
+                                   , 'nvidia-340xx-kernel-dkms'
                                    , 'nvidia-375xx-kernel-dkms' ],
                      }
 
