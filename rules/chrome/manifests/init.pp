3055ed62135fc8ec2d90baadc851e1194aa4c2b3
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2017-01-26 12:16:27 +0200

    Include the "chrome" puppet module as well.  Does not install chrome in itself, but if chrome is installed, it does affect its startup behaviour.

--- rules/chrome/manifests/init.pp
+++ rules/chrome/manifests/init.pp
@@ -5,12 +5,13 @@ class chrome {
   dpkg::simpledivert { '/usr/bin/google-chrome-stable': ; }
 
   file {
+    # disable this (does no harm if Chrome is not installed)
     '/etc/apt/sources.list.d/google-chrome.list':
-      require => Package['google-chrome-stable'],
-      source  => 'puppet:///modules/chrome/google-chrome.list';
+      source => 'puppet:///modules/chrome/google-chrome.list';
 
     '/usr/bin/google-chrome-stable':
-      mode   => '0755',
-      source => 'puppet:///modules/chrome/google-chrome-stable';
+      mode    => '0755',
+      require => Dpkg::Simpledivert['/usr/bin/google-chrome-stable'],
+      source  => 'puppet:///modules/chrome/google-chrome-stable';
   }
 }
