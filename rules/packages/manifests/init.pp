d44815a743b4d71f1437621e3c72158a27d0c459
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2017-01-31 14:42:29 +0200

    Update kernel to 4.9.0.

--- rules/packages/manifests/init.pp
+++ rules/packages/manifests/init.pp
@@ -619,9 +619,9 @@ class packages {
       dkms_modules => $all_dkms_modules,
       package_name => 'linux-image-3.16.0-4-amd64';
 
-    '4.8.0-2-amd64':
+    '4.9.0-1-amd64':
       dkms_modules => $all_dkms_modules,
-      package_name => 'linux-image-4.8.0-2-amd64';
+      package_name => 'linux-image-4.9.0-1-amd64';
   }
 
   #
