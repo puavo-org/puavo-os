e8a1e642eb25d0a09556ccbd5e6ddc5c92109b09
Antti Sokero <antti.sokero@opinsys.fi>
2017-02-21 17:11:40 +0200

    Add Geos-puavo puppet rule.

--- rules/themes/manifests/init.pp
+++ rules/themes/manifests/init.pp
@@ -3,6 +3,10 @@ class themes {
     '/usr/share/themes':
       ensure => directory;
 
+    '/usr/share/themes/Geos-puavo':
+      recurse => true,
+      source  => 'puppet:///modules/themes/Geos-puavo';
+
     '/usr/share/themes/Puavo':
       recurse => true,
       source  => 'puppet:///modules/themes/Puavo';
