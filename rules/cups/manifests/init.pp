f660dd92eb4b27b100cddbdf10e0e444e166fe1e
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2017-03-13 13:14:35 +0200

    Add configuration file for cups-browsed.

--- rules/cups/manifests/init.pp
+++ rules/cups/manifests/init.pp
@@ -2,10 +2,15 @@ class cups {
   include ::packages
 
   file {
+    '/etc/cups/cups-browsed.conf':
+      require => Package['cups-browsed'],
+      source  => 'puppet:///modules/cups/cups-browsed.conf';
+
     '/etc/cups/cupsd.conf':
       require => Package['cups-daemon'],
       source  => 'puppet:///modules/cups/cupsd.conf';
   }
 
-  Package <| title == cups-daemon |>
+  Package <| title == cups-browsed
+          or title == cups-daemon |>
 }
