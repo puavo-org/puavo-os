d2e862da375d4ee391c44d966366ccd53820410f
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2017-03-17 09:57:25 +0200

    Startup puavo-remote-assistance-applet in gdm screen and enable the "appindicatorsupport@rgcjonas.gmail.com" gnome-shell extension so that it goes to the gnome-shell panel in gdm.

--- rules/gdm/manifests/init.pp
+++ rules/gdm/manifests/init.pp
@@ -2,15 +2,27 @@ class gdm {
   include ::packages
   include ::puavo_conf
 
+  exec {
+    '/usr/sbin/dpkg-reconfigure gdm3':
+      refreshonly => true;
+  }
+
   file {
     '/etc/gdm3/daemon.conf':
+      notify  => Exec['/usr/sbin/dpkg-reconfigure gdm3'],
       require => Package['gdm3'],
       source  => 'puppet:///modules/gdm/daemon.conf';
 
     '/etc/gdm3/greeter.dconf-defaults':
+      notify  => Exec['/usr/sbin/dpkg-reconfigure gdm3'],
       require => [ File['/usr/share/backgrounds/puavo-art']
                  , Package['gdm3'] ],
       source  => 'puppet:///modules/gdm/greeter.dconf-defaults';
+
+    '/usr/share/gdm/greeter/autostart/puavo-remote-assistance-applet.desktop':
+      ensure  => link,
+      require => [ Package['gdm3'], Package['puavo-ltsp-client'], ],
+      target  => '/etc/xdg/autostart/puavo-remote-assistance-applet.desktop';
   }
 
   ::puavo_conf::script {
