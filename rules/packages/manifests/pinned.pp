d2bdd324ae3ab3fd5a6c01fe4a315fd25efb3e14
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2017-04-07 11:05:53 +0300

    puavo-wlanap needs a particular (patched) versions of hostapd and vtun.

--- rules/packages/manifests/pinned.pp
+++ rules/packages/manifests/pinned.pp
@@ -16,9 +16,15 @@ class packages::pinned {
   }
 
   ::packages::pinned::pin_package {
-    # XXX puavo-wlanap needs a particular version of vtun (from Wheezy)
+    # puavo-wlanap needs a particular (patched) version of hostapd.
+    'hostapd':
+      taglist => [ 'tag_network', 'tag_debian', ],
+      version => '2:2.4-1+puavo1';
+
+    # puavo-wlanap needs a particular (patched) version of vtun
+    # (from Wheezy).
     'vtun':
       taglist => [ 'tag_network', 'tag_debian', ],
-      version => '3.0.2-4+b1';
+      version => '3.0.2-4+puavo1';
   }
 }
