02a44cf748a114db2a68394d4e8da08f74d740af
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2017-04-25 13:41:27 +0300

    Setup /etc/NetworkManager/NetworkManager.conf with puppet.

--- rules/network_manager/manifests/init.pp
+++ rules/network_manager/manifests/init.pp
@@ -1,3 +1,14 @@
 class network_manager {
   include ::network_manager::allow_modify
+  include ::packages
+
+  # Use dns=dnsmasq option so that
+  # puavo-vpn-client can work correctly.
+  file {
+    '/etc/NetworkManager/NetworkManager.conf':
+      require => Package['network-manager'],
+      source  => 'puppet:///modules/network_manager/NetworkManager.conf';
+  }
+
+  Package <| title == network-manager |>
 }
