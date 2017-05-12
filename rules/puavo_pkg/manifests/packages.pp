cade745ebd2fc79c1dc1bcaacfde947e6a7bd3b1
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2017-05-12 13:30:02 +0300

    Install "Ekapeli" with puavo-pkg.

--- rules/puavo_pkg/manifests/packages.pp
+++ rules/puavo_pkg/manifests/packages.pp
@@ -19,4 +19,10 @@ class puavo_pkg::packages {
 			, 'vstloggerpro' ]
 
   @puavo_pkg::install { $available_packages: ; }
+
+  # Ekapeli is Opinsys-only because the upstream files are not downloadable
+  # without the Ekapeli application and its UI, so we keep the upstream pack
+  # in our own repository.  (Or are they downloadable?  How?)
+  $opinsys_only_packages = [ 'ekapeli' ]
+  @puavo_pkg::install { $opinsys_only_packages: ; }
 }
