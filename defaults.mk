0406245e883a8d368ad0496e4a7955c5f5f166fe
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2017-11-10 12:22:31 +0200

    Our images are served from images.opinsys.fi.

--- defaults.mk
+++ defaults.mk
@@ -1,6 +1,7 @@
 # XXX re-enable our mirror once it has i386-arch for Debian Jessie/Stretch
 # debootstrap_mirror	:= http://mirror.opinsys.fi/debian/
 image_class		:= opinsys
+images_urlbase		:= https://images.opinsys.fi
 rootfs_dir		:= /virtualtmp/opinsys-os/rootfs
 upload_dir              := /srv/aptirepo/puavo-os
 upload_login            := aptirepo-http
