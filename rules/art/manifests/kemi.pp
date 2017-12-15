61fb98a91f2bae6d740f11a40640432ecd4ad47b
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2017-12-15 10:37:18 +0200

    Just formatting.

--- rules/art/manifests/kemi.pp
+++ rules/art/manifests/kemi.pp
@@ -3,10 +3,10 @@ class art::kemi {
     '/usr/share/kemi-art':
       ensure => directory;
 
-    '/usr/share/kemi-art/kemi-logo.png':
-      source  => 'puppet:///modules/art/kemi/kemi-logo.png';
-
     '/usr/share/kemi-art/kemi-login-logo.png':
-      source  => 'puppet:///modules/art/kemi/kemi-login-logo.png';
+      source => 'puppet:///modules/art/kemi/kemi-login-logo.png';
+
+    '/usr/share/kemi-art/kemi-logo.png':
+      source => 'puppet:///modules/art/kemi/kemi-logo.png';
   }
 }
--- rules/art/manifests/opinsys.pp
+++ rules/art/manifests/opinsys.pp
@@ -4,12 +4,12 @@ class art::opinsys {
       ensure => directory;
 
     '/usr/share/opinsys-art/logo16.png':
-      source  => 'puppet:///modules/plymouth/theme/opinsys/logo16.png';
+      source => 'puppet:///modules/plymouth/theme/opinsys/logo16.png';
 
     '/usr/share/opinsys-art/logo.png':
-      source  => 'puppet:///modules/plymouth/theme/opinsys/logo.png';
+      source => 'puppet:///modules/plymouth/theme/opinsys/logo.png';
 
     '/usr/share/opinsys-art/opinsys-support-info-fi.png':
-      source  => 'puppet:///modules/art/opinsys/opinsys-support-info-fi.png';
+      source => 'puppet:///modules/art/opinsys/opinsys-support-info-fi.png';
   }
 }
