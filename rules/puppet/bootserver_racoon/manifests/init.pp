33164c3dde411c6e3a36b2c34d0e802adbe930af
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2018-02-23 07:40:22 +0200

    Remove bootserver_racoon.

--- rules/puppet/bootserver_racoon/manifests/init.pp
+++
@@ -1,9 +0,0 @@
-class bootserver_racoon {
-  package {
-    'racoon':
-      ensure => purged; # IPSec was never configured properly and was
-                        # never used in production. Nevertheless,
-                        # racoon and all other ipsec-related stuff was
-                        # installed. Now we get rid of it.
-  }
-}
