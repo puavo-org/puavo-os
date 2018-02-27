093d6542ed5d15f4dcc03c9190aa0c1cdc6d6dbe
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2018-02-27 14:35:19 +0200

    Do not enable bootserver_backup for bootserver-image, but document what is broken.

--- rules/bootserver_backup/manifests/init.pp
+++ rules/bootserver_backup/manifests/init.pp
@@ -1,17 +1,17 @@
 class bootserver_backup {
-  include ::bootserver_config
+  fail('The templates in bootserver_backup need values from puavo-conf')
 
   file {
     '/root/.ssh':
       ensure => directory,
-      mode   => 700;
+      mode   => '0700';
 
     '/root/.ssh/authorized_keys2':
       content => template('bootserver_backup/authorized_keys2'),
-      mode    => 600;
+      mode    => '0600';
 
     '/root/run-rdiff-backup-server':
       content => template('bootserver_backup/run-rdiff-backup-server'),
-      mode    => 700;
+      mode    => '0700';
   }
 }
--- rules/image/manifests/bundle/bootserver.pp
+++ rules/image/manifests/bundle/bootserver.pp
@@ -1,5 +1,6 @@
 class image::bundle::bootserver {
   include ::bootserver_autopoweron
+  # include ::bootserver_backup         # XXX needs work
   include ::packages
 
   Package <| tag == tag_puavo_bootserver |>
