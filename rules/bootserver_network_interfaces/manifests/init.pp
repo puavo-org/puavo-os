96fd1841fb6dea3909b4fa5cd5298728407ad32a
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2018-03-09 07:55:43 +0200

    Do not setup network interfaces for other hosttype except bootserver.

--- rules/bootserver_network_interfaces/manifests/init.pp
+++ rules/bootserver_network_interfaces/manifests/init.pp
@@ -1,14 +1,19 @@
 class bootserver_network_interfaces {
+  include ::puavo_conf
+
   define setup_if_conf() {
     $interface_name = $title
 
     file {
-      "/etc/network/interfaces.d/${interface_name}":
+      "/etc/network/interfaces.bootserver.d/${interface_name}":
         content => template("bootserver_network_interfaces/etc/network/interfaces.d/${interface_name}");
     }
   }
 
   file {
+    '/etc/network/interfaces.bootserver.d':
+      ensure => directory;
+
     '/etc/network/if-up.d/disable_flow_control':
       content => template('bootserver_network_interfaces/etc/network/if-up.d/disable_flow_control'),
       mode    => '0755';
@@ -16,5 +21,10 @@ class bootserver_network_interfaces {
 
   ::bootserver_network_interfaces::setup_if_conf {
     [ 'lo', 'inet0', 'ltsp0', 'wlan0', ]: ;
+  }
+
+  ::puavo_conf::script {
+    'setup_network_interfaces':
+      source => 'puppet:///modules/bootserver_network_interfaces/setup_network_interfaces';
   }
 }
