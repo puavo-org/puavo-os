2605351871eaacddee96f8baffaf3278f346fc13
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2018-03-16 18:27:47 +0200

    Create and setup /etc/samba/cifs.keytab.

--- rules/bootserver_samba/manifests/init.pp
+++ rules/bootserver_samba/manifests/init.pp
@@ -4,20 +4,13 @@ class bootserver_samba {
 
   file {
     '/etc/pam.d/samba':
-      source => 'puppet:///modules/bootserver_samba/etc_pam.d_samba');
+      source => 'puppet:///modules/bootserver_samba/etc_pam.d_samba';
   }
 
   ::puavo_conf::script {
     'setup_samba':
-      source => 'puppet:///modules/bootserver_samba/setup_samba');
+      source => 'puppet:///modules/bootserver_samba/setup_samba';
   }
-
-  # XXX this should be done somehow
-  # exec {
-  #   'fetch cifs.keytab':
-  #     command => "/usr/bin/smbpasswd -w '${puavo_ldap_password}' && /usr/sbin/kadmin.local -q 'ktadd -norandkey -k /etc/samba/cifs.keytab cifs/${puavo_hostname}.${puavo_domain}'",
-  #     creates => '/etc/samba/cifs.keytab';
-  # }
 
   Package <| title == samba or title == winbind |>
 }
