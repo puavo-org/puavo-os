7b1bab06a0a095d0c1e2f0469832084d181b92ba
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2018-04-19 15:02:39 +0300

    Create a system-user for puavo-infotv at boot, also creating /home. Use uid number 998 for this.

--- rules/infotv/manifests/init.pp
+++ rules/infotv/manifests/init.pp
@@ -5,4 +5,9 @@ class infotv {
     'puavo-infotv.json':
       source => 'puppet:///modules/infotv/puavo-infotv.json';
   }
+
+  ::puavo_conf::script {
+    'setup_infotv':
+      source => 'puppet:///modules/infotv/setup_infotv';
+  }
 }
