c92977e470612c9735f31c6a9d20b0513fcd6d9d
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2018-04-20 12:18:35 +0300

    Reset guestuser home-directory every time there is nodm-login (as guest user).

--- rules/nodm/manifests/init.pp
+++ rules/nodm/manifests/init.pp
@@ -1,6 +1,12 @@
 class nodm {
   include ::puavo_conf
 
+  file {
+    '/etc/pam.d/nodm':
+      mode   => '0755',
+      source => 'puppet:///modules/nodm/etc_pam.d_nodm';
+  }
+
   # nodm configuration file for infotv sessions
   ::puavo_conf::script {
     'setup_nodm':
