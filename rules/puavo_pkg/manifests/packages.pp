70a66080a6f82ccc322ef5e1bef4478d514d91f5
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2018-05-14 12:21:03 +0300

    Fix loggerpro by installing three shared libraries from Ubuntu Trusty. Closes #380.

--- rules/puavo_pkg/manifests/packages.pp
+++ rules/puavo_pkg/manifests/packages.pp
@@ -1,5 +1,6 @@
 class puavo_pkg::packages {
-  include ::puavo_pkg
+  include ::puavo_pkg,
+          ::trusty_libs
 
   # NOTE! adobe-flashplugin and adobe-pepperflashplugin contain both
   # 32-bit and 64-bit versions
@@ -45,5 +46,11 @@ class puavo_pkg::packages {
 
   Puavo_pkg::Install['ohjelmointi-opetuksessa'] {
     require +> Puavo_pkg::Install['arduino-ide'],
+  }
+
+  Puavo_pkg::Install['vstloggerpro'] {
+    require +> [ ::Trusty_libs::Deb_unpack['/opt/trusty/lib/x64_64-linux-gnu/libcairomm-1.0.so.1']
+               , ::Trusty_libs::Deb_unpack['/opt/trusty/lib/x64_64-linux-gnu/libglibmm-2.4.so.1']
+               , ::Trusty_libs::Deb_unpack['/opt/trusty/lib/x64_64-linux-gnu/libgtkmm-2.4.so.1'] ],
   }
 }
