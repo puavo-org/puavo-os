70bac1d88b9c11669099874f709b0c19402e27b2
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2018-06-29 09:01:26 +0300

    Add "puavo-caching-syslog-sender", a helper for sending syslogs to remote server (with cache).  Designed to be used with rsyslog.

--- rules/syslog/manifests/init.pp
+++ rules/syslog/manifests/init.pp
@@ -5,6 +5,10 @@ class syslog {
     '/etc/rsyslog.conf':
       require => Package['rsyslog'],
       source  => 'puppet:///modules/syslog/rsyslog.conf';
+
+    '/usr/local/lib/puavo-caching-syslog-sender':
+      mode   => '0755',
+      source => 'puppet:///modules/syslog/puavo-caching-syslog-sender';
   }
 
   Package <| title == rsyslog |>
