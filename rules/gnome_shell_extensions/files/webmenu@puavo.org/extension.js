d7b693558960c476ecf63c2ea25012d1c2177e71
Jarmo Pietiläinen <jarmo@opinsys.fi>
2018-08-06 09:53:32 +0300

    PuavoMenu: fix argument order in the menu button extension

--- rules/gnome_shell_extensions/files/webmenu@puavo.org/extension.js
+++ rules/gnome_shell_extensions/files/webmenu@puavo.org/extension.js
@@ -43,11 +43,11 @@ function make_button(icon_name, icon_size, spawn_command, is_right) {
 
         let finalCmd = spawn_command.slice();  // slice=a new copy of the array
 
-        finalCmd.push("--pos=" + Math.ceil(x) + "," + Math.ceil(y));
         finalCmd.push("toggle");
         finalCmd.push("corner");
         finalCmd.push(Math.ceil(x).toString());
         finalCmd.push(Math.ceil(y).toString());
+        finalCmd.push("--pos=" + Math.ceil(x) + "," + Math.ceil(y));
         Util.spawn(finalCmd);
     }));
 
