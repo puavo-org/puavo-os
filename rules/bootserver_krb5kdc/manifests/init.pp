6b0b522960802c079742d19b86ecba7c9d25e3c3
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2018-08-29 13:17:17 +0300

    Start krb5-kdc after slapd is ready.  Throw away the sysv-init stuff that is no longer used.

--- rules/bootserver_krb5kdc/manifests/init.pp
+++ rules/bootserver_krb5kdc/manifests/init.pp
@@ -3,22 +3,19 @@ class bootserver_krb5kdc {
   include ::puavo_conf
 
   file {
-    '/etc/default/krb5-kdc':
-      require => Package['krb5-kdc'],
-      source  => 'puppet:///modules/bootserver_krb5kdc/etc_default_krb5-kdc';
+    '/etc/systemd/system/krb5-kdc.service.d':
+      ensure => directory;
 
-    '/etc/init.d/krb5-kdc':
-      mode    => '0755',
-      require => Package['krb5-kdc'],
-      source  => 'puppet:///modules/bootserver_krb5kdc/etc_init.d_krb5-kdc';
+    '/etc/systemd/system/krb5-kdc.service.d/override.conf':
+      source => 'puppet:///modules/bootserver_krb5kdc/krb5-kdc_override.conf';
 
-    # XXX what to do this this?
-    # '/etc/init/krb5-kdc.override':
-    #   source => 'puppet:///modules/bootserver_krb5kdc/krb5-kdc.override';
+    '/etc/logrotate.d/kdc':
+      require => Package['logrotate'],
+      source  => 'puppet:///modules/bootserver_krb5kdc/logrotate.conf';
 
-     '/etc/logrotate.d/kdc':
-       require => Package['logrotate'],
-       source => 'puppet:///modules/bootserver_krb5kdc/logrotate.conf';
+    '/usr/local/lib/puavo-kdc-wait-for-slapd':
+      mode   => '0755',
+      source => 'puppet:///modules/bootserver_krb5kdc/puavo-kdc-wait-for-slapd';
   }
 
   ::puavo_conf::script {
