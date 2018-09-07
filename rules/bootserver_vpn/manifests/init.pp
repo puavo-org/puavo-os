5d470b3a4128ba90c20d3a8f57b58e9ca4d03647
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2018-09-07 08:22:40 +0300

    The alternative vpn for bootservers is no more (maybe it should come back later, though?).

--- rules/bootserver_vpn/manifests/init.pp
+++ rules/bootserver_vpn/manifests/init.pp
@@ -2,9 +2,6 @@ class bootserver_vpn {
   include bootserver_config
 
   file {
-    '/etc/openvpn/altvpn1.conf':
-      content => template('bootserver_vpn/altvpn1.conf');
-
     '/etc/openvpn/puavo.conf':
       content => template('bootserver_vpn/puavo.conf');
 
