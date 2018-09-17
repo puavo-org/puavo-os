41cf9aa32ceb22183b004e15ea158b5ba1d7244f
Jarmo Pietiläinen <jarmo@opinsys.fi>
2018-09-17 13:35:45 +0300

    puavomenu: remove some unused imports

--- parts/puavomenu/main.py
+++ parts/puavomenu/main.py
@@ -19,10 +19,10 @@ import logger
 from constants import *
 from iconcache import ICONS48
 from buttons import ProgramButton, MenuButton
-from utils import localize, expand_variables, get_file_contents
+from utils import localize
 from utils_gui import load_image_at_size, create_separator
 from loader import load_menu_data
-from conditionals import evaluate_file, is_hidden
+from conditionals import evaluate_file
 from sidebar import Sidebar
 
 
