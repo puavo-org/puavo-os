63eb025a99cc4d5c1543bf1d14e5a4a8f59d4123
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2018-09-21 09:58:31 +0300

    Install grub-themes in grub_themes puppet module into /boot/grub/themes.

--- rules/grub_themes/manifests/init.pp
+++ rules/grub_themes/manifests/init.pp
@@ -1,15 +1,14 @@
-class themes {
-
+class grub_themes {
   file {
     '/boot/grub/themes':
       ensure => directory;
 
     '/boot/grub/themes/StylishDark':
       recurse => true,
-      source  => 'puppet:///modules/themes/StylishDark';
+      source  => 'puppet:///modules/grub_themes/StylishDark';
 
     '/boot/grub/themes/Vimix':
       recurse => true,
-      source  => 'puppet:///modules/themes/Vimix';
+      source  => 'puppet:///modules/grub_themes/Vimix';
   }
 }
