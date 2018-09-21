03c6fac013d694b1b0364ab6ed3c1a0a9828daf4
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2018-09-21 12:04:24 +0300

    Set grub theme from a puavo-conf variable puavo.grub.theme, "StylishDark" by default.

--- rules/grub_themes/manifests/init.pp
+++ rules/grub_themes/manifests/init.pp
@@ -1,4 +1,6 @@
 class grub_themes {
+  include ::puavo_conf
+
   file {
     [ '/boot', '/boot/grub', '/boot/grub/themes' ]:
       ensure => directory;
@@ -10,5 +12,10 @@ class grub_themes {
     '/boot/grub/themes/Vimix':
       recurse => true,
       source  => 'puppet:///modules/grub_themes/Vimix';
+  }
+
+  ::puavo_conf::definition {
+    'puavo-grub.json':
+      source => 'puppet:///modules/grub_themes/puavo-grub.json';
   }
 }
