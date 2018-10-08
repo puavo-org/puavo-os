d065973b42d818298f0a461f54b2e0c6e2ce6bf5
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2018-10-08 10:31:51 +0300

    Include both "tag_debian_nonfree" and "tag_debian_desktop_nonfree" on opinsys-images.

--- rules/image/manifests/opinsys.pp
+++ rules/image/manifests/opinsys.pp
@@ -13,5 +13,6 @@ class image::opinsys {
   # Realize all available puavo-pkg packages
   Puavo_pkg::Install <| |>
 
-  Package <| tag == 'tag_debian_nonfree' |>
+  Package <| tag == 'tag_debian_desktop_nonfree'
+          or tag == 'tag_debian_nonfree' |>
 }
