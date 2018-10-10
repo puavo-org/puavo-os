f2bec610c42fb0d6f8e07170b9f1ee3cdb2309a7
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2018-10-10 16:37:49 +0300

    Clean up "bootserver_firewall" puppet-module.

--- rules/bootserver_firewall/manifests/init.pp
+++ rules/bootserver_firewall/manifests/init.pp
@@ -2,19 +2,18 @@ class bootserver_firewall {
   include ::packages
   include ::puavo_conf
 
-  define conffile {
-    $filename = $title
-
-    file {
-      "/etc/shorewall/${filename}":
-        content => template("bootserver_firewall/etc_shorewall/${filename}"),
-        require => Package['shorewall'];
-    }
-  }
-
   file {
     '/etc/default/shorewall':
-      content => template('bootserver_firewall/etc_default_shorewall');
+      require => Package['shorewall'],
+      source  => 'puppet:///modules/bootserver_firewall/etc_default_shorewall';
+
+    '/etc/shorewall/Makefile':
+      require => Package['shorewall'],
+      source  => 'puppet:///modules/bootserver_firewall/etc_shorewall/Makefile';
+
+    '/etc/shorewall/shorewall.conf':
+      require => Package['shorewall'],
+      source  => 'puppet:///modules/bootserver_firewall/etc_shorewall/shorewall.conf';
   }
 
   ::puavo_conf::script {
@@ -23,19 +22,6 @@ class bootserver_firewall {
 
     'setup_firewall':
       source => 'puppet:///modules/bootserver_firewall/setup_firewall';
-  }
-
-  ::bootserver_firewall::conffile {
-    [ 'Makefile'
-    , 'hosts'
-    , 'interfaces'
-    , 'masq'
-    , 'policy'
-    , 'rules'
-    , 'shorewall.conf'
-    , 'tunnels'
-    , 'zones' ]:
-      ;
   }
 
   Package <| title == shorewall |>
