f3cff4fd5e1178db940eb434a9e38c9c42a4b894
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2018-10-25 13:51:11 +0300

    Install our own Opinsys-specific icons for puavo-desktop-applet.

--- rules/art/manifests/opinsys.pp
+++ rules/art/manifests/opinsys.pp
@@ -9,6 +9,12 @@ class art::opinsys {
     '/usr/share/opinsys-art/logo.png':
       source => 'puppet:///modules/plymouth/theme/opinsys/logo.png';
 
+    '/usr/share/opinsys-art/opinsys-applet-idle.png':
+      source => 'puppet:///modules/art/opinsys/opinsys-applet-idle.png';
+
+    '/usr/share/opinsys-art/opinsys-applet-remote-access-active.png':
+      source => 'puppet:///modules/art/opinsys/opinsys-applet-remote-access-active.png';
+
     '/usr/share/opinsys-art/opinsys-support-info-fi.png':
       source => 'puppet:///modules/art/opinsys/opinsys-support-info-fi.png';
   }
