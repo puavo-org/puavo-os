14ce0c3eb2d06095ec6e6a91872cded60f1a2576
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2019-01-25 10:23:10 +0200

    Fix puavo-pkg installation check in puppet rules.

--- rules/puavo_pkg/manifests/init.pp
+++ rules/puavo_pkg/manifests/init.pp
@@ -5,18 +5,18 @@ class puavo_pkg {
 
   # This is the default PUAVO_PKG_ROOTDIR for puavo-pkg that can be changed
   # in /etc/puavo-pkg/puavo-pkg.conf.
-  $pkgrootdir = '/var/lib/puavo-pkg/packages'
+  $pkgrootdir = '/var/lib/puavo-pkg'
 
   define install () {
     $pkgname        = $title
     $pkgpath        = "${puavo_pkg::pkgbasedir}/${pkgname}.tar.gz"
-    $installed_link = "${puavo_pkg::pkgrootdir}/${pkgname}/installed"
+    $installed_link = "${puavo_pkg::pkgrootdir}/installed/${pkgname}"
 
     exec {
       "/usr/sbin/puavo-pkg install ${puavo_pkg::pkgbasedir}/${pkgname}.tar.gz":
         require => Package['puavo-pkg'],
         timeout => 7200,	# 2 hours = 7200 seconds, should be enough
-        unless  => "/usr/bin/test \"\$(readlink \"${installed_link}\")\" = \"\$(md5sum \"${pkgpath}\" | awk '{ print \$1 }')\"";
+        unless  => "/usr/bin/test \"\$(readlink \"${installed_link}\" | xargs basename)\" = \"\$(tar --wildcards -Ozx -f \"${pkgpath}\" '*/.puavo-pkg-version' | awk '\$1 == \"package-id\" { print \$2 }')\"";
     }
   }
 
