3a153938d55459aa9ffe741ac90a5929d9ff4fb1
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2019-03-19 13:03:16 +0200

    Do not try to use Ubuntu apt repositories in Buster.

--- rules/apt/manifests/default_repositories.pp
+++ rules/apt/manifests/default_repositories.pp
@@ -3,7 +3,12 @@ class apt::default_repositories {
   include ::apt::multiarch
   include ::apt::nodejs
   include ::apt::repositories
-  include ::apt::ubuntu_repository
+
+  # XXX issues with using Ubuntu repositories in Buster
+  # XXX (could we drop them?)
+  case $debianversioncodename {
+    'stretch': { include ::apt::ubuntu_repository }
+  }
 
   $mirror = $mirror ? { undef => 'ftp.debian.org', default => "$mirror", }
   $securitymirror = 'security.debian.org'
