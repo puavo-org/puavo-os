5794194572515df8a8558445d80d67c01e50f670
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2019-04-03 10:52:25 +0300

    Remove "libjpeg8:i386" package, it is from Wheezy and apparently Robboscratch runs without it.

--- rules/packages/manifests/compat_32bit.pp
+++ rules/packages/manifests/compat_32bit.pp
@@ -28,7 +28,6 @@ class packages::compat_32bit {
       , 'libglib2.0-0:i386'
       , 'libgtk2.0-0:i386'
       , 'libice6:i386'
-      , 'libjpeg8:i386'                 # needed by RobboScratch2
       , 'libltdl7:i386'
       , 'libmp3lame0:i386'              # needed by mimio-studio
       , 'libnspr4-0d:i386'
