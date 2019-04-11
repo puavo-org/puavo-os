3efa2d3e37d37cb949d3dac097e3106367eeb4fa
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2019-04-11 07:58:22 +0300

    Rename puavo-trigger-nightly-updates -> puavo-nightly-updates and fix logger messages.

--- rules/nightly_updates/manifests/init.pp
+++ rules/nightly_updates/manifests/init.pp
@@ -2,10 +2,10 @@ class nightly_updates {
   include ::puavo_conf
 
   file {
-    '/usr/local/lib/puavo-trigger-nightly-updates':
+    '/usr/local/lib/puavo-nightly-updates':
       mode    => '0755',
       require => ::Puavo_conf::Definition['puavo-admin-nightly-updates.json'],
-      source  => 'puppet:///modules/nightly_updates/puavo-trigger-nightly-updates';
+      source  => 'puppet:///modules/nightly_updates/puavo-nightly-updates';
   }
 
   ::puavo_conf::definition {
