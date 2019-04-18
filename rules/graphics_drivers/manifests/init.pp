b35728ce383f38d03b290e0f9bf6e00214b36b99
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2019-04-18 15:52:21 +0300

    List nvidia drivers in Buster in ::graphics_drivers.

--- rules/graphics_drivers/manifests/init.pp
+++ rules/graphics_drivers/manifests/init.pp
@@ -4,17 +4,9 @@ class graphics_drivers {
 
   $glx_packages = [ 'glx-alternative-mesa', 'libgl1-mesa-glx', 'update-glx' ]
 
-  case $debianversioncodename {
-    'buster': {
-      # XXX update this list
-      $nvidia_packages = []
-    }
-    'stretch': {
-      $nvidia_packages = [ 'nvidia-legacy-304xx-kernel-dkms'
-                         , 'nvidia-legacy-340xx-kernel-dkms'
-                         , 'nvidia-kernel-dkms' ]
-    }
-  }
+  $nvidia_packages = [ 'nvidia-legacy-340xx-kernel-dkms'
+                     , 'nvidia-legacy-390xx-kernel-dkms'
+                     , 'nvidia-kernel-dkms' ]
 
   ::puavo_conf::script {
     'setup_graphics_drivers':
