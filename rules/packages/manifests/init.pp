bad48b766b30bd179be72d725f344287c9a6b028
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2019-05-14 08:04:49 +0300

    Upgrade backports kernel to 4.19.0-0.bpo.4-amd64.

--- rules/packages/manifests/init.pp
+++ rules/packages/manifests/init.pp
@@ -764,9 +764,9 @@ class packages {
       dkms_modules => $all_dkms_modules,
       package_name => 'linux-image-4.9.0-8-amd64';
 
-    '4.19.0-0.bpo.2-amd64':
+    '4.19.0-0.bpo.4-amd64':
       dkms_modules => [ $virtualbox_module ],
-      package_name => 'linux-image-4.19.0-0.bpo.2-amd64';
+      package_name => 'linux-image-4.19.0-0.bpo.4-amd64';
   }
 
   # Packages which are not restricted per se, but which are required by
