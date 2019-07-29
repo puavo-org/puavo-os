c9410d340fd6ddb27b453aa7b3abef953109fa55
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2019-07-29 16:03:01 +0300

    Install Firefox from the puavo-pkg package.
    The 64-bit version, one less Ubuntu dependency again.
    Not everything is ready yet, configurations are not right
    and old profile is not used, but we do not want to install the
    old version.

--- rules/firefox/manifests/init.pp
+++ rules/firefox/manifests/init.pp
@@ -1,20 +1,22 @@
 class firefox {
   include ::packages
+  include ::puavo_pkg::packages
 
   # Firefox configuration system is still a mess... if there really is a more
   # straightforward way, I would like to hear about it.
   file {
-    '/etc/apt/preferences.d/70-firefox.pref':
-      before  => Package['firefox:i386'],
-      content => template('firefox/70-firefox.pref');
-
     '/etc/firefox/puavodesktop.js':
       content => template('firefox/puavodesktop.js'),
-      require => Package['firefox:i386'];
+      require => Puavo_pkg::Install['firefox'];
 
     '/etc/firefox/syspref.js':
       content => template('firefox/syspref.js'),
       require => File['/usr/lib/firefox/firefox-puavodesktop.js'];
+
+    '/etc/puavo-external-files-actions.d/firefox':
+      content => template('firefox/puavo-external-files-actions.d/firefox'),
+      mode    => '0755',
+      require => Package['puavo-ltsp-client'];
 
     '/etc/X11/Xsession.d/48puavo-set-apiserver-envvar':
       content => template('firefox/48puavo-set-apiserver-envvar');
@@ -22,13 +24,8 @@ class firefox {
     '/usr/lib/firefox/firefox-puavodesktop.js':
       content => template('firefox/firefox-puavodesktop.js'),
       require => File['/etc/firefox/puavodesktop.js'];
-
-    '/etc/puavo-external-files-actions.d/firefox':
-      content => template('firefox/puavo-external-files-actions.d/firefox'),
-      mode    => '0755',
-      require => Package['puavo-ltsp-client'];
   }
 
-  Package <| title == "firefox:i386"
-          or title == puavo-ltsp-client |>
+  Package <| title == puavo-ltsp-client |>
+  Puavo_pkg::Install <| title == firefox |>
 }
