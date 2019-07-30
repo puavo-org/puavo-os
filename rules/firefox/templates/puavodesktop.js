84a1a72ba2a741e793b48b84f5124dcdd36e0fce
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2019-07-30 08:56:56 +0300

    Remove flashblock-related preferences from Firefox, these are no longer relevant as the Flashblock-plugin has not been in use for some years now.

--- rules/firefox/templates/puavodesktop.js
+++ rules/firefox/templates/puavodesktop.js
@@ -10,8 +10,6 @@ lockPref("network.dns.disableIPv6", true);
 lockPref("network.seer.enabled", false);
 lockPref("print.postscript.paper_size", "iso_a4");
 lockPref("toolkit.storage.synchronous", 1);
-pref("flashblock.html5video.blocked", false);
-pref("flashblock.whitelist", "elisaviihde.fi,rockway.fi,ugri.net,sanomapro.fi,pelastusopisto.fi,suomeasavelin.net,vetamix.net,vimeo.com,youtube.com,google.com,ksml.fi,quizlet.com,downloads.bbc.co.uk,play.spotify.com,satunetti.fi,fun4thebrain.com,veljeksethanhela.net,starfall.com,gapminder.org,openmatikka.fi,ihmisoikeuspeli.fi,yle.fi,ruutu.fi,twitter.com,facebook.com,dreambroker.com,twitch.tv,adobeconnect.com,yahoo.com,bing.com,mll.fi");
 
 homepage=getenv("HOMEPAGE");
 
