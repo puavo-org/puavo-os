43b5d165a1496c2d5c0044776ed46d48d82cf927
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2019-08-23 08:34:13 +0300

    /usr/local/lib/puavo-maybe-disable-drrs should be run when resuming from sleep.

--- rules/disable_drrs_conditionally/manifests/init.pp
+++ rules/disable_drrs_conditionally/manifests/init.pp
@@ -1,7 +1,6 @@
 class disable_drrs_conditionally {
   file {
-    [ '/etc/systemd/system/multi-user.target.wants/puavo-maybe-disable-drrs.service',
-      '/etc/systemd/system/sleep.target.wants/puavo-maybe-disable-drrs.service' ]:
+    '/etc/systemd/system/multi-user.target.wants/puavo-maybe-disable-drrs.service':
       ensure  => link,
       require => File['/etc/systemd/system/puavo-maybe-disable-drrs.service'],
       target  => '/etc/systemd/system/puavo-maybe-disable-drrs.service';
@@ -9,6 +8,10 @@ class disable_drrs_conditionally {
     '/etc/systemd/system/puavo-maybe-disable-drrs.service':
       require => File['/usr/local/lib/puavo-maybe-disable-drrs'],
       source  => 'puppet:///modules/disable_drrs_conditionally/puavo-maybe-disable-drrs.service';
+
+    '/lib/systemd/system-sleep/puavo-maybe-disable-drrs':
+      mode   => '0755',
+      source => 'puppet:///modules/disable_drrs_conditionally/lib_systemd_system-sleep_puavo-maybe-disable-drrs';
 
     '/usr/local/lib/puavo-maybe-disable-drrs':
       mode   => '0755',
