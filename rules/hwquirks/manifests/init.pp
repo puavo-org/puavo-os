60da1d681c92ff3fe9197de7cba034f7d72c744a
Antti Sokero <antti.sokero@opinsys.fi>
2019-12-03 13:04:46 +0200

    Add hwquirk to disable grub theme on two (quality) Acer laptops.

--- rules/hwquirks/manifests/init.pp
+++ rules/hwquirks/manifests/init.pp
@@ -9,6 +9,9 @@ class hwquirks {
     '/usr/share/puavo-conf/hwquirk-overwrites/50-pm-lidswitchdocked.json':
       source => 'puppet:///modules/hwquirks/50-pm-lidswitchdocked.json';
 
+    '/usr/share/puavo-conf/hwquirk-overwrites/60-disable-grub-theme.json':
+      source => 'puppet:///modules/hwquirks/60-disable-grub-theme.json';
+
     '/usr/share/puavo-conf/hwquirk-overwrites/70-i915-disable-drrs.json':
       source => 'puppet:///modules/hwquirks/70-i915-disable-drrs.json';
   }
