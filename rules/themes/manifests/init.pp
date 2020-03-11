20a4d594ec58d41926cede2bd723772f87af4c11
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2020-03-11 14:40:43 +0200

    Prepare to make some changes to "arc-theme" gnome-shell.css.
    This version contains the gnome-shell.css as is from the upstream
    package.

--- rules/themes/manifests/init.pp
+++ rules/themes/manifests/init.pp
@@ -1,9 +1,18 @@
 class themes {
+  include ::dpkg
   include ::gdm
+
+  ::dpkg::simpledivert {
+    '/usr/share/themes/Arc/gnome-shell/gnome-shell.css':
+      before => File['/usr/share/themes/Arc/gnome-shell/gnome-shell.css'];
+  }
 
   file {
     '/usr/share/themes':
       ensure => directory;
+
+    '/usr/share/themes/Arc/gnome-shell/gnome-shell.css':
+      source => 'puppet:///modules/themes/Arc/gnome-shell/gnome-shell.css';
 
     '/usr/share/themes/Geos-puavo-dark-panel':
       recurse => true,
@@ -18,4 +27,6 @@ class themes {
       require => File['/etc/gdm3/background.img'],
       source  => 'puppet:///modules/themes/Puavo';
   }
+
+  Package <| title == arc-theme |>
 }
