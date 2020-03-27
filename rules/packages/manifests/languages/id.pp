8a64c4bab8c8f3fe90cb1fbee9131d447b2fe119
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2020-03-27 19:45:28 +0200

    "icedove-l10n-id" --> "thunderbird-l10n-id" in Buster.

--- rules/packages/manifests/languages/id.pp
+++ rules/packages/manifests/languages/id.pp
@@ -1,11 +1,11 @@
 class packages::languages::id {
   @package {
     [ 'hyphen-id'
-    , 'icedove-l10n-id'
     , 'kde-l10n-id'
     , 'libreoffice-l10n-id'
     , 'hunspell-id'
-    , 'mythes-id' ]:
+    , 'mythes-id'
+    , 'thunderbird-l10n-id' ]:
       tag => [ 'tag_debian_desktop', 'tag_language_id', ];
   }
 }
