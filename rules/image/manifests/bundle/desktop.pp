8dd34db47b7d645638a015231387b7e6d4919e88
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2020-04-07 15:13:59 +0300

    Remove the "disable_geoclue"-module.
    This is an old puppet-module that no longer does anything useful.
    It is Ubuntu-specific.  I do not remember why I did it, except
    that I was told to and I do not remember what was the rationale
    for it (maybe: too much network traffic?  or it was too
    privacy-invasive?).

--- rules/image/manifests/bundle/desktop.pp
+++ rules/image/manifests/bundle/desktop.pp
@@ -6,7 +6,6 @@ class image::bundle::desktop {
   include ::desktop_cups
   include ::devilspie
   include ::disable_accounts_service
-  include ::disable_geoclue
   include ::disable_unclutter
   include ::exec_restrictions
   include ::firefox
