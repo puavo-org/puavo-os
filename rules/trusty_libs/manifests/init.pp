548eccc27e40b4d7e553831bd3cf4158a9255fd5
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2020-04-17 17:48:23 +0300

    Add some Trusty-libs to make LoggerPro work on Buster.

--- rules/trusty_libs/manifests/init.pp
+++ rules/trusty_libs/manifests/init.pp
@@ -35,6 +35,10 @@ class trusty_libs {
       srcpath => '/usr/lib/i386-linux-gnu/libgtkmm-2.4.so.1.1.0',
       url     => "${trusty_mirror_base}/ubuntu/pool/main/g/gtkmm2.4/libgtkmm-2.4-1c2a_2.24.4-1ubuntu1_i386.deb";
 
+    'x64_64-linux-gnu/libatkmm-1.6.so.1':
+      srcpath => '/usr/lib/x86_64-linux-gnu/libatkmm-1.6.so.1.1.0',
+      url     => "${trusty_mirror_base}/ubuntu/pool/main/a/atkmm1.6/libatkmm-1.6-1_2.22.7-2ubuntu1_amd64.deb";
+
     'x64_64-linux-gnu/libcairomm-1.0.so.1':
       srcpath => '/usr/lib/x86_64-linux-gnu/libcairomm-1.0.so.1.4.0',
       url     => "${trusty_mirror_base}/ubuntu/pool/main/c/cairomm/libcairomm-1.0-1_1.10.0-1ubuntu3_amd64.deb";
@@ -43,6 +47,10 @@ class trusty_libs {
       srcpath => '/usr/lib/x86_64-linux-gnu/libgdkmm-2.4.so.1.1.0',
       url     => "${trusty_mirror_base}/ubuntu/pool/main/g/gtkmm2.4/libgtkmm-2.4-1c2a_2.24.4-1ubuntu1_amd64.deb";
 
+    'x64_64-linux-gnu/libgiomm-2.4.so.1':
+      srcpath => '/usr/lib/x86_64-linux-gnu/libgiomm-2.4.so.1.3.0',
+      url     => "${trusty_mirror_base}/ubuntu/pool/main/g/glibmm2.4/libglibmm-2.4-1c2a_2.39.93-0ubuntu1_amd64.deb";
+
     'x64_64-linux-gnu/libglibmm-2.4.so.1':
       srcpath => '/usr/lib/x86_64-linux-gnu/libglibmm-2.4.so.1.3.0',
       url     => "${trusty_mirror_base}/ubuntu/pool/main/g/glibmm2.4/libglibmm-2.4-1c2a_2.39.93-0ubuntu1_amd64.deb";
@@ -50,5 +58,9 @@ class trusty_libs {
     'x64_64-linux-gnu/libgtkmm-2.4.so.1':
       srcpath => '/usr/lib/x86_64-linux-gnu/libgtkmm-2.4.so.1.1.0',
       url     => "${trusty_mirror_base}/ubuntu/pool/main/g/gtkmm2.4/libgtkmm-2.4-1c2a_2.24.4-1ubuntu1_amd64.deb";
+
+    'x64_64-linux-gnu/libpangomm-1.4.so.1':
+      srcpath => '/usr/lib/x86_64-linux-gnu/libpangomm-1.4.so.1.0.30',
+      url     => "${trusty_mirror_base}/ubuntu/pool/main/p/pangomm/libpangomm-1.4-1_2.34.0-1ubuntu1_amd64.deb";
   }
 }
