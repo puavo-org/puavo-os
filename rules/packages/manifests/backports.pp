0648eeda2e5654bf2cfcc2d67438e5e02930b46a
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2020-05-22 14:23:53 +0300

    Install a bunch of firmware packages from backports. Closes #554.

--- rules/packages/manifests/backports.pp
+++ rules/packages/manifests/backports.pp
@@ -4,10 +4,41 @@ class packages::backports {
   # package on this list does not trigger the installation of a package,
   # that has to be defined elsewhere.
 
-  $package_list = $debianversioncodename ? {
-                    'buster' => [ 'musescore3'
-                                , 'musescore3-common'
-                                , 'musescore-general-soundfont' ],
-                    default  => [],
-                  }
+  $package_list = [ 'amd64-microcode'
+                  , 'b43-fwcutter'
+                  , 'broadcom-sta-dkms'
+                  , 'firmware-amd-graphics'
+                  , 'firmware-atheros'
+                  , 'firmware-b43-installer'
+                  , 'firmware-b43legacy-installer'
+                  , 'firmware-bnx2'
+                  , 'firmware-bnx2x'
+                  , 'firmware-brcm80211'
+                  , 'firmware-cavium'
+                  , 'firmware-crystalhd'
+                  , 'firmware-intel-sound'
+                  , 'firmware-intelwimax'
+                  , 'firmware-ipw2x00'
+                  , 'firmware-ivtv'
+                  , 'firmware-iwlwifi'
+                  , 'firmware-libertas'
+                  , 'firmware-linux'
+                  , 'firmware-linux-free'
+                  , 'firmware-linux-nonfree'
+                  , 'firmware-misc-nonfree'
+                  , 'firmware-myricom'
+                  , 'firmware-netxen'
+                  , 'firmware-qlogic'
+                  , 'firmware-ralink'
+                  , 'firmware-realtek'
+                  , 'firmware-samsung'
+                  , 'firmware-siano'
+                  , 'firmware-ti-connectivity'
+                  , 'firmware-zd1211'
+                  , 'intel-microcode'
+                  , 'iucode-tool'
+
+                  , 'musescore3'
+                  , 'musescore3-common'
+                  , 'musescore-general-soundfont' ]
 }
