4aab8c54d2fd621b95c11024ef22eafe99fb0f73
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2020-05-22 14:15:54 +0300

    Update "current"-kernel to 5.5.0-0.bpo.2-amd64.

--- rules/packages/manifests/init.pp
+++ rules/packages/manifests/init.pp
@@ -733,9 +733,9 @@ class packages {
       dkms_modules => $all_dkms_modules,
       package_name => 'linux-image-4.19.0-8-amd64';
 
-    '5.4.0-0.bpo.2-amd64':
+    '5.5.0-0.bpo.2-amd64':
       dkms_modules => [], # XXX these should be tested
-      package_name => 'linux-image-5.4.0-0.bpo.2-amd64';
+      package_name => 'linux-image-5.5.0-0.bpo.2-amd64';
   }
 
   # Packages which are not restricted per se, but which are required by
