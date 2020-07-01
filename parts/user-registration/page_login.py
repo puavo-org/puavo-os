1db598e13dcba68a057fd49904988a1137cecd6b
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2020-07-01 13:43:08 +0300

    Some fixes to puavo-user-registration, and make the login UI nicer.

--- parts/user-registration/page_login.py
+++ parts/user-registration/page_login.py
@@ -20,12 +20,35 @@ class PageLogin(PageDefinition):
         self.we_are_active = False
 
         # Setup event handling
-        handlers = { 'on_login_clicked': self.on_login_clicked }
+        handlers = {
+          'on_login_clicked':      self.on_login_clicked,
+          'on_password_activated': self.on_password_activated,
+          'on_password_changed':   self.maybe_enable_login_button,
+          'on_username_activated': self.on_username_activated,
+          'on_username_changed':   self.maybe_enable_login_button,
+        }
         self.builder.connect_signals(handlers)
 
 
     def get_main_title(self):
        return _tr('Login')
+
+
+    def on_password_activated(self, widget):
+       do_login_button = self.builder.get_object('do_login')
+       do_login_button.grab_focus()
+       self.on_login_clicked(do_login_button)
+
+
+    def on_username_activated(self, widget):
+       self.builder.get_object('password').grab_focus()
+
+
+    def maybe_enable_login_button(self, widget):
+       have_password = (self.builder.get_object('password').get_text() != '')
+       have_username = (self.builder.get_object('username').get_text() != '')
+       do_login = self.builder.get_object('do_login')
+       do_login.set_sensitive(have_password and have_username)
 
 
     def swap_with(self, account_page):
@@ -41,6 +64,7 @@ class PageLogin(PageDefinition):
         self.main_builder.get_object('login_with_existing_account').set_label(
           _tr('New User Registration'))
         super().activate()
+        self.builder.get_object('username').grab_focus()
 
 
     def deactivate(self):
