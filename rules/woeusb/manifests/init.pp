476135093b81b3c91d507ab908ea8e2c51c8ebab
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2020-10-19 13:46:05 +0300

    Update WoeUSB url.

--- rules/woeusb/manifests/init.pp
+++ rules/woeusb/manifests/init.pp
@@ -3,7 +3,7 @@ class woeusb {
 
   exec {
     'fetch woeusb':
-      command => 'wget -O /usr/local/bin/woeusb.tmp https://raw.githubusercontent.com/slacka/WoeUSB/master/src/woeusb && chmod 755 /usr/local/bin/woeusb.tmp && mv /usr/local/bin/woeusb.tmp /usr/local/bin/woeusb',
+      command => 'wget -O /usr/local/bin/woeusb.tmp https://raw.githubusercontent.com/WoeUSB/WoeUSB/master/sbin/woeusb && chmod 755 /usr/local/bin/woeusb.tmp && mv /usr/local/bin/woeusb.tmp /usr/local/bin/woeusb',
       creates => '/usr/local/bin/woeusb',
       require => Package['wget'];
   }
