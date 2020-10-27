0e2f208c080af4821272d458040b98c098fd303e
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2020-10-27 12:22:22 +0200

    Update "fresh"-kernel to 4.19.0-0.bpo.11-amd64.

--- rules/packages/manifests/init.pp
+++ rules/packages/manifests/init.pp
@@ -778,9 +778,9 @@ class packages {
       dkms_modules => $all_dkms_modules,
       package_name => 'linux-image-4.9.0-13-amd64';
 
-    '4.19.0-0.bpo.6-amd64':
+    '4.19.0-0.bpo.11-amd64':
       dkms_modules => [ $broadcom_sta_dkms_module, $virtualbox_module ],
-      package_name => 'linux-image-4.19.0-0.bpo.6-amd64';
+      package_name => 'linux-image-4.19.0-0.bpo.11-amd64';
   }
 
   # Packages which are not restricted per se, but which are required by
