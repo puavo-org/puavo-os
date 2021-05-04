fe173ba023b4394ef8990be75a5e801c4e14d331
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2021-05-04 10:09:55 +0300

    We have not needed the "other_releases" apt-rules in Buster, hopefully we do not need it in Bullseye either (it would be much nicer not to use it).

--- rules/apt/manifests/repositories.pp
+++ rules/apt/manifests/repositories.pp
@@ -1,32 +1,11 @@
 class apt::repositories {
   include ::apt
 
-  $other_releases = $debianversioncodename ? {
-                      'buster' => {},
-                      default  => {
-                        'jessie' => 80,
-                        'buster' => 60,
-                      }
-                    }
-
   define setup ($localmirror='',
                 $mirror,
                 $mirror_path='',
                 $securitymirror,
                 $securitymirror_path='') {
-    $::apt::repositories::other_releases.each |String $distrib_version,
-                                               Integer $pin_priority| {
-      ::apt::debian_repository {
-        $distrib_version:
-          localmirror         => $localmirror,
-          mirror              => $mirror,
-          mirror_path         => $mirror_path,
-          pin_priority        => $pin_priority,
-          securitymirror      => $securitymirror,
-          securitymirror_path => $securitymirror_path;
-      }
-    }
-
     file {
       '/etc/apt/preferences.d/00-puavo.pref':
         content => template('apt/00-puavo.pref'),
