0c15e67e7c22da7181607b476d923a9a68b35c2a
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2021-07-06 17:03:52 +0300

    Add /etc/cups/cups-browsed.conf to our configurations.

--- rules/desktop_cups/manifests/init.pp
+++ rules/desktop_cups/manifests/init.pp
@@ -2,6 +2,10 @@ class desktop_cups {
   include ::packages
 
   file {
+    '/etc/cups/cups-browsed.conf':
+      require => Package['cups-browsed'],
+      source  => 'puppet:///modules/desktop_cups/cups-browsed.conf';
+
     '/etc/cups/cupsd.conf':
       require => Package['cups-daemon'],
       source  => 'puppet:///modules/desktop_cups/cupsd.conf';
