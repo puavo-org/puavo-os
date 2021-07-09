1a25e17161da50d6b0bd4772c166af12579d7015
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2021-07-09 19:10:37 +0300

    Implement new printer restrictions on the client side.

--- rules/desktop_cups/manifests/init.pp
+++ rules/desktop_cups/manifests/init.pp
@@ -1,5 +1,6 @@
 class desktop_cups {
   include ::packages
+  include ::puavo_conf
 
   file {
     '/etc/cups/cups-browsed.conf':
@@ -9,6 +10,11 @@ class desktop_cups {
     '/etc/cups/cupsd.conf':
       require => Package['cups-daemon'],
       source  => 'puppet:///modules/desktop_cups/cupsd.conf';
+  }
+
+  ::puavo_conf::script {
+    'setup_desktop_printer_restrictions':
+      source => 'puppet:///modules/desktop_cups/setup_desktop_printer_restrictions';
   }
 
   Package <| title == cups-browsed
