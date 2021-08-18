065d904512691154427faef1862c27c9ab921d85
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2021-08-18 14:29:40 +0300

    Allow uid=admin,o=puavo to create organisation owners.
    This is somewhat odd because uid=admin,o=puavo is excepted from ACLs.
    But this attribute constraint looks at the credentials of an
    authenticated user, and "uid=admin,o=puavo" is certainly valid
    user for creating organisation owners.

--- cloud/puavo-ds/master/lib/puavo-ds/overlay/constraint.rb
+++ cloud/puavo-ds/master/lib/puavo-ds/overlay/constraint.rb
@@ -2,8 +2,8 @@ class Constraint < Overlay
     # Save without validation
   def self.add_overlay_config(args)
     self.new( 'objectclass' => ['olcConstraintConfig', 'olcOverlayConfig'],
-              'olcOverlay' => "{#{self.index}}constraint",
-              'olcConstraintAttribute' => ["puavoSchool set \"(this/puavoSchool/puavoSchoolAdmin* | [#{args[:database].olcSuffix}]/owner* & user)\""] ).save(false)
+              'olcOverlay' => "{#{ self.index }}constraint",
+              'olcConstraintAttribute' => [%Q{puavoSchool set "(this/puavoSchool/puavoSchoolAdmin* | [#{ args[:database].olcSuffix }]/owner* | [uid=admin,o=puavo]) & user"}] ).save(false)
   end
 
   def self.index
