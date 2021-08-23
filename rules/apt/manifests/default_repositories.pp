ef0bfdb434477fad84a86ccf4103c4a0086ce07c
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2021-08-23 11:08:02 +0300

    Fix securitymirror apt sources for Bullseye.

--- rules/apt/manifests/default_repositories.pp
+++ rules/apt/manifests/default_repositories.pp
@@ -6,11 +6,13 @@ class apt::default_repositories {
 
   $mirror = $mirror ? { undef => 'ftp.debian.org', default => "$mirror", }
   $securitymirror = 'security.debian.org'
+  $securitymirror_path = '/debian-security'
 
   apt::repositories::setup {
     'apt':
-      localmirror    => $localmirror,
-      mirror         => $mirror,
-      securitymirror => $securitymirror;
+      localmirror         => $localmirror,
+      mirror              => $mirror,
+      securitymirror      => $securitymirror,
+      securitymirror_path => $securitymirror_path;
   }
 }
