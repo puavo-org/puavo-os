33813d1604a4e04060986444df9bb26f3a975acc
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2021-08-23 11:02:12 +0300

    Remove "debian_repository" definition from Puppet-rules.
    We have not used this in Buster.  I think it was to include
    packages from multiple Debian versions.  But it is not much
    better if we can do without including older or newer Debian
    versions in apt sources, as we have managed to do in Buster.
    One reason why that is bad is that "apt update" takes a much
    longer time and all those package lists take more space (that
    is not normally a problem, but sometimes might be).  Besides,
    mixing packages from several release versions can be problematci.

--- rules/apt/manifests/init.pp
+++ rules/apt/manifests/init.pp
@@ -1,23 +1,4 @@
 class apt {
-  define debian_repository ($localmirror='',
-                            $mirror='',
-                            $mirror_path='',
-                            $pin_priority=50,
-                            $securitymirror='',
-                            $securitymirror_path='') {
-    $distrib_version = $title
-
-    file {
-      "/etc/apt/preferences.d/00-${distrib_version}.pref":
-        content => template('apt/00-distrib_version.pref'),
-        notify  => Exec['apt update'];
-
-      "/etc/apt/sources.list.d/${distrib_version}.list":
-        content => template('apt/debian_apt_sources.list'),
-        notify  => Exec['apt update'];
-    }
-  }
-
   define pin ($version) {
     $package = $title
 
