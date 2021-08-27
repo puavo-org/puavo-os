a82f45e4ec97307b33cb96b966fdb5a705df77f3
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2021-08-27 12:50:51 +0300

    Add "puavo-show-printer-restrictions" that can be run to see current device printer restrictions.

--- rules/desktop_cups/manifests/init.pp
+++ rules/desktop_cups/manifests/init.pp
@@ -10,6 +10,11 @@ class desktop_cups {
     '/etc/cups/cupsd.conf':
       require => Package['cups-daemon'],
       source  => 'puppet:///modules/desktop_cups/cupsd.conf';
+
+    '/usr/local/bin/puavo-show-printer-restrictions':
+      mode    => '0755',
+      require => ::Puavo_conf::Script['setup_desktop_printer_restrictions'],
+      source  => 'puppet:///modules/desktop_cups/puavo-show-printer-restrictions';
   }
 
   ::puavo_conf::script {
