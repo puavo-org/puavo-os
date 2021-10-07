413ff72d75d8e72d868f7bb4ec0248de82123c9c
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2021-10-07 12:12:12 +0300

    Remove /etc/xdg/autostart/ibus-anthy-gnome-initial-setup.desktop.
    We want ibus-anthy, but only when manually set up.

--- rules/ibus/manifests/init.pp
+++ rules/ibus/manifests/init.pp
@@ -2,6 +2,10 @@ class ibus {
   include ::packages
 
   file {
+    '/etc/xdg/autostart/ibus-anthy-gnome-initial-setup.desktop':
+      ensure  => absent,
+      require => Package['ibus-anthy'];
+
     '/usr/local/bin/puavo-ibus':
       mode    => '0755',
       require => Package['ibus-anthy'],
