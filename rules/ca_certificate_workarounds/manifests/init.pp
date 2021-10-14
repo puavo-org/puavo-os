e132bb0fbbfdcf42dbee8c4703ebf46e25f8e7c5
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2021-10-14 14:36:52 +0300

    Revert "Drop "mozilla/DST_Root_CA_X3.crt" certificate, it is expired"
    This reverts commit 52e16764df9802c4e660ee15e457417a4c67b26c.
    
    We do not want this commit to Buster/Bullseye, that do not
    suffer from this problem (because openssl is a more recent version).

--- rules/ca_certificate_workarounds/manifests/init.pp
+++
@@ -1,8 +0,0 @@
-class ca_certificate_workarounds {
-  exec {
-    # remove an expired certificate that causes bad behaviour in old openssl
-    'remove an expired certificate':
-      command => '/bin/sed -i \'/^mozilla\/DST_Root_CA_X3.crt$/d\' /etc/ca-certificates.conf && /usr/sbin/update-ca-certificates -f',
-      onlyif  => '/usr/bin/test -e /etc/ssl/certs/DST_Root_CA_X3.pem';
-  }
-}
