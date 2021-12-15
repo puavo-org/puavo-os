9cfa5400a5054cfa2b74cccfd29623b7c5890b94
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2021-12-15 14:53:26 +0200

    Also read Veyon configuration from bootserver and apply it.

--- rules/veyon/manifests/init.pp
+++ rules/veyon/manifests/init.pp
@@ -2,7 +2,7 @@ class veyon {
   include ::puavo_conf
 
   ::puavo_conf::script {
-    'get_veyon_keys':
-      source => 'puppet:///modules/veyon/get_veyon_keys';
+    'setup_veyon':
+      source => 'puppet:///modules/veyon/setup_veyon';
   }
 }
