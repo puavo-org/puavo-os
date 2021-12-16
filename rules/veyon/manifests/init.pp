81d3fdfd68a8b3895444f3ab9a320ef3c8dbe232
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2021-12-16 13:36:16 +0200

    Add "veyon-vnc" to workaround issues in veyon-server / x11vnc integration.

--- rules/veyon/manifests/init.pp
+++ rules/veyon/manifests/init.pp
@@ -1,8 +1,18 @@
 class veyon {
+  include ::packages
   include ::puavo_conf
+
+  file {
+    '/usr/local/bin/veyon-vnc':
+      mode    => '0755',
+      require => Package['x11vnc'],
+      source  => 'puppet:///modules/veyon/veyon-vnc';
+  }
 
   ::puavo_conf::script {
     'setup_veyon':
       source => 'puppet:///modules/veyon/setup_veyon';
   }
+
+  Package <| title == "x11vnc" |>
 }
