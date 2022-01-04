f861102f4cb409e9ffc8205ff29807a5607d3e6a
Tuomas Nurmi <tuomas.nurmi@opinsys.fi>
2022-01-04 08:50:17 +0200

    Fine-tune nextcloud-client config rules to bind to the puavo-pkg.

--- rules/nextcloud/manifests/init.pp
+++ rules/nextcloud/manifests/init.pp
@@ -1,9 +1,11 @@
 class nextcloud {
-  include ::packages
+  ::puavo_pkg::packages
 
+  file { '/etc/Nextcloud': ensure => directory; }
   file {
     '/etc/Nextcloud/Nextcloud.conf':
        source  => 'puppet:///modules/desktop/Nextcloud.conf';
   }
 
+  Puavo_pkg::Install <| title == nextcloud-desktop |>
 }
