ba0e8e3e8404d243417147584ebd0fd54cd584ea
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2022-01-05 07:49:49 +0200

    Remove our Libreoffice Finnish translation fix.
    This should have been removed with the Libreoffice update.

--- rules/image/manifests/bundle/desktop.pp
+++ rules/image/manifests/bundle/desktop.pp
@@ -21,7 +21,6 @@ class image::bundle::desktop {
   include ::keyutils
   # include ::ktouch                    # XXX buster
   include ::libdvdcss
-  include ::libreoffice
   include ::network_manager
   include ::nextcloud
   include ::password_expiration
