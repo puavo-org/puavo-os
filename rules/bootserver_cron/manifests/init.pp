e1302501659b839f0d8afde53a37cfad5cef0cb2
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2022-01-05 11:32:27 +0200

    Simplify puppet syntax a bit.

--- rules/bootserver_cron/manifests/init.pp
+++ rules/bootserver_cron/manifests/init.pp
@@ -3,8 +3,7 @@ class bootserver_cron {
     '/usr/local/sbin/puavo-fix-homedir-permissions':
       mode   => '0755',
       source => 'puppet:///modules/bootserver_cron/puavo-fix-homedir-permissions';
-  }
-  file {
+
     '/usr/local/sbin/puavo-gather-printing-statistics':
       mode   => '0755',
       source => 'puppet:///modules/bootserver_cron/puavo-gather-printing-statistics';
