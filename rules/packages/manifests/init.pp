eabfb44e67d5dd36853452b38b13c6c30e853743
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2022-01-14 13:42:47 +0200

    Install puavo-veyon-applet.

--- rules/packages/manifests/init.pp
+++ rules/packages/manifests/init.pp
@@ -39,6 +39,7 @@ class packages {
     , 'puavo-sharedir-client'
     , 'puavo-usb-factory'
     , 'puavo-user-registration'
+    , 'puavo-veyon-applet'
     , 'puavo-vpn-client'
     , 'puavo-webwindow'
     # , 'puavo-wlanap'          # XXX missing from Bullseye
