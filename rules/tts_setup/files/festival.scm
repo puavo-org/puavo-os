dc8f16079ffa264b9e39b20662b48fe88288b6e0
Tuomas Nurmi <tuomas.nurmi@opinsys.fi>
2022-01-18 12:40:54 +0200

    Set festival to default to Finnish voice.

--- rules/tts_setup/files/festival.scm
+++ rules/tts_setup/files/festival.scm
@@ -4,6 +4,11 @@
             (lambda (utt)
               (utt.wave.rescale utt 0.9 t))))
 
+(if (probe_file "/usr/share/festival/voices/finnish/suopuhe.common/hy_fi_mv_diphone.scm")
+    (begin
+     (load "/usr/share/festival/voices/finnish/suopuhe.common/hy_fi_mv_diphone.scm")
+     (set! voice_default 'hy_fi_mv_diphone)))
+
 (Parameter.set 'Audio_Required_Format'aiff)
 (Parameter.set 'Audio_Command"paplay $FILE --client-name=Festival --stream-name=Speech")
 (Parameter.set 'Audio_Method 'Audio_Command)
