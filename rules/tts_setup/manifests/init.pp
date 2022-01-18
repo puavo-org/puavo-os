24a657edfc2d920134e5050648d6b3759464a73b
Tuomas Nurmi <tuomas.nurmi@opinsys.fi>
2022-01-18 12:22:51 +0200

    Store the to-be-replaced distribution config files, too.

--- rules/tts_setup/manifests/init.pp
+++ rules/tts_setup/manifests/init.pp
@@ -1,12 +1,18 @@
 class tts_setup {
   include ::packages
+  include ::dpkg
+
+  dpkg::simpledivert {
+    [ "/etc/speech-dispatcher/speechd.conf", "/etc/festival.scm", ]: ;
+  }
 
   file {
     [ '/etc/speech-dispatcher', '/etc/speech-dispatcher/modules', ]:
       ensure => directory;
 
     '/etc/speech-dispatcher/speechd.conf':
-      require => Package['speech-dispatcher-pico'],
+      require => [ Dpkg::Simpledivert['/etc/speech-dispatcher/speechd.conf'],
+                   Package['speech-dispatcher-pico'], ],
       source  => 'puppet:///modules/tts_setup/speechd.conf';
 
     '/etc/speech-dispatcher/modules/festival-generic.conf':
@@ -14,7 +20,8 @@ class tts_setup {
       source  => 'puppet:///modules/tts_setup/festival-generic.conf';
 
     '/etc/festival.scm':
-      require => Package['festvox-suopuhe-mv'],
+      require => [ Dpkg::Simpledivert['/etc/festival.scm'],
+                   Package['festvox-suopuhe-mv'], ],
       source  => 'puppet:///modules/tts_setup/festival.scm';
   }
 
