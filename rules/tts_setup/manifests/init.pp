ce541cb63ab288dd8d5f3430d1d9605d08937167
Tuomas Nurmi <tuomas.nurmi@opinsys.fi>
2022-01-20 10:25:37 +0200

    Instead of diverting, bundle the original debian config files and edit them.

--- rules/tts_setup/manifests/init.pp
+++ rules/tts_setup/manifests/init.pp
@@ -1,27 +1,24 @@
 class tts_setup {
   include ::packages
-  include ::dpkg
-
-  dpkg::simpledivert {
-    [ "/etc/speech-dispatcher/speechd.conf", "/etc/festival.scm", ]: ;
-  }
 
   file {
     [ '/etc/speech-dispatcher', '/etc/speech-dispatcher/modules', ]:
       ensure => directory;
 
     '/etc/speech-dispatcher/speechd.conf':
-      require => [ Dpkg::Simpledivert['/etc/speech-dispatcher/speechd.conf'],
-                   Package['speech-dispatcher-pico'], ],
+      require => Package['speech-dispatcher-pico'],
       source  => 'puppet:///modules/tts_setup/speechd.conf';
+
+    '/etc/speech-dispatcher/modules/pico-generic.conf':
+      require => Package['speech-dispatcher-pico'],       
+      source  => 'puppet:///modules/tts_setup/pico-generic.conf';
 
     '/etc/speech-dispatcher/modules/festival-generic.conf':
       require => Package['festvox-suopuhe-mv'],
       source  => 'puppet:///modules/tts_setup/festival-generic.conf';
 
     '/etc/festival.scm':
-      require => [ Dpkg::Simpledivert['/etc/festival.scm'],
-                   Package['festvox-suopuhe-mv'], ],
+      require => Package['festival'],
       source  => 'puppet:///modules/tts_setup/festival.scm';
   }
 
