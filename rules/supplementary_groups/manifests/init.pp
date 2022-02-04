990463d4be26149306a92aa7cbc90a36da93d00b
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2022-02-04 11:55:01 +0200

    Remove supplementary groups configuration for "systemd --user".
    I do not think this is needed anymore, as we do not start
    the systemd user-instance.

--- rules/supplementary_groups/manifests/init.pp
+++ rules/supplementary_groups/manifests/init.pp
@@ -13,25 +13,14 @@ class supplementary_groups {
             , 'video' ]
 
   #
-  # add users to supplementary groups via pam_group / systemd -tricks
+  # add users to supplementary groups via pam_group
   #
 
   file {
     '/etc/security/group.conf':
       content => template('supplementary_groups/group.conf'),
       require => Package['libpam-modules'];
-
-    '/etc/systemd/system/user@.service.d':
-      ensure  => directory,
-      require => Package['systemd'];
-
-    '/etc/systemd/system/user@.service.d/override.conf':
-      content => template('supplementary_groups/user@.service.d_override.conf'),
-      require => Package['systemd'];
   }
 
-  Package <|
-       title == libpam-modules
-    or title == systemd
-  |>
+  Package <| title == libpam-modules |>
 }
