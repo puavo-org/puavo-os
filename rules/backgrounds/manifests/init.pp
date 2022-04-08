e915451d269fcd443eb1d97da7e43dbd9ad6d6ec
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2022-04-08 14:02:09 +0300

    Update greeter background list to include only those on the system (after recent changes on included backgrounds).

--- rules/backgrounds/manifests/init.pp
+++ rules/backgrounds/manifests/init.pp
@@ -24,36 +24,19 @@ class backgrounds {
   }
 
   ::backgrounds::background {
-    [ '163_by_e4v.jpg'
-    , 'Brother_typewriter_by_awdean1.jpg'
-    , 'Candy_by_Bernhard_Hanakam.jpg'
-    , 'Classic_Guitar_Detail_by_Sten_Jørgen_Pettersen.jpg'
-    , 'Cyclotron_by_cyclotron_beam.jpg'
-    , 'Flocking_by_noombox.jpg'
-    , 'Floorboards_by_Dawid_Huczyński.jpg'
-    , 'Forest_by_Moritz_Reisinger.jpg'
+    [ 'brad-huchteman-stone-mountain.jpg'
+    , 'Crocus_Wallpaper_by_Roy_Tanck.jpg'
+    , 'Foggy_Forest_by_Jake_Stewart.jpg'
     , 'Forever_by_Shady_S.jpg'
-    , 'Haukland_Beach_view_by_Michele_Agostini.jpg'
+    , 'Jelly_Fish_by_RaDu_GaLaN.jpg'
     , 'H_by_Manuel_Sagredo.jpg'
-    , 'IMG_7632_by_Jobin_Babu.jpg'
-    , 'Kronach_leuchtet_2014_by_Brian_Fox.jpg'
-    , 'larung_gar_by_night_by_Geza_Radics.jpg'
-    , 'Moss_inflorescence_by_carmelo75.jpg'
-    , 'Mr._Tau_and_The_Tree_-_by_TJ.jpg'
-    , 'Mushrooms_3_by_moritzmhmk.jpg'
-    , 'Music_by_tomasino.cz.jpg'
     , 'Night_lights_by_Alberto_Salvia_Novella.jpg'
-    , 'Redes_de_hilo_by_Juan_Pablo_Lauriente.jpg'
+    , 'On_top_of_the_Rubihorn_by_Matthias_Niess.jpg'
+    , 'Raindrops_On_The_Table_by_Alex_Fazit.jpg'
     , 'Reflections_by_Trenton_Fox.jpg'
-    , 'Some_Light_Reading_by_Brandilyn_Carpenter.jpg'
+    , 'Silver_Back_Gorilla_by_Mike_Arney.jpg'
     , 'TCP118v1_by_Tiziano_Consonni.jpg'
-    , 'Tenerife_Roques_de_Anaga_by_Frederik_Schulz.jpg'
-    , 'Tesla_by_Tomasino.cz.jpg'
-    , 'Trazo_solitario_by_Julio_Diliegros.jpg'
-    , 'ubuntu16_10_by_Khoir_Rudin.png'
-    , 'Wanaka_Tree_by_Stephane_Pakula.jpg'
-    , 'Winter_Fog_by_Daniel_Vesterskov.jpg'
-    , 'Yala_mountain_by_Geza_Radics.jpg' ]: ;
+    , 'Wall_with_door_on_Gozo_by_Matthias_Niess.jpg' ]: ;
   }
 
   Puavo_pkg::Install <| title == 'ubuntu-wallpapers' |>
