aebdaf5f622df40a91e74a02ed2386b030959abd
Tuomas Nurmi <tuomas.nurmi@opinsys.fi>
2022-04-19 08:51:57 +0300

    Remove our menu extension, now that we switched to upstream audio-switcher.

--- rules/gnome_shell_extensions/manifests/init.pp
+++ rules/gnome_shell_extensions/manifests/init.pp
@@ -26,7 +26,6 @@ class gnome_shell_extensions {
       require => ::Themes::Iconlink['scalable/apps/puavo-multitasking-view.svg'];
 
     [ 'appindicatorsupport@rgcjonas.gmail.com'
-    , 'audio-menu-modifier@puavo.org' # XXX can audio-switcher@AndresCidoncha replace this?
     , 'audio-switcher@AndresCidoncha'
     , 'bottompanel@tmoer93'
     , 'BringOutSubmenuOfPowerOffLogoutButton@pratap.fastmail.fm'
