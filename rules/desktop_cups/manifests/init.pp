a0f8a9c95856bd45205e67eeed5ae142540dee8a
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2022-06-02 13:58:23 +0300

    Drop our customizations for /etc/cups/cupsd.conf.
    This is a leftover from the old days and I do not think we still need
    any customizations here.  Plus I suspect there are some issues
    in this when used with cups-browsed.

--- rules/desktop_cups/manifests/init.pp
+++ rules/desktop_cups/manifests/init.pp
@@ -3,10 +3,6 @@ class desktop_cups {
   include ::puavo_conf
 
   file {
-    '/etc/cups/cupsd.conf':
-      require => Package['cups-daemon'],
-      source  => 'puppet:///modules/desktop_cups/cupsd.conf';
-
     '/usr/local/bin/puavo-show-printer-restrictions':
       mode    => '0755',
       require => ::Puavo_conf::Script['setup_desktop_printer_restrictions'],
