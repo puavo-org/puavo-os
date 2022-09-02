21c4912303a2a3075f7ec9261377f07ee803b1d0
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2022-09-02 10:46:14 +0300

    Update kernels.

--- rules/packages/manifests/init.pp
+++ rules/packages/manifests/init.pp
@@ -776,10 +776,10 @@ class packages {
       dkms_modules => $all_dkms_modules,
       package_name => 'linux-image-4.19.0-21-amd64';
 
-    '5.10.0-0.bpo.15-amd64':
+    '5.10.0-0.deb10.17-amd64':
       dkms_modules => [ $broadcom_sta_dkms_module
                       , $virtualbox_module ],
-      package_name => 'linux-image-5.10.0-0.bpo.15-amd64';
+      package_name => 'linux-image-5.10.0-0.deb10.17-amd64-unsigned';
   }
 
   # Packages which are not restricted per se, but which are required by
