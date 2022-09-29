4ae4949dd9f96ac9311bb22efbb81efb39102f21
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2022-09-29 11:05:50 +0300

    Update Grub version in Bullseye.

--- rules/grub/manifests/init.pp
+++ rules/grub/manifests/init.pp
@@ -2,12 +2,12 @@ class grub {
   include ::grub::themes
   include ::puavo_conf
 
-  $grub_version = '2.04-20'
+  $grub_version = '2.06-3~deb11u2'
 
   # Keep track of the "grub-efi-amd64-signed" package
   # version, even though currently installing that package
   # will break Grub in UEFI machines.
-  $grub_version_signed = '1+2.04+20'
+  $grub_version_signed = '1+2.06-3~deb11u2'
 
   file {
     [ '/boot', '/boot/grub', '/boot/grub/puavo' ]:
