45525fab9ebe0d82c5abad9c3731d40824b68bdc
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2022-10-05 13:35:35 +0300

    Get Lazarus 2.2 from bullseye-backports.

--- rules/packages/manifests/backports.pp
+++ rules/packages/manifests/backports.pp
@@ -5,5 +5,19 @@ class packages::backports {
   # that has to be defined elsewhere.
 
   $package_list = [ 'broadcom-sta-dkms'
-                  , 'r8168-dkms' ]
+                  , 'r8168-dkms'
+
+                  # lazarus packages
+                  , 'lazarus'
+                  , 'lazarus-2.2'
+                  , 'lazarus-doc-2.2'
+                  , 'lazarus-ide'
+                  , 'lazarus-ide-2.2'
+                  , 'lazarus-ide-gtk2-2.2'
+                  , 'lazarus-src-2.2'
+                  , 'lcl-2.2'
+                  , 'lcl-gtk2-2.2'
+                  , 'lcl-nogui-2.2'
+                  , 'lcl-units-2.2'
+                  , 'lcl-utils-2.2' ]
 }
