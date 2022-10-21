6e569877ae626f3827bc7bb7c098ae6e260b2686
Tuomas Nurmi <tuomas.nurmi@opinsys.fi>
2022-10-21 15:15:49 +0300

    Install our own puppeted completions under etc.

--- rules/puavo_bash_completions/manifests/init.pp
+++ rules/puavo_bash_completions/manifests/init.pp
@@ -1,13 +1,13 @@
 class puavo_bash_completions {
   include ::packages
   file {
-    '/usr/share/bash_completion/completions/'
+    '/etc/bash_completion.d/'
       ensure => directory;
 
-    '/usr/share/bash_completion/completions/puavo-make-install-disk':
+    '/etc/bash_completion.d/puavo-make-install-disk':
       source => 'puppet:///modules/puavo_bash_completions/puavo-make-install-disk';
 
-    '/usr/share/bash_completion/completions/puavo-bootserver-sync-images':
+    '/etc/bash_completion.d/puavo-bootserver-sync-images':
       source => 'puppet:///modules/puavo_bash_completions/puavo-bootserver-sync-images';
   }
 
