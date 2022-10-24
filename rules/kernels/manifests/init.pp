2ff14db56ae249b13b7c6f6c6ee3ab3258ce2b2d
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2022-10-24 12:32:31 +0300

    Update Buster kernels.

--- rules/kernels/manifests/init.pp
+++ rules/kernels/manifests/init.pp
@@ -28,7 +28,7 @@ class kernels {
     }
   }
 
-  $default_kernel = '4.19.0-21-amd64'
+  $default_kernel = '4.19.0-22-amd64'
   $current_kernel = '5.10.0-0.deb10.17-amd64'
 
   ::kernels::all_kernel_links {
