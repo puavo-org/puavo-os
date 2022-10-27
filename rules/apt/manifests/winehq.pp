466c23e16d22e1ca156230e249a88dd799e0152f
Tuomas Nurmi <tuomas.nurmi@opinsys.fi>
2022-10-27 16:59:55 +0300

    Add the apt source file.

--- rules/apt/manifests/winehq.pp
+++ rules/apt/manifests/winehq.pp
@@ -1,10 +1,13 @@
 class apt::winehq {
   file {
+    '/etc/apt/keyrings':
+      ensure => directory;
+
     '/etc/apt/preferences.d/50-winehq.pref':
       content => template('apt/50-winehq.pref');
 
-    '/etc/apt/sources.list.d/winehq.list':
-      content => template('apt/winehq.list'),
+    '/etc/apt/sources.list.d/winehq-bullseye.sources':
+      content => template('apt/winehq-bullseye.sources'),
       notify  => Exec['apt update'],
       require => File['/etc/apt/keyrings/winehq-archive.key'];
 
