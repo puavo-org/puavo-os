7ab0c55f78e9ba3f871aec466bbdd1b49a5c2c0c
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2022-11-29 10:51:26 +0200

    Support --update-modules in puavo-restore-all-databases.
    Supported only when mdb is used.

--- cloud/puavo-ds/master/lib/puavo-ds/database_config.rb
+++ cloud/puavo-ds/master/lib/puavo-ds/database_config.rb
@@ -29,4 +29,17 @@ module PuavoDatabaseConfig
               'puavoWirelessPrinterQueue eq',
               'sn sub',
               'uid eq,sub' ]
+  MODULES = [ '{0}back_mdb',
+              '{1}dynlist',
+              '{2}unique',
+              '{3}ppolicy',
+              '{4}syncprov',
+              '{5}memberof',
+              '{6}valsort',
+              '{7}auditlog',
+              '{8}refint',
+              '{9}constraint',
+              '{10}accesslog',
+              '{11}deref',
+              '{12}smbkrb5pwd_srv' ]
 end
