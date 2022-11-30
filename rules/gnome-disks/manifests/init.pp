c9946c756d84805dbbc400f46031aba989c3ecca
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2022-11-30 12:10:41 +0200

    Fix issues in our recently introduced gnome-disks wrapper.

--- rules/gnome-disks/manifests/init.pp
+++ rules/gnome-disks/manifests/init.pp
@@ -2,6 +2,8 @@ class gnome-disks {
   include ::dpkg
   include ::packages
 
+  # XXX remove this for Debian Bookworm that should not need this
+
   dpkg::simpledivert {
     '/usr/bin/gnome-disks':
       before => File['/usr/bin/gnome-disks'];
