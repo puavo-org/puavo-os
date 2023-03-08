3747274054ceb2bcc1467d523983dd6ceff3a88a
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2023-03-08 15:06:54 +0200

    Move "puavo-unpack-a-file-from-deb" from puppet-rules to a the deb-package that contains the puavo-pkg tools.
    The idea that this could be used by puavo-pkg packages to
    download files.

--- rules/trusty_libs/manifests/file_unpack.pp
+++
@@ -1,7 +0,0 @@
-class trusty_libs::file_unpack {
-  file {
-    '/usr/local/lib/puavo-unpack-a-file-from-deb':
-      mode   => '0755',
-      source => 'puppet:///modules/trusty_libs/puavo-unpack-a-file-from-deb';
-  }
-}
