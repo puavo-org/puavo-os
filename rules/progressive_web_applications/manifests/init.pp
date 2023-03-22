ca14897000d147935f7d6ca0d3cff064647549b1
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2023-03-22 14:57:13 +0200

    Some fixes to puavo-setup-pwa so we can use "teams" PWA with it.

--- rules/progressive_web_applications/manifests/init.pp
+++ rules/progressive_web_applications/manifests/init.pp
@@ -1,7 +1,21 @@
 class progressive_web_applications {
+  define install ($url) {
+    $pwa_name = $title
+
+    exec {
+      "/usr/local/lib/puavo-setup-pwa ${pwa_name} ${url}":
+        creates => "/var/lib/puavo-pwa/manifests/${name}.json",
+        require => File['/usr/local/lib/puavo-setup-pwa'];
+    }
+  }
+
   file {
     '/usr/local/lib/puavo-setup-pwa':
       mode   => '0755',
       source => 'puppet:///modules/progressive_web_applications/puavo-setup-pwa';
+  }
+
+  Progressive_web_applications::Install {
+    'teams': url => 'https://teams.microsoft.com/manifest.json';
   }
 }
