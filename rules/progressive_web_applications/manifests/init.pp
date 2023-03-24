f8f85f94661f9b97e9a9224190c914266c70c631
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2023-03-24 14:33:35 +0200

    Setup PWAs for guest user when logging in.

--- rules/progressive_web_applications/manifests/init.pp
+++ rules/progressive_web_applications/manifests/init.pp
@@ -11,6 +11,9 @@ class progressive_web_applications {
   }
 
   file {
+    '/etc/X11/Xsession.d/80puavo-setup-pwas':
+      source => 'puppet:///modules/progressive_web_applications/80puavo-setup-pwas';
+
     '/usr/local/bin/puavo-pwa':
       mode   => '0755',
       source => 'puppet:///modules/progressive_web_applications/puavo-pwa';
@@ -27,14 +30,22 @@ class progressive_web_applications {
   # XXX this should actually setup stuff for both chrome and chromium?
   # XXX (in case chrome is installed into the system)
   # XXX Puavo_pkg::Install['google-chrome'] should be conditionally required
-  # XXX (only require in case it is set to be installed)
+  # XXX (only require it in case it is set to be installed)
   exec {
     'setup guestuser PWAs':
       command => '/usr/local/lib/puavo-setup-guestuser-pwas',
       require => [ File['/usr/local/lib/puavo-setup-guestuser-pwas']
+                 , File['/var/lib/puavo-guestsetup']
                  , Package['chromium']
                  , User['puavo-guestsetup'] ],
       user    => 'puavo-guestsetup';
+  }
+
+  file {
+    '/var/lib/puavo-guestsetup':
+      ensure => directory,
+      owner  => 'puavo-guestsetup',
+      group  => 'puavo-guestsetup';
   }
 
   user {
