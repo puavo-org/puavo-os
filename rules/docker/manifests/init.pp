9d904ea7e4fb44ef03d0ab24039a3bb56fada449
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2023-08-11 10:37:36 +0300

    Add a template file for docker-compose.

--- rules/docker/manifests/init.pp
+++ rules/docker/manifests/init.pp
@@ -1,4 +1,17 @@
 class docker {
   include ::docker::collabora
   include ::docker::nextcloud
+
+  file {
+    '/etc/puavo-docker':
+      ensure => directory;
+
+    '/etc/puavo-docker/docker-compose.yml.tmpl':
+      source => 'puppet:///modules/docker/docker-compose.yml.tmpl';
+  }
+
+  ::puavo_conf::definition {
+    'puavo-docker-nextcloud.json':
+      source => 'puppet:///modules/docker/puavo-docker-nextcloud.json';
+  }
 }
--- rules/docker/manifests/nextcloud.pp
+++ rules/docker/manifests/nextcloud.pp
@@ -1,9 +1,4 @@
 class docker::nextcloud {
   include ::docker::postgres
   include ::puavo_conf
-
-  ::puavo_conf::definition {
-    'puavo-docker-nextcloud.json':
-      source => 'puppet:///modules/docker/puavo-docker-nextcloud.json';
-  }
 }
