2d0ea31591f1282acf3d9f11621e5d3e28cc25b6
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2023-08-11 11:07:49 +0300

    Add scripts to setup, configure and run Nextcloud.

--- rules/docker/manifests/nextcloud.pp
+++ rules/docker/manifests/nextcloud.pp
@@ -3,7 +3,22 @@ class docker::nextcloud {
   include ::puavo_conf
 
   file {
+    '/etc/puavo-docker/files/configure-nextcloud':
+      mode   => '0755',
+      source => 'puppet:///modules/docker/configure-nextcloud';
+
+    '/etc/puavo-docker/files/run-nextcloud':
+      mode    => '0755',
+      require => File['/etc/puavo-docker/files/configure-nextcloud'],
+      source  => 'puppet:///modules/docker/run-nextcloud';
+
+    '/etc/puavo-docker/files/setup-nextcloud-docker':
+      mode   => '0755',
+      source => 'puppet:///modules/docker/setup-nextcloud-docker';
+
     '/etc/puavo-docker/files/Dockerfile.nextcloud':
+      require => [ File['/etc/puavo-docker/files/run-nextcloud']
+                 , File['/etc/puavo-docker/files/setup-nextcloud-docker'] ],
       source  => 'puppet:///modules/docker/Dockerfile.nextcloud';
   }
 }
