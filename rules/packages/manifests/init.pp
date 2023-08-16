f19de566d7a3a35136465fa418f2a34007d6515b
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2023-08-16 09:12:15 +0300

    Update nvidia 390 driver.

--- rules/packages/manifests/init.pp
+++ rules/packages/manifests/init.pp
@@ -765,7 +765,7 @@ class packages {
 
   $broadcom_sta_dkms_module = 'broadcom-sta/6.30.223.271'
   $nvidia_dkms_340_module   = 'nvidia-legacy-340xx/340.108'
-  $nvidia_dkms_390_module   = 'nvidia-legacy-390xx/390.154'
+  $nvidia_dkms_390_module   = 'nvidia-legacy-390xx/390.157'
   $nvidia_dkms_410_module   = 'nvidia-current/418.226.00'
   $r8168_module             = 'r8168/8.046.00'
   $virtualbox_module        = 'virtualbox/7.0.6'
