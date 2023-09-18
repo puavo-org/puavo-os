a57b9170992b15d8ba2f2cc7efa09770561f6e4a
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2023-09-18 12:09:45 +0300

    Update kernel versions.

--- rules/kernels/manifests/init.pp
+++ rules/kernels/manifests/init.pp
@@ -28,8 +28,8 @@ class kernels {
     }
   }
 
-  $default_kernel = '5.10.0-24-amd64'
-  $recent_kernel  = '6.1.0-0.deb11.9-amd64'
+  $default_kernel = '5.10.0-25-amd64'
+  $recent_kernel  = '6.1.0-0.deb11.11-amd64'
 
   ::kernels::all_kernel_links {
     'default': kernel => $default_kernel;
--- rules/packages/manifests/init.pp
+++ rules/packages/manifests/init.pp
@@ -806,16 +806,16 @@ class packages {
 		      , $virtualbox_module ]
 
   packages::kernels::kernel_package {
-    '5.10.0-24-amd64':
+    '5.10.0-25-amd64':
       dkms_modules => $all_dkms_modules,
-      package_name => 'linux-image-5.10.0-24-amd64';
+      package_name => 'linux-image-5.10.0-25-amd64';
 
     # XXX The $r8168_module works with 5.18 but the version
     # XXX in backports (in 2022-10-24) does not work with 5.19.
-    '6.1.0-0.deb11.9-amd64':
+    '6.1.0-0.deb11.11-amd64':
       dkms_modules => [ $broadcom_sta_dkms_module
                       , $virtualbox_module ],
-      package_name => 'linux-image-6.1.0-0.deb11.9-amd64-unsigned';
+      package_name => 'linux-image-6.1.0-0.deb11.11-amd64-unsigned';
   }
 
   # Packages which are not restricted per se, but which are required by
