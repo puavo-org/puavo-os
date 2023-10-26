eb2d53c8ffed154bc937f20427eaccd62c557cae
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2023-10-26 14:55:08 +0300

    Update VirtualBox version to 7.0.12.

--- rules/packages/manifests/init.pp
+++ rules/packages/manifests/init.pp
@@ -798,7 +798,7 @@ class packages {
   $nvidia_dkms_390_module   = 'nvidia-legacy-390xx/390.157'
   $nvidia_dkms_470_module   = 'nvidia-current/470.199.02'
   $r8168_module             = 'r8168/8.050.03'
-  $virtualbox_module        = 'virtualbox/7.0.10'
+  $virtualbox_module        = 'virtualbox/7.0.12'
 
   $all_dkms_modules = [ $broadcom_sta_dkms_module
                       , $nvidia_dkms_390_module
