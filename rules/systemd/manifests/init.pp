9ca596a077fe71db4a244543203dd4b3fb3aab4b
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2023-11-16 12:10:16 +0200

    Drop ntp and use systemd-timesyncd.
    Bootservers will not provide ntp services after this.
    Those were never really used anyway (fatclients sync clock at
    boot with rdate which is not using ntp, and laptops use ntp from
    the cloud through vpn).

--- rules/systemd/manifests/init.pp
+++ rules/systemd/manifests/init.pp
@@ -26,16 +26,5 @@ class systemd {
       force  => true;
   }
 
-  # We use ntpd.  Oddly enough, even though this should not
-  # affect ntpd, in some cases systemd-timesyncd can block it
-  # from starting up.  Do not try to use "service", we do not run
-  # systemd inside the container in image build, thus the service
-  # can not be disabled through the puppet "service"-type.
-  file {
-    '/etc/systemd/system/sysinit.target.wants/systemd-timesyncd.service':
-       ensure  => absent,
-       require => Package['systemd'];
-  }
-
   Package <| title == systemd |>
 }
