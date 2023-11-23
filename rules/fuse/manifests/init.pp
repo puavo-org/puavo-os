cbe5cd2a5f52fed182856a7c020b458f4a0c68ca
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2023-11-23 16:10:46 +0200

    Document that special fuse-configuration is needed by the "ekapeli" puavo-pkg package.

--- rules/fuse/manifests/init.pp
+++ rules/fuse/manifests/init.pp
@@ -1,4 +1,6 @@
 class fuse {
+  # This is needed by the "ekapeli" puavo-pkg package,
+  # possibly some other components as well.
   file {
     '/etc/fuse.conf':
       source => 'puppet:///modules/fuse/fuse.conf';
