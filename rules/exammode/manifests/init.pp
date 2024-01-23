521756197429188aaa1fdd9dfc1dc7da52ebe96c
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2024-01-23 11:13:34 +0200

    Add org.puavo.Exam dbus-service for exammode manager.
    Does not really do anything useful yet.

--- rules/exammode/manifests/init.pp
+++ rules/exammode/manifests/init.pp
@@ -4,6 +4,20 @@ class exammode {
   # XXX Note that Wayland may need something like this as well?
 
   file {
+    '/etc/dbus-1/system.d/org.puavo.Exam.conf':
+      source => 'puppet:///modules/exammode/org.puavo.Exam.conf';
+
+    '/usr/local/bin/puavo-switch-to-exammode':
+      mode   => '0755',
+      source => 'puppet:///modules/exammode/puavo-switch-to-exammode';
+
+    '/usr/local/sbin/puavo-exammode-manager':
+      mode   => '0755',
+      source => 'puppet:///modules/exammode/puavo-exammode-manager';
+
+    '/usr/share/dbus-1/system-services/org.puavo.Exam.service':
+      source => 'puppet:///modules/exammode/org.puavo.Exam.service';
+
     '/usr/share/X11/xorg.conf.d/90-disable-vtswitch.conf':
       require => Package['xserver-xorg-core'],
       source  => 'puppet:///modules/exammode/90-disable-vtswitch.conf';
