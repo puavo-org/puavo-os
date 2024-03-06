761a7b2d5205c94833844358647a709fffa65417
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2024-03-06 14:43:07 +0200

    Update Bookworm kernels.

--- rules/kernels/manifests/init.pp
+++ rules/kernels/manifests/init.pp
@@ -28,8 +28,8 @@ class kernels {
     }
   }
 
-  $default_kernel = '6.1.0-17-amd64'
-  $crisp_kernel   = '6.5.0-0.puavo1-amd64'
+  $default_kernel = '6.1.0-18-amd64'
+  $crisp_kernel   = '6.6.0-0.puavo1-amd64'
 
   ::kernels::all_kernel_links {
     'default': kernel => $default_kernel;
--- rules/packages/manifests/init.pp
+++ rules/packages/manifests/init.pp
@@ -841,15 +841,15 @@ class packages {
 		    # , $r8168_module                   # XXX bookworm
 
   packages::kernels::kernel_package {
-    '6.1.0-17-amd64':
+    '6.1.0-18-amd64':
       dkms_modules => $all_dkms_modules,
-      package_name => 'linux-image-6.1.0-17-amd64';
+      package_name => 'linux-image-6.1.0-18-amd64';
 
-    '6.5.0-0.puavo1-amd64':
+    '6.6.0-0.puavo1-amd64':
       # The current nvidia packages (470.199.02-1~deb12u1 and
       # 525.125.06-1~deb12u1) do not compile for 6.5.x.
       dkms_modules => [ $broadcom_sta_dkms_module, $virtualbox_module ],
-      package_name => 'linux-image-6.5.0-0.puavo1-amd64-unsigned';
+      package_name => 'linux-image-6.6.0-0.puavo1-amd64-unsigned';
   }
 
   # Packages which are not restricted per se, but which are required by
