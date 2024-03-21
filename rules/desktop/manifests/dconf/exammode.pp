2384a8a0085354610e4f78a0ef9975688b2afc77
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2024-03-21 12:37:01 +0200

    Lock exammode dconf keys and put them as the first set so that no other dconf-set can override them.

--- rules/desktop/manifests/dconf/exammode.pp
+++ rules/desktop/manifests/dconf/exammode.pp
@@ -2,8 +2,13 @@ class desktop::dconf::exammode {
   include ::desktop::dconf
 
   file {
-    '/etc/dconf/db/puavo-exammode.d':
+    [ '/etc/dconf/db/puavo-exammode.d'
+    , '/etc/dconf/db/puavo-exammode.d/locks' ]:
       ensure => directory;
+
+    '/etc/dconf/db/puavo-exammode.d/locks/puavo_exammode_locks':
+      content => template('desktop/dconf_puavo_exammode_locks'),
+      notify  => Exec['update dconf'];
 
     '/etc/dconf/db/puavo-exammode.d/puavo_exammode_profile':
       content => template('desktop/dconf_puavo_exammode_profile'),
