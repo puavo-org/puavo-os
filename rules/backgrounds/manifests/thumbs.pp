a325506b4ee3843f394f28cdab1ac1c12d2448a2
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2024-05-02 14:22:53 +0300

    Some changes to desktop wallpaper thumbnails generation.
    Use a bit wider scope to gather files to create thumbnails
    for.  Plus stylistic changes.

--- rules/backgrounds/manifests/thumbs.pp
+++ rules/backgrounds/manifests/thumbs.pp
@@ -1,110 +1,54 @@
 class backgrounds::thumbs {
   require ::backgrounds
+  include ::packages
 
-  # The 'id' is used to ensure each directory is
-  # processed only once. Remove the check file
-  # to re-process that directory.
-  $src_dirs =
-  [
+  # The 'id' is used to ensure each directory is processed only once.
+  # Remove the check file to re-process that directory.
+  $src_dirs = [
     {
       'path' => '/usr/share/backgrounds',
       'id'   => 'backgrounds'
     },
     {
-      'path' => '/usr/share/desktop-base/futureprototype-theme/login',
-      'id'   => 'desktop-base-futureprototype-login'
-    },
-    {
-      'path' => '/usr/share/desktop-base/futureprototype-theme/wallpaper',
-      'id'   => 'desktop-base-futureprototype-wallpaper'
-    },
-    {
-      'path' => '/usr/share/desktop-base/futureprototype-theme/wallpaper-withlogo',
-      'id'   => 'desktop-base-futureprototype-wallpaper-withlogo'
-    },
-    {
-      'path' => '/usr/share/desktop-base/joy-theme/login',
-      'id'   => 'desktop-base-joy-theme-login'
-    },
-    {
-      'path' => '/usr/share/desktop-base/joy-theme/wallpaper',
-      'id'   => 'desktop-base-joy-theme-wallpaper'
-    },
-    {
-      'path' => '/usr/share/desktop-base/lines-theme/login',
-      'id'   => 'desktop-base-lines-theme-login'
-    },
-    {
-      'path' => '/usr/share/desktop-base/lines-theme/wallpaper',
-      'id'   => 'desktop-base-lines-theme-wallpaper'
-    },
-    {
-      'path' => '/usr/share/desktop-base/lines-theme/lockscreen',
-      'id'   => 'desktop-base-lines-theme-lockscreen'
-    },
-    {
-      'path' => '/usr/share/desktop-base/moonlight-theme/login',
-      'id'   => 'desktop-base-moonlight-theme-login'
-    },
-    {
-      'path' => '/usr/share/desktop-base/moonlight-theme/wallpaper',
-      'id'   => 'desktop-base-moonlight-theme-wallpaper'
-    },
-    {
-      'path' => '/usr/share/desktop-base/moonlight-theme/lockscreen',
-      'id'   => 'desktop-base-moonlight-theme-lockscreen'
-    },
-    {
-      'path' => '/usr/share/desktop-base/softwaves-theme/login',
-      'id'   => 'desktop-base-softwaves-theme-login'
-    },
-    {
-      'path' => '/usr/share/desktop-base/softwaves-theme/wallpaper',
-      'id'   => 'desktop-base-softwaves-theme-wallpaper'
-    },
-    {
-      'path' => '/usr/share/desktop-base/softwaves-theme/lockscreen',
-      'id'   => 'desktop-base-softwaves-theme-lockscreen'
-    },
-    {
-      'path' => '/usr/share/desktop-base/spacefun-theme/login',
-      'id'   => 'desktop-base-spacefun-theme-login'
+      'path' => '/usr/share/desktop-base',
+      'id'   => 'desktop-base'
     }
   ]
 
-  $dst_dir = "/usr/share/background-thumbnails"
-
-  file {
-    "$dst_dir":
-      ensure => directory,
-      owner  => 'root',
-      group  => 'root',
-      mode   => '0755',
-  }
+  $dst_dir = '/usr/share/background-thumbnails'
 
   $src_dirs.each |$dir| {
-    $stamp = "${dst_dir}/${dir['id']}"
+    $stamp = "${dst_dir}/.${dir['id']}.thumbs_done"
 
     exec {
-      "/usr/bin/makethumbs ${dir['path']} ${dst_dir} && touch ${stamp}":
-        creates   => "${stamp}",
-        logoutput => 'on_failure'
+      "/usr/bin/makethumbs ${dir['path']} ${dst_dir} && /usr/bin/touch ${stamp}":
+        creates   => $stamp,
+        logoutput => 'on_failure',
+        require   => [ File[$dst_dir]
+                     , Package['puavo-devscripts'] ];
     }
   }
 
   file {
-    '/usr/bin/puavo-sync-wallpaper-thumbnails':
+    $dst_dir:
+      ensure => directory,
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0755';
+
+    '/etc/xdg/autostart/puavo-sync-wallpaper-thumbnails.desktop':
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+      require => File['/usr/local/bin/puavo-sync-wallpaper-thumbnails'],
+      source  => 'puppet:///modules/backgrounds/puavo-sync-wallpaper-thumbnails.desktop';
+
+    '/usr/local/bin/puavo-sync-wallpaper-thumbnails':
       source => 'puppet:///modules/backgrounds/puavo-sync-wallpaper-thumbnails',
       owner  => 'root',
       group  => 'root',
-      mode   => '0755',
+      mode   => '0755';
   }
 
-  file {
-    '/etc/xdg/autostart/puavo-sync-wallpaper-thumbnails.desktop':
-      source => 'puppet:///modules/backgrounds/puavo-sync-wallpaper-thumbnails.desktop',
-      owner  => 'root',
-      group  => 'root',
-      mode   => '0644',
-  }
+  Package <| title == "puavo-devscripts" |>
 }
