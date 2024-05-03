55da9673a683727cf886fafe739eb5263d4e73a5
Tuomas Räsänen <tuomas.rasanen@opinsys.fi>
2024-05-03 11:26:30 +0300

    parts/puavomenu: [refactor] use USER_AVATAR_SIZE constant for avatar icon sizes everywhere

--- parts/puavomenu/buttons/avatar.py
+++ parts/puavomenu/buttons/avatar.py
@@ -5,7 +5,7 @@ import logging
 import gi
 from gi.repository import Pango
 
-from constants import SIDEBAR_WIDTH
+from constants import SIDEBAR_WIDTH, USER_AVATAR_SIZE
 import utils_gui
 
 import buttons.base
@@ -15,8 +15,6 @@ import buttons.base
 # HoverIconButtonBase; Gtk.Label cannot be easily made to receive click
 # events.
 class AvatarButton(buttons.base.HoverIconButtonBase):
-    ICON_SIZE = 48
-
     def __init__(self, parent, settings, user_name, initial_image=None, tooltip=None):
         super().__init__(
             parent, settings, label=user_name, icon=None, tooltip=tooltip, data=None
@@ -31,16 +29,18 @@ class AvatarButton(buttons.base.HoverIconButtonBase):
             self.icon = None
 
         self.label_layout.set_alignment(Pango.Alignment.LEFT)
-        self.label_layout.set_width((SIDEBAR_WIDTH - self.ICON_SIZE - 8) * Pango.SCALE)
+        self.label_layout.set_width(
+            (SIDEBAR_WIDTH - USER_AVATAR_SIZE - 8) * Pango.SCALE
+        )
         self.label_layout.set_ellipsize(Pango.EllipsizeMode.END)
 
         self.compute_elements()
 
     def get_preferred_button_size(self):
-        return (SIDEBAR_WIDTH, self.ICON_SIZE)
+        return (SIDEBAR_WIDTH, USER_AVATAR_SIZE)
 
     def compute_elements(self):
-        self.icon_size = self.ICON_SIZE
+        self.icon_size = USER_AVATAR_SIZE
         self.icon_pos = [0, 0]
 
         # Center the text vertically
@@ -49,8 +49,8 @@ class AvatarButton(buttons.base.HoverIconButtonBase):
         ink, _ = self.label_layout.get_extents()
 
         self.label_pos = [
-            self.ICON_SIZE + 16,
-            (self.ICON_SIZE / 2) - ((ink.height / Pango.SCALE) / 2) - 5,
+            USER_AVATAR_SIZE + 16,
+            (USER_AVATAR_SIZE / 2) - ((ink.height / Pango.SCALE) / 2) - 5,
         ]
 
     # Must override the base method - the user avatar is not stored in
@@ -59,7 +59,7 @@ class AvatarButton(buttons.base.HoverIconButtonBase):
     def draw_icon(self, ctx):
         if self.icon:
             ctx.set_source_surface(self.icon, 0, 0)
-            ctx.rectangle(0, 0, self.ICON_SIZE, self.ICON_SIZE)
+            ctx.rectangle(0, 0, USER_AVATAR_SIZE, USER_AVATAR_SIZE)
             ctx.fill()
         else:
             utils_gui.draw_x(
@@ -72,7 +72,7 @@ class AvatarButton(buttons.base.HoverIconButtonBase):
             logging.info('Loading avatar image "%s"...', path)
 
             self.icon = utils_gui.load_image_at_size(
-                path, self.ICON_SIZE, self.ICON_SIZE
+                path, USER_AVATAR_SIZE, USER_AVATAR_SIZE
             )
 
             # trigger a redraw
