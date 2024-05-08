3d991c70297159f6275576c9842466ff193b71cd
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2024-05-08 08:52:43 +0300

    Remove special skel-files for guest-user.
    Just use the normal skel-directory.  I do not see what is
    special about guest-user in this way.

--- rules/guest/manifests/init.pp
+++ rules/guest/manifests/init.pp
@@ -6,9 +6,5 @@ class guest {
     '/etc/guest-session/reset-guestuser-home':
       mode   => '0755',
       source => 'puppet:///modules/guest/guest-session/reset-guestuser-home';
-
-    '/etc/guest-session/skel':
-      recurse => true,
-      source  => 'puppet:///modules/guest/guest-session/skel';
   }
 }
