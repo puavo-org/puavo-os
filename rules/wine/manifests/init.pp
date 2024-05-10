c54e89fc6f4937aaef3b9817b98860badf54f0c6
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2024-05-10 14:12:05 +0300

    Small stylistic changes in "wine" puppet-module.

--- rules/wine/manifests/init.pp
+++ rules/wine/manifests/init.pp
@@ -4,26 +4,21 @@ class wine {
   include ::wine::setup
   include ::wine::strip
 
-
-  file {
-    '/usr/share/applications/wine.desktop':
-      ensure  => link,
-      require => Package['wine-devel'],
-      target  => '/opt/wine-devel/share/applications/wine.desktop';
-  }
-
   file {
     '/usr/bin/wine-development':
       require => Package['wine-devel'],
       mode    => '0755',
       source  => 'puppet:///modules/wine/wine-launcher';
-  }
 
-  file {
     '/usr/bin/wine-stable':
       require => Package['wine-devel'],
       mode    => '0755',
       source  => 'puppet:///modules/wine/wine-launcher';
+
+    '/usr/share/applications/wine.desktop':
+      ensure  => link,
+      require => Package['wine-devel'],
+      target  => '/opt/wine-devel/share/applications/wine.desktop';
   }
 
   Package <| title == 'winehq-devel' |>
