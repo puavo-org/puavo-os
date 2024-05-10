37aefe7dd6c7fb1a4cb0a893bf49e31400cd50d3
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2024-05-10 13:50:51 +0300

    Update rng-tools-configuration.

--- rules/use_urandom/manifests/init.pp
+++ rules/use_urandom/manifests/init.pp
@@ -2,10 +2,10 @@ class use_urandom {
   include ::packages
 
   file {
-    '/etc/default/rng-tools':
-      content => template('use_urandom/rng-tools'),
+    '/etc/default/rng-tools-debian':
+      content => template('use_urandom/rng-tools-debian'),
       require => Package['rng-tools-debian'];
   }
 
-  Package <| title == rng-tools |>
+  Package <| title == rng-tools-debian |>
 }
