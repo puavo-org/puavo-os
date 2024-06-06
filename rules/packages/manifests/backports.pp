ef8f88cd4016f98ab132ef928677602ddd5d5f2e
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2024-06-06 13:06:33 +0300

    Get systemd from backports so we get the "ukify" application.

--- rules/packages/manifests/backports.pp
+++ rules/packages/manifests/backports.pp
@@ -6,5 +6,25 @@ class packages::backports {
 
   $package_list = [
     'kernel-wedge'      # needed by bpo kernel build
+
+    # systemd and its dependencies from backports so we get ukify
+    , 'libnss-myhostname'
+    , 'libnss-mymachines'
+    , 'libnss-systemd'
+    , 'libpam-systemd'
+    , 'libsystemd0'
+    , 'libsystemd-dev'
+    , 'libsystemd0:i386'
+    , 'libsystemd-shared'
+    , 'libudev1'
+    , 'libudev1:i386'
+    , 'libudev-dev'
+    , 'systemd'
+    , 'systemd-boot-efi'
+    , 'systemd-container'
+    , 'systemd-dev'
+    , 'systemd-sysv'
+    , 'systemd-timesyncd'
+    , 'udev'
   ]
 }
