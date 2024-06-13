0a827e02b0acc295a4b056c37d7923230d445d10
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2024-06-13 09:12:22 +0300

    Update Bullseye kernels.

--- rules/kernels/manifests/init.pp
+++ rules/kernels/manifests/init.pp
@@ -28,8 +28,8 @@ class kernels {
     }
   }
 
-  $default_kernel = '5.10.0-29-amd64'
-  $recent_kernel  = '6.1.0-0.deb11.18-amd64'
+  $default_kernel = '5.10.0-30-amd64'
+  $recent_kernel  = '6.1.0-0.deb11.21-amd64'
 
   ::kernels::all_kernel_links {
     'default': kernel => $default_kernel;
