5162d7a16c1da713cfe0903159f1afcfd578562f
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2024-07-26 13:38:10 +0300

    Update "recent" kernel to version "6.1.0-0.deb11.22".

--- rules/kernels/manifests/init.pp
+++ rules/kernels/manifests/init.pp
@@ -29,7 +29,7 @@ class kernels {
   }
 
   $default_kernel = '5.10.0-31-amd64'
-  $recent_kernel  = '6.1.0-0.deb11.21-amd64'
+  $recent_kernel  = '6.1.0-0.deb11.22-amd64'
 
   ::kernels::all_kernel_links {
     'default': kernel => $default_kernel;
