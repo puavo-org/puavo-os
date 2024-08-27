c92b03157babc02885bf76ae1ea7e9b66b31f32b
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2024-08-27 10:36:11 +0300

    Add puavo-conf variable "puavo.printing.debug.cups" for turning on CUPS daemon debug logging.  Disabled by default.

--- rules/desktop_cups/manifests/init.pp
+++ rules/desktop_cups/manifests/init.pp
@@ -2,18 +2,24 @@ class desktop_cups {
   include ::puavo_conf
 
   file {
-    '/etc/cups/cupsd.conf':
-      source => 'puppet:///modules/desktop_cups/etc_cups_cupsd.conf';
-
     '/usr/local/bin/puavo-show-printer-restrictions':
       mode    => '0755',
       require => ::Puavo_conf::Script['setup_desktop_printer_restrictions'],
       source  => 'puppet:///modules/desktop_cups/puavo-show-printer-restrictions';
   }
 
+  ::puavo_conf::definition {
+    'puavo-cups.json':
+      source => 'puppet:///modules/desktop_cups/puavo-cups.json';
+  }
+
   ::puavo_conf::script {
     'setup_cups_browsed':
       source => 'puppet:///modules/desktop_cups/setup_cups_browsed';
+
+    'setup_desktop_cups':
+      require => ::Puavo_conf::Definition['puavo-cups.json'],
+      source  => 'puppet:///modules/desktop_cups/setup_desktop_cups';
 
     'setup_desktop_printer_restrictions':
       source => 'puppet:///modules/desktop_cups/setup_desktop_printer_restrictions';
