eb3d9a98ee9f13bc27b23ba426e34b43606450f1
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2024-09-02 13:33:23 +0300

    Update "crisp"-kernel to version 6.10.6 (in puppet rules).

--- rules/packages/manifests/init.pp
+++ rules/packages/manifests/init.pp
@@ -845,13 +845,13 @@ class packages {
       dkms_modules => $all_dkms_modules,
       package_name => 'linux-image-6.1.0-23-amd64';
 
-    '6.9.7-amd64':
+    '6.10.6-amd64':
       # The current nvidia packages (470.199.02-1~deb12u1 and
       # 525.125.06-1~deb12u1) do not compile for 6.5.x.
       # The same is true for the current VirtualBox version in
       # debian-fasttrack.
       dkms_modules => [ $broadcom_sta_dkms_module ],
-      package_name => 'linux-image-6.9.7-amd64-unsigned';
+      package_name => 'linux-image-6.10.6-amd64-unsigned';
   }
 
   # various contrib/non-free stuff, firmwares and such
