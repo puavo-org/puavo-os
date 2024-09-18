9ba22f893022cb85bd2c398f968272381024efdc
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2024-09-18 13:10:14 +0300

    Add "puavo.exammode.developer-mode.enabled" puavo-conf variable.
    Defaults to "false".  Set to true if you are developing the
    the examination mode.
    
    As the locking mechanisms in the examination mode are now
    mostly effective (but not perfect), there is a need for a switch
    to actually escape them for developing the examination mode itself.

--- rules/desktop/manifests/dconf/exammode.pp
+++ rules/desktop/manifests/dconf/exammode.pp
@@ -3,7 +3,9 @@ class desktop::dconf::exammode {
 
   file {
     [ '/etc/dconf/db/puavo-exammode.d'
-    , '/etc/dconf/db/puavo-exammode.d/locks' ]:
+    , '/etc/dconf/db/puavo-exammode.d/locks'
+    , '/etc/dconf/db/puavo-exammode-devel.d'
+    , '/etc/dconf/db/puavo-exammode-devel.d/locks' ]:
       ensure => directory;
 
     '/etc/dconf/db/puavo-exammode.d/locks/puavo_exammode_locks':
@@ -12,6 +14,14 @@ class desktop::dconf::exammode {
 
     '/etc/dconf/db/puavo-exammode.d/puavo_exammode_profile':
       content => template('desktop/dconf_puavo_exammode_profile'),
+      notify  => Exec['update dconf'];
+
+    '/etc/dconf/db/puavo-exammode-devel.d/locks/puavo_exammode_devel_locks':
+      content => template('desktop/dconf_puavo_exammode_devel_locks'),
+      notify  => Exec['update dconf'];
+
+    '/etc/dconf/db/puavo-exammode-devel.d/puavo_exammode_devel_profile':
+      content => template('desktop/dconf_puavo_exammode_devel_profile'),
       notify  => Exec['update dconf'];
   }
 }
