5075128c6347cfe9a35a5e380d6273962fee0f87
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2024-09-20 09:50:10 +0300

    In puavo-exammode gnome-shell extension, move the "Quit exam" button to the system menu.

--- rules/gnome_shell_extensions/files/puavo-exammode@puavo.org/extension.js
+++ rules/gnome_shell_extensions/files/puavo-exammode@puavo.org/extension.js
@@ -2,36 +2,46 @@ const { Clutter, Gio, GLib, GObject, St } = imports.gi;
 const Main = imports.ui.main;
 const Util = imports.misc.util;
 
-const PanelMenu = imports.ui.panelMenu;
-const PopupMenu = imports.ui.popupMenu;
+const { QuickMenuToggle, SystemIndicator } = imports.ui.quickSettings;
 
 const exam_session_path = '/var/lib/puavo-exammode/session.json';
-const quit_icon_path = '/usr/share/icons/Adwaita/64x64/actions/application-exit-symbolic.symbolic.png'
 
+var control_info_label;
 var exam_name_label;
-var fullscreen_info_label;
+var indicator;
 
-class ExamMenu extends PanelMenu.Button {
-  static { GObject.registerClass(this); }
+const QuitExamButton = GObject.registerClass(
+class QuitExamButton extends QuickMenuToggle {
+    _init() {
+        super._init({
+            label: _('Quit exam'),
+            hasMenu: true,
+            canFocus: true,
+            accessible_name: _('Quit'),
+        });
 
-  constructor() {
-    super(0.0, 'Exam menu');
+        this.menu.addAction(_('Quit, because I am done now'), this.quit);
+    }
 
-    let icon = new St.Icon();
-    let gicon = Gio.icon_new_for_string(quit_icon_path.toString());
-    icon.set_gicon(gicon);
-    this.add_child(icon);
-
-    this.menu.addAction('Quit Exam', event => {
-      // XXX could there be a dbus-module to use instead of Util.spawn?
+    // XXX do not use dbus-send
+    quit() {
       let cmd = [ '/usr/bin/dbus-send', '--dest=org.puavo.Exam',
                     '--print-reply=literal', '--reply-timeout=30000',
                     '--system', '/exammode',
                     'org.puavo.Exam.exammode.QuitSession' ];
       Util.spawn(cmd);
-    });
-  }
-}
+    }
+});
+
+var Indicator = GObject.registerClass(
+class Indicator extends SystemIndicator {
+    _init() {
+        super._init();
+        this.quickSettingsItems.push(new QuitExamButton());
+        let qs = Main.panel.statusArea.quickSettings;
+        qs._addItems(this.quickSettingsItems, 2);
+    }
+});
 
 function init() {
   let [ ok, exam_session_json ] = GLib.file_get_contents(exam_session_path);
@@ -41,25 +51,26 @@ function init() {
   let utf8decoder = new TextDecoder();
   let exam_session_info = JSON.parse( utf8decoder.decode(exam_session_json) );
 
+  info_text = 'adjust text scale with ctrl+ and ctrl-'
+                + '  |  press F11 to toggle fullscreen';
+  control_info_label = new St.Label({
+                         text: info_text,
+                         y_align: Clutter.ActorAlign.CENTER,
+                       });
+
   exam_name_label = new St.Label({
                       text:    exam_session_info['name'],
                       y_align: Clutter.ActorAlign.CENTER,
                     });
 
-  fullscreen_info_label = new St.Label({
-                            text: 'adjust text scale with ctrl+ and ctrl-  |  press F11 to toggle fullscreen',
-                            y_align: Clutter.ActorAlign.CENTER,
-                          });
+  indicator = new Indicator();
 }
 
 function enable() {
-  exam_menu = new ExamMenu();
   Main.panel._centerBox.insert_child_at_index(exam_name_label, 0);
-  Main.panel._rightBox.insert_child_at_index(fullscreen_info_label, 0);
-  Main.panel.addToStatusArea('exam-menu', exam_menu, -1, 'right');
+  Main.panel._rightBox.insert_child_at_index(control_info_label, 0);
 } 
 
 function disable() {
   Main.panel._rightBox.remove_child(exam_name_label);
-  exam_menu.destroy();
 }
