12e18166218d8e618f599d636c1a9bc74c2b2657
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2024-09-23 14:58:04 +0300

    Add another level of confirmation to "quit exam".

--- rules/gnome_shell_extensions/files/puavo-exammode@puavo.org/extension.js
+++ rules/gnome_shell_extensions/files/puavo-exammode@puavo.org/extension.js
@@ -1,5 +1,7 @@
 const { Clutter, Gio, GLib, GObject, St } = imports.gi;
 const Main = imports.ui.main;
+const Dialog = imports.ui.dialog;
+const ModalDialog = imports.ui.modalDialog;
 const Util = imports.misc.util;
 
 const { QuickMenuToggle, SystemIndicator } = imports.ui.quickSettings;
@@ -21,11 +23,42 @@ class QuitExamButton extends QuickMenuToggle {
         });
 
         this.menu.addAction(_('Quit, because I am done now'),
-                            () => { this._quit(this); });
+                            () => { this._do_confirm_quit(this) });
     }
 
     // for some odd reason "this" is not available here
-    _quit(_this) {
+    _do_confirm_quit(_this) { _this._confirm_quit(); }
+
+    _confirm_quit() {
+        let testDialog = new ModalDialog.ModalDialog({
+            destroyOnClose: false,
+            styleClass: 'end-session-dialog',
+        });
+
+        let messageDialogContent = new Dialog.MessageDialogContent();
+        messageDialogContent.title = _('Really quit exam?');
+        messageDialogContent.description = _('QUIT QUIT QUIT');
+        testDialog.contentLayout.add_child(messageDialogContent);
+
+        testDialog.setButtons([
+            {
+                label: _('No, get back'),
+                action: () => { testDialog.destroy(); },
+            },
+            {
+                label: _('Yes, quit'),
+                isDefault: true,
+                action: () => {
+                  testDialog.close(global.get_current_time());
+                  this._quit();
+                },
+            },
+        ]);
+
+        testDialog.open(global.get_current_time(), true, false);
+    }
+
+    _quit() {
         try {
             const dbus_call = Gio.DBus.system.call(
                                 'org.puavo.Exam',
@@ -34,7 +67,7 @@ class QuitExamButton extends QuickMenuToggle {
                                 'QuitSession',
                                 (new GLib.Variant('()', [])),
                                 null, Gio.DBusCallFlags.NONE, -1, null);
-            dbus_call.then(() => {}, _this._quit_error);
+            dbus_call.then(() => {}, this._quit_error);
         } catch (e) {
             console.log('Could not make dbus call to quit exam session: ' + e);
             return;
