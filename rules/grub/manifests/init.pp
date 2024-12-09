caa0294f4f7110a5c099e4177d3fcda8910995c2
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2024-12-09 12:18:47 +0200

    Bump Grub version for Trixie.

--- rules/grub/manifests/init.pp
+++ rules/grub/manifests/init.pp
@@ -2,12 +2,12 @@ class grub {
   include ::grub::themes
   include ::puavo_conf
 
-  $grub_version = '2.06-13+deb12u1'
+  $grub_version = '2.12-5'
 
   # Keep track of the "grub-efi-amd64-signed" package
   # version, even though currently installing that package
   # will break Grub in UEFI machines.
-  $grub_version_signed = '1+2.06-13+deb12u1'
+  $grub_version_signed = '1+2.12-5'
 
   file {
     [ '/boot', '/boot/grub', '/boot/grub/puavo' ]:
