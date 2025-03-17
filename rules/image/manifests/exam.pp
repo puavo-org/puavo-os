ac4006492489f930bb8556bdfb696b4f8dd46b5b
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2025-03-17 12:53:26 +0200

    Install "plocate" to exam image.

--- rules/image/manifests/exam.pp
+++ rules/image/manifests/exam.pp
@@ -4,6 +4,9 @@ class image::exam {
 
   # XXX This list could be made smaller, "tag_puavo" includes many
   # XXX packages not needed for this.
-  Package <| tag == 'tag_kernel'
-          or tag == 'tag_puavo'  |>
+  Package <|
+       tag   == 'tag_kernel'
+    or tag   == 'tag_puavo'
+    or title == 'plocate'
+  |>
 }
