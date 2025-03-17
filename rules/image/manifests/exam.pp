7f67dc3ca0ccdb2adaf0c2e6284add31ec07d760
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2025-03-17 11:58:22 +0200

    Add ::image::exam puppet class.

---
+++ rules/image/manifests/exam.pp
@@ -0,0 +1,8 @@
+class image::exam {
+  include ::packages
+
+  # XXX This list could be made smaller, "tag_puavo" includes many
+  # XXX packages not needed for this.
+  Package <| tag == 'tag_kernel'
+          or tag == 'tag_puavo'  |>
+}
