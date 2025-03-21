9d84e118f9924c7fc23438224561ebf8a792c29b
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2025-03-21 14:14:46 +0200

    Get rid of "puavo-ltsp-client" dependency in examination mode.

--- rules/image/manifests/exam.pp
+++ rules/image/manifests/exam.pp
@@ -10,7 +10,6 @@ class image::exam {
     or title == 'puavo-conf'
     or title == 'puavo-core'
     or title == 'puavo-exammode'
-    or title == 'puavo-ltsp-client'
     or title == 'puavo-pam'
     or title == 'puavo-pkg'
   |>
