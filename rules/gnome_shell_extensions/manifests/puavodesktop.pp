b32889b4c96cf9d34b6cda6c422901f63610ab3a
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2025-03-21 14:25:37 +0200

    Do not include "themes" puppet-module in gnome_shell_extensions.
    These are not (should not be) needed in the exam-image case,
    and those pull in puavo-ltsp-client.

--- rules/gnome_shell_extensions/manifests/puavodesktop.pp
+++ rules/gnome_shell_extensions/manifests/puavodesktop.pp
@@ -2,6 +2,7 @@ class gnome_shell_extensions::puavodesktop {
   include ::gnome_shell_extensions
   include ::gnome_shell_extensions::ding
   include ::gnome_shell_extensions::screenkeyboardcontroller
+  include ::themes
 
   ::Gnome_shell_extensions::Add_extension <| |>
 }
