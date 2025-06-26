620b5146771c874f7c7b8b28c788ac87f2a9f106
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2025-06-26 14:24:49 +0300

    Install the updated crisp-kernel.

--- rules/packages/manifests/init.pp
+++ rules/packages/manifests/init.pp
@@ -856,14 +856,14 @@ class packages {
       dkms_modules => $all_dkms_modules,
       package_name => 'linux-image-6.1.0-37-amd64';
 
-    '6.12.12-amd64':
+    '6.12.27-amd64':
       # The current nvidia packages (470.199.02-1~deb12u1 and
       # 525.125.06-1~deb12u1) do not compile for 6.5.x.
       # The broadcom sta module does not compile for 6.12.x.
       # The same is true for the current VirtualBox version in
       # debian-fasttrack.
       dkms_modules => [],
-      package_name => 'linux-image-6.12.12-amd64-unsigned';
+      package_name => 'linux-image-6.12.27-amd64-unsigned';
   }
 
   # various contrib/non-free stuff, firmwares and such
