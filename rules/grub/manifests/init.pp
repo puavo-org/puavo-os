bb9bca5f3639a4c8211f8c5341d089acbc94e2d6
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2025-08-15 08:46:37 +0300

    Update Grub version.

--- rules/grub/manifests/init.pp
+++ rules/grub/manifests/init.pp
@@ -2,8 +2,8 @@ class grub {
   include ::grub::themes
   include ::puavo_conf
 
-  $grub_version = '2.12-8'
-  $grub_version_signed = "1+2.12+8"
+  $grub_version = '2.12-9'
+  $grub_version_signed = "1+2.12+9"
 
   file {
     [ '/boot', '/boot/grub', '/boot/grub/puavo' ]:
