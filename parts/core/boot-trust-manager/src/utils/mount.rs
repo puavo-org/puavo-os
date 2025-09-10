41a2167eef7ad72f7ed6a9362fb41f0dab02dc3a
Joonas Lehto <joonas.lehto@opinsys.fi>
2025-09-10 11:10:20 +0300

    puavo-boot-trust-manager: Add code documentation to utilities

--- parts/core/boot-trust-manager/src/utils/mount.rs
+++ parts/core/boot-trust-manager/src/utils/mount.rs
@@ -8,6 +8,11 @@ use std::{
 use log::{debug, warn};
 use nix::libc::{self, MNT_DETACH};
 
+/// Attempt to unmount the specified mountpoint.
+///
+/// This first tries a lazy unmount (`MNT_DETACH`) to reduce EBUSY errors,
+/// and falls back to a regular unmount. Returns the last OS error if both
+/// attempts fail.
 pub fn unmount(mountpoint: &PathBuf) -> io::Result<()> {
     let path =
         CString::new(mountpoint.as_os_str().as_bytes()).map_err(|_| {
@@ -32,10 +37,12 @@ pub struct MountGuard {
 }
 
 impl MountGuard {
+    /// Create a new guard object that will unmount the specified mountpoint when dropped.
     pub fn new<P: AsRef<Path>>(mountpoint: P) -> Self {
         Self { mountpoint: mountpoint.as_ref().to_path_buf() }
     }
 
+    /// Explicitly unmount the guarded mount point.
     pub fn unmount(&self) -> io::Result<()> {
         unmount(&self.mountpoint)
     }
--- parts/core/boot-trust-manager/src/utils/power.rs
+++ parts/core/boot-trust-manager/src/utils/power.rs
@@ -1,5 +1,7 @@
 use std::process::Command;
 
+/// Attempt to reboot the system. If the reboot command returns, spin to
+/// prevent further execution.
 pub fn reboot_or_halt() {
     let _ = Command::new("reboot").status();
     loop {}
