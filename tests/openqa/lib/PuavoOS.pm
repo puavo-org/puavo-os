b73897ffa8f90319d36d490cbc79e488bf0f7f98
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2025-09-24 14:24:41 +0300

    OpenQA: share code in both "normal" and "crypt-install"-modes.

---
+++ tests/openqa/lib/PuavoOS.pm
@@ -0,0 +1,37 @@
+package PuavoOS;
+
+use Mojo::Base 'basetest';
+use testapi;
+
+sub darkdm_install {
+  my ($cmd) = @_;
+
+  # Start preinstall with disk encryption
+  type_string($cmd);
+  send_key('ret', wait_screen_change => 1);
+  send_key('ret', wait_screen_change => 1); # select laptop as the host type
+  send_key('ret', wait_screen_change => 1); # select the first disk as target
+
+  type_string('no'); # do not install Windows to another partition
+  send_key('ret', wait_screen_change => 1);
+
+  # Confirm the installation.
+  type_string('yes');
+  send_key('ret', wait_screen_change => 1);
+  record_info('Puavo', 'Preinstalling...');
+
+  assert_screen('grub-installation-done', timeout => 300);
+  record_info('Puavo', 'Grub installation done.');
+
+  assert_screen('install-success', timeout => 300);
+  record_info('Puavo', 'Installation successful.');
+
+  assert_screen('rebooting-after-install', timeout => 300);
+  record_info('Puavo', 'Rebooting after installation.');
+
+  # In order to save the disk for other tests, we need to shutdown.
+  power('acpi');
+  assert_shutdown();
+}
+
+1;
--- tests/openqa/tests/normal_preinstall.pm
+++ tests/openqa/tests/normal_preinstall.pm
@@ -1,35 +1,10 @@
 use Mojo::Base 'basetest';
+use PuavoOS;
 use testapi;
 
 sub run {
   select_console 'sut';
-
-  # Start preinstall.
-  type_string('preinstall');
-  send_key('ret', wait_screen_change => 1);
-  send_key('ret', wait_screen_change => 1); # select laptop as the host type
-  send_key('ret', wait_screen_change => 1); # select the first disk as target
-
-  type_string('no'); # do not install Windows to another partition
-  send_key('ret', wait_screen_change => 1);
-
-  # Confirm the installation.
-  type_string('yes');
-  send_key('ret', wait_screen_change => 1);
-  record_info('Puavo', 'Preinstalling...');
-
-  assert_screen('grub-installation-done', timeout => 300);
-  record_info('Puavo', 'Grub installation done.');
-
-  assert_screen('install-success', timeout => 300);
-  record_info('Puavo', 'Installation successful.');
-
-  assert_screen('rebooting-after-install', timeout => 300);
-  record_info('Puavo', 'Rebooting after installation.');
-
-  # In order to save the disk for other tests, we need to shutdown.
-  power('acpi');
-  assert_shutdown();
+  PuavoOS::darkdm_install('preinstall');
 }
 
 sub test_flags {
