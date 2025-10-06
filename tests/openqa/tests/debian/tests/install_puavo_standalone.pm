08ca0001557106769b8ec32d8fa72db4f9576f00
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2025-10-06 10:59:14 +0300

    OpenQA: Download and run puavo-standalone setup script.

--- tests/openqa/tests/debian/tests/install_puavo_standalone.pm
+++ tests/openqa/tests/debian/tests/install_puavo_standalone.pm
@@ -9,6 +9,8 @@ sub run {
 
   assert_script_run('sudo apt -y update');
   assert_script_run('sudo apt -y dist-upgrade');
+  assert_script_run('wget https://github.com/puavo-org/puavo-standalone/raw/master/setup.sh');
+  assert_script_run('sudo sh setup.sh');
 }
 
 sub test_flags {
