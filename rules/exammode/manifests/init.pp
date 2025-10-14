64b0ea5e862fcc8a8581fd3dd718ae442ab3806e
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2025-10-14 13:47:39 +0300

    Do not create an empty gdm3.service.d directory, it is not needed.

--- rules/exammode/manifests/init.pp
+++ rules/exammode/manifests/init.pp
@@ -26,9 +26,6 @@ class exammode {
     '/etc/dbus-1/system.d/org.puavo.Exam.conf':
       source => 'puppet:///modules/exammode/org.puavo.Exam.conf';
 
-    '/etc/systemd/system/gdm3.service.d':
-      ensure => directory;
-
     '/etc/systemd/system/puavo-exammode-tty.service':
       require => Package['systemd'],
       source  => 'puppet:///modules/exammode/puavo-exammode-tty.service';
