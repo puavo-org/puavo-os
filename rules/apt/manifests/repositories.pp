42d20d0a2c85b282631ed0452110e9869ddff5e5
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2025-10-22 12:49:40 +0300

    Modernize Debian apt sources.

--- rules/apt/manifests/repositories.pp
+++ rules/apt/manifests/repositories.pp
@@ -16,29 +16,35 @@ class apt::repositories {
         notify  => Exec['apt update'];
 
       '/etc/apt/sources.list':
-        content => template('apt/sources.list'),
+        ensure => absent;
+
+      '/etc/apt/sources.list.d/debian.sources':
+        content => template('apt/debian.sources'),
         notify  => Exec['apt update'];
 
-      # Put the local this into a separate file so it can be excluded
-      # in the image build along with the actual archive.
-      '/etc/apt/sources.list.d/puavo-os-local.list':
-        content => template('apt/puavo-os-local.list'),
+      '/etc/apt/sources.list.d/debian-backports.sources':
+        content => template('apt/debian-backports.sources'),
         notify  => Exec['apt update'];
 
-      '/etc/apt/sources.list.d/puavo-os-remote.list':
-        content => template('apt/puavo-os-remote.list'),
-        notify  => Exec['apt update'];
-
-      '/etc/apt/trusted.gpg.d/opinsys.gpg':
-        before => Exec['apt update'],
-        source => 'puppet:///modules/apt/opinsys.gpg';
-    }
-
-    file {
       '/etc/apt/sources.list.d/debian-fasttrack.sources':
         content => template('apt/debian-fasttrack.sources'),
         notify  => Exec['apt update'],
         require => Package['fasttrack-archive-keyring'];
+
+      # Put the local this into a separate file so it can be excluded
+      # in the image build along with the actual archive.
+      '/etc/apt/sources.list.d/puavo-os-local.sources':
+        content => template('apt/puavo-os-local.sources'),
+        notify  => Exec['apt update'];
+
+      '/etc/apt/sources.list.d/puavo-os-remote.sources':
+        content => template('apt/puavo-os-remote.sources'),
+        notify  => Exec['apt update'];
+
+      # XXX should not add to globally trusted GPG keys
+      '/etc/apt/trusted.gpg.d/opinsys.gpg':
+        before => Exec['apt update'],
+        source => 'puppet:///modules/apt/opinsys.gpg';
     }
 
     package {
