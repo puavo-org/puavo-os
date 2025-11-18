ef0081b1d451a5ed5edbbb4efd7c388a0383b82c
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2025-11-18 11:54:07 +0100

    If benchmarks were run before installation, store the results of those to /state so we can keep and send those later. Establish /var/lib/puavo for this and other such data.

--- rules/benchmarks/manifests/init.pp
+++ rules/benchmarks/manifests/init.pp
@@ -8,9 +8,16 @@ class benchmarks {
       require => [ Package['yq']
                  , Puavo_pkg::Install['passmark-performance-test'] ],
       source  => 'puppet:///modules/benchmarks/puavo-benchmark';
+
+    '/var/lib/puavo/benchmark':
+      ensure => directory;
   }
 
-  Package <| title == yq |>
+  # puavo-core should setup /var/lib/puavo
+  Package <|
+       title == puavo-core
+    or title == yq
+  |>
 
   Puavo_pkg::Install <| title == passmark-performance-test |>
 }
