1485c1c7cadf379806e213d382380a04469d653b
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2025-11-25 14:50:29 +0200

    Update the default Trixie kernel to 6.12.57.

--- rules/packages/manifests/init.pp
+++ rules/packages/manifests/init.pp
@@ -858,8 +858,8 @@ class packages {
   @packages::kernels::kernel_package {
     'default':
       dkms_modules => $all_dkms_modules,
-      package_name => 'linux-image-6.12.48+deb13-amd64',
-      version      => '6.12.48+deb13-amd64';
+      package_name => 'linux-image-6.12.57+deb13-amd64',
+      version      => '6.12.57+deb13-amd64';
 
     'modern':
       dkms_modules => [],  # XXX what could work with this?
