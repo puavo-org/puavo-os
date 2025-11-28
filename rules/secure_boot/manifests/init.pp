7ec31fe25b44f24d57b60c1fcd2fe16bea2a66dc
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2025-11-28 13:00:56 +0200

    Rename "/etc/secure-boot" to "/etc/puavo-secure-boot".
    Makes it more clear that these relate to Puavo OS.

--- rules/secure_boot/manifests/init.pp
+++ rules/secure_boot/manifests/init.pp
@@ -1,33 +1,29 @@
 class secure_boot {
-  $base_directory = '/etc/secure-boot'
+  $base_directory = '/etc/puavo-secure-boot'
   $certificates_directory = "${base_directory}/certificates"
 
   file {
-    $base_directory:
+    [ $base_directory, $certificates_directory, ]:
       ensure => directory;
 
-    $certificates_directory:
-      ensure  => directory,
-      require => File[$base_directory];
-
-    "${certificates_directory}/pk":
-      ensure => directory,
-      source => 'puppet:///modules/secure_boot/pk',
-      recurse => true;
-
-    "${certificates_directory}/kek":
-      ensure => directory,
-      source => 'puppet:///modules/secure_boot/kek',
-      recurse => true;
-
     "${certificates_directory}/db":
-      ensure => directory,
-      source => 'puppet:///modules/secure_boot/db',
-      recurse => true;
+      ensure  => directory,
+      recurse => true,
+      source  => 'puppet:///modules/secure_boot/db';
 
     "${certificates_directory}/dbx":
-      ensure => directory,
-      source => 'puppet:///modules/secure_boot/dbx',
-      recurse => true;
+      ensure  => directory,
+      recurse => true,
+      source  => 'puppet:///modules/secure_boot/dbx';
+
+    "${certificates_directory}/kek":
+      ensure  => directory,
+      recurse => true,
+      source  => 'puppet:///modules/secure_boot/kek';
+
+    "${certificates_directory}/pk":
+      ensure  => directory,
+      recurse => true,
+      source  => 'puppet:///modules/secure_boot/pk';
   }
 }
