8212cbc94bbd04402e47e837fe763135e1c2f81e
Christian Sarre <christian@sarre.eu>
2026-01-27 20:55:03 +0200

    rules/bootserver_pxe: Remove leftover 32-bit support.

--- rules/bootserver_pxe/manifests/init.pp
+++ rules/bootserver_pxe/manifests/init.pp
@@ -18,10 +18,6 @@ class bootserver_pxe {
       filedir => '/usr/lib/grub/pxe',
       require => Package['grub-pc'];
 
-    [ 'efi32/grub-pxe-i386.efi' ]:
-      filedir => '/usr/lib/grub/pxe',
-      require => Package['grub-efi-ia32-bin'];
-
     [ 'efi64/grub-pxe-x64.efi' ]:
       filedir => '/usr/lib/grub/pxe',
       require => Package['grub-efi-amd64-bin'];
@@ -32,13 +28,8 @@ class bootserver_pxe {
     [ '/var'
     , '/var/lib'
     , '/var/lib/tftpboot'
-    , '/var/lib/tftpboot/efi32'
     , '/var/lib/tftpboot/efi64' ]:
       ensure => directory;
-
-    [ '/var/lib/tftpboot/efi32/ltsp', '/var/lib/tftpboot/efi64/ltsp' ]:
-      ensure => link,
-      target => '../ltsp';
   }
 
 }
