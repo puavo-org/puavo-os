0dae303ab18ef281c80087b5b6c3659bb3ac188d
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2026-01-28 13:14:45 +0200

    Move most stuff in ::images::allinone to ::images::bundle::allinone.
    The concept of including ::images::allinone in other types of
    images is a bit strange, so make it as a bundle.
    
    The ::plymouth::set_default_theme can not be set twice,
    so make a distinct setting of it for allinone-images.

--- rules/image/manifests/allinone.pp
+++ rules/image/manifests/allinone.pp
@@ -1,28 +1,5 @@
 class image::allinone {
-  include ::adm::users
-  include ::image::bundle::basic
-  include ::image::bundle::bootserver
-  include ::image::bundle::desktop
-  include ::plymouth
+  include ::image::bundle::allinone
 
-  stage {
-    'init':
-      before => Stage['pre-main'];
-
-    'pre-main':
-      before => Stage['main'];
-  }
-
-  class {
-    'apt::default_repositories':
-      stage => pre-main;
-  }
-
-  ::plymouth::install_theme     { 'kites': ; }
   ::plymouth::set_default_theme { 'kites': ; }
-
-  Package <| tag == 'tag_kernel'
-          or tag == 'tag_puavo'  |>
-
-  Kernels::Install_kernel <| |>
 }
---
+++ rules/image/manifests/bundle/allinone.pp
@@ -0,0 +1,27 @@
+class image::bundle::allinone {
+  include ::adm::users
+  include ::image::bundle::basic
+  include ::image::bundle::bootserver
+  include ::image::bundle::desktop
+  include ::plymouth
+
+  stage {
+    'init':
+      before => Stage['pre-main'];
+
+    'pre-main':
+      before => Stage['main'];
+  }
+
+  class {
+    'apt::default_repositories':
+      stage => pre-main;
+  }
+
+  ::plymouth::install_theme { 'kites': ; }
+
+  Package <| tag == 'tag_kernel'
+          or tag == 'tag_puavo'  |>
+
+  Kernels::Install_kernel <| |>
+}
