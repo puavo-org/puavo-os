2baee9a45a1053513da2235a7ab008c374cdf218
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2026-02-13 13:22:31 +0100

    Allow systemd journal persistency.

--- rules/systemd/manifests/init.pp
+++ rules/systemd/manifests/init.pp
@@ -25,11 +25,6 @@ class systemd {
     # This removes a link to /dev/null, masking autospawn activation.
     '/usr/lib/systemd/system/pulseaudio-enable-autospawn.service':
       ensure => absent;
-
-    # no persistent journal logs by default (not useful on fatclients)
-    '/var/log/journal':
-      ensure => absent,
-      force  => true;
   }
 
   Package <|
