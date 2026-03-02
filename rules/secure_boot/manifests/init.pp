5988e3edb980357c84242098c953ab2ea80412ba
Joonas Lehto <joonas.lehto@opinsys.fi>
2026-03-02 12:04:33 +0200

    puavo-secure-boot: Remove centralized PK and KEK auth files and store prepopulated DB certificate list for Setup Mode
    The centralized auth files are no longer required, because they are
    generated locally during installation.

--- rules/secure_boot/manifests/init.pp
+++ rules/secure_boot/manifests/init.pp
@@ -1,29 +1,18 @@
 class secure_boot {
   $base_directory = '/etc/puavo-secure-boot'
-  $certificates_directory = "${base_directory}/certificates"
 
   file {
-    [ $base_directory, $certificates_directory, ]:
+    [ $base_directory, ]:
       ensure => directory;
 
-    "${certificates_directory}/db":
+    "${base_directory}/db":
       ensure  => directory,
       recurse => true,
       source  => 'puppet:///modules/secure_boot/db';
 
-    "${certificates_directory}/dbx":
+    "${base_directory}/dbx":
       ensure  => directory,
       recurse => true,
       source  => 'puppet:///modules/secure_boot/dbx';
-
-    "${certificates_directory}/kek":
-      ensure  => directory,
-      recurse => true,
-      source  => 'puppet:///modules/secure_boot/kek';
-
-    "${certificates_directory}/pk":
-      ensure  => directory,
-      recurse => true,
-      source  => 'puppet:///modules/secure_boot/pk';
   }
 }
