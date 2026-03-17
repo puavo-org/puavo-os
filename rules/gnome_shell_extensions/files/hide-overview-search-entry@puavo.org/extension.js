ac5993ecad00ad171a0ba0c9916f0135421fe1ca
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2026-03-17 12:46:06 +0100

    Update "hide-overview-search-entry@puavo.org" for Gnome Shell 48.

--- rules/gnome_shell_extensions/files/hide-overview-search-entry@puavo.org/extension.js
+++ rules/gnome_shell_extensions/files/hide-overview-search-entry@puavo.org/extension.js
@@ -1,4 +1,4 @@
-// Copyright (C) 2017 Opinsys Oy
+// Copyright (C) 2026 Opinsys Oy
 //
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -14,12 +14,34 @@
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-const Main = imports.ui.main;
+import * as Extension from 'resource:///org/gnome/shell/extensions/extension.js';
+import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 
-function enable() {
-    Main.overview._overview._controls._searchEntry.hide();
-}
+export default class HideOverviewSearchExtension extends Extension.Extension {
+    enable() {
+        const controls = Main.overview._overview.controls;
 
-function disable() {
-    Main.overview._overview._controls._searchEntry.show();
+        this._searchController = controls._searchController;
+
+        // store original methods
+        this._origStartSearch = this._searchController.startSearch;
+        this._origSetSearchActive = this._searchController.setSearchActive;
+
+        // disable search activation
+        this._searchController.startSearch = () => {};
+        this._searchController.setSearchActive = () => {};
+
+        // hide entry
+        Main.overview._overview._controls._searchEntry.hide();
+    }
+
+    disable() {
+        Main.overview._overview._controls._searchEntry.show();
+
+        if (this._origStartSearch)
+            this._searchController.startSearch = this._origStartSearch;
+
+        if (this._origSetSearchActive)
+            this._searchController.setSearchActive = this._origSetSearchActive;
+    }
 }
