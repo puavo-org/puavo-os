1ecdc10a9d9eac43e732127299515cb6c6647f95
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2026-03-18 12:29:42 +0200

    Update "screenkeyboardcontroller@puavo.org" for Gnome Shell 48.
    I suspect this does not work correctly, but this step is
    necessary anyway.

--- rules/gnome_shell_extensions/files/screenkeyboardcontroller@puavo.org/extension.js
+++ rules/gnome_shell_extensions/files/screenkeyboardcontroller@puavo.org/extension.js
@@ -1,122 +1,137 @@
 'use strict';
-const Clutter = imports.gi.Clutter;
-const GLib = imports.gi.GLib;
-const ExtensionUtils = imports.misc.extensionUtils;
-const Keyboard = imports.ui.keyboard;
-const Main = imports.ui.main;
 
-let _deviceAddedHandlerId;
-let _deviceRemovedHandlerId;
-let _originalLastDeviceIsTouchscreen;
-let _seat;
+import Clutter from 'gi://Clutter';
+import GLib from 'gi://GLib';
 
-function _auto_hide() {
-    try {
-        global.log('screenkeyboardcontroller@puavo.org: checking if the device has a real keyboard...');
-        const [res, stdout, stderr, wait_status] = GLib.spawn_command_line_sync('/usr/lib/puavo-ltsp-client/has-real-keyboard');
+import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+import * as Keyboard from 'resource:///org/gnome/shell/ui/keyboard.js';
+import {Extension} from 'resource:///org/gnome/shell/extensions/extension.js';
 
-        let stdout_str = new TextDecoder('utf-8').decode(stdout);
-        let stderr_str = new TextDecoder('utf-8').decode(stderr);
+export default class ScreenKeyboardControllerExtension extends Extension {
+    constructor(metadata) {
+        super(metadata);
 
-        if (stderr_str.length > 0) {
-            global.log(`screenkeyboardcontroller@puavo.org: has-real-keyboard printed to stderr: '${stderr_str}'`);
+        this._deviceAddedHandlerId = null;
+        this._deviceRemovedHandlerId = null;
+        this._originalLastDeviceIsTouchscreen = null;
+        this._seat = null;
+    }
+
+    _auto_hide() {
+        try {
+            console.log('screenkeyboardcontroller@puavo.org: checking if device has real keyboard...');
+
+            const [res, stdout, stderr, wait_status] =
+                GLib.spawn_command_line_sync('/usr/lib/puavo-ltsp-client/has-real-keyboard');
+
+            const stdout_str = new TextDecoder().decode(stdout);
+            const stderr_str = new TextDecoder().decode(stderr);
+
+            if (stderr_str.length > 0) {
+                console.log(`screenkeyboardcontroller@puavo.org: stderr: ${stderr_str}`);
+            }
+
+            GLib.spawn_check_wait_status(wait_status);
+
+            switch (stdout_str.trim()) {
+                case 'yes':
+                    console.log('screenkeyboardcontroller@puavo.org: this device has a real keyboard, screen keyboard is disabled.');
+                    return false;
+                case 'no':
+                    console.log('screenkeyboardcontroller@puavo.org: this device does not have a real keyboard, screen keyboard is enabled.');
+                    return true;
+                default:
+                    throw new Error(`Unexpected output: ${stdout_str}`);
+            }
+        } catch (e) {
+            console.error(e);
         }
 
-        GLib.spawn_check_wait_status(wait_status); // Throws exception if has-real-keyboard failed.
+        // If something goes wrong, we fallback to the original logic,
+        // i.e. act like this extension is not enabled at all.
+        return this._originalLastDeviceIsTouchscreen.call(this);
+    }
 
-        switch (stdout_str.trim()) {
-        case 'yes':
-            global.log("screenkeyboardcontroller@puavo.org: this device has a real keyboard, screen keyboard is disabled.");
-            return false;
-        case 'no':
-            global.log("screenkeyboardcontroller@puavo.org: this device does not have a real keyboard, screen keyboard is enabled.");
-            return true;
-        default:
-            throw new Error(`has-real-keyboard printed unexpected output: '${stdout_str.trim()}'`);
+    _modifiedLastDeviceIsTouchscreen() {
+        console.log('screenkeyboardcontroller@puavo.org: modified touchscreen check called');
+
+        const settings = this.getSettings();
+        const mode = settings.get_string('mode');
+
+        switch (mode) {
+            case 'auto_hide':
+                return this._auto_hide();
+            case 'force_hide':
+                return false;
+            case 'do_nothing':
+                break;
+            default:
+                console.log(`screenkeyboardcontroller@puavo.org: unexpected mode ${mode}`);
         }
-    } catch (e) {
-        logError(e);
-    };
 
-    // If something goes wrong, we fallback to the original logic,
-    // i.e. act like this extension is not enabled at all.
-    return _originalLastDeviceIsTouchscreen.call(this);
-}
+        global.log('screenkeyboardcontroller@puavo.org: doing nothing');
 
-function _modifiedLastDeviceIsTouchscreen() {
-    global.log('screenkeyboardcontroller@puavo.org: someone called me');
-
-    const settings = ExtensionUtils.getSettings();
-    const mode = settings.get_string('mode');
-
-    switch (mode) {
-    case 'auto_hide':
-        return _auto_hide();
-    case 'force_hide':
-        return false;
-    case 'do_nothing':
-        break;
-    default:
-        global.log(`Unexpected mode ${mode}`);
-        break;
+        return this._originalLastDeviceIsTouchscreen.call(this);
     }
 
-    global.log('screenkeyboardcontroller@puavo.org: doing nothing');
-
-    return _originalLastDeviceIsTouchscreen.call(this);
-}
-
-function init() {
-    global.log('screenkeyboardcontroller@puavo.org: initializing...');
-    _seat = Clutter.get_default_backend().get_default_seat();
-    global.log('screenkeyboardcontroller@puavo.org: initialized.');
-}
-
-function _on_device_added(clutter, device) {
-    global.log('screenkeyboardcontroller@puavo.org: device added.');
-    Main.keyboard._syncEnabled();
-}
-
-function _on_device_removed(clutter, device) {
-    global.log('screenkeyboardcontroller@puavo.org: device removed.');
-    Main.keyboard._syncEnabled();
-
-    // This device might have been the last keyboard, we don't
-    // know. Try to open the screen keyboard if it's enabled (i.e. no
-    // other keyboards are available)..
-    Main.keyboard.open(Main.layoutManager.focusIndex);
-}
-
-function enable() {
-    global.log('screenkeyboardcontroller@puavo.org: enabling...');
-    _originalLastDeviceIsTouchscreen = Keyboard.KeyboardManager.prototype._lastDeviceIsTouchscreen;
-    Keyboard.KeyboardManager.prototype._lastDeviceIsTouchscreen = _modifiedLastDeviceIsTouchscreen;
-
-    _deviceAddedHandlerId = _seat.connect('device-added', _on_device_added);
-    _deviceRemovedHandlerId = _seat.connect('device-removed', _on_device_removed);
-
-    global.log('screenkeyboardcontroller@puavo.org: enabled.');
-}
-
-function disable() {
-    global.log('screenkeyboardcontroller@puavo.org: disabling...');
-
-    if (_deviceRemovedHandlerId) {
-        global.log('screenkeyboardcontroller@puavo.org: disconnecting device-removed handler...');
-        _seat.disconnect(_deviceRemovedHandlerId);
-        _deviceRemovedHandlerId = null;
-        global.log('screenkeyboardcontroller@puavo.org: disconnected device-removed handler.');
+    _on_device_added() {
+        console.log('screenkeyboardcontroller@puavo.org: device added');
+        Main.keyboard._syncEnabled();
     }
 
-    if (_deviceAddedHandlerId) {
-        global.log('screenkeyboardcontroller@puavo.org: disconnecting device-added handler...');
-        _seat.disconnect(_deviceAddedHandlerId);
-        _deviceAddedHandlerId = null;
-        global.log('screenkeyboardcontroller@puavo.org: disconnected device-added handler.');
+    _on_device_removed() {
+        console.log('screenkeyboardcontroller@puavo.org: device removed');
+        Main.keyboard._syncEnabled();
+
+        // This device might have been the last keyboard, we don't
+        // know. Try to open the screen keyboard if it's enabled (i.e. no
+        // other keyboards are available)..
+        Main.keyboard.open(Main.layoutManager.focusIndex);
     }
 
-    Keyboard.KeyboardManager.prototype._lastDeviceIsTouchscreen = _originalLastDeviceIsTouchscreen;
-    _originalLastDeviceIsTouchscreen = null;
+    enable() {
+        console.log('screenkeyboardcontroller@puavo.org: enabling extension');
 
-    global.log('screenkeyboardcontroller@puavo.org: disabled.');
+        this._seat = Clutter.get_default_backend().get_default_seat();
+
+        this._originalLastDeviceIsTouchscreen =
+            Keyboard.KeyboardManager.prototype._lastDeviceIsTouchscreen;
+
+        Keyboard.KeyboardManager.prototype._lastDeviceIsTouchscreen =
+            this._modifiedLastDeviceIsTouchscreen.bind(this);
+
+        this._deviceAddedHandlerId =
+            this._seat.connect('device-added', this._on_device_added.bind(this));
+
+        this._deviceRemovedHandlerId =
+            this._seat.connect('device-removed', this._on_device_removed.bind(this));
+
+        console.log('screenkeyboardcontroller@puavo.org: enabled.');
+    }
+
+    disable() {
+        console.log('screenkeyboardcontroller@puavo.org: disabling extension');
+
+        if (this._deviceRemovedHandlerId) {
+            console.log('screenkeyboardcontroller@puavo.org: disconnecting device-removed handler...');
+            this._seat.disconnect(this._deviceRemovedHandlerId);
+            this._deviceRemovedHandlerId = null;
+            console.log('screenkeyboardcontroller@puavo.org: disconnected device-removed handler.');
+        }
+
+        if (this._deviceAddedHandlerId) {
+            console.log('screenkeyboardcontroller@puavo.org: disconnecting device-added handler...');
+            this._seat.disconnect(this._deviceAddedHandlerId);
+            this._deviceAddedHandlerId = null;
+            console.log('screenkeyboardcontroller@puavo.org: disconnected device-added handler.');
+        }
+
+        if (this._originalLastDeviceIsTouchscreen) {
+            Keyboard.KeyboardManager.prototype._lastDeviceIsTouchscreen =
+                this._originalLastDeviceIsTouchscreen;
+            this._originalLastDeviceIsTouchscreen = null;
+        }
+
+        console.log('screenkeyboardcontroller@puavo.org: disabled.');
+    }
 }
