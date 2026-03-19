2b62da72b879344fd08005cfadc863f632021c4e
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2026-03-19 13:00:02 +0100

    Allow "systemd --user" instance to start up.
    I tested it and no longer saw the problem, where the KRB5CCNAME
    environment variable was not passed to all desktop processes.
    
    Let's try with it and see if we still have issues with it.

--- rules/systemd/manifests/init.pp
+++ rules/systemd/manifests/init.pp
@@ -11,28 +11,12 @@ class systemd {
       require => Package['systemd'],
       source  => 'puppet:///modules/systemd/system.conf';
 
-    # Disable "systemd --user" service due to issues with it.
-    # Require the "dbus-x11" package because Gnome does not work without
-    # either it or "systemd --user".
-    '/etc/systemd/system/user@.service':
-      ensure  => link,
-      require => Package['dbus-x11'],
-      target  => '/dev/null';
-
     # set graphical target as the default in case set_systemd_default_target
     # is not run (the exam hosttype should not need it)
     '/etc/systemd/system/default.target':
       ensure => link,
       target => '/usr/lib/systemd/system/graphical.target';
-
-    # Pulseaudio needs help in case "systemd --user" is missing.
-    # This removes a link to /dev/null, masking autospawn activation.
-    '/usr/lib/systemd/system/pulseaudio-enable-autospawn.service':
-      ensure => absent;
   }
 
-  Package <|
-      title == dbus-x11
-   or title == systemd
-  |>
+  Package <| title == systemd |>
 }
