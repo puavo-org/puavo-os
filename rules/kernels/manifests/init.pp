edffb393f94bbb0356843279d1ca62d79799cbe8
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2026-04-08 14:33:17 +0300

    Fix crisp kernel version.

--- rules/kernels/manifests/init.pp
+++ rules/kernels/manifests/init.pp
@@ -29,7 +29,7 @@ class kernels {
   }
 
   $default_kernel = '6.1.0-44-amd64'
-  $crisp_kernel   = '6.12.74+2-amd64'
+  $crisp_kernel   = '6.12.74+3-amd64'
 
   ::kernels::all_kernel_links {
     'default': kernel => $default_kernel;
