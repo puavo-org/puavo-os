7492f9c670228cda417f5d51f3c72c44d6235fcc
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2026-04-16 10:42:31 +0300

    Fix installing the "fasttrack-archive-keyring"-package.
    May be needed depending on the initial apt configuration.
    Problem seen when building "cloud"-packages.

--- rules/apt/manifests/repositories.pp
+++ rules/apt/manifests/repositories.pp
@@ -1,6 +1,17 @@
 class apt::repositories {
   include ::apt
 
+  # Need a special target because installing 'fasttrack-archive-keyring'
+  # requires non-free on apt sources, which means that we need to
+  # configure sources, run "apt update", only then we can install
+  # fasttrack-archive-keyring and then configure debian-fasttrack.sources
+  # and should run "apt update" again.
+  exec {
+    'apt update for fasttrack':
+       command     => '/usr/bin/apt-get update',
+       refreshonly => true;
+  }
+
   define setup ($archivemirror,
                 $archivemirror_path='',
                 $fasttrackmirror,
@@ -28,7 +39,7 @@ class apt::repositories {
 
       '/etc/apt/sources.list.d/debian-fasttrack.sources':
         content => template('apt/debian-fasttrack.sources'),
-        notify  => Exec['apt update'],
+        notify  => Exec['apt update for fasttrack'],
         require => Package['fasttrack-archive-keyring'];
 
       # Put the local this into a separate file so it can be excluded
@@ -51,7 +62,8 @@ class apt::repositories {
 
     package {
       'fasttrack-archive-keyring':
-        ensure => present;
+        ensure  => present,
+        require => Exec['apt update'];
     }
   }
 }
