e8163f70d6e16cbf4c9663b7efb95d1d369fb404
Joonas Lehto <joonas.lehto@opinsys.fi>
2026-05-12 13:16:39 +0300

    kernel-command-line-signer: run format script

--- parts/core/kernel-command-line-signer/sign/log.h
+++ parts/core/kernel-command-line-signer/sign/log.h
@@ -10,13 +10,13 @@
 #define LOG_INFO(format, ...) pr_info(LOG_PREFIX format, ##__VA_ARGS__)
 #define LOG_ERROR(format, ...) pr_err(LOG_PREFIX format, ##__VA_ARGS__)
 
-#else // __KERNEL__
+#else  // __KERNEL__
 
 #include <stdio.h>
 
 #define LOG_INFO(format, ...) printf("  " format "\n", ##__VA_ARGS__)
 #define LOG_ERROR(format, ...) fprintf(stderr, "  " format "\n", ##__VA_ARGS__)
 
-#endif // __KERNEL__
+#endif  // __KERNEL__
 
-#endif // LOG_H
+#endif  // LOG_H
