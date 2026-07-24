e23aa62ab7506c2ff13a1fc49b54b2833fb78af1
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2026-07-24 13:45:31 +0300

    Update Trixie kernels.
    The modern kernel (vmlinuz) does no more install under /boot.
    To fix this, either /usr/lib/modules/7.1.3+deb13-amd64/vmlinuz.unsigned
    must be copied to /boot/vmlinuz-7.1.3+deb13-amd64 or something should
    be changed in the kernel build.

--- rules/kernels/manifests/init.pp
+++ rules/kernels/manifests/init.pp
@@ -15,8 +15,8 @@ class kernels {
   #   Debian Trixie:   modern
 
   $kernel_versions = {
-    'default' => '6.12.95+deb13.1-amd64',
-    'modern'  => '7.0.13+deb13-amd64',
+    'default' => '6.12.96+deb13.1-amd64',
+    'modern'  => '7.1.3+deb13-amd64',
   }
   $kernel_aliases = keys($kernel_versions)
 
