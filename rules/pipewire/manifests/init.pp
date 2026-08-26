9d656367e56efc6c996f735a58dc18145af2cb82
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2026-08-26 09:39:47 +0200

    Do not run pipewire in adm-* user accounts.
    It is problematic, because without this another pipewire instance
    is run and that can reserve a real audio device, which sabotages
    the important user pipewire session.  Besides, for admins,
    "wpctl status" returns information from the admin pipewire-instance,
    which is misleading.
    
    Also block pipewire in the login screen.  This is more controversial
    but I do not think we need it there, and it may interfere with the
    user session (though it exits soon once the desktop login is successful).
    We may revisit this once we see if we need it.

--- rules/pipewire/manifests/init.pp
+++ rules/pipewire/manifests/init.pp
@@ -1,3 +1,23 @@
 class pipewire {
+  include ::packages
   include ::pipewire::jack
+
+  file {
+    [ '/etc/systemd/user/pipewire.service.d'
+    , '/etc/systemd/user/pipewire.socket.d'
+    , '/etc/systemd/user/pipewire-pulse.service.d'
+    , '/etc/systemd/user/pipewire-pulse.socket.d'
+    , '/etc/systemd/user/wireplumber.service.d' ]:
+      ensure => directory;
+
+    [ '/etc/systemd/user/pipewire.service.d/no-pipewire-for-admins-or-gdm.conf'
+    , '/etc/systemd/user/pipewire.socket.d/no-pipewire-for-admins-or-gdm.conf'
+    , '/etc/systemd/user/pipewire-pulse.service.d/no-pipewire-for-admins-or-gdm.conf'
+    , '/etc/systemd/user/pipewire-pulse.socket.d/no-pipewire-for-admins-or-gdm.conf'
+    , '/etc/systemd/user/wireplumber.service.d/no-pipewire-for-admins-or-gdm.conf' ]:
+      require => Package['systemd'],
+      source  => 'puppet:///modules/pipewire/no-pipewire-for-admins-or-gdm.conf';
+  }
+
+  Package <| title == systemd |>
 }
